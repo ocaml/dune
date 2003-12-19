739cfb7eb6d9791b32e893128a45d75c7db6f7b4
gerd <gerd@5e68ba05-c9cc-0310-a7d4-dca2aa431786>
2003-12-19 15:27:02 +0000

    Updated to current version of findlib
    git-svn-id: https://godirepo.camlcity.org/svn/lib-findlib/trunk@38 5e68ba05-c9cc-0310-a7d4-dca2aa431786

--- itest-aux/simple_dbm.ml
+++ itest-aux/simple_dbm.ml
@@ -1,10 +1,10 @@
 (try 
-  Sys.remove "config/testdb.db"
+  Sys.remove "itest-aux/testdb.db"
 with 
   _ -> ());
 
 let dbm =
-  Dbm.opendbm "config/testdb" [ Dbm.Dbm_rdwr; Dbm.Dbm_create ] 0o777 in
+  Dbm.opendbm "itest-aux/testdb" [ Dbm.Dbm_rdwr; Dbm.Dbm_create ] 0o777 in
 
 print_string "OK\n";;
 

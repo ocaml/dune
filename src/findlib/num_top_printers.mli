72bf7e468e2886f28a129e4fdb894b3c017d05fe
gerd <gerd@5e68ba05-c9cc-0310-a7d4-dca2aa431786>
2012-02-28 23:16:37 +0000

    including doc sources license chance for num_top_printers.mli (granted by Zack) (both requests from Debian packager)
    git-svn-id: https://godirepo.camlcity.org/svn/lib-findlib/trunk@151 5e68ba05-c9cc-0310-a7d4-dca2aa431786

--- src/findlib/num_top_printers.mli
+++ src/findlib/num_top_printers.mli
@@ -1,4 +1,4 @@
-(* $Id: num_top_printers.mli,v 1.1 2003/09/30 00:27:37 gerd Exp $
+(* $Id$
  * ----------------------------------------------------------------------
  *
  *)
@@ -9,10 +9,7 @@
 
   Copyright (C) 2003  Stefano Zacchiroli <zack@debian.org>
 
-  This is free software! you can redistribute it and/or modify it under the
-  terms of the GNU Lesser General Public License as published by the Free
-  Software Foundation; either version 2.1 of the License, or (at your option)
-  any later version.
+  Released under the same terms as findlib.
 *)
 
 val nat_printer : Format.formatter -> Nat.nat -> unit

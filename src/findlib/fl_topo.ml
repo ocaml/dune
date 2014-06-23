1cad5f1619aa320e72a085d3671b06219b5cdf2e
gerd <gerd@5e68ba05-c9cc-0310-a7d4-dca2aa431786>
2014-06-23 19:30:16 +0000

    warnings
    git-svn-id: https://godirepo.camlcity.org/svn/lib-findlib/trunk@212 5e68ba05-c9cc-0310-a7d4-dca2aa431786

--- src/findlib/fl_topo.ml
+++ src/findlib/fl_topo.ml
@@ -216,7 +216,7 @@ module Make(H: IdentifiedType) =
 	(* find biggest, non-marked node *)
 	match ordlist with
 	  []             -> raise Not_found
-	| nx :: ordlist' -> if not nx.mark & in_direction nx = [] 
+	| nx :: ordlist' -> if not nx.mark && in_direction nx = [] 
 	                    then nx
 	                    else find_biggest ordlist'
       in
@@ -280,8 +280,10 @@ module Make(H: IdentifiedType) =
 
     let iter_at upwards f ordering startpoints =
 
+(*
       let in_direction n = 
 	if upwards then n.bigger else n.smaller in
+ *)
 
       let against_direction n =
 	if upwards then n.smaller else n.bigger in

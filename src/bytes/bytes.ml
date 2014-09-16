c209076aaac17eebd14d62512b7d2b0ca82e0fb9
gerd <gerd@5e68ba05-c9cc-0310-a7d4-dca2aa431786>
2014-09-16 11:13:18 +0000

    Updating bytes.ml:
    - Bytes.blit_string
    - Bytes.extend
    - Bytes.init
    - Bytes.mapi
    - preferring "val" over "external" in the interface
    
    
    git-svn-id: https://godirepo.camlcity.org/svn/lib-findlib/trunk@232 5e68ba05-c9cc-0310-a7d4-dca2aa431786

--- src/bytes/bytes.ml
+++ src/bytes/bytes.ml
@@ -5,11 +5,32 @@ let of_string = copy
 let to_string = copy
 
 let sub_string = sub
+let blit_string = blit
 
-(* these are trivially safe and could be made into an identity
-   function, but given that OCaml signatures distinguish between "val"
-   and "external", I thought it best to reuse upstream's choice to
-   avoid any potential copmatibility hurdles (not that I know
-   of any) *)
-external unsafe_to_string : t -> string = "%identity"
-external unsafe_of_string : string -> t = "%identity"
+let unsafe_to_string : t -> string = fun s -> s
+let unsafe_of_string : string -> t = fun s -> s
+
+let extend s left right =
+  (* length of the final string *)
+  let dstlen = left + length s + right in
+  (* length of the included portion of the input string *)
+  let srclen = min 0 left + length s + min 0 right in
+  let t = create dstlen in
+  if srclen > 0 then blit s (max 0 (-left)) t (max 0 left) srclen;
+  t
+
+let init len f =
+  let s = create len in
+  for i = 0 to len - 1 do
+    set s i (f i);
+  done;
+  s
+
+let mapi f input =
+  let output = create (length input) in
+  for i = 0 to length input - 1 do
+    output.[i] <- f i input.[i];
+  done;
+  output
+
+let cat = (^)

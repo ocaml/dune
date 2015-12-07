6988f8a8d7975458594de95a64df060815c5af3b
gerd <gerd@5e68ba05-c9cc-0310-a7d4-dca2aa431786>
2015-12-07 12:49:49 +0000

    [Lint] take into account other modes (François Bobot)
    git-svn-id: https://godirepo.camlcity.org/svn/lib-findlib/trunk@266 5e68ba05-c9cc-0310-a7d4-dca2aa431786

--- src/findlib/fl_lint.ml
+++ src/findlib/fl_lint.ml
@@ -7,12 +7,14 @@ open Fl_metascanner
 
 module Have = struct
   module T = struct
+    type mode = [`Byte | `Native | `Toploop | `Preprocessor | `Ppx_driver]
     type t = [
-        `BothByteNative      (** archive(native,byte), plugin(native,byte) *)
-      | `NoByteNative        (** archive(...), plugin(...) no native nor byte *)
-      | `Archive of [`Plugin|`NoPlugin] * [`Byte | `Native]
+        `Mode of [ `TooMany | `None]
+      (** problem in the number of mode (byte,native,syntax,...)
+          in the variable  *)
+      | `Archive of [`Plugin|`NoPlugin] * mode
         (** archive(plugin,...) or archive(...)) *)
-      | `Plugin of [`Plugin|`NoPlugin] * [`Byte | `Native]
+      | `Plugin of [`Plugin|`NoPlugin] * mode
         (** plugin(...) *)
       | `Description
       | `Requires
@@ -29,20 +31,30 @@ let scan_def acc def =
   let add have = Have.Map.add have def acc in
   let has_plugin_pred = List.mem (`Pred "plugin") def.def_preds in
   let plugin = if has_plugin_pred then `Plugin else `NoPlugin in
-  let has_byte_pred = List.mem (`Pred "byte") def.def_preds in
-  let has_native_pred = List.mem (`Pred "native") def.def_preds in
-  (** mode is used only when there is exactly one of "byte","native" *)
-  let mode = if has_byte_pred then `Byte else `Native in
-  match def.def_var with
-  | ("archive"|"plugin") when not (has_byte_pred || has_native_pred) ->
-    add `NoByteNative
-  | ("archive"|"plugin") when has_byte_pred && has_native_pred ->
-    add `BothByteNative
-  | "archive" -> add (`Archive(plugin,mode))
-  | "plugin"  -> add (`Plugin(plugin,mode))
-  | "description" -> add `Description
-  | "requires" -> add `Requires
-  | "version" -> add `Version
+  let modes = [ "byte", `Byte;
+                "native", `Native;
+                "toploop", `Toploop;
+                "preprocessor", `Preprocessor;
+                "ppx_driver", `Ppx_driver
+              ] in
+  let modes =
+    List.filter
+      (fun (p,_) -> List.mem (`Pred p) def.def_preds)
+      modes
+  in
+  let modes = List.map snd modes in
+  match def.def_var, modes with
+  (** For archive the modes are used in multiple ways, so we can't
+      check exhaustiveness or presence.
+  *)
+  | "plugin", [] -> add (`Mode(`None))
+  | "plugin", _::_::_ -> add (`Mode(`TooMany))
+
+  | "archive", [mode] -> add (`Archive(plugin,mode))
+  | "plugin", [mode]  -> add (`Plugin(plugin,mode))
+  | "description", _ -> add `Description
+  | "requires", _ -> add `Requires
+  | "version", _ -> add `Version
   | _ -> acc
 
 
@@ -53,37 +65,38 @@ let warn_def ~warned pkg =
   let mem x  = Have.Map.mem x haves in
   let find x = Have.Map.find x haves in
   let warning fmt = warned := true; Printf.printf fmt in
-  let if_ ?has ?has_not msg =
+  let if_ ?has ?(has_not=[]) msg =
     match has, has_not with
-    | Some has, None when mem has ->
+    | Some has, [] when mem has ->
       warning "%a%s\n\n" print_def (find has) msg;
-    | Some has, Some has_not when mem has && not (mem has_not) ->
+    | Some has, has_not when mem has && not (List.exists mem has_not) ->
       warning "%a%s\n\n" print_def (find has) msg;
-    | None, Some has_not when not (mem has_not) ->
+    | None, has_not when not (List.exists mem has_not) ->
       warning "%s\n\n" msg;
     | _ -> ()
   in
-  if_ ~has_not:`Description
+  if_ ~has_not:[`Description]
     "You should add a description.";
-  if_ ~has_not:`Version
+  if_ ~has_not:[`Version]
     "You should add a version.";
-  if_ ~has_not:`Requires
-    "You should add the required librairies. You can silent this \
+  if_ ~has_not:[`Requires]
+    "You should add the required libraries. You can silent this \
      warning by using the empty string.";
-  if_ ~has:`BothByteNative
-    "This variable shouldn't have at the same time \
-     \"byte\" and \"native\".";
-  if_ ~has:`NoByteNative
+  if_ ~has:(`Mode(`TooMany))
+    "This variable should have only one mode
+     (\"byte\", \"native\").";
+  if_ ~has:(`Mode(`None))
     "This variable should have at least the predicate \
      \"byte\" or \"native\".";
   let with_mode mode =
     if_ ~has:(`Plugin (`Plugin,mode))
       "You must not add the predicate \"plugin\" to the variable \
        \"plugin\".";
-    if_ ~has:(`Archive (`Plugin,mode)) ~has_not:(`Plugin (`NoPlugin,mode))
+    if_ ~has:(`Archive (`Plugin,mode)) ~has_not:[`Plugin (`NoPlugin,mode)]
       "This specification of dynamic loading is deprecated, you should add a \
        \"plugin(...)\" variable.";
-    if_ ~has:(`Archive (`NoPlugin,mode)) ~has_not:(`Plugin (`NoPlugin,mode))
+    if_ ~has:(`Archive (`NoPlugin,mode))
+      ~has_not:[`Plugin (`NoPlugin,mode);`Archive (`Plugin,mode)]
       "This variable indicates how to link statically, you should add a \
        \"plugin(...)\" variable for linking dynamically.";
   in

9aa3f9364f424870e9af362720c5da3f0f58107d
Jérémie Dimino <jeremie@dimino.org>
2017-05-06 02:52:46 +0100

    CHANGES.org --> CHANGES.md

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -3,6 +3,5 @@
 
 let () =
   Pkg.describe "jbuilder"
-    ~readmes:    [ Pkg.std_file "README.org"     ]
-    ~change_logs:[ Pkg.std_file "CHANGES.org"    ]
-    ~licenses:   [ Pkg.std_file "LICENSE.txt"    ]
+    ~readmes: [ Pkg.std_file "README.org"     ]
+    ~licenses:[ Pkg.std_file "LICENSE.txt"    ]

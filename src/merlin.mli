0a298157289dc3f9c4f80cc671997553954db1f4
Rudi Grinberg <rudi.grinberg@gmail.com>
2017-07-29 13:10:22 -0400

    revert incorrect change to comment

--- src/merlin.mli
+++ src/merlin.mli
@@ -11,7 +11,6 @@ val merge_two : t -> t -> t
 
 val merge_all : t list -> t option
 
-(** Add rules for generating the .merlin in a directory and return
-    the final .merlin used *)
+(** Add rules for generating the .merlin in a directory *)
 val add_rules : Super_context.t -> dir:Path.t -> t -> unit
 

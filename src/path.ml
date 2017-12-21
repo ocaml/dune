1a8c328b2dedbcb491e30bce6559fc14b0f5c373
Rudi Grinberg <rudi.grinberg@gmail.com>
2017-12-21 19:26:52 +0800

    Make is_directory work when dir doesn't exist

--- src/path.ml
+++ src/path.ml
@@ -381,7 +381,9 @@ let drop_build_context t =
 
 let exists t = Sys.file_exists (to_string t)
 let readdir t = Sys.readdir (to_string t) |> Array.to_list
-let is_directory t = Sys.is_directory (to_string t)
+let is_directory t =
+  try Sys.is_directory (to_string t)
+  with Sys_error _ -> false
 let rmdir t = Unix.rmdir (to_string t)
 let unlink t = Unix.unlink (to_string t)
 let unlink_no_err t = try Unix.unlink (to_string t) with _ -> ()

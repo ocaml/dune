02c5b005dd231f0f93fbd278547cbf737fa73845
Rudi Grinberg <rudi.grinberg@gmail.com>
2018-02-28 12:21:25 +0700

    Move intallation and META rules to install_rules module

---
+++ src/install_rules.mli
@@ -0,0 +1,19 @@
+module type Params = sig
+  val sctx : Super_context.t
+end
+
+module Archives (P : Params) : sig
+  val lib_archive : Jbuild.Library.t -> dir:Path.t -> ext:string -> Path.t
+  val stubs_archive : Jbuild.Library.t -> dir:Path.t -> Path.t
+  val dll : Jbuild.Library.t -> dir:Path.t -> Path.t
+end
+
+module type Install_params = sig
+  include Params
+  val module_names_of_lib : Jbuild.Library.t -> dir:Path.t -> Module.t list
+end
+
+(** Generate install rules for META and .install files *)
+module Gen (P : Install_params) : sig
+  val init : unit -> unit
+end

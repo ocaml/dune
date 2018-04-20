13eea737f96b0d89f7420843b1c78433b6ff2ecc
Rudi Grinberg <rudi.grinberg@gmail.com>
2018-04-20 17:32:37 +0700

    Don't use getenv_opt in configurator test

--- test/blackbox-tests/test-cases/configurator/config/run.ml
+++ test/blackbox-tests/test-cases/configurator/config/run.ml
@@ -1,9 +1,9 @@
 module Configurator = Configurator.V1
 
 let () =
-  begin match Sys.getenv_opt "DUNE_CONFIGURATOR" with
-  | None -> failwith "DUNE_CONFIGURATOR is not passed"
-  | Some _ -> print_endline "DUNE_CONFIGURATOR is present"
+  begin match Sys.getenv "DUNE_CONFIGURATOR" with
+  | exception Not_found -> failwith "DUNE_CONFIGURATOR is not passed"
+  | _ -> print_endline "DUNE_CONFIGURATOR is present"
   end;
   Configurator.main ~name:"config" (fun t ->
     match Configurator.ocaml_config_var t "version" with

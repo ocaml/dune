1c51c47c220f14b23e7fe73af0f7441d6f4f4bcb
Rudi Grinberg <rudi.grinberg@gmail.com>
2018-04-26 17:09:11 +0700

    Simplify Paths definition

--- src/paths.ml
+++ src/paths.ml
@@ -1,21 +1,15 @@
 open Stdune
 
-let digest_db () = Path.relative_to_build_dir ".digest-db"
+let make s =
+  let d = Path.relative_to_build_dir s in
+  fun () -> d
 
-let db () = Path.relative_to_build_dir ".db"
-
-let log () = Path.relative_to_build_dir "log"
-
-let to_delete_source_tree () =
-  Path.relative_to_build_dir ".to-delete-in-source-tree"
-
-let to_promote () =
-  Path.relative_to_build_dir ".to-promote"
-
-let aliases () = Path.relative_to_build_dir ".aliases"
-
-let misc () = Path.(relative build_dir) ".misc"
-
-let universe_file () = Path.relative Path.build_dir ".universe-state"
-
-let install_dir () = Path.relative_to_build_dir "install"
+let digest_db             = make ".digest-db"
+let db                    = make ".db"
+let log                   = make "log"
+let to_delete_source_tree = make ".to-delete-in-source-tree"
+let to_promote            = make ".to-promote"
+let aliases               = make ".aliases"
+let misc                  = make ".misc"
+let universe_file         = make ".universe-state"
+let install_dir           = make "install"

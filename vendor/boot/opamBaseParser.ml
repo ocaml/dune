4523bd1b2c8f6a4ed06987d675f1b80a0883e7b1
Jeremie Dimino <jdimino@janestreet.com>
2018-04-30 16:14:07 +0100

    Fix for travis

--- vendor/boot/opamBaseParser.ml
+++ vendor/boot/opamBaseParser.ml
@@ -1,6 +1,8 @@
 open OpamParserTypes
 let main _lex _lexbuf fn =
-  assert (fn = "dune.opam");
+  assert (fn = "dune.opam" ||
+          (* For travis *)
+          fn = "jbuilder.opam");
   { file_contents = []
   ; file_name     = fn
   }

2823525c6fae02dcccb885772e1883c6e288fab3
Jeremie Dimino <jdimino@janestreet.com>
2018-05-08 16:15:59 +0100

    Fix bootstrap

--- src/glob_lexer.boot.ml
+++ src/glob_lexer.boot.ml
@@ -2,3 +2,6 @@
    ocaml-re. This speeds up the bootstrap. *)
 
 let parse_string _ = failwith "globs are not available during bootstrap"
+
+(* To force the ordering during bootstrap *)
+let _ = Re.compile

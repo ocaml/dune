0195b4bd03dfca7bb5f3980367b2c2382e47844b
Rudi Grinberg <rudi.grinberg@gmail.com>
2018-07-06 14:41:29 +0700

    Add some helpers to impldement %{deps[i]}
    Signed-off-by: Rudi Grinberg <rudi.grinberg@gmail.com>

--- src/stdune/int.ml
+++ src/stdune/int.ml
@@ -13,3 +13,9 @@ include T
 
 module Set = Set.Make(T)
 module Map = Map.Make(T)
+
+let of_string_exn s =
+  match int_of_string s with
+  | exception Failure _ ->
+    failwith (Printf.sprintf "of_string_exn: invalid int %S" s)
+  | s -> s
--- src/stdune/int.mli
+++ src/stdune/int.mli
@@ -3,3 +3,5 @@ val compare : t -> t -> Ordering.t
 
 module Set : Set.S with type elt = t
 module Map : Map.S with type key = t
+
+val of_string_exn : string -> t

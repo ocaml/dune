eabf67bcacab2c0b2a9f16f42498c07c1b452f19
Rudi Grinberg <rudi.grinberg@gmail.com>
2018-07-23 00:23:38 +0200

    Add jsoo_compilation option to env
    Signed-off-by: Rudi Grinberg <rudi.grinberg@gmail.com>

--- src/dune_env.ml
+++ src/dune_env.ml
@@ -5,10 +5,30 @@ module Stanza = struct
 
   let field_oslu name = Ordered_set_lang.Unexpanded.field name
 
+  module Jsoo_compilation = struct
+    type t =
+      | Separate
+      | Classic
+
+    let default ~profile =
+      match profile with
+      | "dev" -> Separate
+      | _     -> Classic
+
+    let t =
+      Syntax.since Stanza.syntax (1, 1) >>= fun () ->
+      enum
+        [ "separate", Separate
+        ; "classic", Classic
+        ]
+  end
+
   type config =
     { flags          : Ordered_set_lang.Unexpanded.t
     ; ocamlc_flags   : Ordered_set_lang.Unexpanded.t
     ; ocamlopt_flags : Ordered_set_lang.Unexpanded.t
+
+    ; jsoo_compilation : Jsoo_compilation.t option
     }
 
   type pattern =
@@ -24,8 +44,13 @@ module Stanza = struct
     let%map flags = field_oslu "flags"
     and ocamlc_flags = field_oslu "ocamlc_flags"
     and ocamlopt_flags = field_oslu "ocamlopt_flags"
+    and jsoo_compilation = field_o "jsoo_compilation" Jsoo_compilation.t
     in
-    { flags; ocamlc_flags; ocamlopt_flags }
+    { flags
+    ; ocamlc_flags
+    ; ocamlopt_flags
+    ; jsoo_compilation
+    }
 
   let rule =
     enter
--- src/dune_env.mli
+++ src/dune_env.mli
@@ -3,10 +3,20 @@ open Import
 type stanza = Stanza.t = ..
 
 module Stanza : sig
+  module Jsoo_compilation : sig
+    type t =
+      | Separate
+      | Classic
+
+    val default : profile:string -> t
+  end
+
   type config =
     { flags          : Ordered_set_lang.Unexpanded.t
     ; ocamlc_flags   : Ordered_set_lang.Unexpanded.t
     ; ocamlopt_flags : Ordered_set_lang.Unexpanded.t
+
+    ; jsoo_compilation : Jsoo_compilation.t option
     }
 
   type pattern =

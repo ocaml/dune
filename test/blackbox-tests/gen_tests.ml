8ea075dd6b2faad54f2518fe5d56de99b4269262
Jeremie Dimino <jeremie@dimino.org>
2018-08-08 17:29:53 +0100

    Disable last added test
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- test/blackbox-tests/gen_tests.ml
+++ test/blackbox-tests/gen_tests.ml
@@ -139,6 +139,9 @@ let exclusions =
   ; make "github764" ~skip_platforms:[Win]
   ; make "gen-opam-install-file" ~external_deps:true
   ; make "scope-ppx-bug" ~external_deps:true
+  (* The next test is disabled as it relies on configured opam
+     swtiches and it's hard to get that working properly *)
+  ; make "envs-and-contexts" ~external_deps:true ~enabled:false
   ]
 
 let all_tests = lazy (

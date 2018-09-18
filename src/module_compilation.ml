6e3d2a181536fc1ea0ed6186d315c80cd16579a8
Rudi Grinberg <rudi.grinberg@gmail.com>
2018-09-18 04:05:19 +0400

    Use Module.Name.Map.mem rather than find
    Signed-off-by: Rudi Grinberg <rudi.grinberg@gmail.com>

--- src/module_compilation.ml
+++ src/module_compilation.ml
@@ -40,19 +40,21 @@ let build_cm cctx ?sandbox ?(dynlink=true) ~dep_graphs
   let stdlib   = CC.stdlib        cctx in
   let private_obj_dir = CC.private_obj_dir cctx in
   let vlib_modules = CC.vlib_modules cctx in
-  Option.iter (Mode.of_cm_kind cm_kind |> Context.compiler ctx) ~f:(fun compiler ->
+  Mode.of_cm_kind cm_kind
+  |> Context.compiler ctx
+  |> Option.iter ~f:(fun compiler ->
     Option.iter (Module.cm_source m cm_kind) ~f:(fun src ->
       let ml_kind = Cm_kind.source cm_kind in
       let dst = Module.cm_file_unsafe m ~obj_dir cm_kind in
       let extra_args, extra_deps, other_targets =
         match cm_kind, m.intf
-              , Module.Name.Map.find vlib_modules (Module.name m) with
+              , Module.Name.Map.mem vlib_modules (Module.name m) with
         (* If there is no mli, [ocamlY -c file.ml] produces both the
            .cmY and .cmi. We choose to use ocamlc to produce the cmi
            and to produce the cmx we have to wait to avoid race
            conditions. *)
-        | Cmo, None, None -> [], [], [Target.cm m Cmi]
-        | Cmo, None, Some _
+        | Cmo, None, false -> [], [], [Target.cm m Cmi]
+        | Cmo, None, true
         | (Cmo | Cmx), _, _ ->
           force_read_cmi src,
           [Module.cm_file_unsafe m ~obj_dir Cmi],

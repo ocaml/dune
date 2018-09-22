f226e6bc689959c2e68ba325ec71497370c10322
Rudi Grinberg <rudi.grinberg@gmail.com>
2018-09-22 09:13:20 -0500

    Fix .merlin generation to include private modules
    Signed-off-by: Rudi Grinberg <rudi.grinberg@gmail.com>

--- src/merlin.ml
+++ src/merlin.ml
@@ -140,7 +140,12 @@ let dot_merlin sctx ~dir ~more_src_dirs ~scope ~dir_kind
               ( Path.Set.add src_dirs (
                   Lib.src_dir lib
                   |> Path.drop_optional_build_context)
-              , Path.Set.add obj_dirs (Lib.obj_dir lib)))
+              ,
+              let obj_dirs = Path.Set.add obj_dirs (Lib.obj_dir lib) in
+              match Lib.private_obj_dir lib with
+              | None -> obj_dirs
+              | Some private_obj_dir -> Path.Set.add obj_dirs private_obj_dir
+            ))
         in
         let src_dirs =
           Path.Set.union src_dirs (Path.Set.of_list more_src_dirs)

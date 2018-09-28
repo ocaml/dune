1617b3d6435bf0609ba64420cfbcc498bf2d0af6
Etienne Millon <etienne.millon@gmail.com>
2018-09-28 15:07:30 +0200

    Add github info for generated docs (#1351)
    This displays an "Edit on github" link even locally.
    
    Closes #1350
    
    Signed-off-by: Etienne Millon <me@emillon.org>

--- doc/conf.py
+++ doc/conf.py
@@ -149,3 +149,11 @@ texinfo_documents = [
 import sphinx_rtd_theme
 
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+html_context = {
+    'display_github': True,
+    'github_user': 'ocaml',
+    'github_repo': 'dune',
+    'github_version': 'master',
+    'conf_py_path': '/doc/',
+}

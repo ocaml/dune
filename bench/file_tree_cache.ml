da3771700b2f1abceea7f9e9df0943445da753d9
Rudi Grinberg <rudi.grinberg@gmail.com>
2018-12-05 11:04:09 +0000

    Improve benchmarks
    * remove generated dune-project files
    * lazily setup benchmarks
    
    Signed-off-by: Rudi Grinberg <rudi.grinberg@gmail.com>

--- bench/file_tree_cache.ml
+++ bench/file_tree_cache.ml
@@ -5,15 +5,18 @@ open Dune
 
 let deep_path = "a1/a2/a3/a4/a5/a6/a7/a8/a9/10"
 
-let (ft, path) =
+let setup = lazy (
   let tmp = Path.External.of_string (Filename.get_temp_dir_name ()) in
   Path.External.mkdir_p (Path.External.relative tmp deep_path);
   Path.set_root tmp;
   Path.set_build_dir (Path.Kind.of_string "_build");
   let ft = (Dune_load.load ()).file_tree in
   let path = Path.of_string deep_path in
-  (ft, path)
+  at_exit (fun () -> Sys.remove "./dune-project");
+  (ft, path))
 
-let%bench "File_tree.find_dir" =
-  ignore (Sys.opaque_identity (File_tree.find_dir ft path)
-          : File_tree.Dir.t option)
+let%bench_fun "File_tree.find_dir" =
+  let (ft, path) = Lazy.force setup in
+  fun () ->
+    ignore (Sys.opaque_identity (File_tree.find_dir ft path)
+            : File_tree.Dir.t option)

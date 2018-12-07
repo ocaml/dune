af2ebb2aa344ec037a3b26e061cf16baf6c443ec
Etienne Millon <me@emillon.org>
2018-12-07 13:05:17 +0100

    Fix JS tests with newer js_of_ocaml
    Signed-off-by: Etienne Millon <me@emillon.org>

--- test/blackbox-tests/test-cases/js_of_ocaml/lib/x.ml
+++ test/blackbox-tests/test-cases/js_of_ocaml/lib/x.ml
@@ -1,3 +1,5 @@
+module Js = Js_of_ocaml.Js
+
 let buy_it = "buy " ^ Y.it
-let print x = Js_of_ocaml.Js.to_string x##.name
+let print x = Js.to_string x##.name
 external external_print  : Js.js_string Js.t -> unit = "jsPrint"

a1e16914d773534855b49df1195612de95f9be8d
Jeremie Dimino <jeremie@dimino.org>
2019-01-10 17:57:05 +0000

    Fix 4.02 build
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- src/dune_lang/lexer.ml
+++ src/dune_lang/lexer.ml
@@ -1,4 +1,7 @@
-include Lexer_shared
+module Token = Lexer_shared.Token
+module Error = Lexer_shared.Error
+
+type t = Lexer_shared.t
 
 let token = Dune_lexer.token
 let jbuild_token = Jbuild_lexer.token
@@ -6,3 +9,5 @@ let jbuild_token = Jbuild_lexer.token
 let of_syntax = function
   | Syntax.Dune -> token
   | Jbuild -> jbuild_token
+
+exception Error = Lexer_shared.Error

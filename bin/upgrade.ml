31b7da3f83cdb505ad422722758f625969d979cc
Jeremie Dimino <jeremie@dimino.org>
2019-01-14 10:43:15 +0000

    Do load load dune files in the upgrader
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- bin/upgrade.ml
+++ bin/upgrade.ml
@@ -1,6 +1,5 @@
 open! Stdune
 open Import
-open Fiber.O
 
 let doc = "Upgrade jbuilder projects to dune"
 
@@ -14,12 +13,9 @@ let man =
 let info = Term.info "upgrade" ~doc ~man
 
 let term =
-  let%map common = Common.term
-  in
+  let%map common = Common.term in
   Common.set_common common ~targets:[];
-  let log = Log.create common in
-  Scheduler.go ~log ~common (fun () ->
-    Import.Main.setup ~log common >>= fun setup ->
-    Dune.Upgrader.upgrade setup.file_tree)
+  Scheduler.go ~common (fun () ->
+    Dune.Upgrader.upgrade (Dune.File_tree.load Path.root))
 
 let command = term, info

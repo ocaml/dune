72bb0478d1bc1728be30545214ddce099c07b1d8
Jeremie Dimino <jeremie@dimino.org>
2019-01-15 10:43:37 +0000

    Add Opam_file.absolutify_positions
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- src/opam_file.ml
+++ src/opam_file.ml
@@ -20,3 +20,48 @@ let get_field t name =
       | Variable (_, var, value) when name = var ->
         Some value
       | _ -> None)
+
+let absolutify_positions ~file_contents t =
+  let open OpamParserTypes in
+  let bols = ref [] in
+  String.iteri file_contents ~f:(fun i ch ->
+    if ch = '\n' then bols := (i + 1) :: !bols);
+  let bols = Array.of_list (List.rev !bols) in
+  let map_pos (fname, line, col) =
+    (fname, line, bols.(line - 1) + col)
+  in
+  let rec map_value = function
+    | Bool (pos, x) ->
+      Bool (map_pos pos, x)
+    | Int (pos, x) ->
+      Int (map_pos pos, x)
+    | String (pos, x) ->
+      String (map_pos pos, x)
+    | Relop (pos, x, y, z) ->
+      Relop (map_pos pos, x, map_value y, map_value z)
+    | Prefix_relop (pos, x, y) ->
+      Prefix_relop (map_pos pos, x, map_value y)
+    | Logop (pos, x, y, z) ->
+      Logop (map_pos pos, x, map_value y, map_value z)
+    | Pfxop (pos, x, y) ->
+      Pfxop (map_pos pos, x, map_value y)
+    | Ident (pos, x) ->
+      Ident (map_pos pos, x)
+    | List (pos, x) ->
+      List (map_pos pos, List.map x ~f:map_value)
+    | Group (pos, x) ->
+      Group (map_pos pos, List.map x ~f:map_value)
+    | Option (pos, x, y) ->
+      Option (map_pos pos, map_value x, List.map y ~f:map_value)
+    | Env_binding (pos, x, y, z) ->
+      Env_binding (map_pos pos, map_value x, y, map_value z)
+  in
+  let rec map_section s =
+    { s with section_items = List.map s.section_items ~f:map_item }
+  and map_item = function
+    | Section (pos, s) -> Section (map_pos pos, map_section s)
+    | Variable (pos, s, v) -> Variable (map_pos pos, s, map_value v)
+  in
+  { file_contents = List.map t.file_contents ~f:map_item
+  ; file_name = t.file_name
+  }
--- src/opam_file.mli
+++ src/opam_file.mli
@@ -12,3 +12,7 @@ val load : Path.t -> t
 
 (** Extracts a field *)
 val get_field : t -> string -> value option
+
+(** Replace all [pos] value by a triplet [(fname, line,
+    absolute_offset)] *)
+val absolutify_positions : file_contents:string -> opamfile -> opamfile

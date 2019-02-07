2c8c6ea26e9ebcbf55c5b23d9c4cc0aa3cb5cf3d
Rudi Grinberg <rudi.grinberg@gmail.com>
2019-02-07 19:08:05 +0700

    Move Pp_spec to Virtual_rules
    Signed-off-by: Rudi Grinberg <rudi.grinberg@gmail.com>

--- src/virtual_rules.ml
+++ src/virtual_rules.ml
@@ -1,6 +1,33 @@
 open Import
 open! No_io
 
+module Pp_spec : sig
+  type t
+
+  val make
+    :  Dune_file.Preprocess.t Dune_file.Per_module.t
+    -> t
+
+  val pped_modules : t -> Module.Name_map.t -> Module.Name_map.t
+end = struct
+  type t = (Module.t -> Module.t) Dune_file.Per_module.t
+
+  let make preprocess =
+    Dune_file.Per_module.map preprocess ~f:(function
+      | Dune_file.Preprocess.No_preprocessing -> Module.ml_source
+      | Action (_, _) ->
+        fun m -> Module.ml_source (Module.pped m)
+      | Pps { loc = _; pps = _; flags = _; staged } ->
+        if staged then
+          Module.ml_source
+        else
+          fun m -> Module.pped (Module.ml_source m))
+
+  let pped_modules (t : t) modules =
+    Module.Name.Map.map modules ~f:(fun (m : Module.t) ->
+      Dune_file.Per_module.get t (Module.name m) m)
+end
+
 let setup_copy_rules_for_impl ~sctx ~dir vimpl =
   let ctx = Super_context.context sctx in
   let vlib = Vimpl.vlib vimpl in

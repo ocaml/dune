843d2523277af7e06f6a300730bc4e0b8906fb89
Arseniy Alekseyev <aalekseyev@janestreet.com>
2019-03-04 10:39:56 +0000

    add doc
    Signed-off-by: Arseniy Alekseyev <aalekseyev@janestreet.com>

--- src/stdune/table.mli
+++ src/stdune/table.mli
@@ -1,6 +1,18 @@
-module type Key = sig type t val equal : t -> t -> bool val hash : t -> int end
+(**
+   This module re-wraps the hashtable implementation provided by [Hashtbl.Make] under
+   a different interface: we just have a single type [('k, 'v) t], similar to a
+   polymorphic hashtable.
+
+   This means that if you want a hash table generic over the type of keys,
+   you don't have to put your type inside a functor.
+
+   Unlike the polymorphich hashtable ([('k, 'v) Hashtbl.t]), this does not use polymorphic
+   hash and polymorphic equality, so this module does respect abstraction boundaries.
+*)
 
 type ('k, 'v) t
+
+module type Key = sig type t val equal : t -> t -> bool val hash : t -> int end
 
 val create : (module Key with type t = 'k) -> int -> ('k, 'v) t
 

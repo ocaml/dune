ad868bf22501344ff70574b423d16f6a692af1fb
Rudi Grinberg <rudi.grinberg@gmail.com>
2019-04-14 17:06:37 +0700

    Do not compile coq rules for bootstrap
    Signed-off-by: Rudi Grinberg <rudi.grinberg@gmail.com>

---
+++ src/coq_rules.boot.ml
@@ -0,0 +1,5 @@
+module Dir_contents = Dir_contents (* hack for bootstrappign + ocamldep*)
+
+let setup_rules ~sctx:_ ~dir:_ ~dir_contents:_ _ = []
+let install_rules ~sctx:_ ~dir:_ _ = []
+let coqpp_rules ~sctx:_ ~dir:_ _ = []

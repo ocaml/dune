018416f18b8bfe49f782f783a697caa1c103468f
Jeremie Dimino <jeremie@dimino.org>
2019-04-16 16:32:19 +0100

    Start support for passing extra cookies to ppx rewriters
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- src/lib_kind.ml
+++ src/lib_kind.ml
@@ -1,7 +1,44 @@
+module Ppx_args = struct
+  module Cookie = struct
+    type t =
+      { name : string
+      ; value : String_with_vars.t
+      }
+
+    let decode =
+      let open Stanza.Decoder in
+      let+ name = string
+      and+ value = String_with_vars.decode in
+      return { name; value }
+
+    let encode { name; value } =
+      let open Dune_lang.Encoder in
+      List
+        [ string name
+        ; String_with_vars.encode value
+        ]
+  end
+
+  type t =
+    { cookies : Cookies.t list
+    }
+
+  let decode =
+    let open Stanza.Decoder in
+    fields
+      (let+ cookies = field "cookies" (list Cookie.decode) ~default:[] in
+       { cookies })
+
+  let encode { cookies } =
+    let open Dune_lang.Encoder in
+    record_fields
+      [ field_l "cookies" Cookies.encode cookies ]
+end
+
 type t =
   | Normal
-  | Ppx_deriver
-  | Ppx_rewriter
+  | Ppx_deriver of Ppx_args.t
+  | Ppx_rewriter of Ppx_args.t
 
 let decode =
   let open Dune_lang.Decoder in
@@ -12,8 +49,14 @@ let decode =
     ]
 
 let encode t =
-  Dune_lang.Encoder.string (
+  let open Dune_lang.Encoder in
+  match
     match t with
-    | Normal -> "normal"
-    | Ppx_deriver -> "ppx_deriver"
-    | Ppx_rewriter -> "ppx_rewriter")
+    | Normal -> Dune_lang.atom "normal"
+    | Ppx_deriver x ->
+      List (Dune_lang.atom "ppx_deriver" :: Ppx_args.encode x)
+    | Ppx_rewriter x ->
+      List (Dune_lang.atom "ppx_rewriter" :: Ppx_args.encode x)
+  with
+  | List [x] -> x
+  | x -> x
--- src/lib_kind.mli
+++ src/lib_kind.mli
@@ -1,6 +1,19 @@
+module Ppx_args : sig
+  module Cookie = struct
+    type t =
+      { name : string
+      ; value : String_with_vars.t
+      }
+  end
+
+  type t =
+    { cookies : Cookies.t list
+    }
+end
+
 type t =
   | Normal
-  | Ppx_deriver
-  | Ppx_rewriter
+  | Ppx_deriver of Ppx_args.t
+  | Ppx_rewriter of Ppx_args.t
 
 include Dune_lang.Conv with type t := t

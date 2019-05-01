192f467b4270253904d87c9e6de7f2ebdd9e111a
Jeremie Dimino <jeremie@dimino.org>
2019-05-01 09:57:03 +0100

    Refactor root detection
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

---
+++ bin/workspace_root.ml
@@ -0,0 +1,86 @@
+open Stdune
+open Dune
+
+module Kind = struct
+  type t =
+    | Explicit
+    | Dune_workspace
+    | Jbuild_workspace
+    | Dune_project
+    | Cwd
+
+  let priority = function
+    | Explicit -> 0
+    | Dune_workspace -> 1
+    | Jbuild_workspace -> 2
+    | Dune_project -> 3
+    | Cwd -> 4
+
+  let of_dir_contents files =
+    if String.Set.mem files Workspace.filename then
+      Some Dune_workspace
+    else if Wp.t = Jbuilder && String.Set.exists files ~f:(fun fn ->
+      String.is_prefix fn ~prefix:"jbuild-workspace") then
+      Some Jbuild_workspace
+    else if String.Set.mem files Dune_project.filename then
+      Some Dune_project
+    else
+      None
+end
+
+type t =
+  { dir : string
+  ; to_cwd : string list
+  ; kind : Kind.t
+  }
+
+let make kind dir =
+  { kind
+  ; dir
+  ; to_cwd = []
+  }
+
+let find () =
+  let cwd = Sys.getcwd () in
+  let rec loop counter ~candidate ~to_cwd dir =
+    match Sys.readdir dir with
+    | exception (Sys_error msg) ->
+      Errors.warn Loc.none
+        "Unable to read directory %s. \
+         Will not look for root in parent directories@.\
+         Reason: %s@.\
+         To remove this warning, set your root explicitly using --root.@."
+        dir msg;
+      candidate
+    | files ->
+      let files = String.Set.of_list (Array.to_list files) in
+      let candidate =
+        match Kind.of_dir_contents files with
+        | Some kind when Kind.priority kind <= Kind.priority candidate.kind ->
+          { kind; dir; to_cwd }
+        | _ ->
+          candidate
+      in
+      cont counter ~candidate dir ~to_cwd
+  and cont counter ~candidate ~to_cwd dir =
+    if counter > String.length cwd then
+      candidate
+    else
+      let parent = Filename.dirname dir in
+      if parent = dir then
+        candidate
+      else
+        let base = Filename.basename dir in
+        loop (counter + 1) parent ~candidate ~to_cwd:(base :: to_cwd)
+  in
+  loop 0 ~to_cwd:[] cwd
+    ~candidate:{ kind = Cwd; dir = cwd; to_cwd = [] }
+
+let create ~specified_by_user =
+  match specified_by_user with
+  | Some dn -> make Explicit dn
+  | None ->
+    if Config.inside_dune then
+      make Cwd "."
+    else
+      find ()

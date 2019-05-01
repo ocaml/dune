dd5269674632803d9d4c2df2e2e6760691c012a0
Arseniy Alekseyev <aalekseyev@janestreet.com>
2019-05-01 18:54:10 +0100

    add .dir-locals.el
    Signed-off-by: Arseniy Alekseyev <aalekseyev@janestreet.com>

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+(
+ (tuareg-mode (whitespace-line-column . 80))
+ (tuareg-mode (fill-column . 80))
+ )

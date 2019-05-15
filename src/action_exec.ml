5c0484fc5f8b3fcaa5cab756a18eeb6525cb473c
Rudi Grinberg <rudi.grinberg@gmail.com>
2019-05-15 13:49:20 +0800

    Do not allow actions to create dirs outside of build dir
    Signed-off-by: Rudi Grinberg <rudi.grinberg@gmail.com>

--- src/action_exec.ml
+++ src/action_exec.ml
@@ -108,7 +108,13 @@ let rec exec t ~ectx ~dir ~env ~stdout_to ~stderr_to =
     Path.rm_rf path;
     Fiber.return ()
   | Mkdir path ->
-    Path.mkdir_p path;
+    begin
+      if Path.is_in_build_dir path then
+        Path.mkdir_p path
+      else
+        Exn.code_error "Action_exec.exec: mkdir on non build dir"
+          ["path", Path.to_sexp path]
+    end;
     Fiber.return ()
   | Digest_files paths ->
     let s =

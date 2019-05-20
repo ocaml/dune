da4d72c90e8eeccda3d079eca84c2ba1a45113bd
Rudi Grinberg <rudi.grinberg@gmail.com>
2019-05-20 14:57:30 +0800

    Use source paths in bootstrap
    Data only paths must only exist in the source
    
    Signed-off-by: Rudi Grinberg <rudi.grinberg@gmail.com>

--- src/bootstrap.mli
+++ src/bootstrap.mli
@@ -4,4 +4,4 @@ open Stdune
 
 (** Treat the following path as if it was declared as a data only path
     in a [dune] file. *)
-val data_only_path : Path.t -> bool
+val data_only_path : Path.Source.t -> bool

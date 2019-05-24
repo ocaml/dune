853954a6daf7f322af166e676102f6569b5076ea
Andrey Mokhov <andrey.mokhov@gmail.com>
2019-05-24 03:13:45 +0100

    Fix js_of_ocaml_rules.boot.ml
    Signed-off-by: Andrey Mokhov <andrey.mokhov@gmail.com>

--- src/js_of_ocaml_rules.boot.ml
+++ src/js_of_ocaml_rules.boot.ml
@@ -1,7 +1,7 @@
 
 let build_cm _ ~js_of_ocaml:_ ~src:_ ~target:_ = []
 
-let build_exe _ ~js_of_ocaml:_ ~src:_ = []
+let build_exe _ ~js_of_ocaml:_ ~src:_ _ _ = []
 
 let setup_separate_compilation_rules _ _ = ()
 

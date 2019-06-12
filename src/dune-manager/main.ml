be14774dbec97b7aee7134dd7f42634bdfc3bab5
mefyl <mefyl@gruntech.org>
2019-06-12 13:50:27 +0200

    Dune_manager: Create port file.
    Signed-off-by: mefyl <mefyl@gruntech.org>

--- src/dune-manager/main.ml
+++ src/dune-manager/main.ml
@@ -1,16 +1,29 @@
 open Stdune
 open Dune_manager
 
+let runtime_dir =
+  let xdg =
+    try Sys.getenv "XDG_RUNTIME_DIR"
+    with Not_found -> failwith "XDG_RUNTIME_DIR is not set"
+  in
+  Filename.concat xdg "dune-manager"
+
 let () =
-  let manager =
-    DuneManager.make ~root:(Path.of_string "/tmp/dune-memory") ()
+  Path.mkdir_p (Path.of_string runtime_dir) ;
+  let manager = DuneManager.make ()
+  and port_path = Filename.concat runtime_dir "port" in
+  let port_f port =
+    let c = open_out port_path in
+    let f () = output_string c (string_of_int port)
+    and finally () = close_out c in
+    Exn.protect ~f ~finally
   in
   Sys.set_signal Sys.sigint
     (Sys.Signal_handle (fun _ -> DuneManager.stop manager)) ;
   try
-    DuneManager.run
-      ~port_f:(fun port -> Printf.printf "listening on port %i\n%!" port)
-      manager
+    let f () = DuneManager.run ~port_f manager
+    and finally () = Sys.remove port_path in
+    Exn.protect ~f ~finally
   with
   | DuneManager.Error s ->
       Printf.fprintf stderr "%s: fatal error: %s\n" Sys.argv.(0) s ;

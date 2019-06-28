216d4b70e6ded3de0b96a2ab262e56c6c126e4ed
mefyl <mefyl@gruntech.org>
2019-06-28 15:17:51 +0200

    Dune_manager: Parse command line.
    Signed-off-by: mefyl <mefyl@gruntech.org>

--- src/dune-manager/main.ml
+++ src/dune-manager/main.ml
@@ -8,12 +8,20 @@ let runtime_dir =
   in
   Filename.concat xdg "dune-manager"
 
-let () =
+let main () =
+  let port_path = ref (Filename.concat runtime_dir "port")
+  and usage = Printf.sprintf "%s [OPTIONS]" Sys.argv.(0) in
+  Arg.parse_argv Sys.argv
+    [ ( "--port"
+      , Arg.Set_string port_path
+      , Printf.sprintf "file to write listening port to (default: %s)"
+          !port_path ) ]
+    (fun o -> raise (Arg.Bad (Printf.sprintf "unexpected option: %s" o)))
+    usage ;
   Path.mkdir_p (Path.of_string runtime_dir) ;
   let manager = DuneManager.make ()
-  and port_path = Filename.concat runtime_dir "port" in
-  let port_f port =
-    let c = open_out port_path in
+  and port_f port =
+    let c = open_out !port_path in
     let f () = output_string c (string_of_int port)
     and finally () = close_out c in
     Exn.protect ~f ~finally
@@ -22,7 +30,7 @@ let () =
     (Sys.Signal_handle (fun _ -> DuneManager.stop manager)) ;
   try
     let f () = DuneManager.run ~port_f manager
-    and finally () = Sys.remove port_path in
+    and finally () = Sys.remove !port_path in
     Exn.protect ~f ~finally
   with
   | DuneManager.Error s ->
@@ -30,3 +38,12 @@ let () =
       exit 1
   | DuneManager.Stop ->
       ()
+
+let () =
+  try main () with
+  | Arg.Bad reason ->
+      Printf.fprintf stderr "%s: command line error: %s" Sys.argv.(0) reason ;
+      exit 1
+  | Arg.Help help ->
+      Printf.fprintf stdout "Usage: %s" help ;
+      exit 1

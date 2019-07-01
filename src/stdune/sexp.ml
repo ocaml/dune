e1f071a7c5968b050b5e1e386f9f425bf9344524
Jeremie Dimino <jeremie@dimino.org>
2019-07-01 11:13:23 +0100

    Inline only use of Sexp.Encoder
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- src/stdune/sexp.ml
+++ src/stdune/sexp.ml
@@ -9,34 +9,6 @@ type t = Sexp0.t =
 (* XXX otherwise the dependency isn't recorded by bootstrap *)
 module Sexp_intf = Sexp_intf
 
-module Encoder = struct
-  type sexp = t
-  type 'a t = 'a -> sexp
-
-  let unit () = List []
-  let char c = Atom (String.make 1 c)
-  let string s = Atom s
-  let int i = Atom (string_of_int i)
-  let float f = Atom (string_of_float f)
-  let bool b = Atom (string_of_bool b)
-  let pair fa fb (a, b) = List [fa a; fb b]
-  let triple fa fb fc (a, b, c) = List [fa a; fb b; fc c]
-  let list f l = List (List.map l ~f)
-  let array f a = list f (Array.to_list a)
-  let option f = function
-    | None -> List []
-    | Some x -> List [f x]
-
-  let record l =
-    List (List.map l ~f:(fun (n, v) -> List [Atom n; v]))
-
-  let unknown _ = Atom "<unknown>"
-
-  let constr name = function
-    | [] -> Atom name
-    | args -> List (Atom name :: args)
-end
-
 let rec to_string = function
   | Atom s -> Escape.quote_if_needed s
   | List l ->

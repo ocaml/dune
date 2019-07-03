f26eb1272ad67f927ca420f36e0e59a0324e93d0
mefyl <mefyl@gruntech.org>
2019-07-03 17:39:42 +0200

    CSexp: Prefer `Result.t` to exceptions.
    Signed-off-by: mefyl <mefyl@gruntech.org>

--- src/stdune/csexp.ml
+++ src/stdune/csexp.ml
@@ -1,13 +1,15 @@
-exception Parse_error of string
+open Result.O
+
+let ok = Result.ok
 
 type t = Sexp.t
 
 let peek s =
   match Stream.peek s with
   | Some v ->
-      v
+      ok v
   | None ->
-      raise (Parse_error "unexpected end of file")
+      Error "unexpected end of file"
 
 let read_string s l =
   let res = Bytes.make l ' ' in
@@ -18,38 +20,40 @@ let read_string s l =
         BytesLabels.set res v (Stream.next s) ;
         read (v + 1)
   in
-  try read 0 ; Bytes.to_string res
+  try
+    read 0 ;
+    ok (Bytes.to_string res)
   with Stream.Failure ->
-    raise
-      (Parse_error
-         (Printf.sprintf "unexpected end of file in atom of size %i" l))
+    Error (Printf.sprintf "unexpected end of file in atom of size %i" l)
 
 let parse stream =
   let rec read_size acc =
     let c = Stream.next stream in
-    if c = ':' then acc
+    if c = ':' then ok acc
     else
       let idx = int_of_char c - int_of_char '0' in
       if idx < 0 || idx > 9 then
-        raise (Parse_error (Printf.sprintf "invalid character in size: %c" c))
+        Error (Printf.sprintf "invalid character in size: %c" c)
       else read_size ((10 * acc) + idx)
   in
   let rec parse () =
-    match peek stream with
+    peek stream
+    >>= function
     | '(' ->
         Stream.junk stream ;
-        Sexp.List (parse_list ())
+        parse_list () >>| fun l -> Sexp.List l
     | _ ->
-        Atom (read_string stream (read_size 0))
+        read_size 0 >>= read_string stream >>| fun x -> Sexp.Atom x
   and parse_list () =
-    match peek stream with
+    peek stream
+    >>= function
     | ')' ->
-        Stream.junk stream ; []
+        Stream.junk stream ; ok []
     | ':' ->
-        raise (Parse_error "missing size")
+        Error "missing size"
     | _ ->
         let head = parse () in
-        head :: parse_list ()
+        head >>= fun head -> parse_list () >>| fun tail -> head :: tail
   in
   parse ()
 
@@ -61,7 +65,7 @@ let to_buffer ~buf sexp =
         Buffer.add_string buf (string_of_int (String.length str)) ;
         Buffer.add_string buf ":" ;
         Buffer.add_string buf str
-    | Sexp.List e ->
+    | Sexp.List (e : t list) ->
         Buffer.add_char buf '(' ;
         ignore (List.map ~f:loop e) ;
         Buffer.add_char buf ')'
--- src/stdune/csexp.mli
+++ src/stdune/csexp.mli
@@ -1,7 +1,5 @@
 type t = Sexp.t
 
-exception Parse_error of string
-
 val to_buffer : buf:Buffer.t -> t -> unit
 (** [to_buffer ~buf sexp] outputs the S-expression [sexp] converted to
     its canonical form to buffer [buf]. *)
@@ -10,6 +8,6 @@ val to_string : t -> string
 (** [to_string_mach sexp] converts S-expression [sexp] to a string in
     canonical form. *)
 
-val parse : char Stream.t -> t
+val parse : char Stream.t -> (t, string) Result.t
 (** [parse stream] reads one S-expression in canonical form from
     [stream] *)

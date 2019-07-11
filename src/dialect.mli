886fb5eb750494725c8b6c474ff4b771cb3a2677
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2019-07-11 05:26:56 +0200

    Add Dialect interface
    Signed-off-by: Nicolás Ojeda Bär <n.oje.bar@gmail.com>

---
+++ src/dialect.mli
@@ -0,0 +1,27 @@
+open! Stdune
+
+module Filter : sig
+  type t =
+    | No_filter
+    | Action of Loc.t * Action_dune_lang.t
+
+  val to_dyn : t -> Dyn.t
+end
+
+type t
+
+val name : t -> string
+
+val to_dyn : t -> Dyn.t
+
+val extension : t -> Ml_kind.t -> string
+
+val preprocess : t -> Ml_kind.t -> Filter.t
+
+val ml_suffix : t -> Ml_kind.t -> string option
+
+module Set : Set.S with type elt = t
+
+val ocaml : t
+
+val reason : t

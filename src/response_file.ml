e2767c250db278ce60dbfeea0de6c9d0843873dc
Rudi Grinberg <rudi.grinberg@gmail.com>
2019-07-27 16:39:18 +0700

    [refactor] replace poly table in response_file
    Signed-off-by: Rudi Grinberg <rudi.grinberg@gmail.com>

--- src/response_file.ml
+++ src/response_file.ml
@@ -4,10 +4,10 @@ type t =
   | Not_supported
   | Zero_terminated_strings of string
 
-let registry = Hashtbl.create 128
+let registry = Table.create (module Path) 128
 
 let get ~prog =
-  Option.value (Hashtbl.find registry prog) ~default:Not_supported
+  Option.value (Table.find registry prog) ~default:Not_supported
 
 let set ~prog t =
-  Hashtbl.set registry prog t
+  Table.set registry prog t

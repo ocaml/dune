106e89136a7fd64dd25a90394b6b28a4a6690da6
Rudi Grinberg <rudi.grinberg@gmail.com>
2019-07-31 14:28:54 +0700

    [stdune] Attach optional locations to code errors
    Signed-off-by: Rudi Grinberg <rudi.grinberg@gmail.com>

--- src/stdune/code_error.ml
+++ src/stdune/code_error.ml
@@ -1,14 +1,15 @@
 type t =
   { message : string
   ; data : (string * Dyn.t) list
+  ; loc : Loc0.t option
   }
 
 exception E of t
 
-let raise message data =
-  raise (E { message; data })
+let raise ?loc message data =
+  raise (E { message; data ; loc })
 
-let to_dyn { message; data } : Dyn.t =
+let to_dyn { loc = _; message; data } : Dyn.t =
   Tuple
     [ String message
     ; Record data
--- src/stdune/code_error.mli
+++ src/stdune/code_error.mli
@@ -3,10 +3,11 @@
 type t =
   { message : string
   ; data : (string * Dyn.t) list
+  ; loc : Loc0.t option
   }
 
 exception E of t
 
 val to_dyn : t -> Dyn.t
 
-val raise : string -> (string * Dyn.t) list -> _
+val raise : ?loc:Loc0.t -> string -> (string * Dyn.t) list -> _

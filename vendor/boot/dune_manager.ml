daaca397052bac6cc8b5e6e303f74a2ee7c70f41
mefyl <mefyl@gruntech.org>
2019-08-12 19:14:22 +0200

    Fix bootstrap on appveyor.
    Signed-off-by: mefyl <mefyl@gruntech.org>

--- vendor/boot/dune_manager.ml
+++ vendor/boot/dune_manager.ml
@@ -1,7 +1,7 @@
 module Client = struct
   type t = unit
 
-  let result = Result.Error "Dune_memory is disabled during bootstrap."
+  let result = Stdune.Result.Error "Dune_memory is disabled during bootstrap."
 
   let promote _ _ _ _ _ = result
 

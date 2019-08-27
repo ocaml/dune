d15ff6fac3a926f17e3c15d8150f9f7ff01bf06f
Jakub Staroń <jstaron@janestreet.com>
2019-08-27 14:02:17 -0400

    Adds of_string function to Csexp. (#2583)
    * Adds of_string function to Csexp.
    
    Signed-off-by: Jakub Staron <jstaron@janestreet.com>

--- src/stdune/csexp.mli
+++ src/stdune/csexp.mli
@@ -1,12 +1,15 @@
 type t = Sexp.t
 
+(** [to_string sexp] converts S-expression [sexp] to a string in canonical
+  form. *)
+val to_string : t -> string
+
+(** [parse_string string] parses [string] into S-expression *)
+val parse_string : string -> (t, string) Result.t
+
 (** [to_buffer ~buf sexp] outputs the S-expression [sexp] converted to its
   canonical form to buffer [buf]. *)
 val to_buffer : buf:Buffer.t -> t -> unit
-
-(** [to_string_mach sexp] converts S-expression [sexp] to a string in canonical
-  form. *)
-val to_string : t -> string
 
 (** [parse stream] reads one S-expression in canonical form from [stream] *)
 val parse : char Stream.t -> (t, string) Result.t
--- test/expect-tests/dune_lang/csexp_tests.ml
+++ test/expect-tests/dune_lang/csexp_tests.ml
@@ -43,11 +43,18 @@ let%expect_test _ =
   [%expect
     {|((8:metadata(3:foo3:bar))(14:produced-files((9:/tmp/coin63:/tmp/dune-memory/v2/files/b2/b295e63b0b8e8fae971d9c493be0d261.1))))|}]
 
-let parse s =
-  parse (Stream.of_string s)
+let print_parsed r =
+  r
   |> Result.map ~f:Csexp.to_string
   |> Result.to_dyn String.to_dyn String.to_dyn
   |> print_dyn
+
+let parse s = parse (Stream.of_string s) |> print_parsed
+
+let%expect_test _ =
+  parse "(3:foo)";
+  [%expect {|
+    Ok "(3:foo)" |}]
 
 let%expect_test _ =
   parse "";
@@ -72,3 +79,29 @@ let%expect_test _ =
 let%expect_test _ =
   parse "(5:foo)";
   [%expect {| Error "unexpected end of file in atom of size 5" |}]
+
+let%expect_test _ =
+  parse "(3:foo)";
+  [%expect {| Ok "(3:foo)" |}]
+
+let%expect_test _ =
+  let stream = Stream.of_string "(3:foo)(3:foo)" in
+  Csexp.parse stream |> print_parsed;
+  [%expect {| Ok "(3:foo)" |}];
+  Stream.peek stream |> Option.value_exn |> print_char;
+  [%expect {| ( |}]
+
+let%expect_test _ =
+  let stream = Stream.of_string "(3:foo)Additional_stuff" in
+  Csexp.parse stream |> print_parsed;
+  [%expect {| Ok "(3:foo)" |}];
+  Stream.peek stream |> Option.value_exn |> print_char;
+  [%expect {| A |}]
+
+let%expect_test _ =
+  parse_string "(3:foo)(3:foo)" |> print_parsed;
+  [%expect {| Error "not whole string consumed" |}]
+
+let%expect_test _ =
+  parse_string "(3:foo)additional_stuff" |> print_parsed;
+  [%expect {| Error "not whole string consumed" |}]

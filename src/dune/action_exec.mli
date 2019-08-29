6c48b1ec50f4ba92b0f02d6e3dd63f67a22c417a
Jakub Staron <jstaron@janestreet.com>
2019-08-29 18:32:33 +0100

    Clarify TODO.

--- src/dune/action_exec.mli
+++ src/dune/action_exec.mli
@@ -6,7 +6,9 @@ type done_or_more_deps =
     [Protocol.Dependency.t]. Assume action have multiple 'dynamic-run' commands
      that are executed in different directories (by chdir). Assume also, that
      they use dependencies with the same names (but different directories).
-     Then we can not distinguish these two. *)
+     Then we can not distinguish these two. EDIT: This problem appears only if
+     we have multiple 'dynamic-run' within single rule. If we disallow it,
+     nothing need to be done. *)
   | Need_more_deps of Dep.t Dune_action.Protocol.Dependency.Map.t
 
 val exec :

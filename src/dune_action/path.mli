12fa77ad83b8c4771394fd247edaad721374657d
Jakub Staron <jstaron@janestreet.com>
2019-08-30 20:21:45 +0100

    Adds some documentation to `Dune_action.Path` module.

--- src/dune_action/path.mli
+++ src/dune_action/path.mli
@@ -1,15 +1,20 @@
-(* TODO jstaron: Add description of this module. *)
-(* Mention that this module will only allow user to use relative paths. *)
+(** Representation of paths for "dune_action" library. *)
+
+(** We shouldn't use absolute paths when communicating with Dune, so this
+  module allows user to represent only relative paths. *)
 
 type t
 
 module O : sig
+  (** Concatenate two paths. *)
   val ( ^/ ) : t -> t -> t
 end
 
+(** Concatenate two paths. *)
 val concat : t -> t -> t
 
 val to_string : t -> string
 
-(* TODO jstaron: Document and mention that throws exceptions. *)
+(** Convert path to string. Throws an exception if passed string is not a
+  relative path. *)
 val of_string : string -> t

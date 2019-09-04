e3b27304e8df8bd5d776b76b76425ce41ccf7f1a
Jakub Staron <jstaron@janestreet.com>
2019-09-04 16:52:45 +0100

    Remove outdated examples of malicious use of library.

--- src/dune_action/examples/concatenate.ml
+++
@@ -1,8 +0,0 @@
-open Dune_action
-
-let concatenate_files file1 file2 file3 =
-  let contents = both (read_file ~path:file1) (read_file ~path:file2) in
-  map contents ~f:(fun (data1, data2) ->
-      write_file ~path:file3 ~data:(String.concat [ data1; data2 ]))
-
-let () = run @@ concatenate_files "foo" "bar" "foobar"

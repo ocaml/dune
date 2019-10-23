65895e421e81ce8efe25750337a869dd66ca9a34
Rudi Grinberg <rudi.grinberg@gmail.com>
2019-10-23 19:54:14 +0900

    [memo] Move Memo.Witness to Type_eq
    Signed-off-by: Rudi Grinberg <rudi.grinberg@gmail.com>

--- src/stdune/type_eq.ml
+++ src/stdune/type_eq.ml
@@ -1,3 +1,30 @@
 type ('a, 'b) t = T : ('a, 'a) t
 
 let cast (type a b) (T : (a, b) t) (x : a) : b = x
+
+type ('a, 'b) eq = ('a, 'b) t
+
+module Id = struct
+  type _ w = ..
+
+  module type T = sig
+    type a
+
+    type _ w += W : a w
+  end
+
+  type 'a t = (module T with type a = 'a)
+
+  let create (type a) () =
+    ( ( module struct
+      type nonrec a = a
+
+      type _ w += W : a w
+    end )
+      : a t )
+
+  let same (type a b) ((module M1) : a t) ((module M2) : b t) =
+    match M1.W with
+    | M2.W -> Some (T : (a, b) eq)
+    | _ -> None
+end
--- src/stdune/type_eq.mli
+++ src/stdune/type_eq.mli
@@ -3,3 +3,12 @@
 type ('a, 'b) t = T : ('a, 'a) t
 
 val cast : ('a, 'b) t -> 'a -> 'b
+
+module Id : sig
+  type ('a, 'b) eq
+  type 'a t
+
+  val create : unit -> 'a t
+
+  val same : 'a t -> 'b t -> ('a, 'b) eq option
+end with type ('a, 'b) eq := ('a, 'b) t

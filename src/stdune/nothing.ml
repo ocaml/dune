fdd12361e672cdca486b8575e58b9e09e2a3e7d3
Etienne Millon <me@emillon.org>
2019-11-04 09:51:12 +0100

    Nothing: implement using a refutation branch
    We do not need to support 4.02 anymore, so this part can be simplified.
    
    Signed-off-by: Etienne Millon <me@emillon.org>

--- src/stdune/nothing.ml
+++ src/stdune/nothing.ml
@@ -1,19 +1,4 @@
 type t = (int, string) Type_eq.t
 
-(* The purpose of [dummy] is to get the definition of [unreachable_code] that
-   OCaml 4.02 would accept without having to write "assert false".
-
-   The problem is that 4.02 doesn't have refutation branches, so we turn the
-   "nullary" pattern-match into an equivalent one with one branch. *)
-type ('a, 'b, 'c) dummy =
-  | No of 'c
-  | Eq : ('a, 'a, 'c) dummy
-
-let _f x = No x
-
-let to_dummy : type a b. (a, b) Type_eq.t -> (a, b, 'c) dummy =
- fun Type_eq.T -> Eq
-
-let unreachable_code (t : t) =
-  match to_dummy t with
-  | No c -> c
+let unreachable_code = function
+  | (_ : t) -> .

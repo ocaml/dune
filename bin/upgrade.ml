bec3da3d3120fbcf3f6d472be618b7f00598a12b
Jeremie Dimino <jeremie@dimino.org>
2019-11-07 08:29:17 +0000

    Get rid of File_tree.Settings.root
    It's always Path.Source.root
    
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- bin/upgrade.ml
+++ bin/upgrade.ml
@@ -17,8 +17,7 @@ let term =
   let+ common = Common.term in
   Common.set_common common ~targets:[];
   Scheduler.go ~common (fun () ->
-      Dune.File_tree.init Path.Source.root ~recognize_jbuilder_projects:true
-        ~ancestor_vcs:None;
+      Dune.File_tree.init ~recognize_jbuilder_projects:true ~ancestor_vcs:None;
       Dune.Upgrader.upgrade () |> Fiber.return)
 
 let command = (term, info)

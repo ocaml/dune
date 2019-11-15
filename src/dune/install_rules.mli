3b7240a6d00d6b217dad69310160c92985d21e15
Arseniy Alekseyev <aalekseyev@janestreet.com>
2019-11-15 15:52:36 +0000

    add a thought to a comment
    Signed-off-by: Arseniy Alekseyev <aalekseyev@janestreet.com>

--- src/dune/install_rules.mli
+++ src/dune/install_rules.mli
@@ -13,6 +13,10 @@ val meta_and_dune_package_rules : Super_context.t -> dir:Path.Build.t -> unit
 
 (* TODO: A seemingly more sensible approach for [meta_and_dune_package_rules]
    is to only generate rules for the given directory, or stop taking [dir]
-   altogether and take [project] instead. *)
+   altogether and take [project] instead.
+
+   aalekseyev: actually I think we should just remove
+   [meta_and_dune_package_rules] from the interface and have [gen_rules]
+   do everything. *)
 
 val packages : Super_context.t -> Package.Name.Set.t Path.Build.Map.t

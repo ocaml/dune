ed878157ac88d2c774b6a2f4efb7a2158cf663d7
mefyl <mefyl@gruntech.org>
2019-11-19 17:12:08 +0100

    Test durations decoding.
    Signed-off-by: mefyl <mefyl@gruntech.org>

---
+++ test/expect-tests/config_file_test.ml
@@ -0,0 +1,53 @@
+open Dune_lang
+open Dune_tests_common
+
+let parse s =
+  let ast =
+    Parser.parse_string ~fname:"expect_test" ~mode:Parser.Mode.Single s
+  in
+  Dune_lang.Decoder.parse Dune.Config.decode Stdune.Univ_map.empty ast
+  |> Dune.Config.to_dyn |> print_dyn
+
+let%expect_test _ =
+  parse "(cache-trim-period 2m)";
+  [%expect
+    {|
+{ display = "quiet"
+; concurrency = "1"
+; terminal_persistence = "preserve"
+; sandboxing_preference = []
+; cache_mode = "disabled"
+; cache_transport = "direct"
+; cache_check_probability = 0.
+; cache_trim_period = 120
+; cache_trim_size = 10000000000
+}
+ |}]
+
+let%expect_test _ =
+  parse "(cache-trim-period 2)";
+  [%expect.unreachable]
+  [@@expect.uncaught_exn
+    {|
+  ( "File\
+   \n\"expect_test\",\
+   \nline\
+   \n1,\
+   \ncharacters\
+   \n19-20:\
+   \nError: missing duration suffix, specify 's', 'm' or 'h'\
+   \n") |}]
+
+let%expect_test _ =
+  parse "(cache-trim-period 2k)";
+  [%expect.unreachable]
+  [@@expect.uncaught_exn
+    {|
+  ( "File\
+   \n\"expect_test\",\
+   \nline\
+   \n1,\
+   \ncharacters\
+   \n19-21:\
+   \nError: invalid duration suffix k\
+   \n") |}]

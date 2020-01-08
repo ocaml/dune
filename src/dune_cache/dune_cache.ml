8344a422291a3597f1c38663297c42941674394c
mefyl <mefyl@gruntech.org>
2020-01-08 17:36:18 +0100

    Always pick hardlink duplication mode in the cache daemon.
    Signed-off-by: mefyl <mefyl@gruntech.org>

--- src/dune_cache/dune_cache.ml
+++ src/dune_cache/dune_cache.ml
@@ -332,25 +332,21 @@ module Cache = struct
   let teardown _ = ()
 
   let detect_duplication_mode root =
-    if not (Path.Build.build_dir_set ()) then
-      (* We are in the daemon, use hardlink as the default *)
-      Duplication_mode.Hardlink
-    else
-      let () = Path.mkdir_p root in
-      let beacon = Path.relative root "beacon"
-      and target =
-        Path.build (Path.Build.of_local (Path.Local.of_string ".cache-beacon"))
-      in
-      let () = Path.touch beacon in
-      let rec test () =
-        match Path.link beacon target with
-        | exception Unix.Unix_error (Unix.EEXIST, _, _) ->
-          Path.unlink_no_err target;
-          test ()
-        | exception Unix.Unix_error _ -> Duplication_mode.Copy
-        | () -> Duplication_mode.Hardlink
-      in
-      test ()
+    let () = Path.mkdir_p root in
+    let beacon = Path.relative root "beacon"
+    and target =
+      Path.build (Path.Build.of_local (Path.Local.of_string ".cache-beacon"))
+    in
+    let () = Path.touch beacon in
+    let rec test () =
+      match Path.link beacon target with
+      | exception Unix.Unix_error (Unix.EEXIST, _, _) ->
+        Path.unlink_no_err target;
+        test ()
+      | exception Unix.Unix_error _ -> Duplication_mode.Copy
+      | () -> Duplication_mode.Hardlink
+    in
+    test ()
 
   let make ?(root = default_root ())
       ?(duplication_mode = detect_duplication_mode root) handler =

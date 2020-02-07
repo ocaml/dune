e614dd387577526924b78b720c2dba68b7487b31
Jeremie Dimino <jeremie@dimino.org>
2020-02-07 20:06:06 +0100

    Refactor a bit the Findlib module
    - keep only one hash table
    - use Package.Name.t rather than Lib_name.t when appropriate
    - try to re-organise a bit more the code to make it easier to follow
    
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- src/dune/findlib/findlib.mli
+++ src/dune/findlib/findlib.mli
@@ -18,44 +18,22 @@ val create :
 (** The search path for this DB *)
 val paths : t -> Path.t list
 
-module Package : sig
-  val preds : Variant.Set.t
-end
+val findlib_predicates_set_by_dune : Variant.Set.t
 
 module Unavailable_reason : sig
   type t =
     | Not_found
         (** The package is hidden because it contains an unsatisfied 'exist_if'
             clause *)
-    | Hidden of Dune_package.Lib.t
     | Invalid_dune_package of exn
-
-  val to_string : t -> string
 
   val to_dyn : t -> Dyn.t
 end
 
-module Meta_source : sig
-  type t = private
-    { dir : Path.t
-    ; meta_file : Path.t
-    ; meta : Meta.Simplified.t
-    }
-end
-
-module Root_package : sig
-  type t =
-  | Dune of Dune_package.t
-  | Findlib of Meta_source.t
-
-  type unavailable_reason =
-  | Not_found
-  | Invalid_dune_package of exn
-end
-
-(** Lookup a package in the given database *)
-val find_root_package:
-  t -> root_name:Lib_name.t -> (Root_package.t, Root_package.unavailable_reason) result
+(** Lookup a whole package, including sub-packages, in the given database.
+    [root_name] must be a library name without dots. *)
+val find_root_package :
+  t -> Package.Name.t -> (Dune_package.t, Unavailable_reason.t) result
 
 val find :
   t -> Lib_name.t -> (Dune_package.Entry.t, Unavailable_reason.t) result
@@ -65,12 +43,11 @@ val available : t -> Lib_name.t -> bool
 (** List all the packages available in this Database *)
 val all_packages : t -> Dune_package.Entry.t list
 
-(** List all the packages that are not available in this database *)
-val all_unavailable_packages : t -> (Lib_name.t * Unavailable_reason.t) list
+(** List all the packages that have broken [dune-package] files *)
+val all_broken_packages : t -> (Package.Name.t * exn) list
 
 (** A dummy package. This is used to implement [external-lib-deps] *)
-val dummy_package :
-  t -> name:Lib_name.t -> lib_config:Lib_config.t -> Dune_package.Lib.t
+val dummy_package : t -> name:Lib_name.t -> Dune_package.Lib.t
 
 module Config : sig
   type t

84de2d9a7c653cf4b8cf2010d5f065199465d0c8
Arseniy Alekseyev <aalekseyev@janestreet.com>
2020-02-19 17:44:54 +0000

    add comment
    Signed-off-by: Arseniy Alekseyev <aalekseyev@janestreet.com>

--- src/stdune/io.mli
+++ src/stdune/io.mli
@@ -1,4 +1,4 @@
-(** IO operations *)
+(** IO operations. *)
 
 val close_in : in_channel -> unit
 
@@ -8,6 +8,9 @@ val close_both : in_channel * out_channel -> unit
 
 val input_lines : in_channel -> string list
 
+(** This function is not safe to use from multiple threads, even if operating on
+    unrelated channels. If we want to use it from multiple threads, because it uses
+    a statically-allocated global buffer. *)
 val copy_channels : in_channel -> out_channel -> unit
 
 val read_all : in_channel -> string

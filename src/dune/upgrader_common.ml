d8981dfa99825f2f8a5f7d20f196dcd4306ee464
Ulysse Gérard <thevoodoos@gmail.com>
2020-02-25 18:16:31 +0100

    Implement the default mode check + reformat
    Signed-off-by: Ulysse Gérard <thevoodoos@gmail.com>

--- src/dune/upgrader_common.ml
+++ src/dune/upgrader_common.ml
@@ -15,34 +15,32 @@ type todo =
 module Ast_ops = struct
   open Dune_lang.Ast
 
-  let field_of_list ?more:(m=[]) atoms =
-    List(Loc.none, (List.map atoms
-      ~f:(fun a -> Atom(Loc.none, a)))
-      @ m
-      )
+  let field_of_list ?more:(m = []) atoms =
+    List (Loc.none, List.map atoms ~f:(fun a -> Atom (Loc.none, a)) @ m)
 
   let rec replace_first old_name new_name = function
-  | List(loc, Atom(loca, A atom) :: tll)
-    :: tl when atom = old_name ->
-      List(loc, Atom(loca, Dune_lang.Atom.of_string new_name) :: tll) :: tl
-  | List(loc, Quoted_string(loca, str) :: tll)
-    :: tl when str = old_name ->
-      List(loc, Quoted_string(loca, new_name) :: tll) :: tl
-  | hd :: tl -> hd :: (replace_first old_name new_name tl)
-  | [] -> []
+    | List (loc, Atom (loca, A atom) :: tll) :: tl when atom = old_name ->
+      List (loc, Atom (loca, Dune_lang.Atom.of_string new_name) :: tll) :: tl
+    | List (loc, Quoted_string (loca, str) :: tll) :: tl when str = old_name ->
+      List (loc, Quoted_string (loca, new_name) :: tll) :: tl
+    | hd :: tl -> hd :: replace_first old_name new_name tl
+    | [] -> []
 
   let extract_first names =
-    let rec is_names vals names = match vals, names with
-      | (Atom(_, A str) | Quoted_string(_, str)) :: tl, name :: tln
-        when str = name -> is_names tl tln
+    let rec is_names vals names =
+      match (vals, names) with
+      | (Atom (_, A str) | Quoted_string (_, str)) :: tl, name :: tln
+        when str = name ->
+        is_names tl tln
       | _, [] -> true
       | _, _ -> false
     in
     let rec aux rest = function
-    | List(_, elt) :: _ when is_names elt names -> Some elt, List.rev rest
-    | hd :: tl -> aux (hd :: rest) tl
-    | [] -> None, List.rev rest
-    in aux []
+      | List (_, elt) :: _ when is_names elt names -> (Some elt, List.rev rest)
+      | hd :: tl -> aux (hd :: rest) tl
+      | [] -> (None, List.rev rest)
+    in
+    aux []
 
   let is_in_list names sexp =
     match fst (extract_first names sexp) with
@@ -52,40 +50,29 @@ module Ast_ops = struct
   let bump_lang_version v =
     let v = Dune_lang.Syntax.Version.to_string v in
     function
-    | List(loc, (Atom(_, A "lang") as lang)
-        :: (Atom(_, A "dune")  as dune)
-        :: Atom(loc3, A _)
-        :: tll) ::tl ->
-      List(loc, lang :: dune
-        :: Atom(loc3, Dune_lang.Atom.of_string v)
-        :: tll) :: tl
-    | sexp -> sexp
-
-  let alias_to_rule = function
     | List
-        (loc,
-          Atom (loca, A "alias") :: tl
-        ) as ast ->
-        if is_in_list ["action"] tl then
-          let tl = replace_first "name" "alias" tl in
-          List (loc, Atom (loca, Dune_lang.Atom.of_string ("rule")) :: tl)
-        else ast
-    | ast -> ast
+        ( loc
+        , (Atom (_, A "lang") as lang)
+          :: (Atom (_, A "dune") as dune) :: Atom (loc3, A _) :: tll )
+      :: tl ->
+      List (loc, lang :: dune :: Atom (loc3, Dune_lang.Atom.of_string v) :: tll)
+      :: tl
+    | sexp -> sexp
 
   let included_file path = function
     | List
-            ( _
-            , [ Atom (_, A "include")
-              ; (Atom (loc, A fn) | Quoted_string (loc, fn))
-              ] ) ->
-          let dir = Path.Source.parent_exn path in
-          let included_file = Path.Source.relative dir fn in
-          if not (Path.exists (Path.source included_file)) then
-            User_error.raise ~loc
-              [ Pp.textf "File %s doesn't exist."
-                  (Path.Source.to_string_maybe_quoted included_file)
-              ];
-          Some included_file
+        ( _
+        , [ Atom (_, A "include")
+          ; (Atom (loc, A fn) | Quoted_string (loc, fn))
+          ] ) ->
+      let dir = Path.Source.parent_exn path in
+      let included_file = Path.Source.relative dir fn in
+      if not (Path.exists (Path.source included_file)) then
+        User_error.raise ~loc
+          [ Pp.textf "File %s doesn't exist."
+              (Path.Source.to_string_maybe_quoted included_file)
+          ];
+      Some included_file
     | _ -> None
 
   let included_files_paths path = List.filter_map ~f:(included_file path)
@@ -94,15 +81,14 @@ end
 let read_and_parse ?lexer path =
   let raw = Io.read_file (Path.source path) ~binary:false in
   let csts =
-        Dune_lang.Parser.parse_string raw
-          ~fname:(Path.Source.to_string path)
-          ?lexer ~mode:Cst
-        |> List.map
-          ~f:(Dune_lang.Cst.fetch_legacy_comments ~file_contents:raw)
-      in
+    Dune_lang.Parser.parse_string raw
+      ~fname:(Path.Source.to_string path)
+      ?lexer ~mode:Cst
+    |> List.map ~f:(Dune_lang.Cst.fetch_legacy_comments ~file_contents:raw)
+  in
   let comments = Dune_lang.Cst.extract_comments csts in
   let sexps = List.filter_map csts ~f:Dune_lang.Cst.abstract in
-  sexps, comments
+  (sexps, comments)
 
 (* Return a mapping [Path.t -> Dune_lang.Ast.t list] containing [path] and all
    the files in includes, recursiverly *)
--- src/dune/upgrader_v2.ml
+++ src/dune/upgrader_v2.ml
@@ -1,52 +1,69 @@
 open! Stdune
 open Upgrader_common
 
+
+let update_stanza =
+  let open Dune_lang.Ast in
+  function
+    | List (loc, Atom (loca, A "alias") :: tl) as ast ->
+      if Ast_ops.is_in_list [ "action" ] tl then
+        let tl = Ast_ops.replace_first "name" "alias" tl in
+        List (loc, Atom (loca, Dune_lang.Atom.of_string "rule") :: tl)
+      else
+        ast
+    | List (loc, Atom (loca, A "executable") :: tl) as stanza ->
+      (* If no mode is defined, explicitely use the previous default *)
+      if Ast_ops.is_in_list [ "modes" ] tl then
+        stanza
+      else
+        List
+          ( loc
+          , Atom (loca, Dune_lang.Atom.of_string "executable")
+            :: Dune_lang.Atom.(
+                 Ast_ops.field_of_list
+                   [ of_string "modes"; of_string "byte"; of_string "exe" ])
+            :: tl )
+    | stanza -> stanza
+
 let update_formatting sexps =
-  let elt, sexps = Ast_ops.extract_first ["using"; "fmt"] sexps in
+  let elt, sexps = Ast_ops.extract_first [ "using"; "fmt" ] sexps in
   match elt with
   (* Was not using fmt *)
-  | None -> sexps @ [
-    Ast_ops.field_of_list
-      Dune_lang.Atom.[of_string "formatting"; of_string "disabled"]
-  ]
+  | None ->
+    sexps
+    @ [ Ast_ops.field_of_list
+          Dune_lang.Atom.[ of_string "formatting"; of_string "disabled" ]
+      ]
   (* Was using fmt *)
-  | Some ([_;_;_]) -> sexps
+  | Some [ _; _; _ ] -> sexps
   (* Was using fmt enabled_for *)
-  | Some (_ :: _ :: _ :: tl) -> sexps @ [
-    Ast_ops.field_of_list
-      Dune_lang.Atom.[
-        of_string "formatting"]
-      ~more:tl
-  ]
+  | Some (_ :: _ :: _ :: tl) ->
+    sexps
+    @ [ Ast_ops.field_of_list Dune_lang.Atom.[ of_string "formatting" ] ~more:tl
+      ]
   (* Unexpected *)
   | _ -> sexps
 
 let update_project_file todo project =
-    let sexps, comments =
-      Upgrader_common.read_and_parse
-        (Dune_project.file project)
-    in
-    let v = !Dune_project.default_dune_language_version in
-    let sexps = sexps
-      |> Ast_ops.bump_lang_version v
-      |> update_formatting
-    in
-    let new_file_contents = string_of_sexps sexps comments in
-    (* Printf.eprintf "AAAH: %b\n!" (was_formatted sexps); *)
-    todo.to_edit <-
-      (Dune_project.file project, new_file_contents)::todo.to_edit
+  let sexps, comments =
+    Upgrader_common.read_and_parse (Dune_project.file project)
+  in
+  let v = !Dune_project.default_dune_language_version in
+  let sexps = sexps |> Ast_ops.bump_lang_version v |> update_formatting in
+  let new_file_contents = string_of_sexps sexps comments in
+  (* Printf.eprintf "AAAH: %b\n!" (was_formatted sexps); *)
+  todo.to_edit <- (Dune_project.file project, new_file_contents) :: todo.to_edit
 
 let upgrade_dune_file todo fn sexps comments =
-    let new_ast = sexps
-      |> List.map  ~f:Ast_ops.alias_to_rule
-    in
-    let new_file_contents = string_of_sexps new_ast comments in
-    todo.to_edit <- (fn, new_file_contents) :: todo.to_edit
-
+  let new_ast =
+    sexps
+    |> List.map ~f:update_stanza
+  in
+  let new_file_contents = string_of_sexps new_ast comments in
+  todo.to_edit <- (fn, new_file_contents) :: todo.to_edit
 
 let upgrade_dune_files todo dir =
-  if String.Set.mem (File_tree.Dir.files dir) File_tree.Dune_file.fname
-  then
+  if String.Set.mem (File_tree.Dir.files dir) File_tree.Dune_file.fname then
     let path = File_tree.Dir.path dir in
     let fn = Path.Source.relative path File_tree.Dune_file.fname in
     let files = Upgrader_common.scan_included_files fn in

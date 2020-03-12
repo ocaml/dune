260174eaa250453ea8561157c8014e7edffefbf8
Ulysse Gérard <thevoodoos@gmail.com>
2020-03-12 18:17:20 +0100

    Switch `menhir` to new system and fix the tests
    Signed-off-by: Ulysse Gérard <thevoodoos@gmail.com>

--- src/dune/menhir_stanza.ml
+++ src/dune/menhir_stanza.ml
@@ -1,3 +1,7 @@
 let syntax =
-  Dune_lang.Syntax.create ~name:"menhir" ~desc:"the menhir extension"
-    [ (1, 1); (2, 1) ]
+  Dune_lang.Syntax.createn ~name:"menhir" ~desc:"the menhir extension"
+    [ ((1, 0), `Since (1, 0))
+    ; ((1, 1), `Since (1, 4))
+    ; ((2, 0), `Since (1, 4))
+    ; ((2, 1), `Since (2, 2))
+    ]

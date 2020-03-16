055eae499835dbaaddb3a0ae2e56f01f12a2857f
Ulysse Gérard <thevoodoos@gmail.com>
2020-03-16 15:57:05 +0100

    Move action_plugin to new versioning
    Signed-off-by: Ulysse Gérard <thevoodoos@gmail.com>

--- src/dune/action_plugin.ml
+++ src/dune/action_plugin.ml
@@ -1,3 +1,3 @@
 let syntax =
-  Dune_lang.Syntax.create ~name:"action-plugin" ~desc:"action plugin extension"
-    [ (0, 1) ]
+  Dune_lang.Syntax.createn ~name:"action-plugin" ~desc:"action plugin extension"
+    [ ((0, 1), `Since (2, 0)) ]

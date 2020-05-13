ed38b7dea9d8fb1a375056e5c22b40f547408b38
Ulysse Gérard <thevoodoos@gmail.com>
2020-05-13 12:36:48 +0200

    More doc
    Signed-off-by: Ulysse Gérard <thevoodoos@gmail.com>

--- src/dune/merlin_server.mli
+++ src/dune/merlin_server.mli
@@ -1,5 +1,7 @@
 (** Merlin config server *)
 
 (** Once started the server will wait for commands on stdin, read the requested
-    merlin dot file and return its content on stdout. *)
+    merlin dot file and return its content on stdout. The server will only halt
+    when reiving EOF. Bad input will not stop the server but it will answer with
+    an error message *)
 val start : unit -> unit

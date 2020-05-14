c50836c67ecfd89c95a3a08a22dda6c5715a068d
Ulysse Gérard <thevoodoos@gmail.com>
2020-05-14 16:08:20 +0200

    Reformat
    Signed-off-by: Ulysse Gérard <thevoodoos@gmail.com>

--- src/dune/merlin_server.mli
+++ src/dune/merlin_server.mli
@@ -1,7 +1,6 @@
 (** Merlin config server *)
 
 (** Once started the server will wait for commands on stdin, read the requested
-    merlin dot file and return its content on stdout. The server will only halt
-    when reiving EOF. Bad input will not stop the server but it will answer with
-    an error message *)
+    merlin dot file and return its content on stdout. The server will halt when
+    reiceving EOF of a bad csexp. *)
 val start : unit -> unit

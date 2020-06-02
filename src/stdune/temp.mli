901ba7adfea0efdc57e681f4426e9a478cdb3b94
Jeremie Dimino <jeremie@dimino.org>
2020-06-02 16:43:11 +0100

    doc
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- src/stdune/temp.mli
+++ src/stdune/temp.mli
@@ -1,3 +1,9 @@
-val create : prefix: string -> suffix: string -> Path.t
+(** Temporary file management *)
+
+(** This module provides a high-level interface for temporary files. It ensures
+    that all temporary files created by the application are systematically
+    cleaned up on exit. *)
+
+val create : prefix:string -> suffix:string -> Path.t
 
 val destroy : Path.t -> unit

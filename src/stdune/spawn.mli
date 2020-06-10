97c46690a34c3dfb61cccd3b66d60a6f0b5390b1
Rudi Grinberg <me@rgrinberg.com>
2020-06-10 11:25:39 -0700

    Remove extra Env module in Process
    There's an Env in stdune
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- src/stdune/spawn.mli
+++ src/stdune/spawn.mli
@@ -1,11 +1,5 @@
 (** Subset of https://github.com/janestreet/spawn/blob/master/src/spawn.mli *)
 
-module Env : sig
-  type t
-
-  val of_array : string array -> t
-end
-
 val spawn :
      ?env:Env.t
   -> prog:string

a92d2a58c9f505326cac228fd9aed1e18beb8e35
Rudi Grinberg <me@rgrinberg.com>
2020-11-08 09:34:45 -0800

    [build] add Seq primitive
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- src/dune_engine/build.ml
+++ src/dune_engine/build.ml
@@ -7,6 +7,7 @@ type 'a t =
   | Pure : 'a -> 'a t
   | Map : ('a -> 'b) * 'a t -> 'b t
   | Both : 'a t * 'b t -> ('a * 'b) t
+  | Seq : unit t * 'b t -> 'b t
   | Map2 : ('a -> 'b -> 'c) * 'a t * 'b t -> 'c t
   | Paths_for_rule : Path.Set.t -> unit t
   | Paths_glob : File_selector.t -> Path.Set.t t
@@ -52,7 +53,7 @@ let map2 x y ~f = Map2 (f, x, y)
 let delayed f = Map (f, Pure ())
 
 module O = struct
-  let ( >>> ) a b = Map2 ((fun () y -> y), a, b)
+  let ( >>> ) a b = Seq (a, b)
 
   let ( and+ ) a b = Both (a, b)
 
@@ -288,6 +289,7 @@ end = struct
     | Pure _ -> Static_deps.empty
     | Map (_, t) -> static_deps t
     | Both (x, y) -> Static_deps.union (static_deps x) (static_deps y)
+    | Seq (x, y) -> Static_deps.union (static_deps x) (static_deps y)
     | Map2 (_, x, y) -> Static_deps.union (static_deps x) (static_deps y)
     | Deps deps -> { Static_deps.empty with action_deps = deps }
     | Paths_for_rule fns ->
@@ -326,6 +328,9 @@ let fold_labeled (type acc) t ~(init : acc) ~f =
     | Pure _ -> acc
     | Map (_, a) -> loop a acc
     | Both (a, b) ->
+      let acc = loop a acc in
+      loop b acc
+    | Seq (a, b) ->
       let acc = loop a acc in
       loop b acc
     | Map2 (_, a, b) ->
@@ -386,6 +391,10 @@ end = struct
         let a, dyn_deps_a = go a in
         let b, dyn_deps_b = go b in
         ((a, b), Dep.Set.union dyn_deps_a dyn_deps_b)
+      | Seq (a, b) ->
+        let (), dyn_deps_a = go a in
+        let b, dyn_deps_b = go b in
+        (b, Dep.Set.union dyn_deps_a dyn_deps_b)
       | Map2 (f, a, b) ->
         let a, dyn_deps_a = go a in
         let b, dyn_deps_b = go b in

b3050b59f4899eeb26bc66080257e0be96ababbd
Rudi Grinberg <me@rgrinberg.com>
2020-11-10 11:42:17 -0800

    Introduce `dune ocaml` command group
    This subgroup includes: utop, top, and merlin.
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

---
+++ bin/ocaml.ml
@@ -0,0 +1,11 @@
+open Import
+
+let info = Term.info "ocaml"
+
+let group =
+  ( Term.Group.Group
+      [ in_group Utop.command
+      ; in_group Ocaml_merlin.command
+      ; in_group Top.command
+      ]
+  , info )

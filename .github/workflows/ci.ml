a065539e44ff8f5d703dec13baf97efb2c5a8df1
François Bobot <francois.bobot@cea.fr>
2020-12-03 12:08:37 +0100

    [CI] ignore the constraints on local packages
         They are pinned or we use the released version
    
    Signed-off-by: François Bobot <francois.bobot@cea.fr>

--- .github/workflows/ci.ml
+++ .github/workflows/ci.ml
@@ -21,32 +21,53 @@ let run cmd args =
 
 let opam args = run "opam" args
 
-let pin () =
+let packages () =
+  let packages = Sys.readdir "." |> Array.to_list in
   let packages =
-    let packages = Sys.readdir "." |> Array.to_list in
-    let packages =
-      List.fold_left packages ~init:[] ~f:(fun acc fname ->
-          if Filename.check_suffix fname ".opam" then
-            Filename.chop_suffix fname ".opam" :: acc
-          else
-            acc)
-    in
+    List.fold_left packages ~init:[] ~f:(fun acc fname ->
+      if Filename.check_suffix fname ".opam" then
+        Filename.chop_suffix fname ".opam" :: acc
+      else
+        acc)
+  in
+  packages
+
+let ignore_constraint () =
+  "--ignore-constraints-on="^
+  (String.concat ~sep:"," (packages ()))
+
+let pin () =
+  let packages = packages () in
+  let packages =
     if skip_test then
       List.filter packages ~f:(fun pkg -> pkg = "dune")
     else
       packages
   in
   List.iter packages ~f:(fun package ->
-      opam [ "pin"; "add"; package ^ ".next"; "."; "--no-action" ])
+    opam [ "pin"; "add"; package ^ ".next"; "."; "--no-action" ])
+
+let opam_install_deps args =
+  opam ([ "install"; "--deps-only"; ignore_constraint () ] @
+        (if test_dependencies then [ "--with-test" ] else [])
+        @ args
+       )
+
+let install_deps_of_dune () =
+  opam_install_deps ["./dune.opam"]
+
+let install_deps_of_test () =
+  if Sys.win32 then (
+    opam [ "install"; "./dune-configurator.opam"; "--deps-only"; ignore_constraint () ];
+  ) else (
+    opam_install_deps ["."];
+    run "make" [ "dev-deps" ];
+  )
 
 let test () =
   if Sys.win32 then (
-    opam [ "install"; "./dune-configurator.opam"; "--deps-only" ];
     run "make" [ "test-windows" ]
   ) else (
-    opam ([ "install"; "."; "--deps-only" ] @
-          (if test_dependencies then [ "--with-test" ] else []));
-    run "make" [ "dev-deps" ];
     run "make" [ "test" ]
   )
 
@@ -54,6 +75,8 @@ let () =
   match Sys.argv with
   | [| _; "pin" |] -> pin ()
   | [| _; "test" |] -> test ()
+  | [| _; "install-deps-of-dune" |] -> install_deps_of_dune ()
+  | [| _; "install-deps-of-test" |] -> install_deps_of_test ()
   | _ ->
-    prerr_endline "Usage: ci.ml [pin | test]";
+    prerr_endline "Usage: ci.ml [pin | test | install-deps-of-dune | install-deps-of-test]";
     exit 1

670eb20dd34e1fd60c3b37597278c10e8573407e
Antonin Décimo <antonin@tarides.com>
2021-01-15 13:31:02 +0100

    Export C/C++ compiler found in Cxx_flags
    Signed-off-by: Antonin Décimo <antonin@tarides.com>

--- src/dune_rules/cxx_flags.mli
+++ src/dune_rules/cxx_flags.mli
@@ -7,10 +7,20 @@ type phase =
   | Compile
   | Link
 
+(** The detected compiler *)
+type ccomp_type =
+  | Gcc
+  | Msvc
+  | Clang
+  | Other of string
+
 (** The name of the file created in the .dune folder after calling the C
     preprocessor *)
 val preprocessed_filename : string
 
-(** [get_flags c_compiler] returns the necessary flags to turn this compiler
+(** [ccomp_type ctx] returns the C/C++ compiler type. *)
+val ccomp_type : Context.t -> ccomp_type Action_builder.t
+
+(** [get_flags for_:phase ctx] returns the necessary flags to turn this compiler
     into a c++ compiler for some of the most common compilers *)
 val get_flags : for_:phase -> Context.t -> string list Action_builder.t

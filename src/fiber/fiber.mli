5cfce2b88e904704968207113716fd2d5a74c3fc
Jeremie Dimino <jeremie@dimino.org>
2021-02-18 13:00:03 +0000

    Make Fiber.Stream implementation more defensive
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- src/fiber/fiber.mli
+++ src/fiber/fiber.mli
@@ -235,19 +235,42 @@ end
 with type 'a fiber := 'a t
 
 module Stream : sig
-  (** Destructive streams that can be composed to pipelines. *)
+  (** Destructive streams that can be composed to pipelines.
+
+      Streams can be finite or infinite. Streams have no storage and can only
+      have one writer and/or one reader at any given time. If you'd like to
+      access a stream concurrently, you need to protect it via a mutex.
+
+      Trying to access the same side of a stream concurrently will result in an
+      error. *)
 
   type 'a fiber
 
   module In : sig
+    (** Stream inputs.
+
+        A [In.t] value represents the side of a stream where values can be read
+        from. A stream input can only be consumed by one fiber at a time. Trying
+        to access a stream input concurrently will result in an exception.
+
+        When passing a stream input through one of the transformation functions
+        such as {!filter_map} or iteration function, the original stream input
+        can no longer be used for another purpose. *)
     type 'a t
 
+    (** Create a stream that is fed from a generator function. Every time a
+        value is requested, this function is called to produce a new value. If
+        the function raises, the stream will no longer be usable. *)
     val create : (unit -> 'a option fiber) -> 'a t
 
+    (** Create a stream that yields elements from the given list in order. *)
     val of_list : 'a list -> 'a t
 
+    (** The empty stream. *)
     val empty : unit -> 'a t
 
+    (** Consumes and returns the next element from the stream. Once the stream
+        is exhausted, [read] always returns [None]. *)
     val read : 'a t -> 'a option fiber
 
     val filter_map : 'a t -> f:('a -> 'b option) -> 'b t
@@ -258,8 +281,15 @@ module Stream : sig
   end
 
   module Out : sig
+    (** Stream outputs.
+
+        A [Out.t] value represents the side of a stream where values can be
+        pushed to. Only one value can be pushed at a time. Trying to push two
+        values concurrently will result in an error. *)
     type 'a t
 
+    (** Create a stream output. The callback is the consumer for values pushed
+        to the stream. *)
     val create : ('a option -> unit fiber) -> 'a t
 
     val write : 'a t -> 'a option -> unit fiber
@@ -272,9 +302,12 @@ module Stream : sig
   val connect : 'a In.t -> 'a Out.t -> unit fiber
 
   (** [supply i o] like [connect i o] but does not close [o] once [i] is
-      exhausted. Returned fiber terminates when [i] is exhausted*)
+      exhausted, allowing more values to be pused to [o]. Returned fiber
+      terminates when [i] is exhausted*)
   val supply : 'a In.t -> 'a Out.t -> unit fiber
 
+  (** [pipe ()] returns [(i, o)] where values pushed through [o] can be read
+      through [i]. *)
   val pipe : unit -> 'a In.t * 'a Out.t
 end
 with type 'a fiber := 'a t

000ea0de1f8e3bece838cf966187ef0ede97528b
Andrey Mokhov <amokhov@janestreet.com>
2021-03-16 20:40:03 +0000

    Update digest version
    Signed-off-by: Andrey Mokhov <amokhov@janestreet.com>

--- src/dune_engine/build_system.ml
+++ src/dune_engine/build_system.ml
@@ -1376,7 +1376,7 @@ end = struct
 
   (* The current version of the rule digest scheme. We should increment it when
      making any changes to the scheme, to avoid collisions. *)
-  let rule_digest_version = 2
+  let rule_digest_version = 3
 
   let compute_rule_digest (rule : Rule.t) ~deps ~action ~sandbox_mode =
     let env = Rule.effective_env rule in

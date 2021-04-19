fae4488c909ae8127ec79a08d5c74e46ffc593ce
Arseniy Alekseyev <aalekseyev@janestreet.com>
2021-04-19 13:10:35 +0100

    remove unused code
    Signed-off-by: Arseniy Alekseyev <aalekseyev@janestreet.com>

--- test/blackbox-tests/test-cases/dir-target-dep.t/target/cat_dir.ml
+++ test/blackbox-tests/test-cases/dir-target-dep.t/target/cat_dir.ml
@@ -1,11 +1,5 @@
 let dir = Sys.argv.(1)
 
-let write f =
-  let path = Filename.concat dir f in
-  let out = open_out path in
-  output_string out (f ^ " contents\n");
-  close_out_noerr out
-
 let input_all t =
   let buffer = Buffer.create 0 in
   let rec loop () =

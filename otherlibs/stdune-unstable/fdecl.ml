d14cc659ab5662ce0b6f27dd28fdde51c2bad5a2
Jérémie Dimino <jeremie@dimino.org>
2021-04-20 18:13:05 +0100

    Remove unused Fdecl.set_idempotent (#4503)
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- otherlibs/stdune-unstable/fdecl.ml
+++ otherlibs/stdune-unstable/fdecl.ml
@@ -16,14 +16,6 @@ let set t new_ =
     Code_error.raise "Fdecl.set: already set"
       [ ("old", t.to_dyn old); ("new_", t.to_dyn new_) ]
 
-let set_idempotent ~equal t new_ =
-  match t.state with
-  | Unset -> t.state <- Set new_
-  | Set old ->
-    if not (equal new_ old) then
-      Code_error.raise "Fdecl.set_idempotent: already set to a different value"
-        [ ("old", t.to_dyn old); ("new_", t.to_dyn new_) ]
-
 let get t =
   match t.state with
   | Unset -> Code_error.raise "Fdecl.get: not set" []

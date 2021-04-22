653462ad1213119597d2a9f157f70892765f23e3
Rudi Grinberg <me@rgrinberg.com>
2021-04-21 20:46:02 -0700

    Restore old Temp.temp_path behavior
    Now it does not create the file, but only makes sure the path is free.
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- otherlibs/stdune-unstable/temp.mli
+++ otherlibs/stdune-unstable/temp.mli
@@ -22,12 +22,14 @@ val clear_dir : Path.t -> unit
 
 (** [temp_path ~dir ~prefix ~suffix] generates a temporary path in [dir]. The
     base name of the temporary file is formed by concatenating [prefix], then a
-    suitably chosen integer number, then [suffix]. *)
+    suitably chosen integer number, then [suffix]. Note that the path isn't
+    actually created. *)
 val temp_path : dir:Path.t -> prefix:string -> suffix:string -> Path.t
 
-(** Like [temp_path], but passes the temporary file to the callback [f], and
-    makes sure the temporary file is deleted when [f] completes. If [f] raises
-    an exception, the exception is reraised (and the file is still deleted). *)
+(** Like [temp_path], but creates and passes the temporary file to the callback
+    [f], and makes sure the temporary file is deleted when [f] completes. If [f]
+    raises an exception, the exception is reraised (and the file is still
+    deleted). *)
 val with_temp_path :
      dir:Path.t
   -> prefix:string

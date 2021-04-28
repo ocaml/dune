8ed8304352de523a433aa7532cd8b2d6bf6ee49c
Andrey Mokhov <amokhov@janestreet.com>
2021-04-28 12:22:23 +0100

    Make file system events more structured and introduce directory tracking (#4536)
    This PR brings us one step closer to tracking directory contents.
    Specifically, it adds:
    
    * [Fs_memo.dir_contents] to track the directory contents.
    
    * [Fs_memo.Event] that lists all events we expect to be provided by
      the file watching backend.
    
    * Some documentation about our assumptions about various events, as
      well as ideas for future optimisations.
    
    * We also make it explicit in the types that the list of file system
      events is expected to be non-empty in [scheduler.ml].
    
    Signed-off-by: Andrey Mokhov <amokhov@janestreet.com>

---
+++ otherlibs/stdune-unstable/nonempty_list.mli
@@ -0,0 +1,7 @@
+(** A (to be expanded) collection of functions for working with non-empty lists. *)
+
+type 'a t = ( :: ) of 'a * 'a list
+
+val of_list : 'a list -> 'a t option
+
+val to_list : 'a t -> 'a list

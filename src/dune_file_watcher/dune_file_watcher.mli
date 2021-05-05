c64227433a1abb3982a6296c3a6d738799292b21
Arseniy Alekseyev <aalekseyev@janestreet.com>
2021-05-05 18:05:28 +0100

    fix the noise where dne complains about watcher EOF on shutdown
    Signed-off-by: Arseniy Alekseyev <aalekseyev@janestreet.com>

--- src/dune_file_watcher/dune_file_watcher.mli
+++ src/dune_file_watcher/dune_file_watcher.mli
@@ -2,16 +2,22 @@ open! Stdune
 
 type t
 
+module Event : sig
+  type t =
+    | File_changed of Path.t
+    | Watcher_terminated
+end
+
 (** Create a new file watcher. [debounce_interval] is measured in seconds and it
     controls the minimum time between calls to [thread_safe_send_files_changed]. *)
 val create :
      root:Path.t
   -> debounce_interval:float option
-  -> thread_safe_send_files_changed:(Path.t list -> unit)
+  -> thread_safe_send_events:(Event.t list -> unit)
   -> t
 
 (** Create a new file watcher with default settings. *)
-val create_default : thread_safe_send_files_changed:(Path.t list -> unit) -> t
+val create_default : thread_safe_send_events:(Event.t list -> unit) -> t
 
 (** Pid of the external file watcher process *)
 val pid : t -> Pid.t

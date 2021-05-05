ea2b2967089e70a753d8c1d42d7e2c7ceed3bb23
Rudi Grinberg <me@rgrinberg.com>
2021-05-04 23:53:27 -0700

    Add Dune_engine.Process unit test
    Test demonstrates that Process.Io.null cannot be used as output.
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

---
+++ test/expect-tests/process_tests.ml
@@ -0,0 +1,34 @@
+open Stdune
+open! Dune_tests_common
+open Dune_engine
+
+let go =
+  let config =
+    { Scheduler.Config.concurrency = 1
+    ; display = Short
+    ; rpc = None
+    ; stats = None
+    }
+  in
+  Scheduler.Run.go config ~file_watcher:No_watcher ~on_event:(fun _ _ -> ())
+
+let true_ = Bin.which "true" ~path:(Env.path Env.initial) |> Option.value_exn
+
+let%expect_test "null input" =
+  let stdin_from = Process.(Io.null In) in
+  let run () = Process.run ~stdin_from Strict true_ [] in
+  let _res = go run in
+  [%expect {||}]
+
+let%expect_test "null output" =
+  let stdout_to = Process.(Io.null Out) in
+  let stderr_to = Process.(Io.null Out) in
+  let run () = Process.run ~stdout_to ~stderr_to Strict true_ [] in
+  let _res = go run in
+  [%expect.unreachable]
+  [@@expect.uncaught_exn
+    {|
+  (Dune_util.Report_error.Already_reported)
+  Trailing output
+  ---------------
+  Error: close: : Bad file descriptor |}]

d28a15b897c32b1468986aef774d574a70d0ec78
Rudi Grinberg <me@rgrinberg.com>
2021-05-18 14:50:01 -0700

    fix typo
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- nix/opam2nix.nix
+++ nix/opam2nix.nix
@@ -1,4 +1,4 @@
 import (builtins.fetchGit {
-  url = "https://github.com/timberton/opam2nix.git";
+  url = "https://github.com/timbertson/opam2nix.git";
   rev = "90f8d627501f39e16e20c22161ec7d0aec10af32";
 }) { }

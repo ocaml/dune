5d7263a195b0f6e05de7146e1facccc299cd2742
François Bobot <francois.bobot@cea.fr>
2021-05-21 21:18:56 +0200

    [Sites] delay the computation of content of `generate_sites_module`
            Fixes #4415
    
    Signed-off-by: François Bobot <francois.bobot@cea.fr>

--- src/dune_rules/generate_sites_module_rules.ml
+++ src/dune_rules/generate_sites_module_rules.ml
@@ -57,27 +57,32 @@ let plugins_code sctx buf pkg sites =
         (String.capitalize site) plugins plugins site)
 
 let setup_rules sctx ~dir (def : Dune_file.Generate_sites_module.t) =
-  let buf = Buffer.create 1024 in
-  if def.sourceroot then sourceroot_code buf;
-  if def.relocatable then relocatable_code buf;
-  let sites =
-    List.sort_uniq
-      ~compare:(fun (_, pkga) (_, pkgb) -> Package.Name.compare pkga pkgb)
-      (def.sites @ List.map ~f:(fun (loc, (pkg, _)) -> (loc, pkg)) def.plugins)
+  let impl () =
+    let buf = Buffer.create 1024 in
+    if def.sourceroot then sourceroot_code buf;
+    if def.relocatable then relocatable_code buf;
+    let sites =
+      List.sort_uniq
+        ~compare:(fun (_, pkga) (_, pkgb) -> Package.Name.compare pkga pkgb)
+        (def.sites @ List.map ~f:(fun (loc, (pkg, _)) -> (loc, pkg)) def.plugins)
+    in
+    if List.is_non_empty sites then (
+      pr buf "module Sites = struct";
+      List.iter sites ~f:(sites_code sctx buf);
+      pr buf "end"
+    );
+    let plugins =
+      Package.Name.Map.of_list_multi (List.map ~f:snd def.plugins)
+    in
+    if not (Package.Name.Map.is_empty plugins) then (
+      pr buf "module Plugins = struct";
+      Package.Name.Map.iteri plugins ~f:(plugins_code sctx buf);
+      pr buf "end"
+    );
+    Buffer.contents buf
   in
-  if List.is_non_empty sites then (
-    pr buf "module Sites = struct";
-    List.iter sites ~f:(sites_code sctx buf);
-    pr buf "end"
-  );
-  let plugins = Package.Name.Map.of_list_multi (List.map ~f:snd def.plugins) in
-  if not (Package.Name.Map.is_empty plugins) then (
-    pr buf "module Plugins = struct";
-    Package.Name.Map.iteri plugins ~f:(plugins_code sctx buf);
-    pr buf "end"
-  );
-  let impl = Buffer.contents buf in
   let module_ = Module_name.to_string def.module_ ^ ".ml" in
   let file = Path.Build.relative dir module_ in
-  Super_context.add_rule sctx ~dir (Build.write_file file impl);
+  Super_context.add_rule sctx ~dir
+    (Build.write_file_dyn file (Build.delayed impl));
   module_

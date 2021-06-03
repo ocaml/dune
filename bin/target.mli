75d2625408bc03a353cc5d10a66c9956a1335999
Jeremie Dimino <jeremie@dimino.org>
2021-06-03 12:28:24 +0100

    Remove Target.resolve_target
    And adapt the code in utop.ml
    
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- bin/target.mli
+++ bin/target.mli
@@ -6,12 +6,6 @@ type t =
 
 val request : t list -> unit Dune_engine.Action_builder.t
 
-val resolve_target :
-     Workspace_root.t
-  -> setup:Dune_rules.Main.build_system
-  -> Arg.Dep.t
-  -> (t list, Arg.Dep.t * User_message.Style.t Pp.t list) result Memo.Build.t
-
 type resolve_input =
   | Path of Path.t
   | Dep of Arg.Dep.t

d3623abd91361a2531b9414ce020c7fba65d1fe4
Jeremie Dimino <jeremie@dimino.org>
2021-06-03 13:14:01 +0100

    Remove Target.resolve_targets_mixed
    and adapt code in exec.ml
    
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- bin/target.mli
+++ bin/target.mli
@@ -6,18 +6,6 @@ type t =
 
 val request : t list -> unit Dune_engine.Action_builder.t
 
-type resolve_input =
-  | Path of Path.t
-  | Dep of Arg.Dep.t
-
-val resolve_targets_mixed :
-     Workspace_root.t
-  -> Dune_config.t
-  -> Dune_rules.Main.build_system
-  -> resolve_input list
-  -> (t list, Arg.Dep.t * User_message.Style.t Pp.t list) result list
-     Memo.Build.t
-
 val resolve_targets_exn :
      Workspace_root.t
   -> Dune_config.t

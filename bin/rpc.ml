aaef2930f11aa0006b1dad08275263ea4f33b539
Cameron Wong <68918929+cwong-ocaml@users.noreply.github.com>
2021-06-07 08:45:48 -0400

    Add [dune rpc ping] (#4701)
    * add ping command
    
    Signed-off-by: Cameron Wong <cwong@janestreet.com>
    
    * move ping command to rpc
    
    Signed-off-by: Cameron Wong <cwong@janestreet.com>

--- bin/rpc.ml
+++ bin/rpc.ml
@@ -131,6 +131,49 @@ module Status = struct
   let term = (Term.Group.Term term, info)
 end
 
+module Ping = struct
+  (* cwong: Should we put this into [dune-rpc]? *)
+  let interpret_kind = function
+    | Dune_rpc_private.Response.Error.Invalid_request -> "Invalid_request"
+    | Code_error -> "Code_error"
+    | Version_error -> "Version_error"
+
+  let send_ping cli =
+    let open Fiber.O in
+    let+ response =
+      Dune_rpc_impl.Client.request cli Dune_rpc_private.Public.Request.ping ()
+    in
+    match response with
+    | Ok () ->
+      User_message.print
+        (User_message.make
+           [ Pp.text "Server appears to be responding normally" ])
+    | Error (e : Dune_rpc_private.Response.Error.t) ->
+      User_error.raise
+        [ Pp.text "Server returned error: "
+        ; Pp.textf "%s (error kind: %s)" e.message (interpret_kind e.kind)
+        ]
+
+  let on_notification _ = Fiber.return ()
+
+  let exec common =
+    let where = wait_for_server common in
+    Dune_rpc_impl.Run.client where
+      (Dune_rpc_private.Initialize.Request.create
+         ~id:(Dune_rpc_private.Id.make (Sexp.Atom "ping_cmd")))
+      ~on_notification ~f:send_ping
+
+  let info =
+    let doc = "Ping the build server running in the current directory" in
+    Term.info "ping" ~doc
+
+  let term =
+    let+ (common : Common.t) = Common.term in
+    client_term common exec
+
+  let term = (Term.Group.Term term, info)
+end
+
 let info =
   let doc = "Dune's RPC mechanism. Experimental." in
   let man =
@@ -141,4 +184,4 @@ let info =
   in
   Term.info "rpc" ~doc ~man
 
-let group = (Term.Group.Group [ Init.term; Status.term ], info)
+let group = (Term.Group.Group [ Init.term; Status.term; Ping.term ], info)

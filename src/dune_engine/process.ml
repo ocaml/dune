abfa3fe806b18fc6d2c94765091ed844a9b5ea6d
Rudi Grinberg <me@rgrinberg.com>
2021-06-21 13:13:53 -0700

    Introduce `output` type for process output
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- src/dune_engine/process.ml
+++ src/dune_engine/process.ml
@@ -394,16 +394,30 @@ end = struct
 
   type t = (int, error) result
 
+  type output =
+    { with_color : User_message.Style.t Pp.t
+    ; without_color : string
+    }
+
   let parse_output = function
     | "" -> None
     | s ->
       Some
-        (Pp.map_tags (Ansi_color.parse s) ~f:(fun styles ->
-             User_message.Style.Ansi_styles styles))
+        (let with_color =
+           Pp.map_tags (Ansi_color.parse s) ~f:(fun styles ->
+               User_message.Style.Ansi_styles styles)
+         in
+         let without_color = Ansi_color.strip s in
+         { with_color; without_color })
 
   (* In this module, we don't need the "Error: " prefix given that it is already
      included in the error message from the command. *)
-  let fail ~purpose ~dir ~has_embedded_location paragraphs =
+  let fail ~output ~purpose ~dir ~has_embedded_location paragraphs =
+    let paragraphs : User_message.Style.t Pp.t list =
+      match output with
+      | None -> paragraphs
+      | Some output -> paragraphs @ [ output.with_color ]
+    in
     let dir =
       match dir with
       | None -> Path.of_string (Sys.getcwd ())
@@ -421,27 +435,16 @@ end = struct
 
   (* Check if the command output starts with a location, ignoring ansi escape
      sequences *)
-  let outputs_starts_with_location =
-    let rec loop s pos len prefix =
-      match prefix with
-      | [] -> true
-      | c :: rest -> (
-        pos < len
-        &&
-        match s.[pos] with
-        | '\027' -> (
-          match String.index_from s pos 'm' with
-          | None -> false
-          | Some pos -> loop s (pos + 1) len prefix)
-        | c' -> c = c' && loop s (pos + 1) len rest)
-    in
-    fun output ->
-      loop output 0 (String.length output) [ 'F'; 'i'; 'l'; 'e'; ' ' ]
+  let outputs_starts_with_location s = String.is_prefix s ~prefix:"File "
 
   let handle_verbose t ~id ~purpose ~output ~command_line ~dir =
     let open Pp.O in
-    let has_embedded_location = outputs_starts_with_location output in
     let output = parse_output output in
+    let has_embedded_location =
+      match output with
+      | None -> false
+      | Some output -> outputs_starts_with_location output.without_color
+    in
     match t with
     | Ok n ->
       Option.iter output ~f:(fun output ->
@@ -449,7 +452,7 @@ end = struct
             (User_message.make
                [ Pp.tag User_message.Style.Kwd (Pp.verbatim "Output")
                  ++ pp_id id ++ Pp.char ':'
-               ; output
+               ; output.with_color
                ]));
       n
     | Error err ->
@@ -458,13 +461,12 @@ end = struct
         | Failed n -> sprintf "exited with code %d" n
         | Signaled signame -> sprintf "got signal %s" signame
       in
-      fail ~purpose ~dir ~has_embedded_location
-        (Pp.tag User_message.Style.Kwd (Pp.verbatim "Command")
-         ++ Pp.space ++ pp_id id ++ Pp.space ++ Pp.text msg ++ Pp.char ':'
-         ::
-         Pp.tag User_message.Style.Prompt (Pp.char '$')
-         ++ Pp.char ' ' ++ command_line
-         :: Option.to_list output)
+      fail ~output ~purpose ~dir ~has_embedded_location
+        [ Pp.tag User_message.Style.Kwd (Pp.verbatim "Command")
+          ++ Pp.space ++ pp_id id ++ Pp.space ++ Pp.text msg ++ Pp.char ':'
+        ; Pp.tag User_message.Style.Prompt (Pp.char '$')
+          ++ Pp.char ' ' ++ command_line
+        ]
 
   let handle_non_verbose t ~verbosity ~purpose ~output ~prog ~command_line ~dir
       ~has_unexpected_stdout ~has_unexpected_stderr =
@@ -489,27 +491,33 @@ end = struct
     in
     match t with
     | Ok n ->
-      if
-        Option.is_some output
-        || (match verbosity with
-           | Scheduler.Config.Display.Short -> true
-           | Quiet -> false
-           | Verbose -> assert false)
-           &&
-           match purpose with
-           | Internal_job _ -> false
-           | Build_job _ -> true
+      (if
+       Option.is_some output
+       || (match verbosity with
+          | Scheduler.Config.Display.Short -> true
+          | Quiet -> false
+          | Verbose -> assert false)
+          &&
+          match purpose with
+          | Internal_job _ -> false
+          | Build_job _ -> true
       then
+        let output =
+          match output with
+          | None -> []
+          | Some output -> [ output.with_color ]
+        in
         Console.print_user_message
           (User_message.make
              (if show_command then
-               progname_and_purpose Ok :: Option.to_list output
+               progname_and_purpose Ok :: output
              else
-               Option.to_list output));
+               output)));
       n
     | Error err ->
       let msg =
         match err with
+        | Signaled signame -> sprintf "(got signal %s)" signame
         | Failed n ->
           if show_command then
             let unexpected_outputs =
@@ -524,15 +532,13 @@ end = struct
                 (String.enumerate_and unexpected_outputs)
             | _ -> sprintf "(exit %d)" n
           else
-            fail ~purpose ~dir ~has_embedded_location (Option.to_list output)
-        | Signaled signame -> sprintf "(got signal %s)" signame
+            fail ~output ~purpose ~dir ~has_embedded_location []
       in
-      fail ~purpose ~dir ~has_embedded_location
-        (progname_and_purpose Error ++ Pp.char ' '
-         ++ Pp.tag User_message.Style.Error (Pp.verbatim msg)
-         ::
-         Pp.tag User_message.Style.Details (Pp.verbatim command_line)
-         :: Option.to_list output)
+      fail ~output ~purpose ~dir ~has_embedded_location
+        [ progname_and_purpose Error ++ Pp.char ' '
+          ++ Pp.tag User_message.Style.Error (Pp.verbatim msg)
+        ; Pp.tag User_message.Style.Details (Pp.verbatim command_line)
+        ]
 end
 
 let report_process_start stats ~id ~prog ~args ~now =

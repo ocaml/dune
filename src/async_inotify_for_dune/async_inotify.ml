2b555af93af8210ebc26dc97925252076ad26b43
Arseniy Alekseyev <aalekseyev@janestreet.com>
2021-08-24 13:52:53 +0100

    fmt
    Signed-off-by: Arseniy Alekseyev <aalekseyev@janestreet.com>

--- src/async_inotify_for_dune/async_inotify.ml
+++ src/async_inotify_for_dune/async_inotify.ml
@@ -162,14 +162,16 @@ let pump_events t ~spawn_thread =
   let fd = t.fd in
   let () =
     spawn_thread (fun () ->
-      while true do
-        match UnixLabels.select ~read:[ fd ] ~write:[] ~except:[] ~timeout:(-1.) with
-        | _, _, _ ->
-          let events = Inotify.read fd in
-          t.send_emit_events_job_to_scheduler (fun () ->
-            process_raw_events t events)
-        | exception (Unix.Unix_error (EINTR, _, _)) -> ()
-      done)
+        while true do
+          match
+            UnixLabels.select ~read:[ fd ] ~write:[] ~except:[] ~timeout:(-1.)
+          with
+          | _, _, _ ->
+            let events = Inotify.read fd in
+            t.send_emit_events_job_to_scheduler (fun () ->
+                process_raw_events t events)
+          | exception Unix.Unix_error (EINTR, _, _) -> ()
+        done)
   in
   ()
 

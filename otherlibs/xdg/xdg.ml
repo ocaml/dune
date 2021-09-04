1a8e2951e16706264014dd64927c8b474cecc749
Rudi Grinberg <me@rgrinberg.com>
2021-09-03 22:17:38 -0700

    Remove unix from xdg
    It was only used to find the home dir in case $HOME wasn't set. But that
    isn't part of the spec anyway.
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- otherlibs/xdg/xdg.ml
+++ otherlibs/xdg/xdg.ml
@@ -1,14 +1,11 @@
 let home =
   try Sys.getenv "HOME" with
-  | Not_found -> (
-    try (Unix.getpwuid (Unix.getuid ())).Unix.pw_dir with
-    | Unix.Unix_error _
-    | Not_found ->
-      if Sys.win32 then
-        try Sys.getenv "AppData" with
-        | Not_found -> ""
-      else
-        "")
+  | Not_found ->
+    if Sys.win32 then
+      try Sys.getenv "AppData" with
+      | Not_found -> ""
+    else
+      ""
 
 let ( / ) = Filename.concat
 

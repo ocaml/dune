022389a89321fd31f43ef6e9f32486d1bcd423e9
François Bobot <francois.bobot@cea.fr>
2021-09-29 18:14:08 +0200

    [Sites] Add failing tests with library result as dependency
    Signed-off-by: François Bobot <francois.bobot@cea.fr>

--- otherlibs/site/test/plugin_require_thread.t/plugin/plugin1_impl.ml
+++ otherlibs/site/test/plugin_require_thread.t/plugin/plugin1_impl.ml
@@ -1,4 +1,5 @@
 let () =
   let open Thread in
+  let open Result in
   print_endline "Registration of Plugin1";
   Queue.add (fun () -> print_endline "Plugin1 is doing something...") Registration.todo

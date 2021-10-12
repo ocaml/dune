6cb8f439e4723c9ff9bef870d8542533291d6a17
Jeremie Dimino <jeremie@dimino.org>
2021-10-12 13:28:27 +0100

    Simplify stray process test
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- test/blackbox-tests/test-cases/actions/bin/sub_process.ml
+++ test/blackbox-tests/test-cases/actions/bin/sub_process.ml
@@ -1,27 +1,10 @@
-let fn = Sys.getenv "BEACON_FILE"
-
-let handle_sigterm who ~cleanup =
-  Sys.set_signal Sys.sigterm
-    (Signal_handle
-       (fun _ ->
-         close_out (open_out (fn ^ "." ^ who));
-         cleanup ()))
-
-let sub () =
-  handle_sigterm "child" ~cleanup:ignore;
-  let oc = open_out fn in
-  Printf.fprintf oc "%d" (Unix.getpid ());
-  close_out oc;
-  (* Keep re-creating the beacon file so that we can observe that the process is
-     still alive from the cram test: *)
-  while true do
-    Unix.sleepf 0.1;
-    close_out (open_out fn)
-  done
-
 let () =
-  match Unix.fork () with
-  | 0 -> sub ()
-  | pid ->
-    handle_sigterm "parent" ~cleanup:(fun () -> Unix.kill pid Sys.sigterm);
-    ignore (Unix.waitpid [] pid : _ * _)
+  match Sys.argv with
+  | [| _ |] ->
+    exit (Sys.command (Filename.quote_command Sys.executable_name [ "sub" ]))
+  | [| _; "sub" |] ->
+    let oc = open_out (Sys.getenv "BEACON_FILE") in
+    Printf.fprintf oc "%d" (Unix.getpid ());
+    close_out oc;
+    Unix.sleep max_int
+  | _ -> assert false

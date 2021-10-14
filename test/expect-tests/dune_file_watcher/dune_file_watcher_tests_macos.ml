61709fdf8cc8682a5ef185db5eb5aefe49de93c1
Rudi Grinberg <me@rgrinberg.com>
2021-10-14 10:12:19 -0600

    tests(watcher): enable running macos tests
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- test/expect-tests/dune_file_watcher/dune_file_watcher_tests_macos.ml
+++ test/expect-tests/dune_file_watcher/dune_file_watcher_tests_macos.ml
@@ -39,7 +39,10 @@ let%expect_test _ =
   Dune_file_watcher.wait_for_initial_watches_established_blocking watcher;
   Stdio.Out_channel.write_all "x" ~data:"x";
   print_events 1;
-  [%expect {| Timed out waiting for more events: expected 1, saw 0 |}];
+  [%expect {|
+    { path = In_source_tree "."; kind = "Created" }
+    { path = In_source_tree "x"; kind = "Unknown" }
+    Got more events than expected: expected 1, saw 2 |}];
   Unix.rename "x" "y";
   print_events 0;
   [%expect {|
@@ -49,10 +52,17 @@ let%expect_test _ =
   Stdio.Out_channel.write_all "d/w/x" ~data:"x";
   Dune_file_watcher.For_tests.resume watcher;
   print_events 0;
-  [%expect {|
-|}];
+  [%expect.unreachable];
   Stdio.Out_channel.write_all "d/w/y" ~data:"y";
   print_events 1;
-  [%expect {|
-  Timed out waiting for more events: expected 1, saw 0
-|}]
+  [%expect.unreachable]
+[@@expect.uncaught_exn {|
+  (* CR expect_test_collector: This test expectation appears to contain a backtrace.
+     This is strongly discouraged as backtraces are fragile.
+     Please change this test to not include a backtrace. *)
+
+  (Failure "pid unavailable")
+  Raised at Stdlib.failwith in file "stdlib.ml", line 29, characters 17-33
+  Called from Dune_file_watcher.For_tests.suspend in file "src/dune_file_watcher/dune_file_watcher.ml", line 518, characters 40-47
+  Called from Dune_file_watcher_tests_macos.(fun) in file "test/expect-tests/dune_file_watcher/dune_file_watcher_tests_macos.ml", line 47, characters 2-45
+  Called from Expect_test_collector.Make.Instance.exec in file "collector/expect_test_collector.ml", line 244, characters 12-19 |}]

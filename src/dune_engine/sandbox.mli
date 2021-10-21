154f1a69f260a34dbac14a2f7c9ecfb3aff99cee
Jeremie Dimino <jeremie@dimino.org>
2021-10-21 16:29:12 +0100

    _
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- src/dune_engine/sandbox.mli
+++ src/dune_engine/sandbox.mli
@@ -9,6 +9,7 @@ val dir : t -> Path.Build.t
 (** [map_path t p] returns the path corresponding to [p] inside the sandbox. *)
 val map_path : t -> Path.Build.t -> Path.Build.t
 
+(** Create a new sandbox and copy or link dependencies inside it. *)
 val create :
      mode:Sandbox_mode.some
   -> deps:Dep.Facts.t
@@ -18,6 +19,8 @@ val create :
   -> expand_aliases:bool
   -> t
 
+(** Move the targets created by the action from the sandbox to the build
+    directory. *)
 val move_targets_to_build_dir : t -> targets:Path.Build.Set.t -> unit
 
 val destroy : t -> unit

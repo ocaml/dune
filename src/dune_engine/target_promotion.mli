1a9a0c19502bd14ec86c52934bf6d56dbe1fbd47
Andrey Mokhov <amokhov@janestreet.com>
2021-10-29 13:46:35 +0100

    Add the missing module
    Signed-off-by: Andrey Mokhov <amokhov@janestreet.com>

---
+++ src/dune_engine/target_promotion.mli
@@ -0,0 +1,22 @@
+(** Promoting rule targets to the source tree. *)
+
+open! Stdune
+
+val promote :
+     dir:Path.Build.t
+  -> targets:Targets.t
+  -> promote:Rule.Promote.t
+  -> promote_source:
+       (   chmod:(int -> int)
+        -> src:Path.Build.t
+        -> dst:Path.Source.t
+        -> unit Fiber.t)
+  -> unit Fiber.t
+
+(** The set of files created in the source tree that need to be deleted. *)
+val files_in_source_tree_to_delete : unit -> Path.Set.t
+
+val delete_stale_dot_merlin_file :
+     dir:Path.Build.t
+  -> source_files_to_ignore:Path.Source.Set.t
+  -> Path.Source.Set.t

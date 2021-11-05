780030264d12e0e649cfd420dee500d316ee9870
Rudi Grinberg <me@rgrinberg.com>
2021-11-05 15:25:15 -0600

    test: add mli's to watcher tests
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

---
+++ test/expect-tests/dune_file_watcher/dune_file_watcher_tests_lib.mli
@@ -0,0 +1,8 @@
+val critical_section : Mutex.t -> f:(unit -> 'a) -> 'a
+
+val init : unit -> unit
+
+val print_events :
+     try_to_get_events:(unit -> Dune_file_watcher.Fs_memo_event.t list option)
+  -> expected:int
+  -> unit

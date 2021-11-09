061a1c1057c440359ad3f9edf06700a8a82e860e
Rudi Grinberg <me@rgrinberg.com>
2021-11-09 12:10:22 -0600

    fix: do not rely on mutation in top closure
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    ps-id: 469120FC-901E-4E5E-9538-E39D59C4528B

--- otherlibs/stdune-unstable/top_closure.ml
+++ otherlibs/stdune-unstable/top_closure.ml
@@ -2,32 +2,30 @@ module Make (Keys : Top_closure_intf.Keys) (Monad : Monad_intf.S) = struct
   open Monad.O
 
   let top_closure ~key ~deps elements =
-    let visited = ref Keys.empty in
-    let res = ref [] in
-    let rec loop elt ~temporarily_marked =
+    let rec loop res visited elt ~temporarily_marked =
       let key = key elt in
       if Keys.mem temporarily_marked key then
         Monad.return (Error [ elt ])
-      else if not (Keys.mem !visited key) then (
-        visited := Keys.add !visited key;
+      else if not (Keys.mem visited key) then
+        let visited = Keys.add visited key in
         let temporarily_marked = Keys.add temporarily_marked key in
-        deps elt >>= iter_elts ~temporarily_marked >>= function
-        | Ok () ->
-          res := elt :: !res;
-          Monad.return (Ok ())
+        deps elt >>= iter_elts res visited ~temporarily_marked >>= function
         | Error l -> Monad.return (Error (elt :: l))
-      ) else
-        Monad.return (Ok ())
-    and iter_elts elts ~temporarily_marked =
+        | Ok (res, visited) ->
+          let res = elt :: res in
+          Monad.return (Ok (res, visited))
+      else
+        Monad.return (Ok (res, visited))
+    and iter_elts res visited elts ~temporarily_marked =
       Monad.return elts >>= function
-      | [] -> Monad.return (Ok ())
+      | [] -> Monad.return (Ok (res, visited))
       | elt :: elts -> (
-        loop elt ~temporarily_marked >>= function
+        loop res visited elt ~temporarily_marked >>= function
         | Error _ as result -> Monad.return result
-        | Ok () -> iter_elts elts ~temporarily_marked)
+        | Ok (res, visited) -> iter_elts res visited elts ~temporarily_marked)
     in
-    iter_elts elements ~temporarily_marked:Keys.empty >>= function
-    | Ok () -> Monad.return (Ok (List.rev !res))
+    iter_elts [] Keys.empty elements ~temporarily_marked:Keys.empty >>= function
+    | Ok (res, _visited) -> Monad.return (Ok (List.rev res))
     | Error elts -> Monad.return (Error elts)
 end
 [@@inlined always]

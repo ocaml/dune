2b3821a7d119f451bd7b5992ca710d640fd0761a
Jeremie Dimino <jeremie@dimino.org>
2021-11-11 01:06:11 +0000

    _
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- test/blackbox-tests/test-cases/actions/bin/sub_process.ml
+++ test/blackbox-tests/test-cases/actions/bin/sub_process.ml
@@ -3,8 +3,11 @@ let () =
   | [| _ |] ->
     exit (Sys.command (Filename.quote_command Sys.executable_name [ "sub" ]))
   | [| _; "sub" |] ->
-    let oc = open_out (Sys.getenv "BEACON_FILE") in
+    let beacon_file = Sys.getenv "BEACON_FILE" in
+    Printf.printf "Creating %s...\n%!" beacon_file;
+    let oc = open_out beacon_file in
     Printf.fprintf oc "%d" (Unix.getpid ());
     close_out oc;
+    Printf.printf "Done.\n%!";
     Unix.sleep max_int
   | _ -> assert false

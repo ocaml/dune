1635884b5a811b697f07866a5294220ced874198
Jeremie Dimino <jeremie@dimino.org>
2021-11-11 09:13:45 +0000

    _
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- test/blackbox-tests/test-cases/actions/bin/sub_process.ml
+++ test/blackbox-tests/test-cases/actions/bin/sub_process.ml
@@ -1,13 +1,16 @@
 let () =
   match Sys.argv with
-  | [| _ |] ->
-    exit (Sys.command (Filename.quote_command Sys.executable_name [ "sub" ]))
-  | [| _; "sub" |] ->
+  | [| _; log |] ->
+    exit
+      (Sys.command (Filename.quote_command Sys.executable_name [ "sub"; log ]))
+  | [| _; "sub"; log |] ->
     let beacon_file = Sys.getenv "BEACON_FILE" in
-    Printf.printf "Creating %s...\n%!" beacon_file;
+    let log = open_out log in
+    Printf.fprintf log "Creating %s...\n%!" beacon_file;
     let oc = open_out beacon_file in
     Printf.fprintf oc "%d" (Unix.getpid ());
     close_out oc;
-    Printf.printf "Done.\n%!";
+    Printf.fprintf log "Done.\n%!";
+    close_out log;
     Unix.sleep max_int
   | _ -> assert false

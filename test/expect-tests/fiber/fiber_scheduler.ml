b5478d0e2a69ce70b65b78a3d7eabb987510d2ea
Rudi Grinberg <me@rgrinberg.com>
2021-12-01 15:02:21 -0600

    test(fiber): simple test for Fiber.Scheduler
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    ps-id: B0B2DA3C-41C4-47D8-9936-095F397315F6

---
+++ test/expect-tests/fiber/fiber_scheduler.ml
@@ -0,0 +1,28 @@
+open Stdune
+open Fiber.O
+module Scheduler = Fiber.Scheduler
+
+let%expect_test "test fiber scheduler" =
+  let ivar = Fiber.Ivar.create () in
+  let f () =
+    print_endline "waiting for ivar";
+    let+ () = Fiber.Ivar.read ivar in
+    print_endline "ivar filled"
+  in
+  (match Scheduler.start (Fiber.of_thunk f) with
+  | Done _ -> assert false
+  | Stalled s -> (
+    let step =
+      Scheduler.advance s
+        (let open Nonempty_list in
+        [ Fiber.Fill (ivar, ()) ])
+    in
+    match step with
+    | Done () -> ()
+    | Stalled _ -> assert false));
+  [%expect.unreachable]
+[@@expect.uncaught_exn {|
+  "Assert_failure src/fiber/fiber.ml:126:6"
+  Trailing output
+  ---------------
+  waiting for ivar |}]

dcf921ca5cd825ddb054682809bc4032e0d92ebc
Andrey Mokhov <amokhov@janestreet.com>
2021-12-26 21:45:12 +0000

    Use [Path.Permissions] for execute permissions too (#5304)
    Signed-off-by: Andrey Mokhov <amokhov@janestreet.com>

--- otherlibs/stdune/path.ml
+++ otherlibs/stdune/path.ml
@@ -588,10 +588,13 @@ module Permissions = struct
     ; all_users : int
     }
 
-  (* CR-someday amokhov: add [executable]. *)
+  let execute = { current_user = 0o100; all_users = 0o111 }
+
   let write = { current_user = 0o200; all_users = 0o222 }
 
   let add t perm = perm lor t.current_user
+
+  let test t perm = perm land t.current_user <> 0
 
   let remove t perm = perm land lnot t.all_users
 end

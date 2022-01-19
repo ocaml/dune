1f3c684975ea4348153f0cc8f9fb4e9472e3466a
Benoît Montagu <benoit.montagu@inria.fr>
2022-01-19 15:09:47 +0100

    Use Path.to_dyn where appropriate
    Signed-off-by: Benoît Montagu <benoit.montagu@inria.fr>

--- bin/describe.ml
+++ bin/describe.ml
@@ -38,9 +38,6 @@ let info = Term.info "describe" ~doc ~man
     information computed by ``dune describe``, and hopefully make users' life
     easier in decoding the S-expressions into meaningful contents. *)
 module Descr = struct
-  (** [dyn_path p] converts a path to a value of type [Dyn.t] *)
-  let dyn_path (p : Path.t) : Dyn.t = Dyn.String (Path.to_string p)
-
   (** Description of modules *)
   module Mod = struct
     type t =
@@ -56,10 +53,10 @@ module Descr = struct
       let open Dyn in
       record
         [ ("name", Dune_rules.Module_name.to_dyn name)
-        ; ("impl", option dyn_path impl)
-        ; ("intf", option dyn_path intf)
-        ; ("cmt", option dyn_path cmt)
-        ; ("cmti", option dyn_path cmti)
+        ; ("impl", option Path.to_dyn impl)
+        ; ("intf", option Path.to_dyn intf)
+        ; ("cmt", option Path.to_dyn cmt)
+        ; ("cmti", option Path.to_dyn cmti)
         ]
   end
 
@@ -82,7 +79,7 @@ module Descr = struct
         [ ("names", List (List.map ~f:(fun name -> String name) names))
         ; ("requires", Dyn.(list string) (List.map ~f:Digest.to_string requires))
         ; ("modules", List (List.map ~f:Mod.to_dyn modules))
-        ; ("include_dirs", list dyn_path include_dirs)
+        ; ("include_dirs", list Path.to_dyn include_dirs)
         ]
   end
 
@@ -111,9 +108,9 @@ module Descr = struct
         ; ("uid", String (Digest.to_string uid))
         ; ("local", Bool local)
         ; ("requires", (list string) (List.map ~f:Digest.to_string requires))
-        ; ("source_dir", dyn_path source_dir)
+        ; ("source_dir", Path.to_dyn source_dir)
         ; ("modules", List (List.map ~f:Mod.to_dyn modules))
-        ; ("include_dirs", (list dyn_path) include_dirs)
+        ; ("include_dirs", (list Path.to_dyn) include_dirs)
         ]
   end
 

5daccc254b2b2fd977a1dc22fb14caaed1a3e193
Etienne Millon <me@emillon.org>
2022-03-24 13:58:28 +0100

    bench_format: just serialize
    Signed-off-by: Etienne Millon <me@emillon.org>

--- bench/format/bench_format.ml
+++ bench/format/bench_format.ml
@@ -4,15 +4,10 @@ module Value = struct
     | I of int
     | L of t list
 
-  let rec t_of_yojson = function
-    | `Float f -> F f
-    | `Int n -> I n
-    | j -> L ([%of_yojson: t list] j)
-
   let rec yojson_of_t = function
     | F f -> [%yojson_of: float] f
     | I n -> [%yojson_of: int] n
-    | L l -> `List (List.map yojson_of_t l)
+    | L l -> [%yojson_of: t list] l
 end
 
 module Metric = struct
@@ -21,7 +16,7 @@ module Metric = struct
     ; value : Value.t
     ; units : string
     }
-  [@@deriving yojson]
+  [@@deriving yojson_of]
 end
 
 module Bench_result = struct
@@ -29,22 +24,17 @@ module Bench_result = struct
     { name : string
     ; metrics : Metric.t list
     }
-  [@@deriving yojson]
+  [@@deriving yojson_of]
 end
 
 module Version = struct
   type t = V2
 
   let yojson_of_t V2 = `Int 2
-
-  let t_of_yojson j =
-    match [%of_yojson: int] j with
-    | 2 -> V2
-    | _ -> failwith "unknown version"
 end
 
 type t =
   { version : Version.t
   ; results : Bench_result.t list
   }
-[@@deriving yojson]
+[@@deriving yojson_of]

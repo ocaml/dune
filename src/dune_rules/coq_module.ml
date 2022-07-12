94c3a6ca27a3562480b84ca83bbbf605694c5ca7
Rudi Grinberg <me@rgrinberg.com>
2022-07-12 12:25:32 -0500

    refactor(coq): do not hardcode ocaml extensions
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    ps-id: c1e47a8c-7b06-49df-b029-29240dd210cc

--- src/dune_rules/coq_module.ml
+++ src/dune_rules/coq_module.ml
@@ -60,7 +60,7 @@ let cmxs_of_mod ~wrapper_name x =
   let native_base =
     "N" ^ String.concat ~sep:"_" (wrapper_split @ x.prefix @ [ x.name ])
   in
-  [ native_base ^ ".cmi"; native_base ^ ".cmxs" ]
+  [ native_base ^ Cm_kind.ext Cmi; native_base ^ Mode.plugin_ext Native ]
 
 let dep_file x ~obj_dir =
   let vo_dir = build_vo_dir ~obj_dir x in

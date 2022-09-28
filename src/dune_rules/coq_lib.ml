ocaml/dune

commit d30803b98b24f8fc59525578b321117ab364bb15

[coq_rules] Some refactoring and code modularization, no code changes.
Signed-off-by: Emilio Jesus Gallego Arias <e+git@x80.org>

src/dune_rules/coq_rules.ml
@@ -536,9 +536,45 @@ let source_rule ~sctx theories =
     in
     List.concat l |> List.rev_map ~f:(fun m -> Path.build (Coq_module.source m)))
 
-let setup_coqdoc_rules ~sctx ~cctx ~dir (s : Theory.t) coq_modules =
+let setup_cctx_and_modules ~sctx ~dir ~dir_contents (s : Theory.t) theory =
   let name = snd s.name in
+  let wrapper_name = Coq_lib_name.wrapper name in
+  let theories_deps =
+    Resolve.Memo.bind theory ~f:(fun theory ->
+        Resolve.Memo.lift @@ Coq_lib.theories_closure theory)
+  in
+  let* coq_dir_contents = Dir_contents.coq dir_contents in
+  let theory_dirs =
+    Coq_sources.directories coq_dir_contents ~name |> Path.Build.Set.of_list
+  in
+  let coqc_dir = (Super_context.context sctx).build_dir in
+  let+ cctx =
+    Context.create sctx ~coqc_dir ~dir ~wrapper_name ~theories_deps ~theory_dirs
+      s.buildable
+  in
+  let coq_modules = Coq_sources.library coq_dir_contents ~name in
+  (cctx, coq_modules)
+
+let setup_vo_rules ~sctx ~dir ~(cctx : _ Context.t) (s : Theory.t) theory
+    coq_modules =
   let loc = s.buildable.loc in
+  let source_rule =
+    let theories =
+      let open Resolve.Memo.O in
+      let* theory = theory in
+      let+ theories = cctx.theories_deps in
+      theory :: theories
+    in
+    source_rule ~sctx theories
+  in
+  List.concat_map coq_modules ~f:(fun m ->
+      let cctx = Context.for_module cctx m in
+      let { Module_rule.coqc; coqdep } = setup_rule cctx ~source_rule m in
+      [ coqc; coqdep ])
+  |> Super_context.add_rules ~loc ~dir sctx
+
+let setup_coqdoc_rules ~sctx ~dir ~cctx (s : Theory.t) coq_modules =
+  let loc, name = (s.buildable.loc, snd s.name) in
   let rule =
     let file_flags = Context.coqdoc_file_flags cctx in
     fun mode ->
@@ -555,44 +591,18 @@ let setup_coqdoc_rules ~sctx ~cctx ~dir (s : Theory.t) coq_modules =
   rule Latex
 
 let setup_rules ~sctx ~dir ~dir_contents (s : Theory.t) =
-  let loc, name = (s.buildable.loc, snd s.name) in
-  let scope = SC.find_scope_by_dir sctx dir in
-  let coq_lib_db = Scope.coq_libs scope in
   let theory =
+    let scope = SC.find_scope_by_dir sctx dir in
+    let coq_lib_db = Scope.coq_libs scope in
     Coq_lib.DB.resolve coq_lib_db ~coq_lang_version:s.buildable.coq_lang_version
       s.name
   in
-  let* coq_dir_contents = Dir_contents.coq dir_contents in
-  let* cctx =
-    let wrapper_name = Coq_lib_name.wrapper name in
-    let theories_deps =
-      Resolve.Memo.bind theory ~f:(fun theory ->
-          Resolve.Memo.lift @@ Coq_lib.theories_closure theory)
-    in
-    let theory_dirs =
-      Coq_sources.directories coq_dir_contents ~name |> Path.Build.Set.of_list
-    in
-    let coqc_dir = (Super_context.context sctx).build_dir in
-    Context.create sctx ~coqc_dir ~dir ~wrapper_name ~theories_deps ~theory_dirs
-      s.buildable
+  let* cctx, coq_modules =
+    setup_cctx_and_modules ~sctx ~dir ~dir_contents s theory
   in
-  (* List of modules to compile for this library *)
-  let coq_modules = Coq_sources.library coq_dir_contents ~name in
-  let source_rule =
-    let theories =
-      let open Resolve.Memo.O in
-      let* theory = theory in
-      let+ theories = cctx.theories_deps in
-      theory :: theories
-    in
-    source_rule ~sctx theories
-  in
-  let* () = setup_coqdoc_rules ~sctx ~cctx ~dir s coq_modules in
-  List.concat_map coq_modules ~f:(fun m ->
-      let cctx = Context.for_module cctx m in
-      let { Module_rule.coqc; coqdep } = setup_rule cctx ~source_rule m in
-      [ coqc; coqdep ])
-  |> Super_context.add_rules ~loc ~dir sctx
+  let* () = setup_vo_rules ~sctx ~dir ~cctx s theory coq_modules in
+  let+ () = setup_coqdoc_rules ~sctx ~dir ~cctx s coq_modules in
+  ()
 
 let coqtop_args_theory ~sctx ~dir ~dir_contents (s : Theory.t) coq_module =
   let name = s.name in

commit 614ce7d50eeecb68bdf79a88b1b7f1e837d523d9

[coq] Re-add boot library to list of dependencies in Coq_lib.DB
Should fix a bug introduced in #5866 , the handling of boot Coq lib
really needs to be done at the top level as coqdep's `source_rule`
etc... need to be aware of it.

Signed-off-by: Emilio Jesus Gallego Arias <e+git@x80.org>

src/dune_rules/coq_lib.ml
@@ -50,6 +50,8 @@ include struct
     ; boot : t option Resolve.t
     ; id : Id.t
     ; implicit : bool (* Only useful for the stdlib *)
+    ; use_stdlib : bool
+          (* whether this theory uses the stdlib, eventually set to false for all libs *)
     ; src_root : Path.Build.t
     ; obj_root : Path.Build.t
     ; theories : (Loc.t * t) list Resolve.t
@@ -183,6 +185,7 @@ module DB = struct
         let open Memo.O in
         let* boot = if s.boot then Resolve.Memo.return None else boot coq_db in
         let allow_private_deps = Option.is_none s.package in
+        let use_stdlib = true in
         let+ libraries =
           Resolve.Memo.List.map s.buildable.plugins ~f:(fun (loc, lib) ->
               let open Resolve.Memo.O in
@@ -237,8 +240,16 @@ module DB = struct
                   | Some _ -> Resolve.Memo.return ()
                   | None -> Error.private_deps_not_allowed ~loc theory_name
               in
-
               (loc, theory))
+        in
+        let theories =
+          let open Resolve.O in
+          let* boot = boot in
+          match boot with
+          | Some boot when use_stdlib && not s.boot ->
+            let+ theories = theories in
+            (boot.loc, boot) :: theories
+          | Some _ | None -> theories
         in
         let map_error x =
           let human_readable_description () = Id.pp id in
@@ -246,10 +257,10 @@ module DB = struct
         in
         let theories = map_error theories in
         let libraries = map_error libraries in
-
         { loc = s.buildable.loc
         ; boot
         ; id
+        ; use_stdlib
         ; implicit = s.boot
         ; obj_root = dir
         ; src_root = dir

src/dune_rules/coq_rules.ml
@@ -88,7 +88,7 @@ module Bootstrap = struct
     let dir = Coq_lib.src_root lib in
     S
       (if coqdoc then [ A "--coqlib"; Path (Path.build dir) ]
-      else [ A "-boot"; Util.theory_coqc_flag lib ])
+      else [ A "-boot" ])
 
   let flags ~coqdoc t : _ Command.Args.t =
     match t with
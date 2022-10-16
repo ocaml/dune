ab794181976a0a1817b383d2342bc178937c9223
Javier Chavarri <javier.chavarri@gmail.com>
2022-10-16 12:08:08 +0000

    exclude public_cmi_dir from melange includes
    Signed-off-by: Javier Chavarri <javier.chavarri@gmail.com>

--- src/dune_rules/lib_flags.ml
+++ src/dune_rules/lib_flags.ml
@@ -112,17 +112,19 @@ module L = struct
     let dirs =
       List.fold_left ts ~init:Path.Set.empty ~f:(fun acc t ->
           let obj_dir = Lib_info.obj_dir (Lib.info t) in
-          let acc =
-            if visible_cmi t then
-              let public_cmi_dir = Obj_dir.public_cmi_dir obj_dir in
-              Path.Set.add acc public_cmi_dir
-            else acc
-          in
           match mode with
-          | Lib_mode.Ocaml Byte -> acc
-          | Ocaml Native ->
-            let native_dir = Obj_dir.native_dir obj_dir in
-            Path.Set.add acc native_dir
+          | Lib_mode.Ocaml mode -> (
+            let acc =
+              if visible_cmi t then
+                let public_cmi_dir = Obj_dir.public_cmi_dir obj_dir in
+                Path.Set.add acc public_cmi_dir
+              else acc
+            in
+            match mode with
+            | Byte -> acc
+            | Native ->
+              let native_dir = Obj_dir.native_dir obj_dir in
+              Path.Set.add acc native_dir)
           | Melange -> Path.Set.add acc (Obj_dir.melange_dir obj_dir))
     in
     remove_stdlib dirs ts

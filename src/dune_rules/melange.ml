8edad8e3f1ed0eef46f53396edca383c983fcb06
Rudi Grinberg <me@rgrinberg.com>
2022-10-19 10:14:20 -0600

    _

--- src/dune_rules/melange.ml
+++ src/dune_rules/melange.ml
@@ -3,7 +3,7 @@ open Dune_lang.Decoder
 
 let syntax =
   Dune_lang.Syntax.create ~name:"melange" ~desc:"support for Melange compiler"
-    [ ((0, 1), `Since (3, 5)) ]
+    [ ((0, 1), `Since (3, 6)) ]
 
 let extension_key =
   Dune_project.Extension.register syntax (return ((), [])) Unit.to_dyn

913262f376dc05a33f968ebd6c0e64bd1605ee3b
Antonin Décimo <antonin@tarides.com>
2022-10-26 16:26:14 +0200

    refactor(stdune): optimize Ansi_color.strip perf by copying substrings
    `Ansi_color.strip` removes ANSI escape sequences of colors [1].
    
    They start with the ASCII ESC (escape) character and end with the m
    character. The function used to copy valid characters character by
    character, but that is inefficient as we can copy to the destination
    buffer the whole valid substring at once.
    Switching to unsafe string access in the inner loop yields another
    improvement.
    
    The two combined show an improvement of 50% in a micro-benchmark
    realistic test case:
    
    String of 562895 bytes with 1740 ANSI escape sequences.
    ╭─────────────────────────┬───────────────────────────╮
    │name                     │  monotonic-clock          │
    ├─────────────────────────┼───────────────────────────┤
    │  strip strip_char       │        3090329.2703 ns/run│
    │  strip strip_substring  │        1499787.6293 ns/run│
    ╰─────────────────────────┴───────────────────────────╯
    
    [1]: https://en.wikipedia.org/wiki/ANSI_escape_code#SGR_(Select_Graphic_Rendition)_parameters
    
    Signed-off-by: Antonin Décimo <antonin@tarides.com>

--- otherlibs/stdune/ansi_color.ml
+++ otherlibs/stdune/ansi_color.ml
@@ -172,22 +172,24 @@ let prerr =
 let strip str =
   let len = String.length str in
   let buf = Buffer.create len in
-  let rec loop i =
-    if i = len then Buffer.contents buf
+  let rec loop start i =
+    if i = len then (
+      if i - start > 0 then Buffer.add_substring buf str start (i - start);
+      Buffer.contents buf)
     else
-      match str.[i] with
-      | '\027' -> skip (i + 1)
-      | c ->
-        Buffer.add_char buf c;
-        loop (i + 1)
+      match String.unsafe_get str i with
+      | '\027' ->
+        if i - start > 0 then Buffer.add_substring buf str start (i - start);
+        skip (i + 1)
+      | _ -> loop start (i + 1)
   and skip i =
     if i = len then Buffer.contents buf
     else
-      match str.[i] with
-      | 'm' -> loop (i + 1)
+      match String.unsafe_get str i with
+      | 'm' -> loop (i + 1) (i + 1)
       | _ -> skip (i + 1)
   in
-  loop 0
+  loop 0 0
 
 let index_from_any str start chars =
   let n = String.length str in

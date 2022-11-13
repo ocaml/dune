1a138efafca80669d39edf3eba5de0c3e031f088
Rudi Grinberg <me@rgrinberg.com>
2022-11-13 17:33:14 -0600

    refactor(stdune): use [Filename.t] in a few places
    Clarifies the types in a few Path function signatures
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    ps-id: 0cfcadf2-476d-4723-9077-fc42b342add8

--- otherlibs/stdune/path.mli
+++ otherlibs/stdune/path.mli
@@ -317,9 +317,9 @@ val drop_optional_sandbox_root : t -> t
     otherwise fail. *)
 val drop_optional_build_context_src_exn : t -> Source.t
 
-val explode : t -> string list option
+val explode : t -> Filename.t list option
 
-val explode_exn : t -> string list
+val explode_exn : t -> Filename.t list
 
 (** The build directory *)
 val build_dir : t

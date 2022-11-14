5f2f51f500a08b17fb1e1a250dbb2cc742f110d3
Javier Chavarri <javier.chavarri@gmail.com>
2022-11-14 21:33:26 +0000

    use is_prefix for backwards compat
    Signed-off-by: Javier Chavarri <javier.chavarri@gmail.com>

--- src/dune_rules/melange_stanzas.ml
+++ src/dune_rules/melange_stanzas.ml
@@ -50,7 +50,7 @@ module Emit = struct
   let decode =
     let extension_field name =
       let+ loc, extension = located (field name ~default:"js" string) in
-      if String.starts_with ~prefix:"." extension then
+      if String.is_prefix ~prefix:"." extension then
         User_error.raise ~loc [ Pp.textf "extension must not start with '.'" ];
       "." ^ extension
     in

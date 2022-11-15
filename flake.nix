18ec685b44c34080b1e92cff5c9afd284b8132b1
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-11-15 13:20:18 -0500

    Use a melange-provided overlay
    Signed-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>

--- flake.nix
+++ flake.nix
@@ -12,7 +12,6 @@
       flake = false;
     };
     melange.url = "github:melange-re/melange";
-    melange.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, flake-utils, opam-nix, nixpkgs, ocamllsp, opam-repository, melange }@inputs:
     let package = "dune";
@@ -34,31 +33,27 @@
           cinaps = "*";
           ocamlfind = "1.9.2";
         };
-        pkgs = nixpkgs.legacyPackages.${system}.extend (self: super: {
-          ocaml-ng = super.ocaml-ng // {
-            ocamlPackages_4_14 =
-              super.ocaml-ng.ocamlPackages_4_14.overrideScope' (oself: osuper: {
-                dune_3 = osuper.dune_3.overrideAttrs (o: {
-                  src = builtins.fetchurl {
-                    url = https://github.com/ocaml/dune/releases/download/3.5.0/dune-3.5.0.tbz;
-                    sha256 = "041n16sn41wwj6fgi7l10hvbl5x5swygqv33d4csx7rm0iklrgbp";
+        pkgs = nixpkgs.legacyPackages.${system}.appendOverlays [
+          (self: super: {
+            ocaml-ng = super.ocaml-ng // {
+              ocamlPackages_4_14 =
+                super.ocaml-ng.ocamlPackages_4_14.overrideScope' (oself: osuper: {
+                  melange-compiler-libs = with oself; buildDunePackage rec {
+                    pname = "melange-compiler-libs";
+                    version = "0.0.1-414";
+                    src = builtins.fetchurl {
+                      url = "https://github.com/melange-re/melange-compiler-libs/releases/download/${version}/melange-compiler-libs-${version}.tbz";
+                      sha256 = "1d7qvwh62m99qchxq0iij728jhfhx3854bn7map290j5vixdgn6s";
+                    };
+                    duneVersion = "3";
+                    propagatedBuildInputs = [ menhir menhirLib result ];
                   };
                 });
-
-                melange-compiler-libs = with oself; buildDunePackage rec {
-                  pname = "melange-compiler-libs";
-                  version = "0.0.1-414";
-                  src = builtins.fetchurl {
-                    url = "https://github.com/melange-re/melange-compiler-libs/releases/download/${version}/melange-compiler-libs-${version}.tbz";
-                    sha256 = "1d7qvwh62m99qchxq0iij728jhfhx3854bn7map290j5vixdgn6s";
-                  };
-                  duneVersion = "3";
-                  propagatedBuildInputs = [ menhir menhirLib result ];
-                };
-              });
-          };
-          ocamlPackages = self.ocaml-ng.ocamlPackages_4_14;
-        });
+            };
+            ocamlPackages = self.ocaml-ng.ocamlPackages_4_14;
+          })
+          melange.overlays.default
+        ];
         ocamlformat =
           let
             ocamlformat_version =
@@ -147,7 +142,7 @@
               ] ++ (if stdenv.isLinux then [ strace ] else [ ]))
             ++ [
               ocamllsp.outputs.packages.${system}.ocaml-lsp-server
-              (melange.outputs.fromPkgs pkgs).melange
+              pkgs.ocamlPackages.melange
             ]
             ++ nixpkgs.lib.attrsets.attrVals (builtins.attrNames devPackages) scope;
             inputsFrom = [ self.packages.${system}.default ];

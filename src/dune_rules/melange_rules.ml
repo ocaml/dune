8f3d798e923871220f1d3920947af39b8f10c47f
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-11-17 16:16:36 -0500

    feat(melange): implement promotion for melange.emit (#6500)
    Signed-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>

--- src/dune_rules/melange_rules.ml
+++ src/dune_rules/melange_rules.ml
@@ -53,7 +53,7 @@ let js_includes ~loc ~sctx ~target_dir ~requires_link ~scope ~js_ext =
              ; Hidden_deps hidden_deps
              ])))
 
-let build_js ~loc ~dir ~pkg_name ~module_system ~dst_dir ~obj_dir ~sctx
+let build_js ~loc ~dir ~pkg_name ~mode ~module_system ~dst_dir ~obj_dir ~sctx
     ~lib_deps_js_includes ~js_ext m =
   let cm_kind = Lib_mode.Cm_kind.Melange Cmj in
   let open Memo.O in
@@ -75,7 +75,7 @@ let build_js ~loc ~dir ~pkg_name ~module_system ~dst_dir ~obj_dir ~sctx
     "--bs-module-type" :: js_modules_str :: pkg_name_args
   in
   let lib_deps_js_includes = Command.Args.as_any lib_deps_js_includes in
-  Super_context.add_rule sctx ~dir ?loc
+  Super_context.add_rule sctx ~dir ?loc ~mode
     (Command.run
        ~dir:(Path.build (Super_context.context sctx).build_dir)
        compiler
@@ -88,7 +88,7 @@ let build_js ~loc ~dir ~pkg_name ~module_system ~dst_dir ~obj_dir ~sctx
        ])
 
 let add_rules_for_entries ~sctx ~dir ~expander ~dir_contents ~scope
-    ~compile_info ~target_dir (mel : Melange_stanzas.Emit.t) =
+    ~compile_info ~target_dir ~mode (mel : Melange_stanzas.Emit.t) =
   let open Memo.O in
   (* Use "mobjs" rather than "objs" to avoid a potential conflict with a library
      of the same name *)
@@ -140,8 +140,9 @@ let add_rules_for_entries ~sctx ~dir ~expander ~dir_contents ~scope
   let* () =
     Memo.parallel_iter module_list ~f:(fun m ->
         (* Should we check module kind? *)
-        build_js ~dir ~loc:(Some loc) ~pkg_name ~module_system:mel.module_system
-          ~dst_dir ~obj_dir ~sctx ~lib_deps_js_includes ~js_ext m)
+        build_js ~dir ~loc:(Some loc) ~pkg_name ~mode
+          ~module_system:mel.module_system ~dst_dir ~obj_dir ~sctx
+          ~lib_deps_js_includes ~js_ext m)
   in
   let* () =
     match mel.alias with
@@ -171,7 +172,7 @@ let add_rules_for_entries ~sctx ~dir ~expander ~dir_contents ~scope
         ~dialects:(Dune_project.dialects (Scope.project scope))
         ~modes:`Melange_emit )
 
-let add_rules_for_libraries ~dir ~scope ~target_dir ~sctx ~requires_link
+let add_rules_for_libraries ~dir ~scope ~target_dir ~sctx ~requires_link ~mode
     (mel : Melange_stanzas.Emit.t) =
   Memo.parallel_iter requires_link ~f:(fun lib ->
       let open Memo.O in
@@ -200,8 +201,9 @@ let add_rules_for_libraries ~dir ~scope ~target_dir ~sctx ~requires_link
       in
       Memo.parallel_iter source_modules
         ~f:
-          (build_js ~loc:None ~dir ~pkg_name ~module_system:mel.module_system
-             ~dst_dir ~obj_dir ~sctx ~lib_deps_js_includes ~js_ext))
+          (build_js ~loc:None ~dir ~pkg_name ~mode
+             ~module_system:mel.module_system ~dst_dir ~obj_dir ~sctx
+             ~lib_deps_js_includes ~js_ext))
 
 let compile_info ~scope (mel : Melange_stanzas.Emit.t) =
   let open Memo.O in
@@ -221,16 +223,22 @@ let emit_rules ~dir_contents ~dir ~scope ~sctx ~expander mel =
   let open Memo.O in
   let* compile_info = compile_info ~scope mel in
   let target_dir = Path.Build.relative dir mel.target in
+  let mode =
+    match mel.promote with
+    | None -> Rule.Mode.Standard
+    | Some p -> Promote p
+  in
   let f () =
     let+ cctx_and_merlin =
       add_rules_for_entries ~sctx ~dir ~expander ~dir_contents ~scope
-        ~compile_info ~target_dir mel
+        ~compile_info ~target_dir ~mode mel
     and+ () =
       let* requires_link =
         Memo.Lazy.force (Lib.Compile.requires_link compile_info)
       in
       let* requires_link = Resolve.read_memo requires_link in
-      add_rules_for_libraries ~dir ~scope ~target_dir ~sctx ~requires_link mel
+      add_rules_for_libraries ~dir ~scope ~target_dir ~sctx ~requires_link ~mode
+        mel
     in
     cctx_and_merlin
   in
--- src/dune_rules/melange_stanzas.ml
+++ src/dune_rules/melange_stanzas.ml
@@ -12,6 +12,7 @@ module Emit = struct
     ; package : Package.t option
     ; preprocess : Preprocess.With_instrumentation.t Preprocess.Per_module.t
     ; preprocessor_deps : Dep_conf.t list
+    ; promote : Rule.Promote.t option
     ; flags : Ocaml_flags.Spec.t
     ; root_module : (Loc.t * Module_name.t) option
     ; javascript_extension : string
@@ -85,6 +86,7 @@ module Emit = struct
        and+ libraries = field "libraries" decode_lib ~default:[]
        and+ package = field_o "package" Stanza_common.Pkg.decode
        and+ preprocess, preprocessor_deps = Stanza_common.preprocess_fields
+       and+ promote = field_o "promote" Rule_mode_decoder.Promote.decode
        and+ loc_instrumentation, instrumentation = Stanza_common.instrumentation
        and+ flags = Ocaml_flags.Spec.decode
        and+ root_module = field_o "root_module" Module_name.decode_loc
@@ -109,6 +111,7 @@ module Emit = struct
        ; package
        ; preprocess
        ; preprocessor_deps
+       ; promote
        ; flags
        ; root_module
        ; javascript_extension

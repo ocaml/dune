f5a2a18313f02a77207b28289c526b99022267b6
Simmo Saan <simmo.saan@gmail.com>
2022-12-06 19:30:33 +0200

    Use -inner-modules for codept

--- src/dune_rules/codept.ml
+++ src/dune_rules/codept.ml
@@ -114,7 +114,7 @@ let deps_of
               ] )
         ; Dyn path_args
         ; S sig_args
-        ; S (codept_o_arg "-modules" immediate_file)
+        ; S (codept_o_arg "-inner-modules" immediate_file)
         ]
       >>| Action.Full.add_sandbox sandbox
     in

8b2b584f0b725f7024f217065102304d39616b55
Rudi Grinberg <me@rgrinberg.com>
2022-12-09 10:51:55 -0600

    refactor: move [Sub] to own module
    and remove some dead code while we're at it
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    ps-id: aedd8c4e-be8d-4b42-b3f7-f0289f681f7c

--- otherlibs/dune-rpc/private/dune_rpc_private.mli
+++ otherlibs/dune-rpc/private/dune_rpc_private.mli
@@ -240,18 +240,6 @@ module Sub : sig
   type 'a t
 
   val of_procedure : 'a Procedures.Poll.t -> 'a t
-
-  val poll : 'a t -> (Id.t, 'a option) Decl.Request.witness
-
-  val poll_cancel : 'a t -> Id.t Decl.Notification.witness
-
-  module Id : sig
-    type t
-
-    val compare : t -> t -> Ordering.t
-  end
-
-  val id : 'a t -> Id.t
 end
 
 module Public : sig

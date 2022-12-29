334337a6c88c261788c5e668d01666a3c2e12e72
github-action-benchmark <github@users.noreply.github.com>
2022-12-29 22:22:58 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for 905247a2e69cbc0a10fd86a314504e4103eaf40c

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1672352578024,
+  "repoUrl": "https://github.com/ocaml/dune",
+  "entries": {
+    "Melange Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "javier.chavarri@gmail.com",
+            "name": "Javier Chávarri",
+            "username": "jchavarri"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "905247a2e69cbc0a10fd86a314504e4103eaf40c",
+          "message": "melange: add build benchmark to ci (#6791)\n\nSigned-off-by: Javier Chávarri <javier.chavarri@gmail.com>",
+          "timestamp": "2022-12-29T15:53:30-06:00",
+          "tree_id": "06291f117daf78ff12d184f40b41ec0b3755bd2b",
+          "url": "https://github.com/ocaml/dune/commit/905247a2e69cbc0a10fd86a314504e4103eaf40c"
+        },
+        "date": 1672352576731,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "41.88697116556667",
+            "unit": "seconds"
+          }
+        ]
+      }
+    ]
+  }
+}

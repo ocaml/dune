d436c00e67df906d95e1eab740ef45dc2da9e4af
Rudi Grinberg <me@rgrinberg.com>
2022-12-29 11:18:17 -0600

    inline mvar/stream definitions

--- otherlibs/fiber/pool.ml
+++ otherlibs/fiber/pool.ml
@@ -6,12 +6,45 @@ type mvar =
   | Done
   | Task of (unit -> unit t)
 
+module Mvar = struct
+  type t =
+    { writers : (mvar * unit k) Queue.t
+    ; readers : mvar k Queue.t
+    ; mutable value : mvar option
+    }
+
+  let create () =
+    { value = None; writers = Queue.create (); readers = Queue.create () }
+
+  let read t k =
+    match t.value with
+    | None -> suspend (fun k -> Queue.push t.readers k) k
+    | Some v -> (
+      match Queue.pop t.writers with
+      | None ->
+        t.value <- None;
+        k v
+      | Some (v', w) ->
+        t.value <- Some v';
+        resume w () (fun () -> k v))
+
+  let write t x k =
+    match t.value with
+    | Some _ -> suspend (fun k -> Queue.push t.writers (x, k)) k
+    | None -> (
+      match Queue.pop t.readers with
+      | None ->
+        t.value <- Some x;
+        k ()
+      | Some r -> resume r x (fun () -> k ()))
+end
+
 type status =
   | Open
   | Closed
 
 type t =
-  { mvar : mvar Mvar.t
+  { mvar : Mvar.t
   ; mutable status : status
   }
 

788166fab97a89ec11f72f8488e108d61dc3a631
github-action-benchmark <github@users.noreply.github.com>
2023-01-30 07:05:38 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for 4eb93fddd3031bfa6fc5bc69b9ca4811e4aed650

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1675055291525,
+  "lastUpdate": 1675062338039,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -2928,6 +2928,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "33.79529655743334",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4eb93fddd3031bfa6fc5bc69b9ca4811e4aed650",
+          "message": "fix(rules): canonical paths in aliases (#6963)\n\nThe canonical paths were all wrong for modules with (include_subdirs\r\nqualified). This PR addresses the problem.\r\n\r\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-01-30T00:46:29-06:00",
+          "tree_id": "0b3fdec684b050ac3aab97dba47893f9695c9811",
+          "url": "https://github.com/ocaml/dune/commit/4eb93fddd3031bfa6fc5bc69b9ca4811e4aed650"
+        },
+        "date": 1675062336972,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "35.06499752475333",
             "unit": "seconds"
           }
         ]

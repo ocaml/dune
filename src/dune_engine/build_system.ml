cf96a6fc8ec33104190bb4273e1c5a1146cc8e6c
Rudi Grinberg <me@rgrinberg.com>
2023-02-05 10:51:08 -0600

    refactor(engine): fetching execution parameters
    Move the fetching of execution parameters to the rules.
    
    Currently, the execution parameters are obtained from the dune-project
    file. Therefore, this is part of the frontend and not the engine.
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    <!-- ps-id: 4558da89-86ae-446c-af3c-3d1f417ea6be -->

--- src/dune_engine/build_system.ml
+++ src/dune_engine/build_system.ml
@@ -532,10 +532,10 @@ end = struct
     in
     (* Note: we do not run the below in parallel with the above: if we fail to
        compute action execution parameters, we have no use for the action and
-       might as well fail early, skipping unnecessary dependencies. The function
-       [Source_tree.execution_parameters_of_dir] is memoized, and the result is
-       not expected to change often, so we do not sacrifice too much performance
-       here by executing it sequentially. *)
+       might as well fail early, skipping unnecessary dependencies. The
+       function [(Build_config.get ()).execution_parameters] is likely
+       memoized, and the result is not expected to change often, so we do not
+       sacrifice too much performance here by executing it sequentially. *)
     let* action, deps = Action_builder.run action Eager in
     let wrap_fiber f =
       Memo.of_reproducible_fiber

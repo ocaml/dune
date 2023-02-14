dfb86a678e133b2b9a981b36d7ca1035719140d5
Rudi Grinberg <me@rgrinberg.com>
2023-02-13 23:19:32 -0600

    refactor: move some flags to rules
    Some flags are only used in the rules, so they should live there instead
    of the engine.
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    <!-- ps-id: fe52918f-e2c6-463a-a074-9d79f60c6896 -->

--- src/dune_engine/clflags.ml
+++ src/dune_engine/clflags.ml
@@ -6,8 +6,6 @@ end
 
 let report_errors_config = ref Report_errors_config.default
 
-let debug_artifact_substitution = ref false
-
 let debug_digests = ref false
 
 let debug_fs_cache = ref false
@@ -28,13 +26,7 @@ let promote = ref None
 
 let force = ref false
 
-let store_orig_src_dir = ref false
-
 let always_show_command_line = ref false
-
-let promote_install_files = ref false
-
-let ignore_promoted_rules = ref false
 
 type on_missing_dune_project_file =
   | Error

0cc52c9b3cd3079320847c58d0257ebfbd764719
github-action-benchmark <github@users.noreply.github.com>
2023-02-28 20:17:05 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for f36109e8924c5d47cf06b632a86d858734bc1e4d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1677615062253,
+  "lastUpdate": 1677615425050,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -7365,6 +7365,37 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "35.326087940693334",
+            "unit": "seconds"
+          }
+        ]
+      }
+    ],
+    "Synthetic Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "javier.chavarri@gmail.com",
+            "name": "Javier Chávarri",
+            "username": "jchavarri"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f36109e8924c5d47cf06b632a86d858734bc1e4d",
+          "message": "perf: add synthetic benchmark (#7189)\n\n* perf: add synthetic benchmark\r\n\r\nSigned-off-by: Javier Chávarri <javier.chavarri@gmail.com>",
+          "timestamp": "2023-02-28T11:52:20-08:00",
+          "tree_id": "6c9300c3a5b760644e0282b619c9480ee9e83c27",
+          "url": "https://github.com/ocaml/dune/commit/f36109e8924c5d47cf06b632a86d858734bc1e4d"
+        },
+        "date": 1677615424303,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (Linux)",
+            "value": "71.4238815637",
             "unit": "seconds"
           }
         ]

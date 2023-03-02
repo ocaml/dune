e64ff883316dc0216cefaaf633349fb4d4e853ce
github-action-benchmark <github@users.noreply.github.com>
2023-03-02 22:09:03 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for 8d88ee8068abb053fe8ed7c9c21b3a1883dbaf47

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1677794170530,
+  "lastUpdate": 1677794943565,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -7655,6 +7655,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "42.468556154939996",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8d88ee8068abb053fe8ed7c9c21b3a1883dbaf47",
+          "message": "feature: add terminal ui backend based on NoTTY (#6996)\n\nSigned-off-by: Ali Caglayan <alizter@gmail.com>",
+          "timestamp": "2023-03-02T13:48:18-08:00",
+          "tree_id": "fed526d9dc61ac26685496aeb92a7b07d214d084",
+          "url": "https://github.com/ocaml/dune/commit/8d88ee8068abb053fe8ed7c9c21b3a1883dbaf47"
+        },
+        "date": 1677794941961,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "33.17965460165333",
             "unit": "seconds"
           }
         ]

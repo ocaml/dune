035da8e18d60d99f6abb1c3a07798644c3268d3f
github-action-benchmark <github@users.noreply.github.com>
2023-03-03 23:47:39 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for 40a5eae5bf6871db10c55983dee3506d3e4b8dad

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1677887055981,
+  "lastUpdate": 1677887259831,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -8353,6 +8353,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (warm, Linux)",
             "value": "2.0985649265466666",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "40a5eae5bf6871db10c55983dee3506d3e4b8dad",
+          "message": "test: disable concurrent.t on macos (#7212)\n\nSigned-off-by: Ali Caglayan <alizter@gmail.com>",
+          "timestamp": "2023-03-03T15:09:52-08:00",
+          "tree_id": "6e1b1064053ed570b20f1a485cc8ad2ae7c9c3e5",
+          "url": "https://github.com/ocaml/dune/commit/40a5eae5bf6871db10c55983dee3506d3e4b8dad"
+        },
+        "date": 1677887258663,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (cold, Linux)",
+            "value": "80.68347123810666",
             "unit": "seconds"
           }
         ]

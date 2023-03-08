c5f97e6ecf5fe4189a57cefd7151198d13b77c46
github-action-benchmark <github@users.noreply.github.com>
2023-03-08 20:41:51 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for ebf90a3fdefc16604a97df3a627d7c33b68a48c4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678307651873,
+  "lastUpdate": 1678308111406,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -8090,6 +8090,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "41.412890504900005",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "committer": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "distinct": true,
+          "id": "ebf90a3fdefc16604a97df3a627d7c33b68a48c4",
+          "message": "coq_config: add dyn and improve comments\n\nSigned-off-by: Ali Caglayan <alizter@gmail.com>\n\n<!-- ps-id: 9bb3f6fb-281a-47e3-b7fb-138d85becd81 -->",
+          "timestamp": "2023-03-08T21:22:00+01:00",
+          "tree_id": "cafadc3013ac8cae4c0fb5240bf65c0013213217",
+          "url": "https://github.com/ocaml/dune/commit/ebf90a3fdefc16604a97df3a627d7c33b68a48c4"
+        },
+        "date": 1678308109770,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "34.84141596917334",
             "unit": "seconds"
           }
         ]

4a0efff885e4216ffca7aa3cecf7418a656fc230
Ali Caglayan <alizter@gmail.com>
2023-03-12 00:44:50 +0100

    nix: remove coq from the main flake
    Coq has its own flake anyway.
    
    Signed-off-by: Ali Caglayan <alizter@gmail.com>
    
    <!-- ps-id: 3663fd3e-0ef5-4686-8ab4-77c28706648f -->

--- flake.nix
+++ flake.nix
@@ -170,7 +170,6 @@
           buildInputs = testBuildInputs ++ (with pkgs;
             [
               # dev tools
-              coq_8_16
               patdiff
               ccls
             ])

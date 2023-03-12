129e9c388ce463c22b30a07fd5928ebe4f4bb21d
github-action-benchmark <github@users.noreply.github.com>
2023-03-12 21:33:23 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for e5a98ee12081cf3571e5a6d4d1aa14ddf392eb0d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678656258981,
+  "lastUpdate": 1678656803431,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -9163,6 +9163,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "33.255844440733334",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "distinct": true,
+          "id": "e5a98ee12081cf3571e5a6d4d1aa14ddf392eb0d",
+          "message": "fix(stdune): copy file error message consistency\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>\n\n<!-- ps-id: 97fe8b6e-0238-496c-b1d2-0683b775adb5 -->",
+          "timestamp": "2023-03-12T22:11:15+01:00",
+          "tree_id": "a3d2a4f33bcd0f6c19ea09b866740081575d7819",
+          "url": "https://github.com/ocaml/dune/commit/e5a98ee12081cf3571e5a6d4d1aa14ddf392eb0d"
+        },
+        "date": 1678656801679,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "41.715459426020004",
             "unit": "seconds"
           }
         ]

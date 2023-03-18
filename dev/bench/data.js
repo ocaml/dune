f87a139cc1594e29e0862c7e15782dcc9b77f1ec
github-action-benchmark <github@users.noreply.github.com>
2023-03-18 00:15:24 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for 4245029f2914bcfdca915e9916a5155a0afa95fb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679098512729,
+  "lastUpdate": 1679098524340,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -15429,6 +15429,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (cold, Linux)",
             "value": "70.24727995705335",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4245029f2914bcfdca915e9916a5155a0afa95fb",
+          "message": "feature: add action runners (#6737)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-03-17T16:47:22-07:00",
+          "tree_id": "c854afadcc9812714d9415eca4c789aeb33a416e",
+          "url": "https://github.com/ocaml/dune/commit/4245029f2914bcfdca915e9916a5155a0afa95fb"
+        },
+        "date": 1679098523450,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (warm, Linux)",
+            "value": "1.8079790909866666",
             "unit": "seconds"
           }
         ]

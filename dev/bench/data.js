574a9c956232e8cced9ba584a7434e68852d8f97
github-action-benchmark <github@users.noreply.github.com>
2023-03-20 16:50:07 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for cc174110c2dcab83b0068825870e1484cecef464

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679330996128,
+  "lastUpdate": 1679331007000,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -15603,6 +15603,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (cold, Linux)",
             "value": "66.92870879952",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@emillon.org",
+            "name": "Etienne Millon",
+            "username": "emillon"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cc174110c2dcab83b0068825870e1484cecef464",
+          "message": "Doc: split \"General concepts\" into several pages (#7232)\n\nAs discussed with @Alizter, this splits our giant \"General concepts\" page into\r\nsmaller ones.\r\n\r\nIn addition, this transforms references to the various sections into :doc:\r\nreferences to the documents themselves.\r\n\r\nThe content is mostly untouched, except:\r\n\r\n\"see the X section\" -> \"see X\" (now that it's not a section anymore)\r\nthere was a mixup between the \"sandboxing\" and \"dune action plugin\" references\r\n\r\nSigned-off-by: Etienne Millon <me@emillon.org>\r\nCo-authored-by: Christine Rose <christinerose@users.noreply.github.com>",
+          "timestamp": "2023-03-20T17:21:42+01:00",
+          "tree_id": "872414ea39310a1aa2495271c0ed34998d5d9223",
+          "url": "https://github.com/ocaml/dune/commit/cc174110c2dcab83b0068825870e1484cecef464"
+        },
+        "date": 1679331005902,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (warm, Linux)",
+            "value": "1.6140798367",
             "unit": "seconds"
           }
         ]

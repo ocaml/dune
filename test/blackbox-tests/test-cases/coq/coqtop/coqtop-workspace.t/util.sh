31acd3847cfea621bde9e9faa3c99113f5fb477c
Ali Caglayan <alizter@gmail.com>
2023-03-21 00:03:18 +0100

    dune coq top: tweak util script
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- test/blackbox-tests/test-cases/coq/coqtop/coqtop-workspace.t/util.sh
+++ test/blackbox-tests/test-cases/coq/coqtop/coqtop-workspace.t/util.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+OUTPUT="$PWD/coqtop_test.tmp"
+PWD0="$PWD"
 
-export OUTPUT="$PWD/coqtop_test.tmp"
-export PWD0="$PWD"
-
-function coqtop_test() {
+coqtop_test() {
   (true | (dune coq top "$1" 1>$OUTPUT 2>&1)) || cat $OUTPUT
 }
 
-function check_build() {
+check_build() {
   if [[ ! -d "./_build" ]]; then
     echo "No build directory found..."
     exit 1

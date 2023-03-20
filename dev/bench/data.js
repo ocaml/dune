81ee2873d981e5585cf2682431bf9d0804ab2d97
github-action-benchmark <github@users.noreply.github.com>
2023-03-20 14:28:35 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for c4fc52b3f1e68620d3a9712d46d4f710f0c09fb7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679322011599,
+  "lastUpdate": 1679322515496,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -10120,6 +10120,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "36.00519235772667",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "n.oje.bar@gmail.com",
+            "name": "Nicolás Ojeda Bär",
+            "username": "nojb"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c4fc52b3f1e68620d3a9712d46d4f710f0c09fb7",
+          "message": "Doc: fix default location of cache on Windows (#7356)\n\nSigned-off-by: Nicolás Ojeda Bär <n.oje.bar@gmail.com>",
+          "timestamp": "2023-03-20T15:06:35+01:00",
+          "tree_id": "54b9c616aa715efd1b625d36735db5ac4280079d",
+          "url": "https://github.com/ocaml/dune/commit/c4fc52b3f1e68620d3a9712d46d4f710f0c09fb7"
+        },
+        "date": 1679322514182,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "33.27740159946",
             "unit": "seconds"
           }
         ]

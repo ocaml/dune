7b96f105183b2942d97cca7c220efb439005a2ac
github-action-benchmark <github@users.noreply.github.com>
2023-03-24 06:02:37 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for af02feb02e7c9cb9d1c66390f294f0d4603728a7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679606629623,
+  "lastUpdate": 1679637757905,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -18621,6 +18621,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "41.50142452842",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kit.ty.kate@disroot.org",
+            "name": "Kate",
+            "username": "kit-ty-kate"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "af02feb02e7c9cb9d1c66390f294f0d4603728a7",
+          "message": "Add missing lower-bound constraint to the OCaml compiler (#7396)\n\nSigned-off-by: Kate <kit-ty-kate@outlook.com>",
+          "timestamp": "2023-03-23T22:41:49-07:00",
+          "tree_id": "00456b3e1bf491aa02fa27d6c618acfcecb81d26",
+          "url": "https://github.com/ocaml/dune/commit/af02feb02e7c9cb9d1c66390f294f0d4603728a7"
+        },
+        "date": 1679637755615,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "39.19670697596666",
             "unit": "seconds"
           }
         ]

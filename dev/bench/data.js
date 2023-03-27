40e506ee4e804b4fd7e5a7d5e5a5552332461c55
github-action-benchmark <github@users.noreply.github.com>
2023-03-27 16:25:12 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for 353fa34549832596d18c8fc213bc4b15dd6c5013

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679934276522,
+  "lastUpdate": 1679934312630,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -19955,6 +19955,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "22.134410617153335",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anmonteiro@gmail.com",
+            "name": "Antonio Nuno Monteiro",
+            "username": "anmonteiro"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "353fa34549832596d18c8fc213bc4b15dd6c5013",
+          "message": "refactor(merlin): remove `Ocaml_flags.common` and use `Lib_mode.t` ac… (#7422)\n\nSigned-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>",
+          "timestamp": "2023-03-27T09:12:16-07:00",
+          "tree_id": "50c94a0000e2758e6fb3058265a054c75316c975",
+          "url": "https://github.com/ocaml/dune/commit/353fa34549832596d18c8fc213bc4b15dd6c5013"
+        },
+        "date": 1679934310121,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "21.546335464333335",
             "unit": "seconds"
           }
         ]

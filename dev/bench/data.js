fee933d1d84e00d8c410d323fabe87310d91236b
github-action-benchmark <github@users.noreply.github.com>
2023-03-29 14:45:12 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for 54b453911498df24bc0f80ee64ae231b9003b485

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680099159039,
+  "lastUpdate": 1680101112330,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -21753,6 +21753,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "23.714473185446664",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@emillon.org",
+            "name": "Etienne Millon",
+            "username": "emillon"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "54b453911498df24bc0f80ee64ae231b9003b485",
+          "message": "doc: add a reference list of commands in the CLI (#7403)\n\nThe goal is to have a list somewhere, but not something exhaustive.\r\nThe reference documentation is delegated to the installed manpages.\r\nSome internal commands have been omitted.\r\n\r\nSigned-off-by: Etienne Millon <me@emillon.org>\r\nCo-authored-by: Ali Caglayan <alizter@gmail.com>\r\nCo-authored-by: Christine Rose <christinerose@users.noreply.github.com>",
+          "timestamp": "2023-03-29T16:26:11+02:00",
+          "tree_id": "3a2f931e9bb9455fe2d2f6b2fb2a8f7fc047f196",
+          "url": "https://github.com/ocaml/dune/commit/54b453911498df24bc0f80ee64ae231b9003b485"
+        },
+        "date": 1680101109442,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "26.740143590453332",
             "unit": "seconds"
           }
         ]

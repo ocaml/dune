df8926a6cb0202cba3aed3241a8aefed74f0146a
github-action-benchmark <github@users.noreply.github.com>
2023-03-31 02:38:31 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for 43983fa2e5770308b79360c84524edb249f021f2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680229820411,
+  "lastUpdate": 1680230311196,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -11831,6 +11831,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "49.36786832912",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anmonteiro@gmail.com",
+            "name": "Antonio Nuno Monteiro",
+            "username": "anmonteiro"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "43983fa2e5770308b79360c84524edb249f021f2",
+          "message": "feat(alias_rec): look up aliases in all directories, not just source dirs  (#7334)\n\n\r\nSigned-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>\r\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>\r\nCo-authored-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-03-30T19:17:25-07:00",
+          "tree_id": "763166dab5c20348786bff82e7f8bb0c4b693c92",
+          "url": "https://github.com/ocaml/dune/commit/43983fa2e5770308b79360c84524edb249f021f2"
+        },
+        "date": 1680230309590,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "36.83224832012",
             "unit": "seconds"
           }
         ]

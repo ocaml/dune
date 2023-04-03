3a2ac22214a31f55a99b432e238d2f192145f0b9
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2023-04-03 12:50:53 +0200

    Fix EMLINK on Windows (again) (#7472)
    Signed-off-by: Nicolás Ojeda Bär <n.oje.bar@gmail.com>

--- otherlibs/stdune/src/io.ml
+++ otherlibs/stdune/src/io.ml
@@ -372,7 +372,9 @@ let portable_hardlink ~src ~dst =
          filter out the duplicates first. *)
       Path.unlink dst;
       Path.link src dst
-    | Unix.Unix_error (Unix.EMLINK, _, _) ->
+    | Unix.Unix_error (Unix.EMLINK, _, _)
+    | Unix.Unix_error (Unix.EUNKNOWNERR -1142, _, _)
+    (* Needed for OCaml < 5.1 *) ->
       (* If we can't make a new hard link because we reached the limit on the
          number of hard links per file, we fall back to copying. We expect that
          this happens very rarely (probably only for empty files). *)

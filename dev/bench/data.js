45bb7b0f207c59a812eeec668941794acdcd9c46
github-action-benchmark <github@users.noreply.github.com>
2023-04-04 12:56:03 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for 75bb78d550839493932888095dc8ce8de7f065e3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680570427575,
+  "lastUpdate": 1680612963783,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -23841,6 +23841,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "28.308675654226665",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@emillon.org",
+            "name": "Etienne Millon",
+            "username": "emillon"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "75bb78d550839493932888095dc8ce8de7f065e3",
+          "message": "doc: split preprocessing into ref and explanation (#7466)\n\n* doc: split preprocessing into ref and explanation\r\n\r\nThe existing section from \"concepts\" mixes reference info and an\r\nexplanation about how preprocessing works. These concerns are split into\r\ntwo pages.\r\n\r\nSigned-off-by: Etienne Millon <me@emillon.org>\r\nCo-authored-by: Ali Caglayan <alizter@gmail.com>",
+          "timestamp": "2023-04-04T14:39:56+02:00",
+          "tree_id": "97d49e071509ff796b5dcc6dbdd0f1c10220afe4",
+          "url": "https://github.com/ocaml/dune/commit/75bb78d550839493932888095dc8ce8de7f065e3"
+        },
+        "date": 1680612960915,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "27.595933177039996",
             "unit": "seconds"
           }
         ]

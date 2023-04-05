730220ef97e1ffb0332ccc716588a70c0518b11f
github-action-benchmark <github@users.noreply.github.com>
2023-04-05 16:08:39 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for e33a394e75a4116690e506d66ae8115941c056ce

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680710848817,
+  "lastUpdate": 1680710919643,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -25262,6 +25262,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "22.97518510286",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e33a394e75a4116690e506d66ae8115941c056ce",
+          "message": "fix: (opam_file_location inside_opam_directory) .install files (#7493)\n\nbuild .install files in teh correct location\r\n\r\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-04-05T08:52:33-07:00",
+          "tree_id": "b56be8251580ba2d918c095f7bb35498fa5ffd1a",
+          "url": "https://github.com/ocaml/dune/commit/e33a394e75a4116690e506d66ae8115941c056ce"
+        },
+        "date": 1680710917148,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "27.2879314587",
             "unit": "seconds"
           }
         ]

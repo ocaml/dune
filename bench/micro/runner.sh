af65385701b759c3ec979e7381708f7f25a344a7
Rudi Grinberg <me@rgrinberg.com>
2023-04-06 10:14:25 -0600

    chore: fix thread pool benchmark
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    <!-- ps-id: 36689fc3-4e23-4c2e-9a80-e7dd56ad3486 -->

--- bench/micro/runner.sh
+++ bench/micro/runner.sh
@@ -4,7 +4,7 @@ case "$1" in
   "dune" ) test="dune_bench"; main="main";;
   "fiber" ) test="fiber_bench"; main="fiber_bench_main";;
   "memo" ) test="memo_bench"; main="memo_bench_main";;
-  "thread_pool" ) test="thread_pool"; main="thread_pool_bench_main";;
+  "thread_pool" ) test="thread_pool_bench"; main="thread_pool_bench_main";;
   "digest" ) test="digest_bench"; main="digest_bench_main";;
 esac
 shift;

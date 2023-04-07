93df0342071a6443833a745e072aaf34b197d160
github-action-benchmark <github@users.noreply.github.com>
2023-04-07 21:27:17 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for 8b7133d10480bf585f17ffaba3b0e482eec21ff0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680902347050,
+  "lastUpdate": 1680902837247,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -12991,6 +12991,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "40.271115589919994",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anmonteiro@gmail.com",
+            "name": "Antonio Nuno Monteiro",
+            "username": "anmonteiro"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8b7133d10480bf585f17ffaba3b0e482eec21ff0",
+          "message": "x-compilation: take host findlib.conf into account when cross-compiling (#7428)\n\nSigned-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>\r\nCo-authored-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-04-07T14:06:25-07:00",
+          "tree_id": "864bfed7310796578e72d84ba5f2b385af9dad6e",
+          "url": "https://github.com/ocaml/dune/commit/8b7133d10480bf585f17ffaba3b0e482eec21ff0"
+        },
+        "date": 1680902836469,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "37.054619002559996",
             "unit": "seconds"
           }
         ]

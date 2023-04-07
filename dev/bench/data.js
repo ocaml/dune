9992bc1e2a9ce3132dbdbc3fb212671c4204fa66
github-action-benchmark <github@users.noreply.github.com>
2023-04-07 02:16:36 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for 0ae12e34f385f9de18322a33862bcb0a8719c4b0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680833694393,
+  "lastUpdate": 1680833796881,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -26422,6 +26422,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "23.671979443673333",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0ae12e34f385f9de18322a33862bcb0a8719c4b0",
+          "message": "refactor: more foreign source cleanups (#7508)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-04-06T19:01:13-07:00",
+          "tree_id": "75e7558fd7f5e56db72784fccc8e4b5e7e731898",
+          "url": "https://github.com/ocaml/dune/commit/0ae12e34f385f9de18322a33862bcb0a8719c4b0"
+        },
+        "date": 1680833793187,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "27.14708926577333",
             "unit": "seconds"
           }
         ]

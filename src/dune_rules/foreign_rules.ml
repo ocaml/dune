0ae12e34f385f9de18322a33862bcb0a8719c4b0
Rudi Grinberg <me@rgrinberg.com>
2023-04-06 19:01:13 -0700

    refactor: more foreign source cleanups (#7508)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- src/dune_rules/foreign_rules.ml
+++ src/dune_rules/foreign_rules.ml
@@ -12,96 +12,90 @@ module Source_tree_map_reduce =
    and track all files in specified directories as [Hidden_deps]
    dependencies. *)
 let include_dir_flags ~expander ~dir ~include_dirs =
-  let scope = Expander.scope expander in
-  let lib_dir loc lib_name =
-    let open Resolve.Memo.O in
-    let+ lib = Lib.DB.resolve (Scope.libs scope) (loc, lib_name) in
-    Lib_info.src_dir (Lib.info lib)
+  let lib_dir =
+    let scope = Expander.scope expander in
+    fun loc lib_name ->
+      let open Resolve.Memo.O in
+      let+ lib = Lib.DB.resolve (Scope.libs scope) (loc, lib_name) in
+      Lib_info.src_dir (Lib.info lib)
   in
   let args_of_include_dir include_dir =
     Resolve.Memo.args
-      (let open Resolve.Memo.O in
-      let+ loc, include_dir =
-        match (include_dir : Foreign.Stubs.Include_dir.Without_include.t) with
-        | Dir dir ->
-          Resolve.Memo.return
-            (String_with_vars.loc dir, Expander.expand_path expander dir)
-        | Lib (loc, lib_name) ->
-          let+ lib_dir = lib_dir loc lib_name in
-          (loc, Action_builder.return lib_dir)
-      in
-      Command.Args.Dyn
-        (let open Action_builder.O in
-        let* include_dir = include_dir in
-        let+ dep_args =
-          match Path.extract_build_context_dir include_dir with
-          | None ->
-            (* This branch corresponds to an external directory. The
-               current implementation tracks its contents
-               NON-recursively. *)
-            (* TODO: Track the contents recursively. One way to implement
-               this is to change [Build_system.Loaded.Non_build] so that it
-               contains not only files but also directories and traverse
-               them recursively in [Build_system.Exported.Pred]. *)
-            let+ () =
-              let error msg =
-                User_error.raise ~loc
-                  [ Pp.textf "Unable to read the include directory."
-                  ; Pp.textf "Reason: %s." msg
-                  ]
-              in
-              Action_builder.of_memo
-              @@ Fs_memo.is_directory
-                   (Path.as_outside_build_dir_exn include_dir)
-              >>| function
-              | Error msg -> error (Unix_error.Detailed.to_string_hum msg)
-              | Ok false ->
-                error
-                  (Printf.sprintf "%S is not a directory"
-                     (Path.to_string include_dir))
-              | Ok true -> ()
+    @@
+    let open Resolve.Memo.O in
+    let+ loc, include_dir =
+      match (include_dir : Foreign.Stubs.Include_dir.Without_include.t) with
+      | Dir dir ->
+        Resolve.Memo.return
+          (String_with_vars.loc dir, Expander.expand_path expander dir)
+      | Lib (loc, lib_name) ->
+        let+ lib_dir = lib_dir loc lib_name in
+        (loc, Action_builder.return lib_dir)
+    in
+    Command.Args.Dyn
+      (let open Action_builder.O in
+      let* include_dir = include_dir in
+      let+ dep_args =
+        match Path.extract_build_context_dir include_dir with
+        | None ->
+          (* This branch corresponds to an external directory. The
+             current implementation tracks its contents
+             NON-recursively. *)
+          (* TODO: Track the contents recursively. One way to implement
+             this is to change [Build_system.Loaded.Non_build] so that it
+             contains not only files but also directories and traverse
+             them recursively in [Build_system.Exported.Pred]. *)
+          let+ () =
+            let error msg =
+              User_error.raise ~loc
+                [ Pp.textf "Unable to read the include directory."
+                ; Pp.textf "Reason: %s." msg
+                ]
             in
-            let deps =
-              Dep.Set.singleton
-                (Dep.file_selector
-                   (File_selector.create ~dir:include_dir
-                      Predicate_with_id.true_))
-            in
-            Command.Args.Hidden_deps deps
-          | Some (build_dir, source_dir) ->
-            let open Action_builder.O in
-            Action_builder.return
-            @@ Command.Args.Dyn
-                 ((* This branch corresponds to a source directory. We
-                     track its contents recursively. *)
-                  Action_builder.of_memo (Source_tree.find_dir source_dir)
-                  >>= function
-                  | None ->
-                    User_error.raise ~loc
-                      [ Pp.textf "Include directory %S does not exist."
-                          (Path.reach ~from:(Path.build dir) include_dir)
-                      ]
-                  | Some dir ->
-                    let+ l =
-                      Source_tree_map_reduce.map_reduce dir
-                        ~traverse:Sub_dirs.Status.Set.all ~f:(fun t ->
+            Action_builder.of_memo
+            @@ Fs_memo.is_directory (Path.as_outside_build_dir_exn include_dir)
+            >>| function
+            | Error msg -> error (Unix_error.Detailed.to_string_hum msg)
+            | Ok true -> ()
+            | Ok false ->
+              error
+                (sprintf "%S is not a directory" (Path.to_string include_dir))
+          in
+          let deps =
+            File_selector.create ~dir:include_dir Predicate_with_id.true_
+            |> Dep.file_selector |> Dep.Set.singleton
+          in
+          Command.Args.Hidden_deps deps
+        | Some (build_dir, source_dir) ->
+          Action_builder.return
+          @@ Command.Args.Dyn
+               ((* This branch corresponds to a source directory. We
+                   track its contents recursively. *)
+                Action_builder.of_memo (Source_tree.find_dir source_dir)
+                >>= function
+                | None ->
+                  User_error.raise ~loc
+                    [ Pp.textf "Include directory %S does not exist."
+                        (Path.reach ~from:(Path.build dir) include_dir)
+                    ]
+                | Some dir ->
+                  let+ l =
+                    Source_tree_map_reduce.map_reduce dir
+                      ~traverse:Sub_dirs.Status.Set.all ~f:(fun t ->
+                        let deps =
                           let dir =
                             Path.append_source build_dir
                               (Source_tree.Dir.path t)
                           in
-                          let deps =
-                            Dep.Set.singleton
-                              (Dep.file_selector
-                                 (File_selector.create ~dir
-                                    Predicate_with_id.true_))
-                          in
-                          Action_builder.return
-                            (Appendable_list.singleton
-                               (Command.Args.Hidden_deps deps)))
-                    in
-                    Command.Args.S (Appendable_list.to_list l))
-        in
-        Command.Args.S [ A "-I"; Path include_dir; dep_args ]))
+                          File_selector.create ~dir Predicate_with_id.true_
+                          |> Dep.file_selector |> Dep.Set.singleton
+                        in
+                        Command.Args.Hidden_deps deps
+                        |> Appendable_list.singleton |> Action_builder.return)
+                  in
+                  Command.Args.S (Appendable_list.to_list l))
+      in
+      Command.Args.S [ A "-I"; Path include_dir; dep_args ])
   in
   Command.Args.Dyn
     (let open Action_builder.O in
@@ -133,29 +127,27 @@ let build_c ~(kind : Foreign_language.t) ~sctx ~dir ~expander ~include_flags
           ; Fdo.c_flags ctx
           ])
   in
-  let open Memo.O in
   let* with_user_and_std_flags =
+    Memo.map ~f:(Action_builder.map ~f:(List.append base_flags))
+    @@
     match src.kind with
-    | Ctypes field ->
+    | Ctypes field -> (
       Memo.return
-      @@ Action_builder.map ~f:(List.append base_flags)
-           (match field.build_flags_resolver with
-           | Vendored { c_flags; c_library_flags = _ } ->
-             Super_context.foreign_flags sctx ~dir ~expander ~flags:c_flags
-               ~language:C
-           | Pkg_config ->
-             let dir = Path.Build.parent_exn dst in
-             let lib =
-               External_lib_name.to_string field.external_library_name
-             in
-             let open Action_builder.O in
-             let* default_flags =
-               Super_context.default_foreign_flags sctx ~dir ~language:C
-             in
-             let+ pkg_config_flags =
-               Pkg_config.Query.read ~dir (Cflags lib) sctx
-             in
-             default_flags @ pkg_config_flags)
+      @@
+      match field.build_flags_resolver with
+      | Vendored { c_flags; c_library_flags = _ } ->
+        Super_context.foreign_flags sctx ~dir ~expander ~flags:c_flags
+          ~language:C
+      | Pkg_config ->
+        let open Action_builder.O in
+        let+ default_flags =
+          let dir = Path.Build.parent_exn dst in
+          Super_context.default_foreign_flags sctx ~dir ~language:C
+        and+ pkg_config_flags =
+          let lib = External_lib_name.to_string field.external_library_name in
+          Pkg_config.Query.read ~dir (Cflags lib) sctx
+        in
+        default_flags @ pkg_config_flags)
     | Stubs { Foreign.Stubs.flags; _ } ->
       (* DUNE3 will have [use_standard_c_and_cxx_flags] enabled by default. To
          guide users toward this change we emit a warning when dune_lang is >=
@@ -185,7 +177,6 @@ let build_c ~(kind : Foreign_language.t) ~sctx ~dir ~expander ~include_flags
                compiler arguments which is the new recommended behaviour."
           ];
       Super_context.foreign_flags sctx ~dir ~expander ~flags ~language:kind
-      |> Action_builder.map ~f:(List.append base_flags)
   and* c_compiler =
     Super_context.resolve_program ~loc:None ~dir sctx
       (Ocaml_config.c_compiler ctx.ocaml_config)
@@ -195,40 +186,39 @@ let build_c ~(kind : Foreign_language.t) ~sctx ~dir ~expander ~include_flags
     | Msvc -> [ Command.Args.Concat ("", [ A "/Fo"; Target dst ]) ]
     | Other _ -> [ A "-o"; Target dst ]
   in
-  let+ () =
-    Super_context.add_rule sctx ~loc ~dir
-      (let open Action_builder.With_targets.O in
-      let src = Path.build (Foreign.Source.path src) in
-      (* We have to execute the rule in the library directory as the .o is
-         produced in the current directory *)
-      Command.run ~dir:(Path.build dir) c_compiler
-        ([ Command.Args.dyn with_user_and_std_flags
-         ; S [ A "-I"; Path ctx.stdlib_dir ]
-         ; include_flags
-         ]
-        @ output_param @ [ A "-c"; Dep src ])
-      (* With sandboxing we get errors like: bar.c:2:19: fatal error: foo.cxx:
-         No such file or directory #include "foo.cxx". (These errors happen only
-         when compiling c files.) *)
-      >>| Action.Full.add_sandbox Sandbox_config.no_sandboxing)
-  in
-  dst
+  Super_context.add_rule sctx ~loc ~dir
+    (let open Action_builder.With_targets.O in
+    let src = Path.build (Foreign.Source.path src) in
+    (* We have to execute the rule in the library directory as the .o is
+       produced in the current directory *)
+    Command.run ~dir:(Path.build dir) c_compiler
+      ([ Command.Args.dyn with_user_and_std_flags
+       ; S [ A "-I"; Path ctx.stdlib_dir ]
+       ; include_flags
+       ]
+      @ output_param @ [ A "-c"; Dep src ])
+    (* With sandboxing we get errors like: bar.c:2:19: fatal error: foo.cxx:
+       No such file or directory #include "foo.cxx". (These errors happen only
+       when compiling c files.) *)
+    >>| Action.Full.add_sandbox Sandbox_config.no_sandboxing)
 
 (* TODO: [requires] is a confusing name, probably because it's too general: it
    looks like it's a list of libraries we depend on. *)
 let build_o_files ~sctx ~foreign_sources ~(dir : Path.Build.t) ~expander
     ~requires ~dir_contents =
-  let open Memo.O in
-  let ctx = Super_context.context sctx in
-  let all_dirs = Dir_contents.dirs dir_contents in
-  let h_files =
-    List.fold_left all_dirs ~init:[] ~f:(fun acc dc ->
-        String.Set.fold (Dir_contents.text_files dc) ~init:acc ~f:(fun fn acc ->
-            if String.is_suffix fn ~suffix:Foreign_language.header_extension
-            then Path.relative (Path.build (Dir_contents.dir dc)) fn :: acc
-            else acc))
-  in
   let includes =
+    let h_files =
+      Dir_contents.dirs dir_contents
+      |> List.fold_left ~init:[] ~f:(fun acc dc ->
+             Dir_contents.text_files dc
+             |> String.Set.fold ~init:acc ~f:(fun fn acc ->
+                    if
+                      String.is_suffix fn
+                        ~suffix:Foreign_language.header_extension
+                    then
+                      Path.relative (Path.build (Dir_contents.dir dc)) fn :: acc
+                    else acc))
+    in
     Command.Args.S
       [ Hidden_deps (Dep.Set.of_files h_files)
       ; Resolve.args
@@ -242,35 +232,41 @@ let build_o_files ~sctx ~foreign_sources ~(dir : Path.Build.t) ~expander
   in
   String.Map.to_list_map foreign_sources
     ~f:(fun obj (loc, (src : Foreign.Source.t)) ->
-      let dst = Path.Build.relative dir (obj ^ ctx.lib_config.ext_obj) in
-      let extra_flags =
-        include_dir_flags ~expander ~dir
-          ~include_dirs:
-            (match src.kind with
-            | Stubs stubs -> stubs.include_dirs
-            | Ctypes _ -> [])
-      in
-      let extra_deps, sandbox =
-        match src.kind with
-        | Stubs stubs -> Dep_conf_eval.unnamed stubs.extra_deps ~expander
-        | Ctypes _ -> (Action_builder.return (), Sandbox_config.default)
-      in
-      (* We don't sandbox the C compiler, see comment in [build_file] about
-         this. *)
-      ignore sandbox;
-      let extra_deps =
-        Action_builder.map extra_deps ~f:(fun () -> Command.Args.empty)
-      in
-      let include_flags =
-        Command.Args.S [ includes; extra_flags; Dyn extra_deps ]
-      in
       let+ build_file =
-        build_c
-          ~kind:
-            (match Foreign.Source.language src with
-            | C -> C
-            | Cxx -> Cxx)
-          ~sctx ~dir ~expander ~include_flags (loc, src, dst)
+        let include_flags =
+          let extra_deps =
+            let extra_deps, sandbox =
+              match src.kind with
+              | Stubs stubs -> Dep_conf_eval.unnamed stubs.extra_deps ~expander
+              | Ctypes _ -> (Action_builder.return (), Sandbox_config.default)
+            in
+            (* We don't sandbox the C compiler, see comment in [build_file] about
+               this. *)
+            ignore sandbox;
+            Action_builder.map extra_deps ~f:(fun () -> Command.Args.empty)
+          in
+          let extra_flags =
+            include_dir_flags ~expander ~dir
+              ~include_dirs:
+                (match src.kind with
+                | Stubs stubs -> stubs.include_dirs
+                | Ctypes _ -> [])
+          in
+          Command.Args.S [ includes; extra_flags; Dyn extra_deps ]
+        in
+        let dst =
+          let ctx = Super_context.context sctx in
+          Path.Build.relative dir (obj ^ ctx.lib_config.ext_obj)
+        in
+        let+ () =
+          build_c
+            ~kind:
+              (match Foreign.Source.language src with
+              | C -> C
+              | Cxx -> Cxx)
+            ~sctx ~dir ~expander ~include_flags (loc, src, dst)
+        in
+        dst
       in
       (Foreign.Source.mode src, Path.build build_file))
   |> Memo.all_concurrently

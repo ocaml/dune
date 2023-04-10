fd556ee70dcba9a77c7f59877e2bede3aebf4cf4
github-action-benchmark <github@users.noreply.github.com>
2023-04-10 00:29:16 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for 258058c6803525261df9d330d9eca2a4b0a8adc2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681086543287,
+  "lastUpdate": 1681086556169,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -24651,6 +24651,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (cold, Linux)",
             "value": "77.73235920865334",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "258058c6803525261df9d330d9eca2a4b0a8adc2",
+          "message": "fix: improve locations for [installed_c_headers] (#7527)\n\nUse exact locations for [installed_c_headers] entry rules\r\n\r\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-04-09T16:54:04-07:00",
+          "tree_id": "ce55fe036f1766991a30dfbbdd3a2054c7aee73f",
+          "url": "https://github.com/ocaml/dune/commit/258058c6803525261df9d330d9eca2a4b0a8adc2"
+        },
+        "date": 1681086555158,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (warm, Linux)",
+            "value": "1.9866512994666665",
             "unit": "seconds"
           }
         ]

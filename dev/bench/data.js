4a4df3d42fe10143c9b27a268ea1db3bc00ac636
github-action-benchmark <github@users.noreply.github.com>
2023-04-11 09:02:26 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for c122ffa3a75d92024cbe12460389723f99aeb375

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681141028983,
+  "lastUpdate": 1681203746893,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -28249,6 +28249,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "26.58852193346",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@emillon.org",
+            "name": "Etienne Millon",
+            "username": "emillon"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c122ffa3a75d92024cbe12460389723f99aeb375",
+          "message": "chore: merge 3.7.1 changelog (#7516)\n\nSigned-off-by: Etienne Millon <me@emillon.org>",
+          "timestamp": "2023-04-11T10:42:04+02:00",
+          "tree_id": "1166e029f21c3894154b370c252c936323beea32",
+          "url": "https://github.com/ocaml/dune/commit/c122ffa3a75d92024cbe12460389723f99aeb375"
+        },
+        "date": 1681203743520,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "27.986034886393337",
             "unit": "seconds"
           }
         ]

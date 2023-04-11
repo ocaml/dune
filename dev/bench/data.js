aadfec0aafd7b4985f67e0eafd40f4e6279f31cc
github-action-benchmark <github@users.noreply.github.com>
2023-04-11 19:21:56 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for 578bbb5c1ca8183f5657457d5f29ae8f6b4e7b10

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681226940627,
+  "lastUpdate": 1681240916700,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -28597,6 +28597,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "27.468114546266662",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "578bbb5c1ca8183f5657457d5f29ae8f6b4e7b10",
+          "message": "refactor: Use Stanza.Parser.t alias (#7532)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-04-11T12:05:49-07:00",
+          "tree_id": "69b30bb88d602885bf4a690a3f602f994b2b5765",
+          "url": "https://github.com/ocaml/dune/commit/578bbb5c1ca8183f5657457d5f29ae8f6b4e7b10"
+        },
+        "date": 1681240911654,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "27.792827526919996",
             "unit": "seconds"
           }
         ]

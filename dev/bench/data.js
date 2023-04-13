6ba2d8f1214aba3acd7e94e415c912f81c146775
github-action-benchmark <github@users.noreply.github.com>
2023-04-13 17:26:26 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for 05e0bc19af6fd2658c103d2f196d3caafdf2816d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681406438017,
+  "lastUpdate": 1681406786770,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -25576,6 +25576,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (warm, Linux)",
             "value": "1.7899555425533336",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "05e0bc19af6fd2658c103d2f196d3caafdf2816d",
+          "message": "chore(Source_tree): remove dead code (#7528)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-04-13T09:59:21-07:00",
+          "tree_id": "c72a98c8415d1f3b3cf7ad2e41f31dc65c8abb5a",
+          "url": "https://github.com/ocaml/dune/commit/05e0bc19af6fd2658c103d2f196d3caafdf2816d"
+        },
+        "date": 1681406786071,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (cold, Linux)",
+            "value": "68.94292484954",
             "unit": "seconds"
           }
         ]

a309da90bac0335b8152f2e61487d612ee16cfc4
github-action-benchmark <github@users.noreply.github.com>
2023-04-15 22:58:57 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for b5c814e33d0d370b20c9f6269633625b8244feb2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681599024418,
+  "lastUpdate": 1681599537399,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -13889,6 +13889,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "35.00637030758667",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anmonteiro@gmail.com",
+            "name": "Antonio Nuno Monteiro",
+            "username": "anmonteiro"
+          },
+          "committer": {
+            "email": "anmonteiro@gmail.com",
+            "name": "Antonio Nuno Monteiro",
+            "username": "anmonteiro"
+          },
+          "distinct": true,
+          "id": "b5c814e33d0d370b20c9f6269633625b8244feb2",
+          "message": "fix: plugin loading\n\nLoading using the findlib loader was broken in 3.7.0. This PR restores\nthe functionality\n\nSigned-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>",
+          "timestamp": "2023-04-15T15:27:25-07:00",
+          "tree_id": "3581cf2bad436020f70e6dc183896401f8c1408a",
+          "url": "https://github.com/ocaml/dune/commit/b5c814e33d0d370b20c9f6269633625b8244feb2"
+        },
+        "date": 1681599535932,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "33.943243607973336",
             "unit": "seconds"
           }
         ]

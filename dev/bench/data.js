6bd1f340f0a6349cdc652ed0c053f49071878db1
github-action-benchmark <github@users.noreply.github.com>
2023-04-18 12:48:27 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for b7136676eecd775d21d55d2726fbd156172819c1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681822092186,
+  "lastUpdate": 1681822107382,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -27200,6 +27200,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (cold, Linux)",
             "value": "85.35861599809999",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@emillon.org",
+            "name": "Etienne Millon",
+            "username": "emillon"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b7136676eecd775d21d55d2726fbd156172819c1",
+          "message": "doc: add a lexer for opam files (#7574)\n\nIt does not cover the whole opam syntax but is good enough to support\r\nthe few examples we have.\r\n\r\nSigned-off-by: Etienne Millon <me@emillon.org>",
+          "timestamp": "2023-04-18T14:13:22+02:00",
+          "tree_id": "580487b83f581826120a365216e35e77b25b8f94",
+          "url": "https://github.com/ocaml/dune/commit/b7136676eecd775d21d55d2726fbd156172819c1"
+        },
+        "date": 1681822106105,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (warm, Linux)",
+            "value": "2.350056894726667",
             "unit": "seconds"
           }
         ]

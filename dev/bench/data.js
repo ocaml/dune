b2a7c2842129ba24aff2ebed94542ab0e401b8b3
github-action-benchmark <github@users.noreply.github.com>
2023-04-19 06:01:54 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for e4ee4004e911eb022e585a9a3034fe1ce13a7569

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681883602826,
+  "lastUpdate": 1681884114183,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -14179,6 +14179,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "43.85017774114001",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "n.oje.bar@gmail.com",
+            "name": "Nicolás Ojeda Bär",
+            "username": "nojb"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e4ee4004e911eb022e585a9a3034fe1ce13a7569",
+          "message": "merlin: avoid touching case of filename (#7577)\n\nSigned-off-by: Nicolás Ojeda Bär <n.oje.bar@gmail.com>",
+          "timestamp": "2023-04-19T07:38:19+02:00",
+          "tree_id": "50d0a08ed041603c4be1e7f50a255494d3ce4fc5",
+          "url": "https://github.com/ocaml/dune/commit/e4ee4004e911eb022e585a9a3034fe1ce13a7569"
+        },
+        "date": 1681884113162,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "39.77471055112667",
             "unit": "seconds"
           }
         ]

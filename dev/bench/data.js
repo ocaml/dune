1b6d3530fe640044f9c052d38e32a51538fcb6f1
github-action-benchmark <github@users.noreply.github.com>
2023-04-21 15:18:45 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for 2a1c7e0147d81ed04593c3cdb2cfe0e2686efeab

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1682090279695,
+  "lastUpdate": 1682090325617,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -33175,6 +33175,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "21.86222021060667",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "javier.chavarri@gmail.com",
+            "name": "Javier Chávarri",
+            "username": "jchavarri"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2a1c7e0147d81ed04593c3cdb2cfe0e2686efeab",
+          "message": "doc(melange): write manual (#7477)\n\nSigned-off-by: Javier Chávarri <javier.chavarri@gmail.com>",
+          "timestamp": "2023-04-21T08:03:42-07:00",
+          "tree_id": "7aae7ae2ba32a01123da4224571dae2921c3fce2",
+          "url": "https://github.com/ocaml/dune/commit/2a1c7e0147d81ed04593c3cdb2cfe0e2686efeab"
+        },
+        "date": 1682090320917,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "25.442922212206668",
             "unit": "seconds"
           }
         ]

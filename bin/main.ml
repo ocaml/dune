54c1f99cb36012e34ed50acc4b3ab607a937b72f
arozovyk <artemiy.rozovyk@ocamlpro.com>
2023-04-27 15:36:10 +0200

    memtrace

--- bin/main.ml
+++ bin/main.ml
@@ -91,16 +91,17 @@ let exit_and_flush code =
   Console.finish ();
   exit (Exit_code.code code)
 
-let () =
-  Dune_rules.Colors.setup_err_formatter_colors ();
-  try
-    match Cmd.eval_value cmd ~catch:false with
-    | Ok _ -> exit_and_flush Success
-    | Error _ -> exit_and_flush Error
-  with
-  | Scheduler.Run.Shutdown.E Requested -> exit_and_flush Success
-  | Scheduler.Run.Shutdown.E (Signal _) -> exit_and_flush Signal
-  | exn ->
-    let exn = Exn_with_backtrace.capture exn in
-    Dune_util.Report_error.report exn;
-    exit_and_flush Error
+let () = Memtrace.trace_if_requested ~context:"dune_refine_deps_wip1" ();;
+
+Dune_rules.Colors.setup_err_formatter_colors ();
+try
+  match Cmd.eval_value cmd ~catch:false with
+  | Ok _ -> exit_and_flush Success
+  | Error _ -> exit_and_flush Error
+with
+| Scheduler.Run.Shutdown.E Requested -> exit_and_flush Success
+| Scheduler.Run.Shutdown.E (Signal _) -> exit_and_flush Signal
+| exn ->
+  let exn = Exn_with_backtrace.capture exn in
+  Dune_util.Report_error.report exn;
+  exit_and_flush Error

048188548762f302ca1895cae5eb83377fb6eee2
Ali Caglayan <alizter@gmail.com>
2023-05-01 05:18:45 +0200

    tui: add error message separator with counter
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- src/dune_tui/dune_tui.ml
+++ src/dune_tui/dune_tui.ml
@@ -115,6 +115,27 @@ module Tui () = struct
 
   let reset_count = ref 0
 
+  let horizontal_line_with_count total index =
+    let module A = Notty.A in
+    let module I = Notty.I in
+    let twidth, _ = Term.size term in
+    let status =
+      let left = I.uchar A.(fg red) (Uchar.of_int 0x169c) 1 1 in
+      let index = I.string A.(fg blue) (string_of_int (index + 1)) in
+      let mid = I.string A.(fg red) "/" in
+      let total = I.string A.(fg blue) (string_of_int total) in
+      let right = I.uchar A.(fg red) (Uchar.of_int 0x169B) 1 1 in
+      I.(left <|> index <|> mid <|> total <|> right)
+    in
+    I.(
+      hsnap ~align:`Left twidth status
+      </> uchar A.(fg red) (Uchar.of_int 0x2015) twidth 1)
+
+  let line_separated_message ~total index msg =
+    Notty.I.(
+      image_of_user_message_style_pp (User_message.pp msg)
+      <-> horizontal_line_with_count total index)
+
   let image ~status_line ~messages =
     let status =
       match (status_line : User_message.Style.t Pp.t option) with
@@ -122,8 +143,8 @@ module Tui () = struct
       | Some message -> List.map ~f:image_of_user_message_style_pp [ message ]
     in
     let messages =
-      List.map messages ~f:(fun msg ->
-          image_of_user_message_style_pp (User_message.pp msg))
+      List.mapi messages
+        ~f:(line_separated_message ~total:(List.length messages))
     in
     let reset_count =
       image_of_user_message_style_pp

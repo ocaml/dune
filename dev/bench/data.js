a0ab254490c1aeecf4e5faa7050df7f0829abf61
github-action-benchmark <github@users.noreply.github.com>
2023-05-07 21:27:09 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for 2ccb419ec375b9862cdec14bc8610c22ee85f941

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1683494774773,
+  "lastUpdate": 1683494829158,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -29462,6 +29462,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (warm, Linux)",
             "value": "2.1757015909999997",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2ccb419ec375b9862cdec14bc8610c22ee85f941",
+          "message": "chore: update flakes (#7691)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-05-07T13:59:18-07:00",
+          "tree_id": "27355551eec6071ddf4a41af9b8e06f29e4c2c77",
+          "url": "https://github.com/ocaml/dune/commit/2ccb419ec375b9862cdec14bc8610c22ee85f941"
+        },
+        "date": 1683494827908,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (cold, Linux)",
+            "value": "75.54141428632666",
             "unit": "seconds"
           }
         ]

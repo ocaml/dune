295235926ab54e457dc1ab34b0397d1a1c3b7a79
github-action-benchmark <github@users.noreply.github.com>
2023-05-09 19:52:34 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for 6ec01b98fd5ddc3b49f963f60e490a652787f465

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1683661644776,
+  "lastUpdate": 1683661954784,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -15078,6 +15078,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "39.6646346657",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6ec01b98fd5ddc3b49f963f60e490a652787f465",
+          "message": "refactor(pkg): simplify entries in cookie (#7701)\n\nDon't store original .install file information we aren't going to use.\r\n\r\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-05-09T12:26:13-07:00",
+          "tree_id": "432b2feb63de657d190d668b8cd4f7ff22aebe1d",
+          "url": "https://github.com/ocaml/dune/commit/6ec01b98fd5ddc3b49f963f60e490a652787f465"
+        },
+        "date": 1683661953571,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "49.43362574180666",
             "unit": "seconds"
           }
         ]

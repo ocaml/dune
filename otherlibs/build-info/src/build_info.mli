bf90c0c0848f80854a93c2f9393f6f555aef72d4
Etienne Millon <me@emillon.org>
2023-05-16 16:25:09 +0200

    doc: add API docs for dune-build-info
    The goal is to have great docs for this feature directly on
    <https://ocaml.org/p/dune-build-info/latest/doc/index.html>
    
    Signed-off-by: Etienne Millon <me@emillon.org>

--- otherlibs/build-info/src/build_info.mli
+++ otherlibs/build-info/src/build_info.mli
@@ -1,18 +1,37 @@
-(** Provide build information *)
+(** See {!V1} for the current version. *)
 
 module V1 : sig
+  (** Provide build-time information.
+
+      The entry points in this module are {!version} and
+      {!Statically_linked_libraries.to_list}.
+
+      {e Implementation note:} this module is implemented using special support
+      from Dune. When an executable is linked, a special "blank" placeholder is
+      stored as a string. A special post-link phase called
+      {e artifact substitution} can replace this placeholder with encoded data
+      that will be decoded by this library.
+
+      Artifact substitution happens when an executable is installed or promoted
+      to the source tree. *)
+
   module Version : sig
+    (** Version numbers. *)
+
     type t
 
     val to_string : t -> string
   end
 
-  (** The version at which the current executable was built. The version is
-      [None] during development, it is only [Some _] once the executable is
-      installed or promoted to the source tree. *)
+  (** The version at which the current executable was built.
+
+      The version is [None] during development, it is only [Some _] once
+      artifact substitution happened. *)
   val version : unit -> Version.t option
 
   module Statically_linked_library : sig
+    (** A library with an optional version number. *)
+
     type t
 
     (** The most visible name of the library. If it is a public library, this
@@ -23,10 +42,12 @@ module V1 : sig
   end
 
   module Statically_linked_libraries : sig
-    (** All the libraries that where statically linked in *)
+    (** Entry points to find {!Statically_linked_library} values. *)
 
+    (** All the libraries that where statically linked in *)
     val to_list : unit -> Statically_linked_library.t list
 
+    (** Find a particular library by name. *)
     val find : name:string -> Statically_linked_library.t option
   end
 end

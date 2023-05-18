88a216902bfb364cdd78fba4fbc17126a0f7fc37
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-05-18 01:18:50 -0700

    configurator: document `PKG_CONFIG_PATH` and `PKG_CONFIG_ARGN` (#7756)
    Signed-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>

--- otherlibs/configurator/src/v1.mli
+++ otherlibs/configurator/src/v1.mli
@@ -74,7 +74,8 @@ module Pkg_config : sig
 
   type t
 
-  (** Search pkg-config in the PATH. Returns [None] if pkg-config is not found. *)
+  (** Search pkg-config in PATH. Prefers the [PKG_CONFIG_PATH] environment
+      variable if set. Returns [None] if pkg-config is not found. *)
   val get : configurator -> t option
 
   type package_conf =
@@ -84,8 +85,9 @@ module Pkg_config : sig
 
   (** [query t ~package] query pkg-config for the [package]. The package must
       not contain a version constraint. Multiple, unversioned packages are
-      separated with spaces, for example "gtk+-3.0 gtksourceview-3.0". Returns
-      [None] if [package] is not available *)
+      separated with spaces, for example "gtk+-3.0 gtksourceview-3.0". If set,
+      the [PKG_CONFIG_ARGN] environment variable specifies a list of arguments
+      to pass to pkg-config. Returns [None] if [package] is not available *)
   val query : t -> package:string -> package_conf option
 
   val query_expr : t -> package:string -> expr:string -> package_conf option
@@ -94,8 +96,9 @@ module Pkg_config : sig
   (** [query_expr_err t ~package ~expr] query pkg-config for the [package].
       [expr] may contain a version constraint, for example "gtk+-3.0 >= 3.18".
       [package] must be just the name of the package. If [expr] is specified,
-      [package] must be specified as well. Returns [Error error_msg] if
-      [package] is not available *)
+      [package] must be specified as well. If set, the [PKG_CONFIG_ARGN]
+      environment variable specifies a list of arguments to pass to pkg-config.
+      Returns [Error error_msg] if [package] is not available *)
   val query_expr_err :
     t -> package:string -> expr:string -> (package_conf, string) result
 end

939f7829d192993c367a23a22d67a35f3c29fc09
github-action-benchmark <github@users.noreply.github.com>
2023-05-28 12:05:12 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for a7cdf972ea8bcf8e4924bcc1f58ebda978e89589

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1685268802393,
+  "lastUpdate": 1685275511967,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -45500,6 +45500,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "23.32610162106",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a7cdf972ea8bcf8e4924bcc1f58ebda978e89589",
+          "message": "refactor: move Wrapped to Dune_lang (#7825)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-05-28T04:51:15-07:00",
+          "tree_id": "8b4ace5ea1cc22db8ae038d2878534640c710e25",
+          "url": "https://github.com/ocaml/dune/commit/a7cdf972ea8bcf8e4924bcc1f58ebda978e89589"
+        },
+        "date": 1685275507990,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "24.813417594806666",
             "unit": "seconds"
           }
         ]

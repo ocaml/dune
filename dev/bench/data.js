e13c3d7c64d0d448741e49a35077113625c10bd2
github-action-benchmark <github@users.noreply.github.com>
2023-05-31 08:18:47 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for 30d7355c4b3b15c28d774b2555cd917cdef1fa08

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1685520524742,
+  "lastUpdate": 1685521127333,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -17717,6 +17717,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "37.244366009233325",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@emillon.org",
+            "name": "Etienne Millon",
+            "username": "emillon"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "30d7355c4b3b15c28d774b2555cd917cdef1fa08",
+          "message": "doc: add xrefs to actions (#7842)\n\nThe following will generates a link to the documentation for action x:\r\n\r\n:dune:ref:`action-x`\r\nSome examples are added.\r\n\r\nSigned-off-by: Etienne Millon <me@emillon.org>",
+          "timestamp": "2023-05-31T09:44:47+02:00",
+          "tree_id": "7bb40e595a4bc1aac1a58b647c301863a4fe6311",
+          "url": "https://github.com/ocaml/dune/commit/30d7355c4b3b15c28d774b2555cd917cdef1fa08"
+        },
+        "date": 1685521125379,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "44.98321964964",
             "unit": "seconds"
           }
         ]

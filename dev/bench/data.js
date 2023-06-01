7603c39e74a5f3bad2b6a11f3ace45ed2136f008
github-action-benchmark <github@users.noreply.github.com>
2023-06-01 17:15:30 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for fe5ad8ea0ca5a16d6e1d9e5c378ca6746b2d7ab3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1685639377402,
+  "lastUpdate": 1685639730668,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -39438,6 +39438,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (warm, Linux)",
             "value": "1.913242857466667",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fe5ad8ea0ca5a16d6e1d9e5c378ca6746b2d7ab3",
+          "message": "test: make action runner test more reproducible (#7856)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-06-01T09:46:32-07:00",
+          "tree_id": "423387f6dba1bb63d5c36aeefa4ef23c82b1d0f1",
+          "url": "https://github.com/ocaml/dune/commit/fe5ad8ea0ca5a16d6e1d9e5c378ca6746b2d7ab3"
+        },
+        "date": 1685639729943,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (cold, Linux)",
+            "value": "69.48656726056667",
             "unit": "seconds"
           }
         ]

38b4c67d977f6e86a1f7cc8e9b8fc6b88870c7b4
github-action-benchmark <github@users.noreply.github.com>
2023-06-04 13:18:57 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for 8246390804965bd43091b195c83f2045fb2baf65

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1685884125274,
+  "lastUpdate": 1685884737796,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -18442,6 +18442,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "45.71375188492001",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "committer": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "distinct": true,
+          "id": "8246390804965bd43091b195c83f2045fb2baf65",
+          "message": "refactor: use Pp.enumerate in load_rules\n\nSigned-off-by: Ali Caglayan <alizter@gmail.com>",
+          "timestamp": "2023-06-04T05:53:04-07:00",
+          "tree_id": "cfb835f654e91cb0fbf2648d9179017daf7f2c33",
+          "url": "https://github.com/ocaml/dune/commit/8246390804965bd43091b195c83f2045fb2baf65"
+        },
+        "date": 1685884736594,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "44.941788877940006",
             "unit": "seconds"
           }
         ]

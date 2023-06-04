7514331a0599c22c87a3d76163f27470567f3994
Rudi Grinberg <me@rgrinberg.com>
2023-06-04 10:36:29 +0100

    refactor(stdune): rename [Top_closure_intf] with [Top_closure]
    For consistency with other functors in [Stdune]
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    <!-- ps-id: 2297d8af-53f9-432a-83e3-8cf4054316a2 -->

--- src/dune_engine/context_name.mli
+++ src/dune_engine/context_name.mli
@@ -23,4 +23,4 @@ module Infix : Comparator.OPS with type t = t
 include Comparable_intf.S with type key := t
 
 module Top_closure :
-  Top_closure_intf.S with type key := t and type 'a monad := 'a Monad.Id.t
+  Top_closure with type key := t and type 'a monad := 'a Monad.Id.t

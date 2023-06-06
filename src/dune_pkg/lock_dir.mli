7b5bdc6a2d449d62de2dce9f4242e90f2348613e
Stephen Sherratt <stephen@sherra.tt>
2023-06-06 22:44:35 +1000

    Add function for reading Lock_dir.t from lockdir
    Signed-off-by: Stephen Sherratt <stephen@sherra.tt>

--- src/dune_pkg/lock_dir.mli
+++ src/dune_pkg/lock_dir.mli
@@ -53,3 +53,5 @@ val metadata : Filename.t
 module Metadata : Dune_sexp.Versioned_file.S with type data := unit
 
 val write_disk : lock_dir_path:Path.Source.t -> t -> unit
+
+val read_disk : lock_dir_path:Path.Source.t -> t Or_exn.t

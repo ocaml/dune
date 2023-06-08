8b615e990bdcc0fb6b87ab0823a78d69b1bf5906
Ali Caglayan <alizter@gmail.com>
2023-06-08 20:55:10 +0200

    refactor(describe): factor out describe_common library
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

---
+++ bin/describe/describe_common.mli
@@ -0,0 +1,47 @@
+open Import
+
+val sanitize_for_tests : bool ref
+
+type options =
+  { with_deps : bool
+  ; with_pps : bool
+  }
+
+module Descr : sig
+  module Item : sig
+    type t
+  end
+
+  module Workspace : sig
+    type t = Item.t list
+
+    val to_dyn : options -> t -> Dyn.t
+  end
+end
+
+module Crawl : sig
+  val workspace :
+       options
+    -> Dune_engine.No_io.Path.Source.t list option
+    -> Dune_rules.Main.build_system
+    -> Context.t
+    -> Descr.Workspace.t Memo.t
+end
+
+module Sanitize_for_tests : sig
+  module Workspace : sig
+    val sanitize : Context.t -> Descr.Item.t list -> Descr.Item.t list
+  end
+end
+
+module Opam_files : sig
+  val get : unit -> Dyn.t Memo.t
+end
+
+module External_lib_deps : sig
+  val get : Dune_rules.Main.build_system -> Super_context.t -> Dyn.t Memo.t
+end
+
+module Preprocess : sig
+  val run : Super_context.t -> string -> unit Memo.t
+end

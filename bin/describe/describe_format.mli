4c65cd466af69e50363130832abc05a744ef2f53
Ali Caglayan <alizter@gmail.com>
2023-06-09 14:32:00 +0200

    describe: add comments to describe_format
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- bin/describe/describe_format.mli
+++ bin/describe/describe_format.mli
@@ -6,6 +6,8 @@ type t =
   | Sexp
   | Csexp
 
+(** Command line option for taking a serialisation format *)
 val arg : t Term.t
 
+(** [print_dyn t dyn] prints the dyn to stdout serialised as configured in [t] *)
 val print_dyn : t -> Dyn.t -> unit

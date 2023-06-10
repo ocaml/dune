fc86cc529a45df52ab389a415cf0727c2a609ffb
github-action-benchmark <github@users.noreply.github.com>
2023-06-10 22:40:52 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for 1c0939ee8371951ec0f0d1bd0a1c810ca1b23122

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1686436639303,
+  "lastUpdate": 1686436852149,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -19254,6 +19254,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "41.31717453431333",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anmonteiro@gmail.com",
+            "name": "Antonio Nuno Monteiro",
+            "username": "anmonteiro"
+          },
+          "committer": {
+            "email": "anmonteiro@gmail.com",
+            "name": "Antonio Nuno Monteiro",
+            "username": "anmonteiro"
+          },
+          "distinct": true,
+          "id": "1c0939ee8371951ec0f0d1bd0a1c810ca1b23122",
+          "message": "feat: allow multiple globs in library's (stdlib (internal_modules ...))\n\nSigned-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>",
+          "timestamp": "2023-06-10T15:13:40-07:00",
+          "tree_id": "932c217826b10470aac6415a18095c8179f2766e",
+          "url": "https://github.com/ocaml/dune/commit/1c0939ee8371951ec0f0d1bd0a1c810ca1b23122"
+        },
+        "date": 1686436850013,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "47.50761822178",
             "unit": "seconds"
           }
         ]

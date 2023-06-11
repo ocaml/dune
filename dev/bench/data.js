66e2601d6539512e3dae7e7997a02ab34730eb64
github-action-benchmark <github@users.noreply.github.com>
2023-06-11 22:26:14 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for c7cf52ba973fd2251ef34645dda62b525fde0612

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1686522052499,
+  "lastUpdate": 1686522374001,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -43092,6 +43092,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (warm, Linux)",
             "value": "1.6585989844733335",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anmonteiro@gmail.com",
+            "name": "Antonio Nuno Monteiro",
+            "username": "anmonteiro"
+          },
+          "committer": {
+            "email": "anmonteiro@gmail.com",
+            "name": "Antonio Nuno Monteiro",
+            "username": "anmonteiro"
+          },
+          "distinct": true,
+          "id": "c7cf52ba973fd2251ef34645dda62b525fde0612",
+          "message": "fix(melange) attach `melange.emit` targets to the default alias\n\nSigned-off-by: Haochen Kotoi-Xie <hx@kxc.inc>\nSigned-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>",
+          "timestamp": "2023-06-11T14:59:58-07:00",
+          "tree_id": "1f9a8f74f2f411adda4bc148913e7fe3c09891c3",
+          "url": "https://github.com/ocaml/dune/commit/c7cf52ba973fd2251ef34645dda62b525fde0612"
+        },
+        "date": 1686522373306,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (cold, Linux)",
+            "value": "63.56302209700001",
             "unit": "seconds"
           }
         ]

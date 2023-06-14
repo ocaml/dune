ed52c38afe816ef071750d0a65283199608b287b
github-action-benchmark <github@users.noreply.github.com>
2023-06-14 10:20:48 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for c66e1405894553bdde3bcae6987c6caff6229ee0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1686737645228,
+  "lastUpdate": 1686738047982,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -44832,6 +44832,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (warm, Linux)",
             "value": "1.8140192097733332",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c66e1405894553bdde3bcae6987c6caff6229ee0",
+          "message": "feature: add config toggle for threaded console (#7953)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-06-14T02:52:18-07:00",
+          "tree_id": "ed0b5118fafdcfaf609a05b62a7e4f9def39e5fa",
+          "url": "https://github.com/ocaml/dune/commit/c66e1405894553bdde3bcae6987c6caff6229ee0"
+        },
+        "date": 1686738047170,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (cold, Linux)",
+            "value": "79.53201258330665",
             "unit": "seconds"
           }
         ]

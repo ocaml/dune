bd14e40efc522e9e993b7f0042037a2478565ddc
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-06-13 23:14:01 -0700

    fix: stop catching all exceptions in `Dune_package.Or_meta.load` (#7940)
    Signed-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>

--- src/dune_rules/dune_package.ml
+++ src/dune_rules/dune_package.ml
@@ -456,19 +456,29 @@ module Or_meta = struct
 
   let load file =
     let dir = Path.parent_exn file in
-    Path.as_outside_build_dir_exn file
-    |> Fs_memo.with_lexbuf_from_file ~f:(fun lexbuf ->
-           match
-             Vfile.parse_contents lexbuf ~f:(fun lang ->
-                 String_with_vars.set_decoding_env
-                   (Pform.Env.initial lang.version)
-                   (decode ~lang ~dir))
-           with
-           | x -> Ok x
-           | exception User_error.E message -> Error message
-           | exception exn ->
-             (* XXX stop catching code errors, invalid args, etc. *)
-             Error (User_message.make [ Exn.pp exn ]))
+    let file_path = Path.as_outside_build_dir_exn file in
+    Fs_memo.with_lexbuf_from_file file_path ~f:(fun lexbuf ->
+        match
+          Vfile.parse_contents lexbuf ~f:(fun lang ->
+              String_with_vars.set_decoding_env
+                (Pform.Env.initial lang.version)
+                (decode ~lang ~dir))
+        with
+        | contents -> Ok contents
+        | exception User_error.E message -> Error message
+        | exception Sys_error msg ->
+          Error
+            (User_message.make
+               [ Pp.textf "Failed to read %s:"
+                   (Path.Outside_build_dir.to_string_maybe_quoted file_path)
+               ; Pp.text msg
+               ])
+        | exception End_of_file ->
+          Error
+            (User_message.make
+               [ Pp.textf "Unexpected end of file when reading %s."
+                   (Path.Outside_build_dir.to_string_maybe_quoted file_path)
+               ]))
 
   let pp ~dune_version ppf t =
     let t = encode ~dune_version t in

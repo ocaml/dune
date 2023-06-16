8df6a8aaa6f61f6ad37b2f2487b8af3ee042176c
github-action-benchmark <github@users.noreply.github.com>
2023-06-16 13:52:03 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for 691fe334c99a914d74b478d01eb812cdf4a946dc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1686923401788,
+  "lastUpdate": 1686923523740,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -45383,6 +45383,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (warm, Linux)",
             "value": "1.6922787331600002",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "691fe334c99a914d74b478d01eb812cdf4a946dc",
+          "message": "fix(rpc): treat connection reset as eofs (#7977)\n\nThe only thing this does in practice is hide ugly backtraces from the\r\nuser.\r\n\r\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-06-16T14:25:29+01:00",
+          "tree_id": "2b7dec42beef272f10b23c516dfc353c180b7b6d",
+          "url": "https://github.com/ocaml/dune/commit/691fe334c99a914d74b478d01eb812cdf4a946dc"
+        },
+        "date": 1686923522456,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (cold, Linux)",
+            "value": "71.67526290794667",
             "unit": "seconds"
           }
         ]

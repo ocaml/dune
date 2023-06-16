d572beb7e0830b6ead4580a07d44622b9ba1e293
github-action-benchmark <github@users.noreply.github.com>
2023-06-16 13:56:28 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for 761395b60d0e154a91137f48201dd890e16763a9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1686923536493,
+  "lastUpdate": 1686923788193,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -45441,6 +45441,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (warm, Linux)",
             "value": "1.96167224438",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "761395b60d0e154a91137f48201dd890e16763a9",
+          "message": "create dune show command group (#7946)\n\nThis is an alias of dune describe.\r\n\r\nSigned-off-by: Ali Caglayan <alizter@gmail.com>",
+          "timestamp": "2023-06-16T14:27:23+01:00",
+          "tree_id": "c10c55ebe894e39f008f93fc00d63c0329416da0",
+          "url": "https://github.com/ocaml/dune/commit/761395b60d0e154a91137f48201dd890e16763a9"
+        },
+        "date": 1686923786769,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (cold, Linux)",
+            "value": "78.66059976643334",
             "unit": "seconds"
           }
         ]

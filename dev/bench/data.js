4f54c59e4beb9409e0222b70cfd7e756ad3ce3f7
github-action-benchmark <github@users.noreply.github.com>
2023-06-16 15:13:58 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for 71bb51f8aed6c30d411efe2c0e45fa120cf8e3cb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1686927930798,
+  "lastUpdate": 1686928438597,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -20211,6 +20211,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "37.65841747241333",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "71bb51f8aed6c30d411efe2c0e45fa120cf8e3cb",
+          "message": "feature(pkg): include source fetching --trace-file (#7961)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-06-16T07:50:44-07:00",
+          "tree_id": "0275850e68f346e92fba2745bc18ba9a9eca526e",
+          "url": "https://github.com/ocaml/dune/commit/71bb51f8aed6c30d411efe2c0e45fa120cf8e3cb"
+        },
+        "date": 1686928437767,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "46.42887867281333",
             "unit": "seconds"
           }
         ]

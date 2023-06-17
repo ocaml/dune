055dc9416f88aa5263ed509504e0ab2b93a5246d
github-action-benchmark <github@users.noreply.github.com>
2023-06-17 11:01:01 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for 1542085bb99d3d79406ea93f3438d56a5bbf90d6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1686951182983,
+  "lastUpdate": 1686999661132,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -58144,6 +58144,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "26.273247616579997",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1542085bb99d3d79406ea93f3438d56a5bbf90d6",
+          "message": "refactor: clean up [Dune_rpc_server] api (#7969)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-06-17T03:43:35-07:00",
+          "tree_id": "84c28c6cfa9359c016e5bf748abdf2b7ec80f653",
+          "url": "https://github.com/ocaml/dune/commit/1542085bb99d3d79406ea93f3438d56a5bbf90d6"
+        },
+        "date": 1686999654031,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "31.04834774277333",
             "unit": "seconds"
           }
         ]

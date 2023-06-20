ea0fca389d9ca776fcd4a5cab8355149ad604ccf
github-action-benchmark <github@users.noreply.github.com>
2023-06-20 20:50:15 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for 5c047d4ed5f4ccc797cf0cbd54f762fe69eec1dc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1687287034785,
+  "lastUpdate": 1687294214918,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -60232,6 +60232,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "20.861132052526667",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5c047d4ed5f4ccc797cf0cbd54f762fe69eec1dc",
+          "message": "refactor(dap): hide the implementation of dynamic dependencies (#8006)\n\nThe representation of dynamic dependencies as they're implemented by DAP\r\nis an implementation detail that the engine shouldn't be concerned with.\r\n\r\nInstead, we reuse the standard representation of dependencies.\r\n\r\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-06-20T13:35:53-07:00",
+          "tree_id": "b3d2edfc0394d10113b7b5e0ca593bceb20e0bbc",
+          "url": "https://github.com/ocaml/dune/commit/5c047d4ed5f4ccc797cf0cbd54f762fe69eec1dc"
+        },
+        "date": 1687294208468,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "25.625604650886668",
             "unit": "seconds"
           }
         ]

c3d243417fd350a1eac739b26a90bd0c0a07c1d1
github-action-benchmark <github@users.noreply.github.com>
2023-06-21 15:20:48 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for a873b081ede132e2876285c8896c11ce440b1e4a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1687360835839,
+  "lastUpdate": 1687360848413,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -48080,6 +48080,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (cold, Linux)",
             "value": "74.24593707232",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anmonteiro@gmail.com",
+            "name": "Antonio Nuno Monteiro",
+            "username": "anmonteiro"
+          },
+          "committer": {
+            "email": "anmonteiro@gmail.com",
+            "name": "Antonio Nuno Monteiro",
+            "username": "anmonteiro"
+          },
+          "distinct": true,
+          "id": "a873b081ede132e2876285c8896c11ce440b1e4a",
+          "message": "fix(melange): don't crash when transitive PPX isn't found\n\nSigned-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>",
+          "timestamp": "2023-06-21T07:54:25-07:00",
+          "tree_id": "51717c8a266200b5cccfa3094f914cc3d87beabc",
+          "url": "https://github.com/ocaml/dune/commit/a873b081ede132e2876285c8896c11ce440b1e4a"
+        },
+        "date": 1687360847839,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (warm, Linux)",
+            "value": "2.064461179793333",
             "unit": "seconds"
           }
         ]

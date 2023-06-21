490a6d329bdcb28a7a00820d2f38f70dfd0da537
Stephen Sherratt <stephen@sherra.tt>
2023-06-21 16:51:34 +0200

    Solver includes test and doc deps of local packages
    This adds packages depended on by local projects with filters :with-test
    and :with-doc to the lockdir.
    
    Signed-off-by: Stephen Sherratt <stephen@sherra.tt>

--- src/dune_pkg/opam_solver.ml
+++ src/dune_pkg/opam_solver.ml
@@ -10,26 +10,57 @@ module Context_either (L : CONTEXT) (R : CONTEXT) :
 
   type rejection = (L.rejection, R.rejection) Either.t
 
-  let pp_rejection f = function
-    | Left l -> L.pp_rejection f l
-    | Right r -> R.pp_rejection f r
+  let pp_rejection f = Either.map ~l:(L.pp_rejection f) ~r:(R.pp_rejection f)
 
-  let candidates t name =
+  let candidates =
     let convert_rejections ~f =
       List.map ~f:(fun (version, result) ->
           (version, Result.map_error ~f result))
     in
-    match t with
-    | Left l -> L.candidates l name |> convert_rejections ~f:Either.left
-    | Right r -> R.candidates r name |> convert_rejections ~f:Either.right
+    Either.map
+      ~l:(fun l name ->
+        L.candidates l name |> convert_rejections ~f:Either.left)
+      ~r:(fun r name ->
+        R.candidates r name |> convert_rejections ~f:Either.right)
 
-  let user_restrictions = function
-    | Left l -> L.user_restrictions l
-    | Right r -> R.user_restrictions r
+  let user_restrictions =
+    Either.map ~l:L.user_restrictions ~r:R.user_restrictions
 
-  let filter_deps = function
-    | Left l -> L.filter_deps l
-    | Right r -> R.filter_deps r
+  let filter_deps = Either.map ~l:L.filter_deps ~r:R.filter_deps
+end
+
+(* Helper module for working with [OpamTypes.filtered_formula] *)
+module Filtered_formula : sig
+  open OpamTypes
+
+  (** Update a filtered formula setting the "with-test" and "with-doc" variables
+      to true. *)
+  val with_test_and_doc : filtered_formula -> filtered_formula
+end = struct
+  open OpamTypes
+
+  (* [map_formula_filters_bottom_up t ~f] calls [f] on each node of each filter
+     in t, bottom-up *)
+  let map_filters_bottom_up ~f =
+    OpamFilter.gen_filter_formula
+      (OpamFormula.partial_eval (function
+        | Filter flt -> `Formula (Atom (Filter (OpamFilter.map_up f flt)))
+        | Constraint _ as constraint_ -> `Formula (Atom constraint_)))
+
+  (* update a filtered formula replacing instances of a given variable with a given boolean value *)
+  let resolve_simple_bool_variable filtered_formula variable_to_resolve value =
+    map_filters_bottom_up filtered_formula ~f:(function
+      | FIdent ([], variable, None) as filter ->
+        if OpamVariable.equal variable variable_to_resolve then FBool value
+        else filter
+      | other -> other)
+
+  let with_test_and_doc filtered_formula =
+    List.fold_left [ "with-test"; "with-doc" ] ~init:filtered_formula
+      ~f:(fun acc variable_name ->
+        resolve_simple_bool_variable acc
+          (OpamVariable.of_string variable_name)
+          true)
 end
 
 (* Helper functor which adds a set of local packages to a [CONTEXT] *)
@@ -65,7 +96,17 @@ end = struct
 
   let user_restrictions t = Base_context.user_restrictions t.base_context
 
-  let filter_deps t = Base_context.filter_deps t.base_context
+  (* Override [filter_deps] so that local packages get test and doc dependencies *)
+  let filter_deps t package filtered_formula =
+    let package_is_local =
+      OpamPackage.Name.Map.mem (OpamPackage.name package) t.local_packages
+    in
+    let filtered_formula =
+      if package_is_local then
+        Filtered_formula.with_test_and_doc filtered_formula
+      else filtered_formula
+    in
+    Base_context.filter_deps t.base_context package filtered_formula
 end
 
 module Context_for_dune = struct

9105b3d92b5fdef5c5b6a8ce023ca45e162bf2d3
github-action-benchmark <github@users.noreply.github.com>
2023-06-23 16:03:07 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for e357e8f4291ac045d0d69c5d4dca68e12d0d4aec

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1687536177012,
+  "lastUpdate": 1687536187508,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -48776,6 +48776,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (cold, Linux)",
             "value": "64.96285140480667",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "marek@tarides.com",
+            "name": "Marek Kubica",
+            "username": "Leonidas-from-XIV"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e357e8f4291ac045d0d69c5d4dca68e12d0d4aec",
+          "message": "Implementation of `OpamSysPoll` in Dune-terms (#7868)\n\nSigned-off-by: Marek Kubica <marek@tarides.com>",
+          "timestamp": "2023-06-23T08:38:15-07:00",
+          "tree_id": "dc6cbf69503a772f81b27063528a8dcdcf908e56",
+          "url": "https://github.com/ocaml/dune/commit/e357e8f4291ac045d0d69c5d4dca68e12d0d4aec"
+        },
+        "date": 1687536186894,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (warm, Linux)",
+            "value": "1.6698229392200001",
             "unit": "seconds"
           }
         ]

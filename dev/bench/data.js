b23c9992a68fbb9898aa16d25fc9197919237ca5
github-action-benchmark <github@users.noreply.github.com>
2023-07-05 18:51:03 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for bb0668329ee4a48bdd7727703ca131f025070e5e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1688575763167,
+  "lastUpdate": 1688583063737,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -66496,6 +66496,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "27.890655349786666",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bb0668329ee4a48bdd7727703ca131f025070e5e",
+          "message": "refactor: use Path.External.t for dump_memo_graph_file (#8117)\n\nSigned-off-by: Ali Caglayan <alizter@gmail.com>",
+          "timestamp": "2023-07-05T11:38:05-07:00",
+          "tree_id": "adbcdd5b625ec79d512e933c58a7fd25f85831c0",
+          "url": "https://github.com/ocaml/dune/commit/bb0668329ee4a48bdd7727703ca131f025070e5e"
+        },
+        "date": 1688583056793,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "23.200141362506667",
             "unit": "seconds"
           }
         ]

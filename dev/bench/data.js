31ed9408788205898a974e0d24a276bfc869d1e7
github-action-benchmark <github@users.noreply.github.com>
2023-07-06 12:14:55 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for 1d0030a29777c7711081184edafe0e09b14f8aa9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1688645680016,
+  "lastUpdate": 1688645695103,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -52720,6 +52720,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (cold, Linux)",
             "value": "84.35235258407333",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@emillon.org",
+            "name": "Etienne Millon",
+            "username": "emillon"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1d0030a29777c7711081184edafe0e09b14f8aa9",
+          "message": "refactor: use Command.Args.t for Modules.pp (#8109)\n\n* refactor: use Command.Args.t for Modules.pp\r\n\r\nThis allows using the concatenation DSL instead of list and string\r\nmanipulation.\r\n\r\nSigned-off-by: Etienne Millon <me@emillon.org>",
+          "timestamp": "2023-07-06T13:43:02+02:00",
+          "tree_id": "9b47e98ebabf6a8ba3f2c41a8198d986e458bb68",
+          "url": "https://github.com/ocaml/dune/commit/1d0030a29777c7711081184edafe0e09b14f8aa9"
+        },
+        "date": 1688645693906,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (warm, Linux)",
+            "value": "2.3734456531666663",
             "unit": "seconds"
           }
         ]

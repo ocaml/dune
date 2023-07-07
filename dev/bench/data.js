027c8d4016d5c60e3610a560feb17b994faf5333
github-action-benchmark <github@users.noreply.github.com>
2023-07-07 20:03:40 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for bc0535bb7e4c001fab62db702ad67d4c6914825f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1688760207966,
+  "lastUpdate": 1688760220122,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -67598,6 +67598,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "22.32110092968667",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bc0535bb7e4c001fab62db702ad67d4c6914825f",
+          "message": "chore: cleanup imports in bin/ (#8138)\n\nSigned-off-by: Ali Caglayan <alizter@gmail.com>",
+          "timestamp": "2023-07-07T12:48:05-07:00",
+          "tree_id": "87682465c534deb2d3d825e10b70c36feb3edd20",
+          "url": "https://github.com/ocaml/dune/commit/bc0535bb7e4c001fab62db702ad67d4c6914825f"
+        },
+        "date": 1688760211879,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "18.820232791346665",
             "unit": "seconds"
           }
         ]

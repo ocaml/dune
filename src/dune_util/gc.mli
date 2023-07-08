615a649ef094d3870a673dd566b3706346e74f3f
Ali Caglayan <alizter@gmail.com>
2023-07-08 15:33:38 +0200

    bench: add GC stats to bench (#8063)
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- src/dune_util/gc.mli
+++ src/dune_util/gc.mli
@@ -1,8 +1,7 @@
 open Stdune
 
-(** [to_sexp stat] converts the GC statistics [stat] to an S-expression. *)
-val to_sexp : Stdlib.Gc.stat -> Sexp.t
-
 (** [serialize stat ~path] serializes the GC statistics [stat] to the file
     [path]. *)
 val serialize : Stdlib.Gc.stat -> path:Path.t -> unit
+
+val decode : Stdlib.Gc.stat Dune_sexp.Decoder.t

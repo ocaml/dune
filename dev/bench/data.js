e77cb14b2cdf448eb91972b4a5dd6b9e66f3e133
github-action-benchmark <github@users.noreply.github.com>
2023-07-08 09:15:02 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for a0364ad9849d469db6f7b11d49f6270008fcaa10

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1688807369088,
+  "lastUpdate": 1688807702544,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -53619,6 +53619,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (warm, Linux)",
             "value": "2.2392853679933333",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a0364ad9849d469db6f7b11d49f6270008fcaa10",
+          "message": "fix: add Context_name to import list (#8143)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-07-08T01:49:56-07:00",
+          "tree_id": "8937810c2cc666ba7327e8f3efdcab8badb968ab",
+          "url": "https://github.com/ocaml/dune/commit/a0364ad9849d469db6f7b11d49f6270008fcaa10"
+        },
+        "date": 1688807701190,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (cold, Linux)",
+            "value": "65.24277408227333",
             "unit": "seconds"
           }
         ]

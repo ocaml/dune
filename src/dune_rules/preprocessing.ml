ocaml/dune

commit 1d0030a29777c7711081184edafe0e09b14f8aa9

refactor: use Command.Args.t for Modules.pp (#8109)
* refactor: use Command.Args.t for Modules.pp

This allows using the concatenation DSL instead of list and string
manipulation.

Signed-off-by: Etienne Millon <me@emillon.org>

src/dune_rules/preprocessing.ml
@@ -625,32 +625,20 @@ let pp_one_module sctx ~lib_name ~scope ~preprocessor_deps
     @@
     if staged then
       let dash_ppx_flag =
-        Action_builder.memoize ~cutoff:(List.equal String.equal) "ppx command"
-          (let* () = Action_builder.return () in
-           let* exe, driver, flags =
-             ppx_driver_and_flags sctx ~expander ~loc ~scope ~flags ~lib_name
-               pps
-           in
-           let+ () = Action_builder.path (Path.build exe)
-           and+ () = preprocessor_deps
-           and+ driver_flags =
-             Expander.expand_and_eval_set expander driver.info.as_ppx_flags
-               ~standard:(Action_builder.return [ "--as-ppx" ])
-           in
-           let driver_flags = driver_flags in
-           let command =
-             List.map ~f:String.quote_for_shell
-               (List.concat
-                  [ [ Path.reach (Path.build exe)
-                        ~from:
-                          (Path.build (Super_context.context sctx).build_dir)
-                    ]
-                  ; driver_flags
-                  ; flags
-                  ])
-             |> String.concat ~sep:" "
-           in
-           [ "-ppx"; command ])
+        let+ args =
+          Action_builder.memoize ~cutoff:(List.equal String.equal) "ppx command"
+            (let* exe, driver, flags =
+               ppx_driver_and_flags sctx ~expander ~loc ~scope ~flags ~lib_name
+                 pps
+             in
+             let* driver_flags =
+               Expander.expand_and_eval_set expander driver.info.as_ppx_flags
+                 ~standard:(Action_builder.return [ "--as-ppx" ])
+             and* () = preprocessor_deps in
+             Command.expand_no_targets ~dir:(Path.build dir)
+               (S [ Dep (Path.build exe); As driver_flags; As flags ]))
+        in
+        [ "-ppx"; String.quote_list_for_shell args ]
       in
       let pp =
         let sandbox =

src/dune_rules/module_compilation.ml
@@ -203,15 +203,11 @@ let build_cm cctx ~force_write_cmi ~precompiled_cmi ~cm_kind (m : Module.t)
      else Command.Args.empty
    in
    let flags, sandbox =
-     let flags = Ocaml_flags.get (CC.flags cctx) mode in
+     let flags = Command.Args.dyn (Ocaml_flags.get (CC.flags cctx) mode) in
      match Module.pp_flags m with
      | None -> (flags, sandbox)
      | Some (pp, sandbox') ->
-       ( (let open Action_builder.O in
-          let+ flags = flags
-          and+ pp_flags = pp in
-          flags @ pp_flags)
-       , Sandbox_config.inter sandbox sandbox' )
+       (S [ flags; Command.Args.dyn pp ], Sandbox_config.inter sandbox sandbox')
    in
    let output =
      match phase with
@@ -251,7 +247,7 @@ let build_cm cctx ~force_write_cmi ~precompiled_cmi ~cm_kind (m : Module.t)
       Action_builder.with_no_targets (Action_builder.paths extra_deps)
       >>> Action_builder.with_no_targets other_cm_files
       >>> Command.run ~dir:(Path.build ctx.build_dir) compiler
-            [ Command.Args.dyn flags
+            [ flags
             ; cmt_args
             ; Command.Args.S obj_dirs
             ; Command.Args.as_any

commit a1e732ff8c5702bf6675956458ba171e04b1d02c

fix(staged_pps): expand path relative to root
Fixes #8158

This is an error introduced in #8109, not part of a release.

Signed-off-by: Etienne Millon <me@emillon.org>

src/dune_rules/preprocessing.ml
@@ -635,7 +635,8 @@ let pp_one_module sctx ~lib_name ~scope ~preprocessor_deps
                Expander.expand_and_eval_set expander driver.info.as_ppx_flags
                  ~standard:(Action_builder.return [ "--as-ppx" ])
              and* () = preprocessor_deps in
-             Command.expand_no_targets ~dir:(Path.build dir)
+             Command.expand_no_targets
+               ~dir:(Path.build (Super_context.context sctx).build_dir)
                (S [ Dep (Path.build exe); As driver_flags; As flags ]))
         in
         [ "-ppx"; String.quote_list_for_shell args ]

test/blackbox-tests/test-cases/github8158.t
@@ -33,10 +33,6 @@ directory.
   > EOF
   $ touch bin/e.ml
 
-This fails, but should work (and print nothing):
+This works:
 
-  $ dune exec bin/e.exe 2>&1 | sed -e 's/camlppx....../camlppxXXXXXX/g' -e 's/build_......_dune/build_XXXXXX_dune/g'
-  File "bin/.e.eobjs/_unknown_", line 1, characters 0-0:
-  sh: 1: ../.ppx/059965dd2a1761d0e00c111505aef5ac/ppx.exe: not found
-  File "bin/e.ml", line 1:
-  Error: I/O error: ../.ppx/059965dd2a1761d0e00c111505aef5ac/ppx.exe --as-ppx '/tmp/build_XXXXXX_dune/build_XXXXXX_dune/camlppxXXXXXX' '/tmp/build_XXXXXX_dune/build_XXXXXX_dune/camlppxXXXXXX'
+  $ dune exec bin/e.exe
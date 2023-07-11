810d819cc93c94785c2eb444a03011ac199b2f7d
github-action-benchmark <github@users.noreply.github.com>
2023-07-11 15:56:08 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for 276c3d887ad364dc081c85bbd047c6b7ea9c5ce9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1689087354732,
+  "lastUpdate": 1689090968050,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -70440,6 +70440,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "23.063482542006664",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "276c3d887ad364dc081c85bbd047c6b7ea9c5ce9",
+          "message": "feature(pkg): read toolchain from built packages (#8161)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-07-11T08:43:35-07:00",
+          "tree_id": "e747af114ce27ec278c3c958532f1c394ace43cb",
+          "url": "https://github.com/ocaml/dune/commit/276c3d887ad364dc081c85bbd047c6b7ea9c5ce9"
+        },
+        "date": 1689090960231,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "22.398848754013333",
             "unit": "seconds"
           }
         ]

276c3d887ad364dc081c85bbd047c6b7ea9c5ce9
Rudi Grinberg <me@rgrinberg.com>
2023-07-11 16:43:35 +0100

    feature(pkg): read toolchain from built packages (#8161)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- src/dune_rules/which.ml
+++ src/dune_rules/which.ml
@@ -4,19 +4,42 @@ open Memo.O
 let programs_for_which_we_prefer_opt_ext =
   [ "ocamlc"; "ocamldep"; "ocamlmklib"; "ocamlobjinfo"; "ocamlopt" ]
 
-let best_path ~dir program =
-  let exe_path program =
-    let fn = Path.relative dir (program ^ Bin.exe) in
-    let+ exists = Fs_memo.file_exists (Path.as_outside_build_dir_exn fn) in
-    if exists then Some fn else None
-  in
-  if List.mem programs_for_which_we_prefer_opt_ext program ~equal:String.equal
-  then
-    let* path = exe_path (program ^ ".opt") in
-    match path with
-    | None -> exe_path program
-    | Some _ as path -> Memo.return path
-  else exe_path program
+module Best_path = struct
+  module Make
+      (Monad : Monad.S) (Fs : sig
+        val file_exists : Path.t -> bool Monad.t
+      end) =
+  struct
+    open Monad.O
+
+    let best_path ~dir program =
+      let exe_path program =
+        let fn = Path.relative dir (program ^ Bin.exe) in
+        let+ exists = Fs.file_exists fn in
+        Option.some_if exists fn
+      in
+      if
+        List.mem programs_for_which_we_prefer_opt_ext program
+          ~equal:String.equal
+      then
+        let* path = exe_path (program ^ ".opt") in
+        match path with
+        | None -> exe_path program
+        | Some _ as path -> Monad.return path
+      else exe_path program
+  end
+
+  let memo =
+    let module M =
+      Make
+        (Memo)
+        (struct
+          let file_exists f =
+            Fs_memo.file_exists (Path.as_outside_build_dir_exn f)
+        end)
+    in
+    M.best_path
+end
 
 module rec Rec : sig
   val which : path:Path.t list -> string -> Path.t option Memo.t
@@ -27,7 +50,7 @@ end = struct
     match path with
     | [] -> Memo.return None
     | dir :: path -> (
-      let* res = best_path ~dir program in
+      let* res = Best_path.memo ~dir program in
       match res with
       | None -> which ~path program
       | Some prog -> Memo.return (Some prog))

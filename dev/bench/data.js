b167618f57cd971f04393f8c08f0473dae4b4c24
github-action-benchmark <github@users.noreply.github.com>
2023-07-12 20:55:41 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for 32c68c2a743d4324433d91e196b5a1f65fa274f1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1689195190314,
+  "lastUpdate": 1689195341524,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -71281,6 +71281,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "26.566716163773332",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "32c68c2a743d4324433d91e196b5a1f65fa274f1",
+          "message": "rpc: improve error message (#8191)\n\nSigned-off-by: Ali Caglayan <alizter@gmail.com>",
+          "timestamp": "2023-07-12T13:39:56-07:00",
+          "tree_id": "b2517744b983157f60aeef245ccaf0abd4b38b08",
+          "url": "https://github.com/ocaml/dune/commit/32c68c2a743d4324433d91e196b5a1f65fa274f1"
+        },
+        "date": 1689195332162,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "27.788612098053335",
             "unit": "seconds"
           }
         ]

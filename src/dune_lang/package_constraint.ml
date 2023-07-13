fb067e80342a07124aad2d6e5ea9cf757087bbaf
Stephen Sherratt <stephen@sherra.tt>
2023-07-13 13:03:37 +1000

    Clarify handling of values in package constraints
    This restricts the argument to the `Bvar` constructor to variables.
    Previously string literals were also permitted in this position though
    the parser never produced such values. Also adds some documentation
    about the constraint language.
    
    Signed-off-by: Stephen Sherratt <stephen@sherra.tt>

--- src/dune_lang/package_constraint.ml
+++ src/dune_lang/package_constraint.ml
@@ -26,38 +26,55 @@ module Op = struct
     List.find_exn ~f map |> fst |> Dune_sexp.Encoder.string
 end
 
-module Var = struct
+module Variable = struct
+  type t = { name : string }
+
+  let of_name name = { name }
+
+  let to_dyn { name } = Dyn.record [ ("name", Dyn.string name) ]
+
+  let encode { name } = Dune_sexp.Encoder.string (":" ^ name)
+
+  let decode =
+    Dune_sexp.Decoder.atom_matching ~desc:"variable" (fun s ->
+        if String.is_prefix s ~prefix:":" then Some (of_name (String.drop s 1))
+        else None)
+end
+
+module Value = struct
   type t =
-    | Literal of string
-    | Var of string
+    | String_literal of string
+    | Variable of Variable.t
+
+  let to_dyn = function
+    | String_literal s -> Dyn.variant "String_literal" [ Dyn.string s ]
+    | Variable v -> Dyn.variant "Variable" [ Variable.to_dyn v ]
 
   let encode = function
-    | Literal v -> Dune_sexp.Encoder.string v
-    | Var v -> Dune_sexp.Encoder.string (":" ^ v)
+    | String_literal s -> Dune_sexp.Encoder.string s
+    | Variable v -> Variable.encode v
 
   let decode =
     let open Dune_sexp.Decoder in
-    let+ s = string in
-    if String.is_prefix s ~prefix:":" then Var (String.drop s 1) else Literal s
-
-  let to_dyn = function
-    | Literal v -> Dyn.String v
-    | Var v -> Dyn.String (":" ^ v)
+    (let+ variable = Variable.decode in
+     Variable variable)
+    <|> let+ s = string in
+        String_literal s
 end
 
 type t =
-  | Bvar of Var.t
-  | Uop of Op.t * Var.t
-  | Bop of Op.t * Var.t * Var.t
+  | Bvar of Variable.t
+  | Uop of Op.t * Value.t
+  | Bop of Op.t * Value.t * Value.t
   | And of t list
   | Or of t list
 
 let rec encode c =
   let open Dune_sexp.Encoder in
   match c with
-  | Bvar x -> Var.encode x
-  | Uop (op, x) -> pair Op.encode Var.encode (op, x)
-  | Bop (op, x, y) -> triple Op.encode Var.encode Var.encode (op, x, y)
+  | Bvar x -> Variable.encode x
+  | Uop (op, x) -> pair Op.encode Value.encode (op, x)
+  | Bop (op, x, y) -> triple Op.encode Value.encode Value.encode (op, x, y)
   | And conjuncts -> list sexp (string "and" :: List.map ~f:encode conjuncts)
   | Or disjuncts -> list sexp (string "or" :: List.map ~f:encode disjuncts)
 
@@ -77,8 +94,8 @@ let decode =
   let ops =
     List.map Op.map ~f:(fun (name, op) ->
         ( name
-        , let+ x = Var.decode
-          and+ y = maybe Var.decode
+        , let+ x = Value.decode
+          and+ y = maybe Value.decode
           and+ loc = loc
           and+ version = Dune_sexp.Syntax.get_exn Stanza.syntax in
           match y with
@@ -108,14 +125,15 @@ let decode =
       peek_exn >>= function
       | Atom (_loc, A s) when String.is_prefix s ~prefix:":" ->
         let+ () = junk in
-        Bvar (Var (String.drop s 1))
+        Bvar (Variable.of_name (String.drop s 1))
       | _ -> sum (ops @ logops))
 
 let rec to_dyn =
   let open Dyn in
   function
-  | Bvar v -> variant "Bvar" [ Var.to_dyn v ]
-  | Uop (b, x) -> variant "Uop" [ Op.to_dyn b; Var.to_dyn x ]
-  | Bop (b, x, y) -> variant "Bop" [ Op.to_dyn b; Var.to_dyn x; Var.to_dyn y ]
+  | Bvar v -> variant "Bvar" [ Variable.to_dyn v ]
+  | Uop (b, x) -> variant "Uop" [ Op.to_dyn b; Value.to_dyn x ]
+  | Bop (b, x, y) ->
+    variant "Bop" [ Op.to_dyn b; Value.to_dyn x; Value.to_dyn y ]
   | And t -> variant "And" (List.map ~f:to_dyn t)
   | Or t -> variant "Or" (List.map ~f:to_dyn t)

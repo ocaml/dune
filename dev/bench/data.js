b1834821ffe7314849e4e42611957416b79dc65b
github-action-benchmark <github@users.noreply.github.com>
2023-07-14 07:55:59 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for b1eab1f2a0b3e095f6bdb006ac2b058efaac3371

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1689321346470,
+  "lastUpdate": 1689321359151,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -57012,6 +57012,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (cold, Linux)",
             "value": "76.26529881512666",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "stephen@sherra.tt",
+            "name": "Stephen Sherratt",
+            "username": "gridbugs"
+          },
+          "committer": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "distinct": true,
+          "id": "b1eab1f2a0b3e095f6bdb006ac2b058efaac3371",
+          "message": "Clarify handling of values in package constraints\n\nThis restricts the argument to the `Bvar` constructor to variables.\nPreviously string literals were also permitted in this position though\nthe parser never produced such values. Also adds some documentation\nabout the constraint language.\n\nSigned-off-by: Stephen Sherratt <stephen@sherra.tt>",
+          "timestamp": "2023-07-14T08:27:40+01:00",
+          "tree_id": "7f1e29ac0ddcee9db4f0836239a6365f35c51655",
+          "url": "https://github.com/ocaml/dune/commit/b1eab1f2a0b3e095f6bdb006ac2b058efaac3371"
+        },
+        "date": 1689321358386,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (warm, Linux)",
+            "value": "2.111225248373333",
             "unit": "seconds"
           }
         ]

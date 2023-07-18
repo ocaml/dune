cb13c4894b97464835cc660769bef8a1fb3b7a59
github-action-benchmark <github@users.noreply.github.com>
2023-07-18 08:59:58 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for 32fcaebb88ea0547fef78fde7e88acafe6407d20

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1689593747110,
+  "lastUpdate": 1689670798331,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -74181,6 +74181,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "26.9068826841",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@emillon.org",
+            "name": "Etienne Millon",
+            "username": "emillon"
+          },
+          "committer": {
+            "email": "etienne.millon@gmail.com",
+            "name": "Etienne Millon",
+            "username": "emillon"
+          },
+          "distinct": true,
+          "id": "32fcaebb88ea0547fef78fde7e88acafe6407d20",
+          "message": "test: add repro for #7806\n\nSigned-off-by: Etienne Millon <me@emillon.org>",
+          "timestamp": "2023-07-18T10:47:03+02:00",
+          "tree_id": "e386e8b9dcfc84270f5dac89fdb461b96f7a1dd1",
+          "url": "https://github.com/ocaml/dune/commit/32fcaebb88ea0547fef78fde7e88acafe6407d20"
+        },
+        "date": 1689670789940,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "21.973710852293333",
             "unit": "seconds"
           }
         ]

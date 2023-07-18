7cad8f9c31ca07855b19af9ddd4feb2df9a4194b
github-action-benchmark <github@users.noreply.github.com>
2023-07-18 09:07:15 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for 32fcaebb88ea0547fef78fde7e88acafe6407d20

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1689670798331,
+  "lastUpdate": 1689671234912,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -24387,6 +24387,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "45.70759458905332",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@emillon.org",
+            "name": "Etienne Millon",
+            "username": "emillon"
+          },
+          "committer": {
+            "email": "etienne.millon@gmail.com",
+            "name": "Etienne Millon",
+            "username": "emillon"
+          },
+          "distinct": true,
+          "id": "32fcaebb88ea0547fef78fde7e88acafe6407d20",
+          "message": "test: add repro for #7806\n\nSigned-off-by: Etienne Millon <me@emillon.org>",
+          "timestamp": "2023-07-18T10:47:03+02:00",
+          "tree_id": "e386e8b9dcfc84270f5dac89fdb461b96f7a1dd1",
+          "url": "https://github.com/ocaml/dune/commit/32fcaebb88ea0547fef78fde7e88acafe6407d20"
+        },
+        "date": 1689671234143,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "38.47844452388667",
             "unit": "seconds"
           }
         ]

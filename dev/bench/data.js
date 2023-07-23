16d07b28b9e316c5415bf3b3f285efc64a57419f
github-action-benchmark <github@users.noreply.github.com>
2023-07-23 14:22:03 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for 08376aefdc58a1bde5c8777e431903a4660767f6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1690122107467,
+  "lastUpdate": 1690122123556,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -24619,6 +24619,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "42.49815120496667",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "08376aefdc58a1bde5c8777e431903a4660767f6",
+          "message": "bug: dune ocaml top-module crashes on absolute path (#7369)\n\nSigned-off-by: Ali Caglayan <alizter@gmail.com>",
+          "timestamp": "2023-07-23T07:01:54-07:00",
+          "tree_id": "9b9e565830746df5a3e53a5dd447e426c96a1913",
+          "url": "https://github.com/ocaml/dune/commit/08376aefdc58a1bde5c8777e431903a4660767f6"
+        },
+        "date": 1690122122708,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "39.71685507958",
             "unit": "seconds"
           }
         ]

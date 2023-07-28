11b72ebe55723509f6f858d9d87cd19526747d51
github-action-benchmark <github@users.noreply.github.com>
2023-07-28 15:31:11 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for 22c63dffb061103197114c96fda8d4c7f2ed9c46

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1690547815190,
+  "lastUpdate": 1690558271746,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -78357,6 +78357,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "23.696107581233335",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alanh@ccs.neu.edu",
+            "name": "Alan",
+            "username": "alan-j-hu"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "22c63dffb061103197114c96fda8d4c7f2ed9c46",
+          "message": "fix(sendfile): Flush the out_channel used in the fallback path (#8288)\n\nFixes #8284\r\n\r\nSigned-off-by: Alan Hu <alanh@ccs.neu.edu>",
+          "timestamp": "2023-07-28T16:15:35+01:00",
+          "tree_id": "e059f32675d5c3e108a980fda951471d6f716249",
+          "url": "https://github.com/ocaml/dune/commit/22c63dffb061103197114c96fda8d4c7f2ed9c46"
+        },
+        "date": 1690558263031,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "23.703011589126664",
             "unit": "seconds"
           }
         ]

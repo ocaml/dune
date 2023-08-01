4a11c8dee17f6d95e3d09357832d98b0331b4880
github-action-benchmark <github@users.noreply.github.com>
2023-08-01 09:35:19 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for 4338b51cc5a8d0880f3f950324695fe4b03b33a7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1690878057948,
+  "lastUpdate": 1690882518982,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -79749,6 +79749,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "22.442677389660002",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4338b51cc5a8d0880f3f950324695fe4b03b33a7",
+          "message": "refactor(memo): remove unnecessary tupling of a constructor (#8313)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-08-01T10:22:28+01:00",
+          "tree_id": "03673abee34607248df971790c926ab033317f70",
+          "url": "https://github.com/ocaml/dune/commit/4338b51cc5a8d0880f3f950324695fe4b03b33a7"
+        },
+        "date": 1690882509901,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "23.76136066991333",
             "unit": "seconds"
           }
         ]

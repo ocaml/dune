69a0902d23da6bd21248eeb3d1972e2eea8942b4
github-action-benchmark <github@users.noreply.github.com>
2023-08-02 13:37:55 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for ac6d3b9ae6452eaef913e50435d637b4b3ac1193

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1690981536434,
+  "lastUpdate": 1690983474946,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -80445,6 +80445,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "24.7843612483",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ac6d3b9ae6452eaef913e50435d637b4b3ac1193",
+          "message": "bench: log more info (#8264)\n\nWe add more info to the log in order to understand what the bench is\r\ndoing, especially if something goes wrong.\r\n\r\nSigned-off-by: Ali Caglayan <alizter@gmail.com>",
+          "timestamp": "2023-08-02T14:25:07+01:00",
+          "tree_id": "e68a3e087de8792cd586e45f9e28432903864bd5",
+          "url": "https://github.com/ocaml/dune/commit/ac6d3b9ae6452eaef913e50435d637b4b3ac1193"
+        },
+        "date": 1690983465222,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "22.475120471146667",
             "unit": "seconds"
           }
         ]

04b989b7dfa24d4cedb38b139a8526bc63b61cd9
github-action-benchmark <github@users.noreply.github.com>
2023-08-02 15:43:59 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for 0139b1e9366f94e7fdfa488938597e0cf70f9ab3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1690991027421,
+  "lastUpdate": 1690991039272,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -63073,6 +63073,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (cold, Linux)",
             "value": "69.76388122106",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0139b1e9366f94e7fdfa488938597e0cf70f9ab3",
+          "message": "chore: bump version (#8323)\n\nSigned-off-by: Ali Caglayan <alizter@gmail.com>",
+          "timestamp": "2023-08-02T16:17:17+01:00",
+          "tree_id": "729dc995424177fd07ed87a8047fe2adc4c1561e",
+          "url": "https://github.com/ocaml/dune/commit/0139b1e9366f94e7fdfa488938597e0cf70f9ab3"
+        },
+        "date": 1690991038496,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (warm, Linux)",
+            "value": "1.9133904153466668",
             "unit": "seconds"
           }
         ]

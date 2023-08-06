7a6b9716241dcbede91a8095841c3778768ed832
Rudi Grinberg <me@rgrinberg.com>
2023-08-06 18:57:59 +0100

    refactor: more precise types (#8347)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- src/dune_rules/utop.mli
+++ src/dune_rules/utop.mli
@@ -4,8 +4,8 @@ open Import
 
 (** Return the name of the utop target inside a directory where some libraries
     are defined. *)
-val utop_exe : string
+val utop_exe : Filename.t
 
-val utop_dir_basename : string
+val utop_dir_basename : Filename.t
 val libs_under_dir : Super_context.t -> db:Lib.DB.t -> dir:Path.t -> Lib.t list Memo.t
 val setup : Super_context.t -> dir:Path.Build.t -> unit Memo.t

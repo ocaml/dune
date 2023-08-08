0ea787aa519f406d2accaae1f362b617e247e028
github-action-benchmark <github@users.noreply.github.com>
2023-08-08 15:45:40 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for fee7ae2f8393d5d153bfff24e8ff21f4e7bf4242

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691509204847,
+  "lastUpdate": 1691509540124,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -65799,6 +65799,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (warm, Linux)",
             "value": "2.1548453543133337",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fee7ae2f8393d5d153bfff24e8ff21f4e7bf4242",
+          "message": "refactor(engine): simplify rule loading (#8287)\n\nThe function [load_build_directory_exn] is quite large. This PR splits\r\nit into a few logical parts:\r\n\r\n* Computing which source paths to ignore\r\n* Computing the copying rules for source files\r\n* Computing the descendants that artifact deletion should leave alone\r\n\r\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-08-08T08:20:30-07:00",
+          "tree_id": "b279559d961f45edbe058ad1c232878453abe7e9",
+          "url": "https://github.com/ocaml/dune/commit/fee7ae2f8393d5d153bfff24e8ff21f4e7bf4242"
+        },
+        "date": 1691509539367,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (cold, Linux)",
+            "value": "66.42192181050001",
             "unit": "seconds"
           }
         ]

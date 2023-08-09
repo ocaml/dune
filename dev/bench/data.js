54fcc08fe36320a2c41362ff3a0de69dde0a0a24
github-action-benchmark <github@users.noreply.github.com>
2023-08-09 18:28:53 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for 788d41e2c785922f506ee0a9af4439f347efd325

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691577418947,
+  "lastUpdate": 1691605733003,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -84737,6 +84737,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "23.73300860226667",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "788d41e2c785922f506ee0a9af4439f347efd325",
+          "message": "refactor: do not load context to get dune file sexp (#8357)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-08-09T11:15:57-07:00",
+          "tree_id": "126a06e4961fac9c4dcf8cc0ed6c3c68dbe2e7d2",
+          "url": "https://github.com/ocaml/dune/commit/788d41e2c785922f506ee0a9af4439f347efd325"
+        },
+        "date": 1691605722490,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "22.934762229866664",
             "unit": "seconds"
           }
         ]

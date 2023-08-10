372402791ffa5e91a0257a2011655a2bafc5e70e
github-action-benchmark <github@users.noreply.github.com>
2023-08-10 22:55:08 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for 82d153233f233a98a99e5ffea82f6ce4edee42ec

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691707769909,
+  "lastUpdate": 1691708108867,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -66321,6 +66321,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (warm, Linux)",
             "value": "2.3031415554800003",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "82d153233f233a98a99e5ffea82f6ce4edee42ec",
+          "message": "refactor: remove module prefix (#8366)\n\nSigned-off-by: Ali Caglayan <alizter@gmail.com>",
+          "timestamp": "2023-08-10T15:29:46-07:00",
+          "tree_id": "ecdc671a74fcd0e1dd6eb1d5a6c58f3c3cf670cc",
+          "url": "https://github.com/ocaml/dune/commit/82d153233f233a98a99e5ffea82f6ce4edee42ec"
+        },
+        "date": 1691708108131,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (cold, Linux)",
+            "value": "67.575334428",
             "unit": "seconds"
           }
         ]

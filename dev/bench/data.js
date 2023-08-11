77bb68d4f681f7f62bf51f98928c140ef6e86cf7
github-action-benchmark <github@users.noreply.github.com>
2023-08-11 09:11:10 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for c4cfdecda2d02766873a90f627e998766f640d61

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691744730775,
+  "lastUpdate": 1691745070083,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -66495,6 +66495,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (warm, Linux)",
             "value": "1.95471106018",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "committer": {
+            "email": "marek@xivilization.net",
+            "name": "Marek Kubica",
+            "username": "Leonidas-from-XIV"
+          },
+          "distinct": true,
+          "id": "c4cfdecda2d02766873a90f627e998766f640d61",
+          "message": "fix: use proper apostrophe in text\n\nSigned-off-by: Ali Caglayan <alizter@gmail.com>",
+          "timestamp": "2023-08-11T10:45:45+02:00",
+          "tree_id": "071efa2048cd25911cf3c6d7e5ba04ef3c289064",
+          "url": "https://github.com/ocaml/dune/commit/c4cfdecda2d02766873a90f627e998766f640d61"
+        },
+        "date": 1691745069020,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (cold, Linux)",
+            "value": "67.03552096196667",
             "unit": "seconds"
           }
         ]

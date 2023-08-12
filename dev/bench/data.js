be305c0efb2a1fd83e400edadad1a823244966d6
github-action-benchmark <github@users.noreply.github.com>
2023-08-12 11:21:24 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for fe2a1f98729ab0eccbaf10edd537046af74712a3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691839273209,
+  "lastUpdate": 1691839284495,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -67220,6 +67220,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (cold, Linux)",
             "value": "66.89588225835332",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fe2a1f98729ab0eccbaf10edd537046af74712a3",
+          "message": "refactor: move fiber to vendor (#7865)\n\nbecause it's now being developed in ocaml-dune/fiber\r\n\r\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-08-12T03:55:51-07:00",
+          "tree_id": "a3958e2d55e61e1785c23eb6426611490df5cb0f",
+          "url": "https://github.com/ocaml/dune/commit/fe2a1f98729ab0eccbaf10edd537046af74712a3"
+        },
+        "date": 1691839283796,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (warm, Linux)",
+            "value": "1.8233277981333333",
             "unit": "seconds"
           }
         ]

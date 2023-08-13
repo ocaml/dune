84ec743ba629115d4137f5810a4de269e1cd4b74
github-action-benchmark <github@users.noreply.github.com>
2023-08-13 15:25:34 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for 87a449caa4f9870e5208ca37d88e04fb40da2d79

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691939908531,
+  "lastUpdate": 1691940334301,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -67626,6 +67626,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (warm, Linux)",
             "value": "1.8329442131466667",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "87a449caa4f9870e5208ca37d88e04fb40da2d79",
+          "message": "refactor: use immutable arrays in [Per_item] (#8385)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-08-13T07:53:38-07:00",
+          "tree_id": "f757dd7cbf927ecdfa66d8569365ca6c18f5bfc7",
+          "url": "https://github.com/ocaml/dune/commit/87a449caa4f9870e5208ca37d88e04fb40da2d79"
+        },
+        "date": 1691940333385,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (cold, Linux)",
+            "value": "84.50009602303334",
             "unit": "seconds"
           }
         ]

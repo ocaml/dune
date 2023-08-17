b6d9e4456626b7139b5f93aefdbd3715f464f5f9
Ali Caglayan <alizter@gmail.com>
2023-08-17 20:17:13 +0100

    test: repro rpc jobs bug (#8410)
    This demonstrates that RPC jobs events are not stopped when dune is
    interrupted and restarted.
    
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- test/expect-tests/dune_rpc_e2e/dune_rpc_e2e.mli
+++ test/expect-tests/dune_rpc_e2e/dune_rpc_e2e.mli
@@ -1,6 +1,11 @@
 open Stdune
 
-val with_dune_watch : ?env:string list -> (Pid.t -> 'a Fiber.t) -> 'a Fiber.t
+val with_dune_watch
+  :  ?watch_mode_args:string list
+  -> ?env:string list
+  -> (Pid.t -> 'a Fiber.t)
+  -> 'a Fiber.t
+
 val dune_build : Dune_rpc_client.Client.t -> string -> unit Fiber.t
 
 val run_client

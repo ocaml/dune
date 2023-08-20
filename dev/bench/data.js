dabb6db19d0f082ac62b7cb321ac69b3dbfe88de
github-action-benchmark <github@users.noreply.github.com>
2023-08-20 13:07:45 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for 5177d4ffb26ca52543c8e4140c10feb2c75fc295

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1692536533422,
+  "lastUpdate": 1692536865486,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -70758,6 +70758,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (warm, Linux)",
             "value": "1.6466337294866669",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5177d4ffb26ca52543c8e4140c10feb2c75fc295",
+          "message": "refactor: inline [Super_context.build_dir_is_vendored] (#8434)\n\nPolluting [Super_context] and isn't very useful\r\n\r\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-08-20T05:41:10-07:00",
+          "tree_id": "8922ac9cdd12fc082e17f6b29ee1ecffe8847877",
+          "url": "https://github.com/ocaml/dune/commit/5177d4ffb26ca52543c8e4140c10feb2c75fc295"
+        },
+        "date": 1692536864633,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (cold, Linux)",
+            "value": "65.64679694687334",
             "unit": "seconds"
           }
         ]

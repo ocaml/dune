09d1b3b1a26bef97675a88681b3d21a0ede68ad0
github-action-benchmark <github@users.noreply.github.com>
2023-08-20 18:26:41 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for 083c87513c7148ca2bdc105a066ec2516ebdd5fe

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1692555661882,
+  "lastUpdate": 1692556001012,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -70845,6 +70845,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (warm, Linux)",
             "value": "1.8033785485600002",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "083c87513c7148ca2bdc105a066ec2516ebdd5fe",
+          "message": "refactor: move [with_vendored_flags] (#8436)\n\nMake it live in [Ocaml_flags] instead. With two benefits:\r\n\r\n* We get to hide [with_vendored_warnings]\r\n* We remove yet another thing from [Super_context]\r\n\r\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-08-20T11:01:29-07:00",
+          "tree_id": "6a039fb815315ba91f2fc4dbbb6ab1b34d55882d",
+          "url": "https://github.com/ocaml/dune/commit/083c87513c7148ca2bdc105a066ec2516ebdd5fe"
+        },
+        "date": 1692556000429,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (cold, Linux)",
+            "value": "67.15696713444667",
             "unit": "seconds"
           }
         ]

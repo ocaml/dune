ab125e02c66158baa57b37dcac13e5a8f9d44ac3
Rudi Grinberg <me@rgrinberg.com>
2023-08-28 13:33:58 +0100

    refactor: remove digest dependency in [dune_sexp] (#8497)
    This is only used for a single conversion function that can easily be
    defined by whoever was calling this function.
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- src/dune_sexp/atom.mli
+++ src/dune_sexp/atom.mli
@@ -1,5 +1,3 @@
-open Import
-
 type t = private A of string [@@unboxed]
 
 val equal : t -> t -> bool
@@ -15,5 +13,4 @@ val of_int : int -> t
 val of_float : float -> t
 val of_bool : bool -> t
 val of_int64 : Int64.t -> t
-val of_digest : Digest.t -> t
 val to_dyn : t -> Dyn.t

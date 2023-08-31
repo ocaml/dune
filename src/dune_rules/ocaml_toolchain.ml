434d8b4f144731c81d28afdae2a3d02a75c26beb
Rudi Grinberg <me@rgrinberg.com>
2023-08-31 23:47:05 +0100

    refactor: simplify toolchain construction
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    <!-- ps-id: 338fa90e-ec56-46d1-8cc5-9bf39dd67e2a -->

--- src/dune_rules/ocaml_toolchain.ml
+++ src/dune_rules/ocaml_toolchain.ml
@@ -22,67 +22,71 @@ let make_builtins ~ocaml_config ~version =
     Meta.builtins ~stdlib_dir ~version)
 ;;
 
-let of_env_with_findlib name env findlib_config ~which =
-  let not_found ?hint prog =
-    Action.Prog.Not_found.create ?hint ~context:name ~program:prog ~loc:None ()
+let make_ocaml_config ~env ~ocamlc =
+  let+ vars =
+    Process.run_capture_lines ~display:Quiet ~env Strict ocamlc [ "-config" ]
+    |> Memo.of_reproducible_fiber
+    >>| Ocaml_config.Vars.of_lines
   in
-  let get_tool_using_findlib_config prog =
-    Memo.Option.bind findlib_config ~f:(Findlib_config.tool ~prog)
+  match
+    match vars with
+    | Error msg -> Error (Ocaml_config.Origin.Ocamlc_config, msg)
+    | Ok vars ->
+      let open Result.O in
+      let+ ocfg = Ocaml_config.make vars in
+      vars, ocfg
+  with
+  | Ok x -> x
+  | Error (Ocaml_config.Origin.Makefile_config file, msg) ->
+    User_error.raise ~loc:(Loc.in_file file) [ Pp.text msg ]
+  | Error (Ocamlc_config, msg) ->
+    User_error.raise
+      [ Pp.textf "Failed to parse the output of '%s -config':" (Path.to_string ocamlc)
+      ; Pp.text msg
+      ]
+;;
+
+let compiler t (mode : Ocaml.Mode.t) =
+  match mode with
+  | Byte -> Ok t.ocamlc
+  | Native -> t.ocamlopt
+;;
+
+let best_mode t : Mode.t =
+  match t.ocamlopt with
+  | Ok _ -> Native
+  | Error _ -> Byte
+;;
+
+let make name ~which ~env ~get_ocaml_tool =
+  let not_found ?hint program =
+    Action.Prog.Not_found.create ?hint ~context:name ~loc:None ~program ()
   in
   let* ocamlc =
-    let ocamlc = "ocamlc" in
-    get_tool_using_findlib_config ocamlc
-    >>= function
-    | Some x -> Memo.return x
-    | None ->
-      which ocamlc
-      >>| (function
-      | Some x -> x
-      | None -> not_found ocamlc |> Action.Prog.Not_found.raise)
+    let program = "ocamlc" in
+    which program
+    >>| function
+    | Some x -> x
+    | None -> not_found program |> Action.Prog.Not_found.raise
   in
   let ocaml_bin = Path.parent_exn ocamlc in
   let get_ocaml_tool prog =
-    get_tool_using_findlib_config prog
-    >>= function
-    | Some x -> Memo.return (Ok x)
+    get_ocaml_tool ~dir:ocaml_bin prog
+    >>| function
+    | Some prog -> Ok prog
     | None ->
-      Which.Best_path.memo ~dir:ocaml_bin prog
-      >>| (function
-      | Some p -> Ok p
-      | None ->
-        let hint =
-          sprintf
-            "ocamlc found in %s, but %s/%s doesn't exist (context: %s)"
-            (Path.to_string ocaml_bin)
-            (Path.to_string ocaml_bin)
-            prog
-            (Context_name.to_string name)
-        in
-        Error (not_found ~hint prog))
+      let hint =
+        sprintf
+          "ocamlc found in %s, but %s/%s doesn't exist (context: %s)"
+          (Path.to_string ocaml_bin)
+          (Path.to_string ocaml_bin)
+          prog
+          (Context_name.to_string name)
+      in
+      Error (not_found ~hint prog)
   in
-  let* ocaml_config_vars, ocaml_config =
-    let+ vars =
-      Process.run_capture_lines ~display:Quiet ~env Strict ocamlc [ "-config" ]
-      |> Memo.of_reproducible_fiber
-      >>| Ocaml_config.Vars.of_lines
-    in
-    match
-      match vars with
-      | Error msg -> Error (Ocaml_config.Origin.Ocamlc_config, msg)
-      | Ok vars ->
-        let open Result.O in
-        let+ ocfg = Ocaml_config.make vars in
-        vars, ocfg
-    with
-    | Ok x -> x
-    | Error (Ocaml_config.Origin.Makefile_config file, msg) ->
-      User_error.raise ~loc:(Loc.in_file file) [ Pp.text msg ]
-    | Error (Ocamlc_config, msg) ->
-      User_error.raise
-        [ Pp.textf "Failed to parse the output of '%s -config':" (Path.to_string ocamlc)
-        ; Pp.text msg
-        ]
-  and* ocamlopt = get_ocaml_tool "ocamlopt"
+  let* ocaml_config_vars, ocaml_config = make_ocaml_config ~env ~ocamlc in
+  let* ocamlopt = get_ocaml_tool "ocamlopt"
   and* ocaml = get_ocaml_tool "ocaml"
   and* ocamldep = get_ocaml_tool "ocamldep"
   and* ocamlmklib = get_ocaml_tool "ocamlmklib"
@@ -100,103 +104,39 @@ let of_env_with_findlib name env findlib_config ~which =
     ; ocaml_config
     ; ocaml_config_vars
     ; version
-    ; lib_config = Lib_config.create ocaml_config ~ocamlopt
     ; builtins = Memo.Lazy.force builtins
+    ; lib_config = Lib_config.create ocaml_config ~ocamlopt
     }
 ;;
 
-let compiler t (mode : Ocaml.Mode.t) =
-  match mode with
-  | Byte -> Ok t.ocamlc
-  | Native -> t.ocamlopt
-;;
-
-let best_mode t : Mode.t =
-  match t.ocamlopt with
-  | Ok _ -> Native
-  | Error _ -> Byte
+let of_env_with_findlib name env findlib_config ~which =
+  let get_tool_using_findlib_config prog =
+    Memo.Option.bind findlib_config ~f:(Findlib_config.tool ~prog)
+  in
+  let which program =
+    get_tool_using_findlib_config program
+    >>= function
+    | Some x -> Memo.return (Some x)
+    | None -> which program
+  in
+  let get_ocaml_tool ~dir prog =
+    get_tool_using_findlib_config prog
+    >>= function
+    | Some x -> Memo.return (Some x)
+    | None -> Which.best_in_dir ~dir prog
+  in
+  make name ~env ~get_ocaml_tool ~which
 ;;
 
 let of_binaries name env binaries =
-  let not_found ?hint prog =
-    Action.Prog.Not_found.create ?hint ~context:name ~program:prog ~loc:None ()
-  in
   let which =
     let map =
       Path.Set.to_list binaries
       |> Filename.Map.of_list_map_exn ~f:(fun binary -> Path.basename binary, binary)
     in
-    fun basename -> Filename.Map.find map basename
+    fun basename ->
+      Which.candidates basename |> List.find_map ~f:(Filename.Map.find map) |> Memo.return
   in
-  let ocamlc =
-    let ocamlc = "ocamlc" in
-    match which ocamlc with
-    | Some x -> x
-    | None -> not_found ocamlc |> Action.Prog.Not_found.raise
-  in
-  let ocaml_bin = Path.parent_exn ocamlc in
-  let module Best_path =
-    Which.Best_path.Make
-      (Monad.Id)
-      (struct
-        let file_exists path = Path.Set.mem binaries path
-      end)
-  in
-  let get_ocaml_tool prog =
-    match Best_path.best_path ~dir:ocaml_bin prog with
-    | Some p -> Ok p
-    | None ->
-      let hint =
-        sprintf
-          "ocamlc found in %s, but %s/%s doesn't exist (context: %s)"
-          (Path.to_string ocaml_bin)
-          (Path.to_string ocaml_bin)
-          prog
-          (Context_name.to_string name)
-      in
-      Error (not_found ~hint prog)
-  in
-  let+ ocaml_config_vars, ocaml_config =
-    let+ vars =
-      Process.run_capture_lines ~display:Quiet ~env Strict ocamlc [ "-config" ]
-      |> Memo.of_reproducible_fiber
-      >>| Ocaml_config.Vars.of_lines
-    in
-    match
-      match vars with
-      | Error msg -> Error (Ocaml_config.Origin.Ocamlc_config, msg)
-      | Ok vars ->
-        let open Result.O in
-        let+ ocfg = Ocaml_config.make vars in
-        vars, ocfg
-    with
-    | Ok x -> x
-    | Error (Ocaml_config.Origin.Makefile_config file, msg) ->
-      User_error.raise ~loc:(Loc.in_file file) [ Pp.text msg ]
-    | Error (Ocamlc_config, msg) ->
-      User_error.raise
-        [ Pp.textf "Failed to parse the output of '%s -config':" (Path.to_string ocamlc)
-        ; Pp.text msg
-        ]
-  in
-  let ocamlopt = get_ocaml_tool "ocamlopt"
-  and ocaml = get_ocaml_tool "ocaml"
-  and ocamldep = get_ocaml_tool "ocamldep"
-  and ocamlmklib = get_ocaml_tool "ocamlmklib"
-  and ocamlobjinfo = get_ocaml_tool "ocamlobjinfo" in
-  let version = Ocaml.Version.of_ocaml_config ocaml_config in
-  let builtins = make_builtins ~version ~ocaml_config in
-  { bin_dir = ocaml_bin
-  ; ocaml
-  ; ocamlc
-  ; ocamlopt
-  ; ocamldep
-  ; ocamlmklib
-  ; ocamlobjinfo
-  ; ocaml_config
-  ; ocaml_config_vars
-  ; version
-  ; builtins = Memo.Lazy.force builtins
-  ; lib_config = Lib_config.create ocaml_config ~ocamlopt
-  }
+  let get_ocaml_tool ~dir:_ prog = which prog in
+  make name ~env ~get_ocaml_tool ~which
 ;;

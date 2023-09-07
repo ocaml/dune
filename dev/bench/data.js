e10a9d2146b7e9f2e77528928aed23d6c3a6b1e8
github-action-benchmark <github@users.noreply.github.com>
2023-09-07 10:02:44 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for 0566d5c525fabd737149e98a6848c11e66ca4acc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1694080656633,
+  "lastUpdate": 1694080963852,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -28911,6 +28911,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "39.61789835133334",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "stephen@sherra.tt",
+            "name": "Stephen Sherratt",
+            "username": "gridbugs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0566d5c525fabd737149e98a6848c11e66ca4acc",
+          "message": "Github action to build monorepo bunch docker image (#8500)\n\nSigned-off-by: Stephen Sherratt <stephen@sherra.tt>",
+          "timestamp": "2023-09-07T10:38:46+01:00",
+          "tree_id": "9d33d8ae2dc9ef57f70d8c932e9bef56982c5197",
+          "url": "https://github.com/ocaml/dune/commit/0566d5c525fabd737149e98a6848c11e66ca4acc"
+        },
+        "date": 1694080962257,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "48.25420604106",
             "unit": "seconds"
           }
         ]

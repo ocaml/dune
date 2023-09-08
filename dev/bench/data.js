6befc1bc5f409b8da0d71e54c81c3d8e059964eb
github-action-benchmark <github@users.noreply.github.com>
2023-09-08 19:55:53 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for 4b3daf6245f209fad1d603f8bbe2b70a7d2dec79

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1694202520091,
+  "lastUpdate": 1694202952972,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -29085,6 +29085,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "49.05069989430001",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4b3daf6245f209fad1d603f8bbe2b70a7d2dec79",
+          "message": "refactor: remove re usage from findlib (#8612)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-09-08T20:35:27+01:00",
+          "tree_id": "3daf2eba121b3be1f7ea16dbf37704f4cd36376e",
+          "url": "https://github.com/ocaml/dune/commit/4b3daf6245f209fad1d603f8bbe2b70a7d2dec79"
+        },
+        "date": 1694202952151,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "42.072611040540004",
             "unit": "seconds"
           }
         ]

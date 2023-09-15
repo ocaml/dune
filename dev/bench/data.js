9549f22f4e63e9ac1cd4a3912b12d06db7f3b9b7
github-action-benchmark <github@users.noreply.github.com>
2023-09-15 10:37:31 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for c8f9d34c62a77d9f4e069d191090d27d0374db12

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1694773821706,
+  "lastUpdate": 1694774250919,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -29752,6 +29752,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "41.051865756779996",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c8f9d34c62a77d9f4e069d191090d27d0374db12",
+          "message": "refactor: move switch to a separate module (#8659)\n\nSo that it can be used both in [Workspace] and [Context]\r\n\r\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-09-15T11:16:48+01:00",
+          "tree_id": "8745901d78cf81af95af0f05e77bb87294c9edd4",
+          "url": "https://github.com/ocaml/dune/commit/c8f9d34c62a77d9f4e069d191090d27d0374db12"
+        },
+        "date": 1694774249840,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "40.42857473793333",
             "unit": "seconds"
           }
         ]

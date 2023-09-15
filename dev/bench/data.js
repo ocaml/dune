f19da6172bb54777be364d51edfcaa65a48dd682
github-action-benchmark <github@users.noreply.github.com>
2023-09-15 19:35:39 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for 1d4205f9b7c1878ed005746da1e52f06afc41cad

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1694806199375,
+  "lastUpdate": 1694806538876,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -74673,6 +74673,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (warm, Linux)",
             "value": "1.9136945404266668",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1d4205f9b7c1878ed005746da1e52f06afc41cad",
+          "message": "chore(ci): switch to OCaml 5.2 trunk and intro 5.1 jobs (#8666)\n\nSigned-off-by: Ali Caglayan <alizter@gmail.com>",
+          "timestamp": "2023-09-15T20:09:41+01:00",
+          "tree_id": "7a4e3de60374631b9780f2ab1b863608568973bd",
+          "url": "https://github.com/ocaml/dune/commit/1d4205f9b7c1878ed005746da1e52f06afc41cad"
+        },
+        "date": 1694806538094,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (cold, Linux)",
+            "value": "66.83664791849999",
             "unit": "seconds"
           }
         ]

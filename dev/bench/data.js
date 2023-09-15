c0c1b3c076a0a9dcfc443a475c7763416e5c9f4e
github-action-benchmark <github@users.noreply.github.com>
2023-09-15 20:13:40 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for f418dae6c37cded2ed17c307edbfe351da588c16

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1694806549550,
+  "lastUpdate": 1694808820234,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -96221,6 +96221,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "22.85032263046",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f418dae6c37cded2ed17c307edbfe351da588c16",
+          "message": "fix: add original error to spurious error message (#8646)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-09-15T21:00:18+01:00",
+          "tree_id": "4e73a1548db6f7584e28e5e55b184ae8aacf23a2",
+          "url": "https://github.com/ocaml/dune/commit/f418dae6c37cded2ed17c307edbfe351da588c16"
+        },
+        "date": 1694808807417,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "23.769637304266666",
             "unit": "seconds"
           }
         ]

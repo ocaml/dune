b42ac897957905264cad5ed1e29b6e9e3434a906
github-action-benchmark <github@users.noreply.github.com>
2023-09-18 13:30:08 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for f2c760c74af1608c768adb8eb4e9ef5f07ed9b45

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695042385285,
+  "lastUpdate": 1695043808638,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -97613,6 +97613,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "23.43519610690667",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f2c760c74af1608c768adb8eb4e9ef5f07ed9b45",
+          "message": "fix(pkg): add support for %{arch} (#8678)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-09-18T14:13:38+01:00",
+          "tree_id": "3a28ee31c98cee0fd1efee78c291cbaadca905dc",
+          "url": "https://github.com/ocaml/dune/commit/f2c760c74af1608c768adb8eb4e9ef5f07ed9b45"
+        },
+        "date": 1695043792160,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "27.977932147380002",
             "unit": "seconds"
           }
         ]

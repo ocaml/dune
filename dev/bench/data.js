0b40710955e2bf89af75b67e4ff2c74b98d90711
github-action-benchmark <github@users.noreply.github.com>
2023-09-19 15:03:08 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for 977a943858ae1a32e3cbd0e5708952371cccf229

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695135148021,
+  "lastUpdate": 1695135788206,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -99701,6 +99701,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "27.169691530146668",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "977a943858ae1a32e3cbd0e5708952371cccf229",
+          "message": "test(pkg): test env updates in opam-package-with-setenv (#8702)\n\nSigned-off-by: Ali Caglayan <alizter@gmail.com>",
+          "timestamp": "2023-09-19T15:49:05+01:00",
+          "tree_id": "8688f67392abfb15d46c6d14c32651b8e37a9319",
+          "url": "https://github.com/ocaml/dune/commit/977a943858ae1a32e3cbd0e5708952371cccf229"
+        },
+        "date": 1695135774463,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "23.414047440399997",
             "unit": "seconds"
           }
         ]

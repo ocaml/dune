721e81bd94a39a87f9bbbd4f9f4bd209e6e0975e
github-action-benchmark <github@users.noreply.github.com>
2023-09-19 11:21:34 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for 4299604a9f117e33e988ae7a4668c54dd5183700

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695122487360,
+  "lastUpdate": 1695122494022,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -77051,6 +77051,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (cold, Linux)",
             "value": "67.22185260900666",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4299604a9f117e33e988ae7a4668c54dd5183700",
+          "message": "test(pkg): remove printenv (#8700)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-09-19T11:53:58+01:00",
+          "tree_id": "adc9a9c5e9a0501fba263bf25fda97b2476bf8cf",
+          "url": "https://github.com/ocaml/dune/commit/4299604a9f117e33e988ae7a4668c54dd5183700"
+        },
+        "date": 1695122493056,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (warm, Linux)",
+            "value": "2.032436456673333",
             "unit": "seconds"
           }
         ]

60aeed1ec2d4ce719c700b84c1ddad2e51508ddd
Rudi Grinberg <me@rgrinberg.com>
2023-09-22 19:05:28 +0100

    feature(memo): introduce variables (#8732)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- test/expect-tests/memo/memoize_tests.ml
+++ test/expect-tests/memo/memoize_tests.ml
@@ -2086,3 +2086,39 @@ let%expect_test "loss of concurrency" =
     start b
     finish b |}]
 ;;
+
+let%expect_test "variables - print new" =
+  let var = Memo.Var.create ~name:"foo" 200 in
+  run (Memo.Var.read var) |> printfn "var: %d";
+  [%expect {| var: 200 |}]
+;;
+
+let%expect_test "variables - invalidation" =
+  let var = Memo.Var.create ~name:"foo" 200 in
+  let run () = run (Memo.Var.read var) |> printfn "var: %d" in
+  run ();
+  [%expect {| var: 200 |}];
+  let invalidation = Memo.Var.set var 400 in
+  Memo.Invalidation.details_hum invalidation |> String.concat ~sep:"\n" |> print_endline;
+  Memo.reset invalidation;
+  run ();
+  [%expect {|
+    Variable foo changed
+    var: 400 |}]
+;;
+
+let%expect_test "variables - cutoff" =
+  let var = Memo.Var.create ~name:"foo" ~cutoff:(fun x y -> x mod 2 = y mod 2) 200 in
+  let node = Memo.Var.read var |> Memo.map ~f:Fun.id in
+  let set_invalidate_print_run value =
+    let invalidation = Memo.Var.set var value in
+    Memo.reset invalidation;
+    node |> run |> printfn "var: %d"
+  in
+  set_invalidate_print_run 202;
+  [%expect {| var: 200 |}];
+  set_invalidate_print_run 203;
+  [%expect {| var: 203 |}];
+  set_invalidate_print_run 202;
+  [%expect {| var: 202 |}]
+;;

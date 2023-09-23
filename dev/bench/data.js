eece43dbffaacdaae5a877c01dac80a0e82431d8
github-action-benchmark <github@users.noreply.github.com>
2023-09-23 08:57:56 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for 597e6b929008217466ed9a0dbecf48b7a2473cdd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695459046483,
+  "lastUpdate": 1695459476743,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -31318,6 +31318,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "41.69857500566667",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "597e6b929008217466ed9a0dbecf48b7a2473cdd",
+          "message": "refactor: remove [Artifacts.Bin.t] (#8741)\n\nThis intermediate module is useless now\r\n\r\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-09-23T09:37:55+01:00",
+          "tree_id": "064a0b9b197f6b3a05c4ec9a534ad890dd1da090",
+          "url": "https://github.com/ocaml/dune/commit/597e6b929008217466ed9a0dbecf48b7a2473cdd"
+        },
+        "date": 1695459475411,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "38.00301025845334",
             "unit": "seconds"
           }
         ]

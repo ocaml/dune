63b4bb975cad44e6bc4598f21e77b7632063beea
seanbreckenridge <seanbrecke@gmail.com>
2023-09-23 11:06:46 -0700

    fix: replace obsolete wait3 with wait4 (#8720)
    * fix: replace obsolete wait3 with wait4
    
    https://www.gnu.org/software/libc/manual/html_node/BSD-Wait-Functions.html
    
    `wait4` is available on android unlike `wait3`.

--- otherlibs/stdune/src/proc.ml
+++ otherlibs/stdune/src/proc.ml
@@ -46,16 +46,26 @@ module Process_info = struct
     }
 end
 
-external stub_wait3
-  :  Unix.wait_flag list
+external stub_wait4
+  :  int
+  -> Unix.wait_flag list
   -> int * Unix.process_status * float * Resource_usage.t
-  = "dune_wait3"
+  = "dune_wait4"
 
-let wait flags =
+type wait =
+  | Any
+  | Pid of Pid.t
+
+let wait wait flags =
   if Sys.win32
-  then Code_error.raise "wait3 not available on windows" []
+  then Code_error.raise "wait4 not available on windows" []
   else (
-    let pid, status, end_time, resource_usage = stub_wait3 flags in
+    let pid =
+      match wait with
+      | Any -> -1
+      | Pid pid -> Pid.to_int pid
+    in
+    let pid, status, end_time, resource_usage = stub_wait4 pid flags in
     { Process_info.pid = Pid.of_int pid
     ; status
     ; end_time
--- src/dune_engine/scheduler.ml
+++ src/dune_engine/scheduler.ml
@@ -597,7 +597,7 @@ end = struct
 
   let wait_unix t =
     Mutex.unlock t.mutex;
-    let proc_info = Proc.wait [] in
+    let proc_info = Proc.wait Any [] in
     Mutex.lock t.mutex;
     Process_table.remove t proc_info
   ;;

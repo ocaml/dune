de30c6bc4ed1cb6e5d00a4a1a79badb7d939987d
github-action-benchmark <github@users.noreply.github.com>
2023-09-25 13:11:43 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for 6a7e83aad32f6ad9db7d8f994300ad17b65c4579

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695647491630,
+  "lastUpdate": 1695647503581,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -80183,6 +80183,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (cold, Linux)",
             "value": "68.38668875116001",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6a7e83aad32f6ad9db7d8f994300ad17b65c4579",
+          "message": "refactor(pkg): use parallel_map and concurrent bindings (#8753)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-09-25T13:45:42+01:00",
+          "tree_id": "5b64501a136f9b199ed901a6f27b4e50d10f01c6",
+          "url": "https://github.com/ocaml/dune/commit/6a7e83aad32f6ad9db7d8f994300ad17b65c4579"
+        },
+        "date": 1695647502790,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (warm, Linux)",
+            "value": "1.8501978029266668",
             "unit": "seconds"
           }
         ]

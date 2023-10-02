b632252afa4dc5580f8923b55f0c393a5ce742e4
github-action-benchmark <github@users.noreply.github.com>
2023-10-02 19:01:38 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for 92c36677fdf76d0f3e3f37814bab2c105f626cf1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1696272202384,
+  "lastUpdate": 1696273298785,
   "repoUrl": "https://github.com/ocaml/dune",
   "entries": {
     "Melange Benchmark": [
@@ -108633,6 +108633,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "21.378599736686667",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "92c36677fdf76d0f3e3f37814bab2c105f626cf1",
+          "message": "refactor(pkg): move solve step into own function (#8818)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-10-02T19:47:36+01:00",
+          "tree_id": "cc73d2414ff62613c9b46fd6bc33459509fb3d38",
+          "url": "https://github.com/ocaml/dune/commit/92c36677fdf76d0f3e3f37814bab2c105f626cf1"
+        },
+        "date": 1696273282561,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "25.285222163606672",
             "unit": "seconds"
           }
         ]

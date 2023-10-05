de07380e3528d5622ddc98ab3c4f0e9897005f46
Hugo Heuzard <hugo.heuzard@gmail.com>
2023-10-05 12:19:08 +0200

    Parallel inline_tests: expose bug with empty partition list
    Signed-off-by: Hugo Heuzard <hugo.heuzard@gmail.com>

--- test/blackbox-tests/test-cases/inline_tests/parallel.t/run.t
+++ test/blackbox-tests/test-cases/inline_tests/parallel.t/run.t
@@ -27,3 +27,20 @@ See that we indeed have 3 partitions
   p1
   p2
   p3
+
+
+  $ dune build --display short @test3/runtest
+        ocamlc test3/.test_lib3.inline-tests/.test_lib3.inline-tests.eobjs/byte/dune__exe__Inline_test_runner_test_lib3.{cmi,cmo,cmt}
+      ocamlopt test3/.test_lib3.inline-tests/.test_lib3.inline-tests.eobjs/native/dune__exe__Inline_test_runner_test_lib3.{cmx,o}
+      ocamlopt test3/.test_lib3.inline-tests/inline_test_runner_test_lib3.exe
+  inline_test_runner_test_lib3 test3/.test_lib3.inline-tests/partitions-best
+  File "test3/dune", line 4, characters 1-40:
+  4 |  (inline_tests (backend fake_backend_2)))
+       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  Error: Rule dependencies are configured to require sandboxing, but the rule
+  has no actions that could potentially require sandboxing.
+  [1]
+
+See that we have no partition.
+
+  $ cat _build/default/test3/.test_lib3.inline-tests/partitions-best
--- test/blackbox-tests/test-cases/inline_tests/parallel.t/test2/test_lib2.ml
+++ test/blackbox-tests/test-cases/inline_tests/parallel.t/test2/test_lib2.ml
@@ -1,19 +1,23 @@
 let () =
-  Fake_backend_runner.register ~libname:"test_lib" ~partition:"p1" "first test"
+  Fake_backend_runner.register ~libname:"test_lib2" ~partition:"p1" "first test"
     (fun () -> assert true)
 
 let () =
-  Fake_backend_runner.register ~libname:"test_lib" ~partition:"p1" "second test"
+  Fake_backend_runner.register ~libname:"test_lib2" ~partition:"p1" "second test"
     (fun () -> ()    )
 
 let () =
-  Fake_backend_runner.register ~libname:"test_lib" ~partition:"p2" "first test"
+  Fake_backend_runner.register ~libname:"test_lib2" ~partition:"p2" "first test"
     (fun () -> ())
 
 let () =
-  Fake_backend_runner.register ~libname:"test_lib" ~partition:"p3" "first test"
+  Fake_backend_runner.register ~libname:"test_lib2" ~partition:"p3" "first test"
     (fun () -> failwith "This failure is expected")
 
 let () =
   Fake_backend_runner.register ~libname:"other_lib" ~partition:"p1"
+    "second test" (fun () -> assert false)
+
+let () =
+  Fake_backend_runner.register ~libname:"other_lib" ~partition:"pn"
     "second test" (fun () -> assert false)

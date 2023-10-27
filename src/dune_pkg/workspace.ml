ce9afedebc1af6617a56c55d9d405d18ca5ec204
Marek Kubica <marek@tarides.com>
2023-10-27 16:11:17 +0200

    fix(pkg) Update default git repo to the git version of opam-repository
    We don't support the tarball anymore, but forgot to update the
    `:standard` repository.
    
    Reported by @Alizter
    
    Signed-off-by: Marek Kubica <marek@tarides.com>

--- src/dune_pkg/workspace.ml
+++ src/dune_pkg/workspace.ml
@@ -34,7 +34,10 @@ module Repository = struct
 
   let equal { name; source } t = Name.equal name t.name && String.equal source t.source
   let hash { name; source } = Tuple.T2.hash Name.hash String.hash (name, source)
-  let default = { name = "default"; source = "https://opam.ocaml.org/index.tar.gz" }
+
+  let default =
+    { name = "default"; source = "git+https://github.com/ocaml/opam-repository.git" }
+  ;;
 
   let decode =
     let open Decoder in

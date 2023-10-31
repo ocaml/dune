13ff294c012a63229e0151ea20a0e4203bcda5d1
Rudi Grinberg <me@rgrinberg.com>
2023-10-30 18:14:40 -0600

    do not do some much maintenance upfront
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- otherlibs/stdune/src/appendable_list.ml
+++ otherlibs/stdune/src/appendable_list.ml
@@ -2,7 +2,7 @@ type 'a t =
   | Empty
   | Singleton of 'a
   | Cons of 'a * 'a t
-  | List of 'a * 'a * 'a list
+  | List of 'a list
   | Append of 'a t * 'a t
   | Concat of 'a t list
 
@@ -13,17 +13,11 @@ let ( @ ) a b =
   match a, b with
   | Empty, _ -> b
   | _, Empty -> a
-  | Singleton a, Singleton b -> List (a, b, [])
   | Singleton a, _ -> Cons (a, b)
   | _, _ -> Append (a, b)
 ;;
 
-let cons x xs =
-  match xs with
-  | Empty -> Singleton x
-  | List (y, z, rest) -> List (x, y, z :: rest)
-  | _ -> Cons (x, xs)
-;;
+let cons x xs = Cons (x, xs)
 
 let to_list_rev =
   let rec loop acc stack =
@@ -34,7 +28,7 @@ let to_list_rev =
        | Empty -> loop acc stack
        | Singleton x -> loop (x :: acc) stack
        | Cons (x, xs) -> loop (x :: acc) (xs :: stack)
-       | List (x, y, xs) -> loop (List.rev_append xs (y :: x :: acc)) stack
+       | List xs -> loop (List.rev_append xs acc) stack
        | Append (xs, ys) -> loop acc (xs :: ys :: stack)
        | Concat [] -> loop acc stack
        | Concat (x :: xs) -> loop acc (x :: Concat xs :: stack))
@@ -44,24 +38,16 @@ let to_list_rev =
 
 let to_list xs = List.rev (to_list_rev xs)
 
-let is_empty = function
-  | Empty -> true
-  | _ -> false
+let rec is_empty = function
+  | List (_ :: _) | Singleton _ | Cons _ -> false
+  | Append (x, y) -> is_empty x && is_empty y
+  | Concat xs -> is_empty_list xs
+  | List [] | Empty -> true
+
+and is_empty_list = function
+  | [] -> true
+  | x :: xs -> is_empty x && is_empty_list xs
 ;;
 
-let rec concat = function
-  | [] -> Empty
-  | [ x ] -> if is_empty x then Empty else x
-  | x :: xs as list -> if is_empty x then concat xs else Concat list
-;;
-
-let of_list = function
-  | [] -> Empty
-  | [ x ] -> Singleton x
-  | x :: y :: xs -> List (x, y, xs)
-;;
-
-let rec of_list_concat = function
-  | [] -> Empty
-  | x :: xs as list -> if is_empty x then of_list_concat xs else Concat list
-;;
+let concat list = Concat list
+let of_list x = List x

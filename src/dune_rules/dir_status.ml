6a594538a008724d4548b62ed4dff5228e2482d5
Rudi Grinberg <me@rgrinberg.com>
2023-11-03 18:11:56 -0600

    refactor: dir_status cleanups (#9081)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- src/dune_rules/dir_status.ml
+++ src/dune_rules/dir_status.ml
@@ -1,4 +1,5 @@
 open Import
+open Memo.O
 open Dune_file
 
 module T = struct
@@ -73,12 +74,36 @@ module rec DB : sig
   val get : dir:Path.Build.t -> t Memo.t
 end = struct
   open DB
-  open Memo.O
 
   let enclosing_group ~dir =
     match Path.Build.parent dir with
     | None -> Memo.return No_group
     | Some parent_dir -> get ~dir:parent_dir >>| current_group parent_dir
+  ;;
+
+  let has_dune_file ~dir st_dir ~build_dir_is_project_root (d : Dune_file.t) =
+    match get_include_subdirs d.stanzas with
+    | Some (loc, Include mode) -> Memo.return (T.Group_root (st_dir, (loc, mode), d))
+    | Some (_, No) -> Memo.return (Standalone (st_dir, d))
+    | None ->
+      if build_dir_is_project_root
+      then Memo.return (Standalone (st_dir, d))
+      else
+        enclosing_group ~dir
+        >>= (function
+        | No_group -> Memo.return @@ Standalone (st_dir, d)
+        | Group_root group_root ->
+          let+ () =
+            match find_module_stanza d.stanzas with
+            | None -> Memo.return ()
+            | Some loc ->
+              get ~dir:group_root
+              >>| (function
+              | Group_root (_, (_, qualification), _) ->
+                error_no_module_consumer ~loc qualification
+              | _ -> Code_error.raise "impossible as we looked up a group root" [])
+          in
+          Is_component_of_a_group_but_not_the_root { stanzas = Some d; group_root })
   ;;
 
   let get_impl dir =
@@ -87,11 +112,11 @@ end = struct
      | Some dir -> Source_tree.find_dir dir)
     >>= function
     | None ->
-      let+ enclosing_group = enclosing_group ~dir in
-      (match enclosing_group with
-       | No_group -> Generated
-       | Group_root group_root ->
-         Is_component_of_a_group_but_not_the_root { stanzas = None; group_root })
+      enclosing_group ~dir
+      >>| (function
+      | No_group -> Generated
+      | Group_root group_root ->
+        Is_component_of_a_group_but_not_the_root { stanzas = None; group_root })
     | Some st_dir ->
       let build_dir_is_project_root =
         let project_root = Source_tree.Dir.project st_dir |> Dune_project.root in
@@ -99,38 +124,16 @@ end = struct
       in
       Only_packages.stanzas_in_dir dir
       >>= (function
+      | Some d -> has_dune_file ~dir st_dir ~build_dir_is_project_root d
       | None ->
         if build_dir_is_project_root
         then Memo.return (Source_only st_dir)
         else
-          let+ enclosing_group = enclosing_group ~dir in
-          (match enclosing_group with
-           | No_group -> Source_only st_dir
-           | Group_root group_root ->
-             Is_component_of_a_group_but_not_the_root { stanzas = None; group_root })
-      | Some d ->
-        (match get_include_subdirs d.stanzas with
-         | Some (loc, Include mode) -> Memo.return (T.Group_root (st_dir, (loc, mode), d))
-         | Some (_, No) -> Memo.return (Standalone (st_dir, d))
-         | None ->
-           if build_dir_is_project_root
-           then Memo.return (Standalone (st_dir, d))
-           else
-             let* enclosing_group = enclosing_group ~dir in
-             (match enclosing_group with
-              | No_group -> Memo.return @@ Standalone (st_dir, d)
-              | Group_root group_root ->
-                let+ () =
-                  match find_module_stanza d.stanzas with
-                  | None -> Memo.return ()
-                  | Some loc ->
-                    let+ group = get ~dir:group_root in
-                    (match group with
-                     | Group_root (_, (_, qualification), _) ->
-                       error_no_module_consumer ~loc qualification
-                     | _ -> Code_error.raise "impossible as we looked up a group root" [])
-                in
-                Is_component_of_a_group_but_not_the_root { stanzas = Some d; group_root })))
+          enclosing_group ~dir
+          >>| (function
+          | No_group -> Source_only st_dir
+          | Group_root group_root ->
+            Is_component_of_a_group_but_not_the_root { stanzas = None; group_root }))
   ;;
 
   let get =

9cd2757adf94f681ffcf83650576794e089c14c3
Rudi Grinberg <me@rgrinberg.com>
2023-11-06 00:49:05 -0600

    refactor(pkg): split pkg commands into modules (#9093)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

---
+++ bin/pkg/pkg.ml
@@ -0,0 +1,14 @@
+open Import
+
+let info =
+  let doc = "Experimental package management" in
+  let man =
+    [ `S "DESCRIPTION"
+    ; `P {|Commands for doing package management with dune|}
+    ; `Blocks Common.help_secs
+    ]
+  in
+  Cmd.info "pkg" ~doc ~man
+;;
+
+let group = Cmd.group info [ Lock.command; Print_solver_env.command; Outdated.command ]

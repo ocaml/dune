5f8e642095a5bd55056a4ffed74c975b9254cdd3
Rudi Grinberg <me@rgrinberg.com>
2023-11-14 21:35:52 -0600

    refactor: improve substitution in files
    * Allow threading a monad through substitution
    * Do not assume that we're substituting via a map
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    <!-- ps-id: 7b96f787-87a7-4543-b5c5-ae5405283d27 -->

--- src/dune_pkg/substs.mli
+++ src/dune_pkg/substs.mli
@@ -22,9 +22,21 @@ module Var : sig
   val to_dyn : t -> Dyn.t
 end
 
-val subst
-  :  OpamVariable.variable_contents Var.Map.t
-  -> Package_name.t
-  -> src:Path.t
-  -> dst:Path.Build.t
-  -> unit
+module Make (Monad : sig
+    type 'a t
+
+    module O : sig
+      val ( let+ ) : 'a t -> ('a -> 'b) -> 'b t
+    end
+
+    module List : sig
+      val map : 'a list -> f:('a -> 'b t) -> 'b list t
+    end
+  end) : sig
+  val subst
+    :  (Var.t -> OpamVariable.variable_contents option Monad.t)
+    -> Package_name.t
+    -> src:Path.t
+    -> dst:Path.Build.t
+    -> unit Monad.t
+end

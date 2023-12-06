fcae9a845aacf88c72bb6e124c1f7b6bea2ae536
Rudi Grinberg <me@rgrinberg.com>
2023-12-05 22:22:54 -0600

    refactor(pkg): simplify formula checking (#9386)
    * Make it explicit that we stop on the first success
    * Remove additional processing for errors
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- src/dune_pkg/resolve_opam_formula.ml
+++ src/dune_pkg/resolve_opam_formula.ml
@@ -104,58 +104,58 @@ type unsatisfied_formula =
   [ `Formula_could_not_be_satisfied of Unsatisfied_formula_hint.t list ]
 
 let formula_to_package_names version_by_package_name opam_formula =
-  let dnf =
+  let check_conjunction conjunction =
+    Result.List.map conjunction ~f:(fun (opam_package_name, version_constraint_opt) ->
+      let package_name = Package_name.of_opam_package_name opam_package_name in
+      match Package_name.Map.find version_by_package_name package_name with
+      | None ->
+        (* this package wasn't part of the solution so the current
+           conjunction can't be satisfied *)
+        Error (Unsatisfied_formula_hint.Missing_package package_name)
+      | Some version ->
+        (match version_constraint_opt with
+         | None ->
+           (* no constraint so any version will satisfy *)
+           Ok ()
+         | Some (constraint_ : OpamFormula.version_constraint) ->
+           let opam_version = Package_version.to_opam_package_version version in
+           let version_formula = OpamFormula.Atom constraint_ in
+           if OpamFormula.check_version_formula version_formula opam_version
+           then Ok ()
+           else
+             (* CR-rgrinberg: shouldn't we accumulate all these errors? *)
+             Error
+               (Unsatisfied_formula_hint.Unsatisfied_version_constraint
+                  { package_name
+                  ; found_version = version
+                  ; version_constraint = Version_constraint.of_opam constraint_
+                  })))
+    |> Result.map ~f:(fun (_ : unit list) -> ())
+  in
+  let satisfied_or_error_hint_per_conjunction =
+    (* check if each conjunction can be satisfied and if they can't, produce
+       a hint indicating why it wasn't satisfied *)
+    let rec loop errors = function
+      | [] ->
+        (match errors with
+         | [] -> Ok []
+         | _ :: _ -> Error (List.rev errors))
+      | conjunction :: xs ->
+        (match check_conjunction conjunction with
+         | Ok () -> Ok conjunction
+         | Error e -> loop (e :: errors) xs)
+    in
     (* a list of conjunctions where each conjunction represents on set of
        packages with version constrainst that completely satisfies the
        dependency formula *)
-    OpamFormula.to_dnf opam_formula
+    OpamFormula.to_dnf opam_formula |> loop []
   in
-  if List.is_empty dnf
-  then (* there are no dependencies so the formula is automatically satisfied *)
-    Ok []
-  else (
-    let satisfied_or_error_hint_per_conjunction =
-      (* check if each conjunction can be satisfied and if they can't, produce a hint indicating why it wasn't satisfied *)
-      List.map dnf ~f:(fun conjunction ->
-        List.map conjunction ~f:(fun (opam_package_name, version_constraint_opt) ->
-          let package_name = Package_name.of_opam_package_name opam_package_name in
-          match Package_name.Map.find version_by_package_name package_name with
-          | None ->
-            (* this package wasn't part of the solution so the current
-               conjunction can't be satisfied *)
-            Error (Unsatisfied_formula_hint.Missing_package package_name)
-          | Some version ->
-            (match version_constraint_opt with
-             | None ->
-               (* no constraint so any version will satisfy *)
-               Ok ()
-             | Some (constraint_ : OpamFormula.version_constraint) ->
-               let opam_version = Package_version.to_opam_package_version version in
-               let version_formula = OpamFormula.Atom constraint_ in
-               if OpamFormula.check_version_formula version_formula opam_version
-               then Ok ()
-               else
-                 Error
-                   (Unsatisfied_formula_hint.Unsatisfied_version_constraint
-                      { package_name
-                      ; found_version = version
-                      ; version_constraint = Version_constraint.of_opam constraint_
-                      })))
-        |> Result.List.all
-        |> Result.map ~f:(fun (_ : unit list) -> conjunction))
-    in
-    match List.find_map satisfied_or_error_hint_per_conjunction ~f:Result.to_option with
-    | None ->
-      let hints =
-        List.filter_map satisfied_or_error_hint_per_conjunction ~f:(function
-          | Error hint -> Some hint
-          | Ok _ -> None)
-      in
-      Error (`Formula_could_not_be_satisfied hints)
-    | Some satisfied_conjunction ->
-      Ok
-        (List.map satisfied_conjunction ~f:(fun (opam_package_name, _) ->
-           Package_name.of_opam_package_name opam_package_name)))
+  match satisfied_or_error_hint_per_conjunction with
+  | Error hints -> Error (`Formula_could_not_be_satisfied hints)
+  | Ok satisfied_conjunction ->
+    Ok
+      (List.map satisfied_conjunction ~f:(fun (opam_package_name, _) ->
+         Package_name.of_opam_package_name opam_package_name))
 ;;
 
 let filtered_formula_to_package_names

0afc32068985b2a6b05837453b93b96b7512182d
Ambre Austen Suhamy <ambre.suhamy@pm.me>
2023-12-20 15:34:18 +0100

    Added check on package names in dependencies (#9472)
    Signed-off-by: Ambre Austen Suhamy <ambre@tarides.com>
    Co-authored-by: Marek Kubica <marek@tarides.com>

--- src/dune_lang/package_dependency.ml
+++ src/dune_lang/package_dependency.ml
@@ -5,6 +5,34 @@ type t =
   ; constraint_ : Package_constraint.t option
   }
 
+module Well_formed_name = struct
+  module T = struct
+    include Package_name.Opam_compatible
+
+    let module_ = "Package.Dependency.Name"
+    let description = "package dependency"
+    let description_of_valid_string = Some description_of_valid_string
+
+    let better_hint s =
+      match String.lsplit2 s ~on:'.' with
+      | None -> make_valid s
+      | Some (before, after) ->
+        if Option.is_some (of_string_opt before) && not (String.is_empty after)
+        then sprintf "(%s (= %s))" before after
+        else make_valid s
+    ;;
+
+    let hint_valid = Some better_hint
+  end
+
+  module TT = Dune_util.Stringlike.Make (T)
+
+  let decode =
+    let open Dune_sexp.Decoder in
+    TT.decode >>| T.to_package_name
+  ;;
+end
+
 let encode { name; constraint_ } =
   let open Dune_sexp.Encoder in
   match constraint_ with
@@ -20,7 +48,7 @@ let decode =
     { name; constraint_ = Some expr }
   in
   enter constrained
-  <|> let+ name = Package_name.decode in
+  <|> let+ name = Well_formed_name.decode in
       { name; constraint_ = None }
 ;;
 

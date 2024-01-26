5802eae42adba5c909670a6d77dd3c331b1f1ead
Javier Chávarri <javier.chavarri@gmail.com>
2024-01-26 17:05:30 +0000

    replace sources bool with variant
    Signed-off-by: Javier Chávarri <javier.chavarri@gmail.com>

--- src/dune_rules/stanzas/copy_files.ml
+++ src/dune_rules/stanzas/copy_files.ml
@@ -1,13 +1,17 @@
 open Import
 open Dune_lang.Decoder
 
+type origin =
+  | Build
+  | Source
+
 type t =
   { add_line_directive : bool
   ; alias : Alias.Name.t option
   ; mode : Rule.Mode.t
   ; enabled_if : Blang.t
   ; files : String_with_vars.t
-  ; sources : bool
+  ; origin : origin
   ; syntax_version : Dune_lang.Syntax.Version.t
   }
 
@@ -25,7 +29,12 @@ let long_form =
   and+ files = field "files" (check >>> String_with_vars.decode)
   and+ sources = field_b "sources" ~check:(Dune_lang.Syntax.since Stanza.syntax (3, 14))
   and+ syntax_version = Dune_lang.Syntax.get_exn Stanza.syntax in
-  { add_line_directive = false; alias; mode; enabled_if; files; sources; syntax_version }
+  let origin =
+    match sources with
+    | true -> Source
+    | false -> Build
+  in
+  { add_line_directive = false; alias; mode; enabled_if; files; origin; syntax_version }
 ;;
 
 let decode =
@@ -40,7 +49,7 @@ let decode =
     ; mode = Standard
     ; enabled_if = Blang.true_
     ; files
-    ; sources = false
+    ; origin = Build
     ; syntax_version
     }
 ;;
--- src/dune_rules/stanzas/copy_files.mli
+++ src/dune_rules/stanzas/copy_files.mli
@@ -1,12 +1,16 @@
 open Import
 
+type origin =
+  | Build
+  | Source
+
 type t =
   { add_line_directive : bool
   ; alias : Alias.Name.t option
   ; mode : Rule.Mode.t
   ; enabled_if : Blang.t
   ; files : String_with_vars.t
-  ; sources : bool
+  ; origin : origin
   ; syntax_version : Dune_lang.Syntax.Version.t
   }
 

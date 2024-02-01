2bcf2f3ceb5c3c89cfb5c7b1ae724830ab0ff0ce
Etienne Millon <me@emillon.org>
2024-02-01 17:11:46 +0100

    use a map
    Signed-off-by: Etienne Millon <me@emillon.org>

--- src/install/paths.ml
+++ src/install/paths.ml
@@ -1,53 +1,9 @@
 open Import
 
-type 'path t =
-  { lib : 'path
-  ; lib_root : 'path
-  ; libexec : 'path
-  ; libexec_root : 'path
-  ; bin : 'path
-  ; sbin : 'path
-  ; toplevel : 'path
-  ; share : 'path
-  ; share_root : 'path
-  ; etc : 'path
-  ; doc : 'path
-  ; stublibs : 'path
-  ; man : 'path
-  }
+(** Invariant: map contains [Section.all] except [Misc] *)
+type 'path t = 'path Section.Map.t
 
-let map
-  { lib
-  ; lib_root
-  ; libexec
-  ; libexec_root
-  ; bin
-  ; sbin
-  ; toplevel
-  ; share
-  ; share_root
-  ; etc
-  ; doc
-  ; stublibs
-  ; man
-  }
-  ~f
-  =
-  { lib = f lib
-  ; lib_root = f lib_root
-  ; libexec = f libexec
-  ; libexec_root = f libexec_root
-  ; bin = f bin
-  ; sbin = f sbin
-  ; toplevel = f toplevel
-  ; share = f share
-  ; share_root = f share_root
-  ; etc = f etc
-  ; doc = f doc
-  ; stublibs = f stublibs
-  ; man = f man
-  }
-;;
+let map t ~f = Section.Map.map t ~f
 
 let get_field ~relative ~package ~(roots : _ Roots.t) (section : Section.t) =
   let package = Package_name.to_string package in
@@ -65,43 +21,19 @@ let get_field ~relative ~package ~(roots : _ Roots.t) (section : Section.t) =
   | Doc -> relative roots.doc_root package
   | Stublibs -> relative roots.lib_root "stublibs"
   | Man -> roots.man
-  | Misc -> Code_error.raise "Install.Paths.get" []
+  | Misc -> Code_error.raise "Install.Paths.get_field" []
 ;;
 
-let make ~relative ~package ~(roots : _ Roots.t) =
+let make ~relative ~package ~roots =
   let f = get_field ~relative ~package ~roots in
-  { lib_root = f Lib_root
-  ; libexec_root = f Libexec_root
-  ; share_root = f Share_root
-  ; bin = f Bin
-  ; sbin = f Sbin
-  ; man = f Man
-  ; toplevel = f Toplevel
-  ; stublibs = f Stublibs
-  ; lib = f Lib
-  ; libexec = f Libexec
-  ; share = f Share
-  ; etc = f Etc
-  ; doc = f Doc
-  }
+  let mapped_sections = Section.Set.remove Section.all Misc in
+  Section.Set.to_map mapped_sections ~f
 ;;
 
-let get t (section : Section.t) =
-  match section with
-  | Lib -> t.lib
-  | Lib_root -> t.lib_root
-  | Libexec -> t.libexec
-  | Libexec_root -> t.libexec_root
-  | Bin -> t.bin
-  | Sbin -> t.sbin
-  | Toplevel -> t.toplevel
-  | Share -> t.share
-  | Share_root -> t.share_root
-  | Etc -> t.etc
-  | Doc -> t.doc
-  | Stublibs -> t.stublibs
-  | Man -> t.man
-  | Misc -> Code_error.raise "Install.Paths.get" []
+let get t section =
+  match Section.Map.find t section with
+  | Some path -> path
+  | None -> Code_error.raise "Install.Paths.get" []
 ;;
 
 let get_local_location context section package_name =

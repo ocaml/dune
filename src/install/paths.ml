71888c0a6abb25094ee21765b98573fdd2841e28
Etienne Millon <me@emillon.org>
2024-02-01 16:29:42 +0100

    Revert "refactor: do not build a record in Install.Paths"
    This reverts commit a710d18fd5adf5e57dbc21100b277589bde0cc29.

--- src/install/paths.ml
+++ src/install/paths.ml
@@ -1,55 +1,87 @@
 open Import
 
-module Root_field = struct
-  type t =
-    | Lib_root
-    | Libexec_root
-    | Share_root
-    | Etc_root
-    | Doc_root
-
-  let get (roots : _ Roots.t) = function
-    | Lib_root -> roots.lib_root
-    | Libexec_root -> roots.libexec_root
-    | Share_root -> roots.share_root
-    | Etc_root -> roots.etc_root
-    | Doc_root -> roots.doc_root
-  ;;
-end
-
 type 'path t =
-  { package : Package_name.t
-  ; roots : 'path Roots.t
-  ; subdir : Root_field.t -> string -> 'path
+  { lib : 'path
+  ; lib_root : 'path
+  ; libexec : 'path
+  ; libexec_root : 'path
+  ; bin : 'path
+  ; sbin : 'path
+  ; toplevel : 'path
+  ; share : 'path
+  ; share_root : 'path
+  ; etc : 'path
+  ; doc : 'path
+  ; stublibs : 'path
+  ; man : 'path
   }
 
-let map { roots; package; subdir } ~f =
-  let roots = Roots.map ~f roots in
-  let subdir field s = f (subdir field s) in
-  { roots; package; subdir }
+let map
+  { lib
+  ; lib_root
+  ; libexec
+  ; libexec_root
+  ; bin
+  ; sbin
+  ; toplevel
+  ; share
+  ; share_root
+  ; etc
+  ; doc
+  ; stublibs
+  ; man
+  }
+  ~f
+  =
+  { lib = f lib
+  ; lib_root = f lib_root
+  ; libexec = f libexec
+  ; libexec_root = f libexec_root
+  ; bin = f bin
+  ; sbin = f sbin
+  ; toplevel = f toplevel
+  ; share = f share
+  ; share_root = f share_root
+  ; etc = f etc
+  ; doc = f doc
+  ; stublibs = f stublibs
+  ; man = f man
+  }
 ;;
 
-let make ~relative ~package ~roots =
-  let subdir field s = relative (Root_field.get roots field) s in
-  { package; roots; subdir }
-;;
-
-let get { roots; subdir; package } (section : Section.t) =
+let make ~relative ~package ~(roots : _ Roots.t) =
   let package = Package_name.to_string package in
+  { lib_root = roots.lib_root
+  ; libexec_root = roots.libexec_root
+  ; share_root = roots.share_root
+  ; bin = roots.bin
+  ; sbin = roots.sbin
+  ; man = roots.man
+  ; toplevel = relative roots.lib_root "toplevel"
+  ; stublibs = relative roots.lib_root "stublibs"
+  ; lib = relative roots.lib_root package
+  ; libexec = relative roots.libexec_root package
+  ; share = relative roots.share_root package
+  ; etc = relative roots.etc_root package
+  ; doc = relative roots.doc_root package
+  }
+;;
+
+let get t (section : Section.t) =
   match section with
-  | Lib -> subdir Lib_root package
-  | Lib_root -> roots.lib_root
-  | Libexec -> subdir Libexec_root package
-  | Libexec_root -> roots.libexec_root
-  | Bin -> roots.bin
-  | Sbin -> roots.sbin
-  | Toplevel -> subdir Lib_root "toplevel"
-  | Share -> subdir Share_root package
-  | Share_root -> roots.share_root
-  | Etc -> subdir Etc_root package
-  | Doc -> subdir Doc_root package
-  | Stublibs -> subdir Lib_root "stublibs"
-  | Man -> roots.man
+  | Lib -> t.lib
+  | Lib_root -> t.lib_root
+  | Libexec -> t.libexec
+  | Libexec_root -> t.libexec_root
+  | Bin -> t.bin
+  | Sbin -> t.sbin
+  | Toplevel -> t.toplevel
+  | Share -> t.share
+  | Share_root -> t.share_root
+  | Etc -> t.etc
+  | Doc -> t.doc
+  | Stublibs -> t.stublibs
+  | Man -> t.man
   | Misc -> Code_error.raise "Install.Paths.get" []
 ;;
 

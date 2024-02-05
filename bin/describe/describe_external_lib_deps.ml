ocaml/dune

commit 9105a43b719cc66aefd7c7f84816bd1a77f236c7

refactor: introduce simpler env stanza db (#8447)
This allows us to define env related values outside of a single module with two concrete benefits:

* No module dependency cycles since not everything has to go through Env_node
* Very specific options live closer to where they are used

In addition, we take the opportunity to fix the issue of setting formatting options in the workspace file.

Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

src/dune_rules/env_stanza_db.ml
@@ -1,0 +1,99 @@
+open Import
+open Memo.O
+
+module Node = struct
+  type t =
+    { value : Dune_env.t
+    ; parent : t option Memo.t
+    }
+
+  let by_context dir =
+    let open Memo.O in
+    let+ context = Context.DB.by_dir dir in
+    let { Context.Env_nodes.context; workspace } = Context.env_nodes context in
+    match context, workspace with
+    | None, None -> None
+    | Some value, None | None, Some value -> Some { value; parent = Memo.return None }
+    | Some context, Some workspace ->
+      Some
+        { value = context
+        ; parent = Memo.return (Some { value = workspace; parent = Memo.return None })
+        }
+  ;;
+
+  let in_dir ~dir =
+    Only_packages.stanzas_in_dir dir
+    >>| function
+    | None -> None
+    | Some stanzas ->
+      List.find_map stanzas.stanzas ~f:(function
+        | Dune_env.T config -> Some config
+        | _ -> None)
+  ;;
+
+  let rec by_dir dir =
+    let parent =
+      let* scope = Scope.DB.find_by_dir dir in
+      if Path.Build.equal dir (Scope.root scope)
+      then by_context dir
+      else (
+        match Path.Build.parent dir with
+        | None -> by_context dir
+        | Some parent -> by_dir parent)
+    in
+    in_dir ~dir
+    >>= function
+    | Some value -> Memo.return (Some { value; parent })
+    | None -> parent
+  ;;
+end
+
+let value ~default ~f =
+  let rec loop = function
+    | None -> Memo.return default
+    | Some { Node.value; parent } ->
+      let* next =
+        f value
+        >>| function
+        | Some x -> `Ok x
+        | None -> `Parent
+      in
+      (match next with
+       | `Ok x -> Memo.return x
+       | `Parent -> parent >>= loop)
+  in
+  fun ~dir -> Node.by_dir dir >>= loop
+;;
+
+let profile ~dir =
+  let name, _ = Path.Build.extract_build_context_exn dir in
+  let context = Context_name.of_string name in
+  Per_context.profile context
+;;
+
+let value ~default ~dir ~f =
+  let profile = lazy (profile ~dir) in
+  value ~default ~dir ~f:(fun stanza ->
+    let* profile = Lazy.force profile in
+    match Dune_env.find_opt stanza ~profile with
+    | None -> Memo.return None
+    | Some stanza -> f stanza)
+;;
+
+let bin_annot ~dir =
+  value ~default:true ~dir ~f:(fun (t : Dune_env.config) -> Memo.return t.bin_annot)
+;;
+
+let inline_tests ~dir =
+  value ~default:None ~dir ~f:(fun (t : Dune_env.config) ->
+    Memo.return
+    @@
+    match t.inline_tests with
+    | None -> None
+    | Some s -> Some (Some s))
+  >>= function
+  | Some s -> Memo.return s
+  | None ->
+    let+ profile = profile ~dir in
+    if Profile.is_inline_test profile then Dune_env.Inline_tests.Enabled else Disabled
+;;

src/dune_rules/odoc.ml
@@ -217,14 +217,27 @@ end = struct
   let odoc_input t = t
 end
 
-let odoc_base_flags sctx quiet build_dir =
+module Flags = struct
+  type warnings = Dune_env.Odoc.warnings =
+    | Fatal
+    | Nonfatal
+
+  type t = { warnings : warnings }
+
+  let default = { warnings = Nonfatal }
+
+  let get ~dir =
+    Env_stanza_db.value ~default ~dir ~f:(fun config ->
+      match config.odoc.warnings with
+      | None -> Memo.return None
+      | Some warnings -> Memo.return (Some { warnings }))
+    |> Action_builder.of_memo
+  ;;
+end
+
+let odoc_base_flags quiet build_dir =
   let open Action_builder.O in
-  let+ conf =
-    let* env_node =
-      Action_builder.of_memo @@ Super_context.env_node sctx ~dir:build_dir
-    in
-    Env_node.odoc env_node
-  in
+  let+ conf = Flags.get ~dir:build_dir in
   match conf.warnings with
   | Fatal ->
     (* if quiet has been passed, we're running odoc on an external
@@ -245,7 +258,7 @@ let run_odoc sctx ~dir command ~quiet ~flags_for args =
     let* () = Action_builder.return () in
     match flags_for with
     | None -> Action_builder.return Command.Args.empty
-    | Some path -> odoc_base_flags sctx quiet path
+    | Some path -> odoc_base_flags quiet path
   in
   let deps = Action_builder.env_var "ODOC_SYNTAX" in
   let open Action_builder.With_targets.O in

src/dune_rules/compilation_context.ml
@@ -180,7 +180,7 @@ let create
   and+ bin_annot =
     match bin_annot with
     | Some b -> Memo.return b
-    | None -> Super_context.bin_annot super_context ~dir:(Obj_dir.dir obj_dir)
+    | None -> Env_stanza_db.bin_annot ~dir:(Obj_dir.dir obj_dir)
   in
   { super_context
   ; scope

src/dune_rules/env_node.ml
@@ -1,13 +1,5 @@
 open Import
 
-module Odoc = struct
-  type warnings = Dune_env.Odoc.warnings =
-    | Fatal
-    | Nonfatal
-
-  type t = { warnings : warnings }
-end
-
 type t =
   { scope : Scope.t
   ; local_binaries : File_binding.Expanded.t list Memo.Lazy.t
@@ -16,13 +8,9 @@ type t =
   ; link_flags : Link_flags.t Memo.Lazy.t
   ; external_env : Env.t Memo.Lazy.t
   ; artifacts : Artifacts.t Memo.Lazy.t
-  ; inline_tests : Dune_env.Inline_tests.t Memo.Lazy.t
   ; menhir_flags : string list Action_builder.t Memo.Lazy.t
-  ; odoc : Odoc.t Action_builder.t Memo.Lazy.t
   ; js_of_ocaml : string list Action_builder.t Js_of_ocaml.Env.t Memo.Lazy.t
   ; coq_flags : Coq_flags.t Action_builder.t Memo.Lazy.t
-  ; format_config : Format_config.t Memo.Lazy.t
-  ; bin_annot : bool Memo.Lazy.t
   }
 
 let scope t = t.scope
@@ -32,18 +20,9 @@ let foreign_flags t = t.foreign_flags
 let link_flags t = Memo.Lazy.force t.link_flags
 let external_env t = Memo.Lazy.force t.external_env
 let artifacts t = Memo.Lazy.force t.artifacts
-let inline_tests t = Memo.Lazy.force t.inline_tests
 let js_of_ocaml t = Memo.Lazy.force t.js_of_ocaml
 let menhir_flags t = Memo.Lazy.force t.menhir_flags |> Action_builder.of_memo_join
-let format_config t = Memo.Lazy.force t.format_config
-
-let set_format_config t format_config =
-  { t with format_config = Memo.Lazy.of_val format_config }
-;;
-
-let odoc t = Memo.Lazy.force t.odoc |> Action_builder.of_memo_join
 let coq_flags t = Memo.Lazy.force t.coq_flags
-let bin_annot t = Memo.Lazy.force t.bin_annot
 
 let expand_str_lazy expander sw =
   match String_with_vars.text_only sw with
@@ -66,7 +45,6 @@ let make
   ~default_context_flags
   ~default_env
   ~default_artifacts
-  ~default_bin_annot
   =
   let open Memo.O in
   let config = Dune_env.find config_stanza ~profile in
@@ -76,17 +54,6 @@ let make
        | None -> Memo.return root
        | Some t -> Memo.Lazy.force t >>= field)
       >>= extend)
-  in
-  let inherited_if_absent ~field ~root ~f_absent =
-    Memo.lazy_ (fun () ->
-      match root with
-      | Some x -> Memo.return x
-      | None ->
-        (match inherit_from with
-         | None -> f_absent None
-         | Some t ->
-           let* field = Memo.Lazy.force t >>= field in
-           f_absent (Some field)))
   in
   let config_binaries = Option.value config.binaries ~default:[] in
   let local_binaries =
@@ -127,18 +94,6 @@ let make
         ~spec:config.flags
         ~default:flags
         ~eval:(Expander.expand_and_eval_set expander))
-  in
-  let inline_tests =
-    match config with
-    | { inline_tests = Some s; _ } -> Memo.Lazy.of_val s
-    | { inline_tests = None; _ } ->
-      inherited
-        ~field:inline_tests
-        Memo.return
-        ~root:
-          (if Profile.is_inline_test profile
-           then Dune_env.Inline_tests.Enabled
-           else Disabled)
   in
   let js_of_ocaml =
     inherited
@@ -195,22 +150,6 @@ let make
           let+ expander = Memo.Lazy.force expander in
           Expander.expand_and_eval_set expander menhir_flags ~standard:flags)
   in
-  let odoc =
-    let open Odoc in
-    let root =
-      (* DUNE4: Enable for dev profile in the future *)
-      Action_builder.return { warnings = Nonfatal }
-    in
-    inherited
-      ~field:(fun t -> Memo.return (odoc t))
-      ~root
-      (fun warnings ->
-        Memo.return
-        @@
-        let open Action_builder.O in
-        let+ { warnings } = warnings in
-        { warnings = Option.value config.odoc.warnings ~default:warnings })
-  in
   let coq_flags : Coq_flags.t Action_builder.t Memo.Lazy.t =
     inherited
       ~field:coq_flags
@@ -231,21 +170,6 @@ let make
          in
          { Coq_flags.coq_flags; coqdoc_flags })
   in
-  let format_config =
-    inherited_if_absent
-      ~field:format_config
-      ~root:config.format_config
-      ~f_absent:(function
-      | Some x -> Memo.return x
-      | None ->
-        Code_error.raise
-          "format config should always have a default value taken from the project root"
-          [])
-  in
-  let bin_annot =
-    inherited ~field:bin_annot ~root:default_bin_annot (fun default ->
-      Memo.return (Option.value ~default config.bin_annot))
-  in
   { scope
   ; ocaml_flags
   ; foreign_flags
@@ -253,12 +177,8 @@ let make
   ; external_env
   ; artifacts
   ; local_binaries
-  ; inline_tests
   ; js_of_ocaml
   ; menhir_flags
-  ; odoc
   ; coq_flags
-  ; format_config
-  ; bin_annot
   }
 ;;

src/dune_rules/env_node.mli
@@ -2,14 +2,6 @@
 
 open Import
 
-module Odoc : sig
-  type warnings = Dune_env.Odoc.warnings =
-    | Fatal
-    | Nonfatal
-
-  type t = { warnings : warnings }
-end
-
 type t
 
 val make
@@ -24,13 +16,11 @@ val make
   -> default_context_flags:string list Action_builder.t Foreign_language.Dict.t
   -> default_env:Env.t
   -> default_artifacts:Artifacts.t
-  -> default_bin_annot:bool
   -> t
 
 val scope : t -> Scope.t
 val external_env : t -> Env.t Memo.t
 val ocaml_flags : t -> Ocaml_flags.t Memo.t
-val inline_tests : t -> Dune_env.Inline_tests.t Memo.t
 val js_of_ocaml : t -> string list Action_builder.t Js_of_ocaml.Env.t Memo.t
 val foreign_flags : t -> string list Action_builder.t Foreign_language.Dict.t
 val link_flags : t -> Link_flags.t Memo.t
@@ -40,9 +30,5 @@ val link_flags : t -> Link_flags.t Memo.t
 val local_binaries : t -> File_binding.Expanded.t list Memo.t
 
 val artifacts : t -> Artifacts.t Memo.t
-val odoc : t -> Odoc.t Action_builder.t
 val coq_flags : t -> Coq_flags.t Action_builder.t Memo.t
 val menhir_flags : t -> string list Action_builder.t
-val format_config : t -> Format_config.t Memo.t
-val set_format_config : t -> Format_config.t -> t
-val bin_annot : t -> bool Memo.t

src/dune_rules/env_stanza_db.mli
@@ -1,0 +1,10 @@
+open Import
+
+val value
+  :  default:'a
+  -> dir:Path.Build.t
+  -> f:(Dune_env.config -> 'a option Memo.t)
+  -> 'a Memo.t
+
+val bin_annot : dir:Path.Build.t -> bool Memo.t
+val inline_tests : dir:Path.Build.t -> Dune_env.Inline_tests.t Memo.t

src/dune_rules/format_rules.ml
@@ -154,29 +154,53 @@ let gen_rules_output
   Rules.Produce.Alias.add_deps alias_formatted (Action_builder.return ())
 ;;
 
+let format_config ~dir =
+  let open Memo.O in
+  let+ value =
+    Env_stanza_db.value ~default:None ~dir ~f:(fun (t : Dune_env.config) ->
+      Memo.return
+      @@
+      match t.format_config with
+      | Some x -> Some (Some x)
+      | None -> None)
+  and+ default =
+    (* we always force the default for error checking *)
+    Path.Build.drop_build_context_exn dir
+    |> Source_tree.nearest_dir
+    >>| Source_tree.Dir.project
+    >>| Dune_project.format_config
+  in
+  Option.value value ~default
+;;
+
+let with_config ~dir f =
+  let open Memo.O in
+  let* config = format_config ~dir in
+  if Format_config.is_empty config
+  then
+    (* CR-rgrinberg: this [is_empty] check is weird. We should use [None]
+       to represent that no settings have been set. *)
+    Memo.return ()
+  else f config
+;;
+
 let gen_rules sctx ~output_dir =
   let open Memo.O in
   let dir = Path.Build.parent_exn output_dir in
-  let* config = Super_context.env_node sctx ~dir >>= Env_node.format_config in
-  Memo.when_
-    (not (Format_config.is_empty config))
-    (fun () ->
-      let* expander = Super_context.expander sctx ~dir in
-      let* scope = Scope.DB.find_by_dir output_dir in
-      let project = Scope.project scope in
-      let dialects = Dune_project.dialects project in
-      let version = Dune_project.dune_version project in
-      gen_rules_output sctx config ~version ~dialects ~expander ~output_dir)
+  with_config ~dir (fun config ->
+    let* expander = Super_context.expander sctx ~dir in
+    (* CR-rgrinberg: initializing the library database seems unnecessary here *)
+    let* scope = Scope.DB.find_by_dir output_dir in
+    let project = Scope.project scope in
+    let dialects = Dune_project.dialects project in
+    let version = Dune_project.dune_version project in
+    gen_rules_output sctx config ~version ~dialects ~expander ~output_dir)
 ;;
 
-let setup_alias sctx ~dir =
-  let open Memo.O in
-  let* config = Super_context.env_node sctx ~dir >>= Env_node.format_config in
-  Memo.when_
-    (not (Format_config.is_empty config))
-    (fun () ->
-      let output_dir = Path.Build.relative dir formatted_dir_basename in
-      let alias = Alias.fmt ~dir in
-      let alias_formatted = Alias.fmt ~dir:output_dir in
-      Rules.Produce.Alias.add_deps alias (Action_builder.dep (Dep.alias alias_formatted)))
+let setup_alias ~dir =
+  with_config ~dir (fun (_ : Format_config.t) ->
+    let output_dir = Path.Build.relative dir formatted_dir_basename in
+    let alias = Alias.fmt ~dir in
+    let alias_formatted = Alias.fmt ~dir:output_dir in
+    Rules.Produce.Alias.add_deps alias (Action_builder.dep (Dep.alias alias_formatted)))
 ;;

src/dune_rules/format_rules.mli
@@ -9,6 +9,6 @@ val gen_rules : Super_context.t -> output_dir:Path.Build.t -> unit Memo.t
 
 (** This must be called from the main directory, i.e. the ones containing the
     source files and the the [formatted_dir_basename] sub-directory. *)
-val setup_alias : Super_context.t -> dir:Path.Build.t -> unit Memo.t
+val setup_alias : dir:Path.Build.t -> unit Memo.t
 
 val formatted_dir_basename : Filename.t

commit febf36e752aa41498d49314edcc8e057fda76109

[3.13] backport #9769 (#9926)
* refactor: rename [Dune_load.conf] to [Dune_load.t] (#9766)

Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

* refactor: Make [Dune_load.t] abstract (#9767)

Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

* refactor: move [Dune_load.Dune_files.in_dir] (#9768)

It doesn't need to be in the [Dune_files] submodule

Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

* fix: performance regression from #8447 (#9769)

Get rid of the slow dune file comparison in #9738

Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

---------

Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
Co-authored-by: Rudi Grinberg <me@rgrinberg.com>

bin/describe/describe_external_lib_deps.ml
@@ -143,7 +143,8 @@ let libs db (context : Context.t) (build_system : Dune_rules.Main.build_system) 
   let { Dune_rules.Main.conf; contexts = _; _ } = build_system in
   let open Memo.O in
   let* dune_files =
-    Dune_rules.Dune_load.Dune_files.eval conf.dune_files ~context:(Context.name context)
+    Dune_rules.Dune_load.dune_files conf
+    |> Dune_rules.Dune_load.Dune_files.eval ~context:(Context.name context)
   in
   Memo.parallel_map dune_files ~f:(fun (dune_file : Dune_rules.Dune_file.t) ->
     Memo.parallel_map dune_file.stanzas ~f:(fun stanza ->

bin/describe/describe_pp.ml
@@ -56,7 +56,7 @@ let get_pped_file super_context file =
          >>| Source_tree.Dir.path
          >>| Path.source
        in
-       let* dune_file = Dune_rules.Dune_load.Dune_files.in_dir (dir |> in_build_dir) in
+       let* dune_file = Dune_rules.Only_packages.stanzas_in_dir (dir |> in_build_dir) in
        let staged_pps =
          Option.bind dune_file ~f:(fun dune_file ->
            dune_file.stanzas

bin/describe/describe_workspace.ml
@@ -527,7 +527,8 @@ module Crawl = struct
     let sctx = Context_name.Map.find_exn scontexts context_name in
     let open Memo.O in
     let* dune_files =
-      Dune_load.Dune_files.eval conf.dune_files ~context:context_name
+      Dune_load.dune_files conf
+      |> Dune_load.Dune_files.eval ~context:context_name
       >>| List.filter ~f:(dune_file_is_in_dirs dirs)
     in
     let* exes, exe_libs =
@@ -550,9 +551,9 @@ module Crawl = struct
     in
     let* project_libs =
       (* the list of libraries declared in the project *)
-      Memo.parallel_map conf.projects ~f:(fun project ->
-        let* scope = Scope.DB.find_by_project context project in
-        Scope.libs scope |> Lib.DB.all)
+      Dune_load.projects conf
+      |> Memo.parallel_map ~f:(fun project ->
+        Scope.DB.find_by_project context project >>| Scope.libs >>= Lib.DB.all)
       >>| Lib.Set.union_all
       >>| Lib.Set.filter ~f:(lib_is_in_dirs dirs)
     in

bin/install_uninstall.ml
@@ -81,9 +81,9 @@ module Workspace = struct
   let get () =
     let open Memo.O in
     Memo.run
-      (let+ conf = Dune_rules.Dune_load.load ()
+      (let+ packages = Dune_rules.Dune_load.load () >>| Dune_rules.Dune_load.packages
        and+ contexts = Context.DB.all () in
-       { packages = conf.packages; contexts })
+       { packages; contexts })
   ;;
 
   let package_install_file t ~findlib_toolchain pkg =

src/dune_rules/dune_load.ml
@@ -215,70 +215,26 @@ module Dune_files = struct
 
   type t = one list
 
-  let interpret =
-    let impl (dir, project, dune_file) =
-      let file = Source_tree.Dune_file.path dune_file in
-      let static = Source_tree.Dune_file.get_static_sexp dune_file in
-      match Source_tree.Dune_file.kind dune_file with
-      | Ocaml_script ->
-        Memo.return
-          (Script
-             { script =
-                 { dir
-                 ; project
-                 ; file =
-                     (* we can't introduce ocaml syntax with [(sudir ..)] *)
-                     Option.value_exn file
-                 }
-             ; from_parent = static
-             })
-      | Plain ->
-        let open Memo.O in
-        let+ stanzas = Dune_file.parse static ~dir ~file ~project in
-        Literal stanzas
-    in
-    let module Input = struct
-      type t = Path.Source.t * Dune_project.t * Source_tree.Dune_file.t
-
-      let equal =
-        Tuple.T3.equal Path.Source.equal Dune_project.equal Source_tree.Dune_file.equal
-      ;;
-
-      let hash = Tuple.T3.hash Path.Source.hash Dune_project.hash Poly.hash
-      let to_dyn = Dyn.opaque
-    end
-    in
-    let memo = Memo.create "Dune_files.interpret" ~input:(module Input) impl in
-    fun ~dir ~project ~(dune_file : Source_tree.Dune_file.t) ->
-      Memo.exec memo (dir, project, dune_file)
-  ;;
-
-  let in_dir dir =
-    let source_dir = Path.Build.drop_build_context_exn dir in
-    Source_tree.find_dir source_dir
-    >>= function
-    | None -> Memo.return None
-    | Some d ->
-      (match Source_tree.Dir.dune_file d with
-       | None -> Memo.return None
-       | Some dune_file ->
-         let project = Source_tree.Dir.project d in
-         interpret ~dir:source_dir ~project ~dune_file
-         >>= (function
-          | Literal dune_file -> Memo.return (Some dune_file)
-          | Script script ->
-            let context =
-              match Install.Context.of_path dir with
-              | Some c -> c
-              | None ->
-                User_error.raise
-                  [ Pp.textf
-                      "no context in directory %s"
-                      (Path.Build.to_string_maybe_quoted dir)
-                  ]
-            in
-            let+ dune_file = Script.eval_one ~context script in
-            Some dune_file))
+  let interpret ~dir project dune_file =
+    let file = Source_tree.Dune_file.path dune_file in
+    let static = Source_tree.Dune_file.get_static_sexp dune_file in
+    match Source_tree.Dune_file.kind dune_file with
+    | Ocaml_script ->
+      Memo.return
+        (Script
+           { script =
+               { dir
+               ; project
+               ; file =
+                   (* we can't introduce ocaml syntax with [(sudir ..)] *)
+                   Option.value_exn file
+               }
+           ; from_parent = static
+           })
+    | Plain ->
+      let open Memo.O in
+      let+ stanzas = Dune_file.parse static ~dir ~file ~project in
+      Literal stanzas
   ;;
 
   let eval dune_files ~context =
@@ -293,12 +249,17 @@ module Dune_files = struct
   ;;
 end
 
-type conf =
+type t =
   { dune_files : Dune_files.t
   ; packages : Package.t Package.Name.Map.t
   ; projects : Dune_project.t list
   ; projects_by_root : Dune_project.t Path.Source.Map.t
   }
+
+let dune_files t = t.dune_files
+let packages t = t.packages
+let projects t = t.projects
+let projects_by_root t = t.projects_by_root
 
 module Projects_and_dune_files =
   Monoid.Product
@@ -351,7 +312,7 @@ let load () =
   let+ dune_files =
     Appendable_list.to_list dune_files
     |> Memo.parallel_map ~f:(fun (dir, project, dune_file) ->
-      Dune_files.interpret ~dir ~project ~dune_file)
+      Dune_files.interpret ~dir project dune_file)
   in
   { dune_files
   ; packages

src/dune_rules/dune_load.mli
@@ -10,17 +10,16 @@ module Dune_files : sig
   type t
 
   val eval : t -> context:Context_name.t -> Dune_file.t list Memo.t
-  val in_dir : Path.Build.t -> Dune_file.t option Memo.t
 end
 
-type conf = private
-  { dune_files : Dune_files.t
-  ; packages : Package.t Package.Name.Map.t
-  ; projects : Dune_project.t list
-  ; projects_by_root : Dune_project.t Path.Source.Map.t
-  }
+type t
+
+val dune_files : t -> Dune_files.t
+val packages : t -> Package.t Package.Name.Map.t
+val projects : t -> Dune_project.t list
+val projects_by_root : t -> Dune_project.t Path.Source.Map.t
 
 (** Load all dune files. This function is memoized. *)
-val load : unit -> conf Memo.t
+val load : unit -> t Memo.t
 
 val find_project : dir:Path.Build.t -> Dune_project.t Memo.t

src/dune_rules/env_stanza_db.ml
@@ -130,7 +130,7 @@ module Inherit = struct
         (sprintf "%s-root" name)
         ~input:(module Path.Source)
         (fun dir ->
-          let* { Dune_load.projects_by_root; _ } = Dune_load.load ()
+          let* projects_by_root = Dune_load.load () >>| Dune_load.projects_by_root
           and* envs = Memo.Lazy.force for_context in
           let project = Path.Source.Map.find_exn projects_by_root dir in
           let root = root context project in

src/dune_rules/lib.ml
@@ -858,13 +858,14 @@ end = struct
   let projects_by_package =
     Memo.lazy_ (fun () ->
       let open Memo.O in
-      let+ { projects; _ } = Dune_load.load () in
-      List.concat_map projects ~f:(fun project ->
+      Dune_load.load ()
+      >>| Dune_load.projects
+      >>| List.concat_map ~f:(fun project ->
         Dune_project.packages project
         |> Package.Name.Map.to_list_map ~f:(fun _ (pkg : Package.t) ->
           let name = Package.name pkg in
           name, project))
-      |> Package.Name.Map.of_list_exn)
+      >>| Package.Name.Map.of_list_exn)
   ;;
 
   let instantiate_impl (db, name, info, hidden) =

src/dune_rules/main.ml
@@ -3,7 +3,7 @@ open Import
 let () = Inline_tests.linkme
 
 type build_system =
-  { conf : Dune_load.conf
+  { conf : Dune_load.t
   ; contexts : Context.t list
   ; scontexts : Super_context.t Context_name.Map.t
   }

src/dune_rules/main.mli
@@ -12,7 +12,7 @@ val init
   -> unit
 
 type build_system =
-  { conf : Dune_load.conf
+  { conf : Dune_load.t
   ; contexts : Context.t list
   ; scontexts : Super_context.t Context_name.Map.t
   }

src/dune_rules/odoc.ml
@@ -23,7 +23,7 @@ let find_project_by_key =
     Memo.create "project-by-keys" ~input:(module Input) make_map
   in
   fun key ->
-    let* { projects; _ } = Dune_load.load () in
+    let* projects = Dune_load.load () >>| Dune_load.projects in
     let+ map = Memo.exec memo projects in
     Dune_project.File_key.Map.find_exn map key
 ;;

src/dune_rules/odoc_new.ml
@@ -485,7 +485,7 @@ module Valid = struct
   ;;
 
   let get ctx ~all =
-    let* { projects; _ } = Dune_load.load () in
+    let* projects = Dune_load.load () >>| Dune_load.projects in
     Memo.exec valid_libs_and_packages (ctx, all, projects)
   ;;
 

src/dune_rules/only_packages.ml
@@ -31,7 +31,7 @@ let conf =
     match Clflags.t () with
     | No_restriction -> Memo.return None
     | Restrict { names; command_line_option } ->
-      let* { packages; _ } = Dune_load.load () in
+      let* packages = Dune_load.load () >>| Dune_load.packages in
       Package.Name.Set.iter names ~f:(fun pkg_name ->
         if not (Package.Name.Map.mem packages pkg_name)
         then (
@@ -100,30 +100,42 @@ let filter_out_stanzas_from_hidden_packages ~visible_pkgs =
       | _ -> None))
 ;;
 
+type filtered_stanzas =
+  { all : Dune_file.t list
+  ; map : Dune_file.t Path.Source.Map.t
+  }
+
 let filtered_stanzas =
   let db =
     Per_context.create_by_name ~name:"filtered_stanzas"
     @@ fun context ->
-    let* only_packages = Memo.Lazy.force conf
-    and+ { Dune_load.dune_files; packages = _; projects = _; projects_by_root = _ } =
-      Dune_load.load ()
-    in
-    let+ stanzas = Dune_load.Dune_files.eval ~context dune_files in
-    match only_packages with
-    | None -> stanzas
-    | Some visible_pkgs ->
-      List.map stanzas ~f:(fun (dir_conf : Dune_file.t) ->
-        { dir_conf with
-          stanzas = filter_out_stanzas_from_hidden_packages ~visible_pkgs dir_conf.stanzas
-        })
+    Memo.lazy_ (fun () ->
+      let+ only_packages = Memo.Lazy.force conf
+      and+ stanzas =
+        Dune_load.load () >>| Dune_load.dune_files >>= Dune_load.Dune_files.eval ~context
+      in
+      let all =
+        match only_packages with
+        | None -> stanzas
+        | Some visible_pkgs ->
+          List.map stanzas ~f:(fun (dune_file : Dune_file.t) ->
+            { dune_file with
+              stanzas =
+                filter_out_stanzas_from_hidden_packages ~visible_pkgs dune_file.stanzas
+            })
+      in
+      let map =
+        Path.Source.Map.of_list_map_exn all ~f:(fun (dune_file : Dune_file.t) ->
+          dune_file.dir, dune_file)
+      in
+      { all; map })
+    |> Memo.Lazy.force
   in
   fun ctx -> Staged.unstage db ctx
 ;;
 
 let get () =
-  let* { Dune_load.dune_files = _; packages; projects = _; projects_by_root = _ } =
-    Dune_load.load ()
-  in
+  let* packages = Dune_load.load () >>| Dune_load.packages in
   let+ only_packages = Memo.Lazy.force conf in
   Option.value only_packages ~default:packages
 ;;
@@ -131,17 +143,16 @@ let get () =
 let stanzas_in_dir dir =
   if Path.Build.is_root dir
   then Memo.return None
-  else
-    Dune_load.Dune_files.in_dir dir
-    >>= function
+  else (
+    match Install.Context.of_path dir with
     | None -> Memo.return None
-    | Some dune_file ->
-      let+ stanzas =
-        Memo.Lazy.force conf
-        >>| function
-        | None -> dune_file.stanzas
-        | Some visible_pkgs ->
-          filter_out_stanzas_from_hidden_packages ~visible_pkgs dune_file.stanzas
-      in
-      Some { dune_file with stanzas }
+    | Some ctx ->
+      let+ filtered_stanzas = filtered_stanzas ctx in
+      let dir = Path.Build.drop_build_context_exn dir in
+      Path.Source.Map.find filtered_stanzas.map dir)
+;;
+
+let filtered_stanzas ctx =
+  let+ filtered_stanzas = filtered_stanzas ctx in
+  filtered_stanzas.all
 ;;

src/dune_rules/scope.ml
@@ -336,7 +336,7 @@ module DB = struct
       let scopes =
         Memo.Lazy.create
         @@ fun () ->
-        let* { Dune_load.projects_by_root; _ } = Dune_load.load () in
+        let* projects_by_root = Dune_load.load () >>| Dune_load.projects_by_root in
         let* stanzas = Only_packages.filtered_stanzas (Context.name context) in
         create_from_stanzas ~projects_by_root ~context stanzas
       in
cb5fd367fe92ad6c4749fddd79fc0d70692b21e2
Stephen Sherratt <stephen@sherra.tt>
2024-02-16 18:49:59 +1100

    pkg: back single_run_file_cache with fiber_cache
    Signed-off-by: Stephen Sherratt <stephen@sherra.tt>

--- src/dune_pkg/single_run_file_cache.ml
+++ src/dune_pkg/single_run_file_cache.ml
@@ -1,14 +1,9 @@
 open! Import
-open! Stdune
 
-type entry =
-  { filename : Filename.t
-  ; complete : unit Fiber.Ivar.t
-  }
-
-type t =
+type 'error t =
   { temp_dir : Path.t lazy_t
-  ; key_to_filename : entry String.Table.t
+  ; key_to_filename : (string, (Path.t, 'error) result) Fiber_cache.t
+  ; mutable num_entries : int
   }
 
 let create () =
@@ -18,33 +13,24 @@ let create () =
        at_exit (fun () -> Temp.destroy Dir temp_dir);
        temp_dir)
   in
-  { temp_dir; key_to_filename = String.Table.create 1 }
+  { temp_dir; key_to_filename = Fiber_cache.create (module String); num_entries = 0 }
 ;;
 
-let with_ { temp_dir; key_to_filename } ~key ~f =
+let with_ ({ temp_dir; key_to_filename; _ } as t) ~key ~f =
   let open Fiber.O in
   let temp_dir = Lazy.force temp_dir in
-  match String.Table.find key_to_filename key with
-  | Some { filename; complete } ->
-    (* If the file is currently being created then this ivar will be empty.
-       Wait for it to become filled, indicating that the file is ready. *)
-    let+ () = Fiber.Ivar.read complete in
-    Ok (Path.relative temp_dir filename)
-  | None ->
-    let filename = string_of_int (String.Table.length key_to_filename) in
+  Fiber_cache.find_or_add key_to_filename key ~f:(fun () ->
+    let filename = string_of_int t.num_entries in
+    t.num_entries <- t.num_entries + 1;
     let output_path = Path.relative temp_dir filename in
-    let complete = Fiber.Ivar.create () in
-    String.Table.add_exn key_to_filename key { filename; complete };
-    let* result = f output_path in
-    (* Fill the ivar signaling that the file is read. *)
-    let+ () = Fiber.Ivar.fill complete () in
-    (match result with
-     | Ok () ->
-       if not (Path.exists output_path)
-       then
-         Code_error.raise
-           "Callback failed to create file"
-           [ "output", Path.to_dyn output_path; "key", Dyn.string key ];
-       Ok output_path
-     | Error _ as e -> e)
+    let+ result = f output_path in
+    match result with
+    | Ok () ->
+      if not (Path.exists output_path)
+      then
+        Code_error.raise
+          "Callback failed to create file"
+          [ "output", Path.to_dyn output_path; "key", Dyn.string key ];
+      Ok (Path.relative temp_dir filename)
+    | Error _ as e -> e)
 ;;

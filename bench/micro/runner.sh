2ab5657fc72ab65b00ee2a8022a07718bfd19ccd
Rudi Grinberg <me@rgrinberg.com>
2024-02-21 10:07:43 -0800

    bench: [Path.reach] and [Path.root] (#10074)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- bench/micro/runner.sh
+++ bench/micro/runner.sh
@@ -6,6 +6,7 @@ case "$1" in
   "memo" ) test="memo_bench"; main="memo_bench_main";;
   "thread_pool" ) test="thread_pool_bench"; main="thread_pool_bench_main";;
   "digest" ) test="digest_bench"; main="digest_bench_main";;
+  "path" ) test="path_bench"; main="path_bench_main";;
 esac
 shift;
 export BENCH_LIB="$test"

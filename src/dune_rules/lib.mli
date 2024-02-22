bfdb91e1c65d1533d75bbbcc95b353ba68c9c9e1
Rudi Grinberg <me@rgrinberg.com>
2024-02-22 00:58:21 -0800

    refactor: remove [Li.DB.{equal,hash}] (#10107)
    they aren't correct anyway
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- src/dune_rules/lib.mli
+++ src/dune_rules/lib.mli
@@ -91,8 +91,6 @@ module DB : sig
   (** A database allow to resolve library names *)
   type t = db
 
-  val equal : t -> t -> bool
-  val hash : t -> int
   val installed : Context.t -> t Memo.t
 
   module Resolve_result : sig

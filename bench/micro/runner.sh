4f825ae31caa4579d8f44ec6b7ea32580cbdc324
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-04-01 14:23:13 -0700

    chore: remove bench/micro/fiber_bench*.ml
    these are being tracked in ocaml-dune/fiber
    
    Signed-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>

--- bench/micro/runner.sh
+++ bench/micro/runner.sh
@@ -2,7 +2,6 @@
 export BENCHMARKS_RUNNER=TRUE
 case "$1" in
   "dune" ) test="dune_bench"; main="main";;
-  "fiber" ) test="fiber_bench"; main="fiber_bench_main";;
   "memo" ) test="memo_bench"; main="memo_bench_main";;
   "thread_pool" ) test="thread_pool_bench"; main="thread_pool_bench_main";;
   "digest" ) test="digest_bench"; main="digest_bench_main";;

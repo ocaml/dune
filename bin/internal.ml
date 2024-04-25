6f878dd002bb5454ddc5e71f7fb9545609dd2a81
pmwhite <philipwhite@cedarville.edu>
2024-04-25 18:29:08 -0400

    Remove action runner commands. (#10456)
    Right now this is only a useful feature for Jane Street. Removing it to
    avoid adding extra maintenance burden to external Dune. (motivated by
    
    Signed-off-by: Philip White <code@trailingwhite.space>
    Co-authored-by: Philip White <code@trailingwhite.space>

--- bin/internal.ml
+++ bin/internal.ml
@@ -9,8 +9,4 @@ let latest_lang_version =
         |> Dune_lang.Syntax.Version.to_string))
 ;;
 
-let group =
-  Cmd.group
-    (Cmd.info "internal")
-    [ Internal_dump.command; latest_lang_version; Internal_action_runner.group ]
-;;
+let group = Cmd.group (Cmd.info "internal") [ Internal_dump.command; latest_lang_version ]

215cb6bf764bfae8d193af6e0b8da4c17cb17eac
Rudi Grinberg <me@rgrinberg.com>
2024-05-06 09:10:45 -0600

    refactor: get rid of [Path.External.drop_prefix]
    Just as easily replaced by [Path.drop_prefix]. None of its uses were
    performance critical, so the reduced complexity is more important here.
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- otherlibs/stdune/src/path.ml
+++ otherlibs/stdune/src/path.ml
@@ -495,21 +495,10 @@ module External : sig
   val as_local : t -> string
   val append_local : t -> Local.t -> t
   val of_filename_relative_to_initial_cwd : string -> t
-  val drop_prefix : t -> prefix:t -> Local.t option
 end = struct
   module Table = String.Table
 
   type t = string
-
-  let drop_prefix path ~prefix =
-    if prefix = path
-    then Some Local.root
-    else (
-      let prefix = if String.is_suffix ~suffix:"/" prefix then prefix else prefix ^ "/" in
-      let open Option.O in
-      let+ suffix = String.drop_prefix path ~prefix in
-      Local.of_string suffix)
-  ;;
 
   let to_string t = t
   let equal = String.equal

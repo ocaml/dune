d43d7b5c293d711a67266257a36523127e71ec2f
Marek Kubica <marek@tarides.com>
2024-05-08 19:10:56 +0200

    fix(pkg): Create temporary directories on same FS (#10214)
    * fix(pkg): Create temporary directories on same FS
    
    Fixes #10213

--- src/dune_pkg/fetch.ml
+++ src/dune_pkg/fetch.ml
@@ -195,9 +195,13 @@ let unpack ~target ~archive =
     Pp.textf "unable to extract %S" (Path.to_string archive))
 ;;
 
-let with_download url checksum ~f =
+let with_download url checksum ~target ~f =
   let url = OpamUrl.to_string url in
-  let temp_dir = Temp.create Dir ~prefix:"dune" ~suffix:(Filename.basename url) in
+  let temp_dir =
+    let prefix = "dune" in
+    let suffix = Filename.basename url in
+    Temp_dir.dir_for_target ~target ~prefix ~suffix
+  in
   let output = Path.relative temp_dir "download" in
   Fiber.finalize ~finally:(fun () ->
     Temp.destroy Dir temp_dir;
@@ -222,10 +226,9 @@ let with_download url checksum ~f =
 ;;
 
 let fetch_curl ~unpack:unpack_flag ~checksum ~target (url : OpamUrl.t) =
-  with_download url checksum ~f:(fun output ->
+  with_download url checksum ~target ~f:(fun output ->
     match unpack_flag with
     | false ->
-      Path.mkdir_p (Path.parent_exn target);
       Path.rename output target;
       Fiber.return @@ Ok ()
     | true ->

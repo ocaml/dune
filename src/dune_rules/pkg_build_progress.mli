b4aeb628bb44f7d5ef88e3aa8f2b50981ed3e8e3
Stephen Sherratt <stephen@sherra.tt>
2024-09-05 12:22:08 +1000

    Allow printing custom build progress messages
    Expose the function for formatting build progress messages so other
    code can print messages in the same style. This will be used to print
    messages such as "Running <dev-tool>" in the same format as package
    build progress messages.
    
    Signed-off-by: Stephen Sherratt <stephen@sherra.tt>

--- src/dune_rules/pkg_build_progress.mli
+++ src/dune_rules/pkg_build_progress.mli
@@ -1,5 +1,19 @@
 open! Import
 
+(** Formats a message in the style of a progress message. The text of
+    the message will be "<verb> <object_>" with the verb colored to match
+    the verb in progress messages (e.g. "Downloading", and the message
+    will be left-padded so that the space between the verb and object_
+    lines up with the spaces in progress messages, as long as the length
+    of the verb does not exceed 12 characters.
+
+    TODO(steve): unify this with the logic for printing build progress
+    messages in dune_engine/process.ml *)
+val format_user_message
+  :  verb:string
+  -> object_:User_message.Style.t Pp.t
+  -> User_message.t
+
 (** An action which prints a progress message about a package to
     the console so users can be informed about which of their
     project's dependencies are currently being installed.

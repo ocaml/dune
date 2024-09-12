0d417899ddecf29670d470bfc81a50645e6cb986
Etienne Marais <dev@maiste.fr>
2024-09-12 17:34:05 +0200

    fix: remove unused functions
    Signed-off-by: Etienne Marais <dev@maiste.fr>

--- bin/common.ml
+++ bin/common.ml
@@ -1052,7 +1052,6 @@ type t =
   }
 
 let capture_outputs t = t.builder.capture_outputs
-let profile t = t.builder.workspace_config.profile
 let root t = t.root
 let watch t = t.builder.watch
 let x t = t.builder.workspace_config.x

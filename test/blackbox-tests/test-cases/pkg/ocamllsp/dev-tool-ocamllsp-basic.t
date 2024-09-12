32fc569576dda4425e0c77e87c98ad3bb90b35df
Stephen Sherratt <stephen@sherra.tt>
2024-09-12 14:22:35 +1000

    Add tests for ocamllsp dev tool
    Signed-off-by: Stephen Sherratt <stephen@sherra.tt>

---
+++ test/blackbox-tests/test-cases/pkg/ocamllsp/dev-tool-ocamllsp-basic.t
@@ -0,0 +1,32 @@
+Test that the "dune tools exec ocamllsp" command causes ocamllsp to be
+locked, built and run when the command is run from a dune project with
+a lockdir containing an "ocaml" lockfile.
+
+  $ . ../helpers.sh
+  $ . ./helpers.sh
+
+  $ mkrepo
+  $ make_mock_ocamllsp_package
+  $ mkpkg ocaml 5.2.0
+
+  $ setup_ocamllsp_workspace
+
+  $ cat > dune-project <<EOF
+  > (lang dune 3.16)
+  > 
+  > (package
+  >  (name foo)
+  >  (allow_empty))
+  > EOF
+
+  $ make_lockdir
+  $ cat > dune.lock/ocaml.pkg <<EOF
+  > (version 5.2.0)
+  > EOF
+
+  $ dune tools exec ocamllsp
+  Solution for dev-tools.locks/ocaml-lsp-server:
+  - ocaml.5.2.0
+  - ocaml-lsp-server.0.0.1
+       Running 'ocamllsp'
+  hello from fake ocamllsp
---
+++ test/blackbox-tests/test-cases/pkg/ocamllsp/helpers.sh
@@ -0,0 +1,27 @@
+# Create a dune-workspace file with mock repos set up for the main
+# project and the ocamllsp lockdir.
+setup_ocamllsp_workspace() {
+  cat > dune-workspace <<EOF
+(lang dune 3.16)
+(lock_dir
+ (path "dev-tools.locks/ocaml-lsp-server")
+ (repositories mock))
+ (lock_dir
+  (repositories mock))
+(repository
+ (name mock)
+ (source "file://$(pwd)/mock-opam-repository"))
+EOF
+}
+
+# Create a fake ocaml-lsp-server package containing an executable that
+# just prints a message.
+make_mock_ocamllsp_package() {
+  mkpkg ocaml-lsp-server <<EOF
+install: [
+  [ "sh" "-c" "echo '#!/bin/sh' > %{bin}%/ocamllsp" ]
+  [ "sh" "-c" "echo 'echo hello from fake ocamllsp' >> %{bin}%/ocamllsp" ]
+  [ "sh" "-c" "chmod a+x %{bin}%/ocamllsp" ]
+]
+EOF
+}

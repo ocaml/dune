51ba85638a6a8c4c40917250f92c611b1926ba22
Alpha DIALLO <moyodiallo@gmail.com>
2024-09-20 12:59:11 +0200

    refactoring
    Signed-off-by: Alpha DIALLO <moyodiallo@gmail.com>

--- src/dune_rules/enabled_if.ml
+++ src/dune_rules/enabled_if.ml
@@ -56,23 +56,23 @@ let decode_value ~allowed_vars ?(is_error = true) () =
       ~override_decode_bare_literal:None
       (String_with_vars.decode_manually (fun env var ->
          let name = Dune_lang.Template.Pform.name var in
-         match Dune_lang.Template.Pform.payload var with
-         | _ when not @@ List.exists ~f:(String.equal name) common_vars_list ->
+         if List.exists ~f:(String.equal name) common_vars_list
+         then (
+           match List.assoc allowed_vars name with
+           | None ->
+             emit_warning allowed_vars is_error var;
+             Pform.Env.parse env var
+           | Some min_ver ->
+             let current_ver = Pform.Env.syntax_version env in
+             if min_ver > current_ver
+             then (
+               let loc = Dune_lang.Template.Pform.loc var in
+               let what = Dune_lang.Template.Pform.describe var in
+               Dune_lang.Syntax.Error.since loc Stanza.syntax min_ver ~what)
+             else Pform.Env.unsafe_parse_without_checking_version env var)
+         else (
            emit_warning allowed_vars is_error var;
-           Pform.Env.parse env var
-         | _ ->
-           (match List.assoc allowed_vars name with
-            | None ->
-              emit_warning allowed_vars is_error var;
-              Pform.Env.parse env var
-            | Some min_ver ->
-              let current_ver = Pform.Env.syntax_version env in
-              if min_ver > current_ver
-              then (
-                let loc = Dune_lang.Template.Pform.loc var in
-                let what = Dune_lang.Template.Pform.describe var in
-                Dune_lang.Syntax.Error.since loc Stanza.syntax min_ver ~what)
-              else Pform.Env.unsafe_parse_without_checking_version env var)))
+           Pform.Env.parse env var)))
 ;;
 
 let decode ~allowed_vars ?is_error ~since () =

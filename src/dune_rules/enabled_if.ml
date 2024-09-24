aeed64953acc683d93f92187102bab70fbb30c62
Alpha Issiaga DIALLO <moyodiallo@gmail.com>
2024-09-24 12:06:46 +0200

    fix: enabled_if with `env` variable (#10936)
    * fix: enabled_if with `env` variable
    
    Signed-off-by: Alpha DIALLO <moyodiallo@gmail.com>

--- src/dune_rules/enabled_if.ml
+++ src/dune_rules/enabled_if.ml
@@ -19,6 +19,7 @@ let common_vars_list =
   ; "ocaml_version"
   ; "context_name"
   ; "arch_sixtyfour"
+  ; "env"
   ]
 ;;
 
@@ -29,6 +30,7 @@ let common_vars ~since =
          match var with
          | "context_name" -> var, (2, 7)
          | "arch_sixtyfour" -> var, (3, 11)
+         | "env" -> var, (3, 15)
          | _ -> var, since)
        common_vars_list)
 ;;
@@ -53,24 +55,24 @@ let decode_value ~allowed_vars ?(is_error = true) () =
     Blang.Ast.decode
       ~override_decode_bare_literal:None
       (String_with_vars.decode_manually (fun env var ->
-         match Dune_lang.Template.Pform.payload var with
-         | Some _ ->
+         let name = Dune_lang.Template.Pform.name var in
+         if List.exists ~f:(String.equal name) common_vars_list
+         then (
+           match List.assoc allowed_vars name with
+           | None ->
+             emit_warning allowed_vars is_error var;
+             Pform.Env.parse env var
+           | Some min_ver ->
+             let current_ver = Pform.Env.syntax_version env in
+             if min_ver > current_ver
+             then (
+               let loc = Dune_lang.Template.Pform.loc var in
+               let what = Dune_lang.Template.Pform.describe var in
+               Dune_lang.Syntax.Error.since loc Stanza.syntax min_ver ~what)
+             else Pform.Env.unsafe_parse_without_checking_version env var)
+         else (
            emit_warning allowed_vars is_error var;
-           Pform.Env.parse env var
-         | None ->
-           let name = Dune_lang.Template.Pform.name var in
-           (match List.assoc allowed_vars name with
-            | None ->
-              emit_warning allowed_vars is_error var;
-              Pform.Env.parse env var
-            | Some min_ver ->
-              let current_ver = Pform.Env.syntax_version env in
-              if min_ver > current_ver
-              then (
-                let loc = Dune_lang.Template.Pform.loc var in
-                let what = Dune_lang.Template.Pform.describe var in
-                Dune_lang.Syntax.Error.since loc Stanza.syntax min_ver ~what)
-              else Pform.Env.unsafe_parse_without_checking_version env var)))
+           Pform.Env.parse env var)))
 ;;
 
 let decode ~allowed_vars ?is_error ~since () =
--- test/blackbox-tests/test-cases/enabled_if/eif-install-forbidden_var.t/run.t
+++ test/blackbox-tests/test-cases/enabled_if/eif-install-forbidden_var.t/run.t
@@ -4,6 +4,6 @@ Tests for enabled_if in install stanza using forbidden variable.
   6 |  (enabled_if (= %{project_root} ""))
                       ^^^^^^^^^^^^^^^
   Error: Only architecture, system, model, os_type, ccomp_type, profile,
-  ocaml_version, context_name and arch_sixtyfour variables are allowed in this
-  'enabled_if' field. Please upgrade your dune language to at least 3.15.
+  ocaml_version, context_name, arch_sixtyfour and env variables are allowed in
+  this 'enabled_if' field. Please upgrade your dune language to at least 3.15.
   [1]

f19d5644e56456a7ef5b1bac54b229dd9c5d800a
Kate <kit.ty.kate@disroot.org>
2024-10-06 17:44:40 +0100

    Call the C++ compiler with -std=c++11 when using OCaml >= 5.0 (#10962)
    * Call the C++ compiler with -std=c++11 when using OCaml >= 5.0
    
    Signed-off-by: Etienne Marais <dev@maiste.fr>

--- src/ocaml/version.mli
+++ src/ocaml/version.mli
@@ -76,3 +76,8 @@ val has_META_files : t -> bool
 
 (** Whether the compiler supports occurrences indexation *)
 val supports_bin_annot_occurrences : t -> bool
+
+(** Whether the compiler supports the -H flag *)
+val supports_hidden_includes : t -> bool
+
+val add_std_cxx_flag : t -> bool

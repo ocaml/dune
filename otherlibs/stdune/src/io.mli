51a197111ac9f89400a49bb132739592a25d3bf9
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2024-12-12 09:13:18 +0100

    Io.copy_channels: make it reentrant
    Signed-off-by: Nicolás Ojeda Bär <n.oje.bar@gmail.com>

--- otherlibs/stdune/src/io.mli
+++ otherlibs/stdune/src/io.mli
@@ -4,9 +4,6 @@ val close_in : in_channel -> unit
 val close_out : out_channel -> unit
 val close_both : in_channel * out_channel -> unit
 val input_lines : in_channel -> string list
-
-(** This function is not safe to use from multiple threads, even if operating on
-    unrelated channels because it uses a statically-allocated global buffer. *)
 val copy_channels : in_channel -> out_channel -> unit
 
 (** Try to read everything from a channel. Returns [Error ()] if the contents

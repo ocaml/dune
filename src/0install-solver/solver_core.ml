e63ec45fbd775cd4fcfadbd96a04fd6186859180
Rudi Grinberg <me@rgrinberg.com>
2024-12-26 23:49:27 +0000

    refactor(pkg): move cache to separate module (#11244)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- src/0install-solver/solver_core.ml
+++ src/0install-solver/solver_core.ml
@@ -5,76 +5,7 @@
 (** Select a compatible set of components to run a program. *)
 
 module List = Stdune.List
-
-module type CACHE_ENTRY = sig
-  type t
-  type value
-
-  val compare : t -> t -> int
-end
-
 open Fiber.O
-
-module Cache (CacheEntry : CACHE_ENTRY) : sig
-  (** The cache is used in [build_problem], while the clauses are still being added. *)
-  type t
-
-  module M : Map.S with type key = CacheEntry.t
-
-  (** Once the problem is built, an immutable snapshot is taken. *)
-  type snapshot = CacheEntry.value M.t
-
-  val create : unit -> t
-
-  (** [lookup cache make key] will look up [key] in [cache].
-      * If not found, create it with [value, process = make key], add [value] to the cache,
-      * and then call [process ()] on it.
-      * [make] must not be recursive (since the key hasn't been added yet),
-      * but [process] can be. In other words, [make] does whatever setup *must*
-      * be done before anyone can use this cache entry, while [process] does
-      * setup that can be done afterwards. *)
-  val lookup
-    :  t
-    -> (CacheEntry.t -> (CacheEntry.value * (unit -> unit Fiber.t)) Fiber.t)
-    -> CacheEntry.t
-    -> CacheEntry.value Fiber.t
-
-  val snapshot : t -> snapshot
-  val get : CacheEntry.t -> snapshot -> CacheEntry.value option
-  val get_exn : CacheEntry.t -> snapshot -> CacheEntry.value
-  val filter_map : (CacheEntry.t -> 'a -> 'b option) -> 'a M.t -> 'b M.t
-end = struct
-  module M = Map.Make (CacheEntry)
-
-  type snapshot = CacheEntry.value M.t
-  type t = snapshot ref
-
-  let create () = ref M.empty
-
-  let lookup table make key =
-    match M.find_opt key !table with
-    | Some x -> Fiber.return x
-    | None ->
-      let* value, process = make key in
-      table := M.add key value !table;
-      let+ () = process () in
-      value
-  ;;
-
-  let snapshot table = !table
-  let get = M.find_opt
-  let get_exn = M.find
-
-  let filter_map f m =
-    M.merge
-      (fun key ao _bo ->
-        match ao with
-        | Some x -> f key x
-        | None -> assert false)
-      m
-      M.empty
-  ;;
-end
 
 module Make (Model : S.SOLVER_INPUT) = struct
   (** We attach this data to each SAT variable. *)
@@ -163,7 +94,7 @@ module Make (Model : S.SOLVER_INPUT) = struct
           vars
     end
 
-  module ImplCache = Cache (struct
+  module ImplCache = Cache.Make (struct
       include Model.Role
 
       type value = impl_candidates

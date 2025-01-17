c0f90836ffa830c9c4156d9bec9164a71ac6f6fd
Etienne Marais <dev@maiste.fr>
2025-01-17 08:52:58 +0100

    fix: apply reviews
    Signed-off-by: Etienne Marais <dev@maiste.fr>

--- src/dune_rules/pkg_toolchain.ml
+++ src/dune_rules/pkg_toolchain.ml
@@ -49,8 +49,10 @@ let is_compiler_and_toolchains_enabled name =
       (* TODO don't hardcode these names here *)
       [ Package_name.of_string "ocaml-base-compiler"
       ; Package_name.of_string "ocaml-variants"
-      ; Package_name.of_string
-          "ocaml-compiler" (* HACK: This is required for ocaml.5.3.0 *)
+      ; Package_name.of_string "ocaml-compiler"
+        (* The [ocaml-compiler] package is required to include all the
+           packages that might install a compiler, starting from ocaml.5.3.0.
+        *)
       ]
     in
     List.mem compiler_package_names name ~equal:Package_name.equal
@@ -163,25 +165,19 @@ let modify_install_action ~prefix ~suffix action =
   else modify_install_action action ~installation_prefix:prefix ~suffix
 ;;
 
-(* Create an empty config.cache file so other packages see that the
-   compiler package is installed. *)
-let touch_config_cache =
-  Dune_lang.Action.Progn
-    [ Dune_lang.Action.Run
-        [ Slang.text "touch"
-        ; Slang.concat
-            [ Slang.pform (Pform.Var (Pform.Var.Pkg Pform.Var.Pkg.Build))
-            ; Slang.text "/config.cache"
-            ]
-        ]
-    ; Dune_lang.Action.Run
-        [ Slang.text "touch"
-        ; Slang.concat
-            [ Slang.pform (Pform.Var (Pform.Var.Pkg Pform.Var.Pkg.Build))
-            ; Slang.text "/config.status"
-            ]
-        ]
+let touch file =
+  Dune_lang.Action.Run
+    [ Slang.text "touch"
+    ; Slang.concat
+        [ Slang.pform (Pform.Var (Pform.Var.Pkg Pform.Var.Pkg.Build)); Slang.text file ]
     ]
+;;
+
+(* Create an empty config.cache and config.status files so other packages see
+   that the compiler package is installed.
+   TODO: extract this from the .install *)
+let touch_compiler_install =
+  Dune_lang.Action.Progn [ touch "/config.cache"; touch "/config.status" ]
 ;;
 
 let modify_build_action ~prefix action =
@@ -190,7 +186,7 @@ let modify_build_action ~prefix action =
   then
     (* If the toolchain is already installed, just create config.cache file.
        TODO(steve): Move this check to action execution time *)
-    touch_config_cache
+    touch_compiler_install
   else action
 ;;
 

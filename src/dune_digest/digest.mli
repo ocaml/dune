44e4e4495a5d12b72c1b17ad2c7fa5fdb9f60623
Rudi Grinberg <rudi.grinberg@gmail.com>
2025-05-01 03:39:14 +0800

    refactor(dune_digest): remove some unused code (#11724)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- src/dune_digest/digest.mli
+++ src/dune_digest/digest.mli
@@ -60,13 +60,3 @@ val path_with_stats
 (** Digest a file taking the [executable] bit into account. Should not be called
     on a directory. *)
 val file_with_executable_bit : executable:bool -> Path.t -> t
-
-(** Override the implementations of digest computation. Can be used to record
-    the reverse digest map. *)
-val override_impl : file:(string -> t) -> string:(string -> t) -> unit
-
-(** [Direct_impl] does a plain hashing, with no heed to the overrides given by
-    [override_impl]. *)
-module Direct_impl : sig
-  val string : string -> t
-end

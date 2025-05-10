f42d1cefae5793fb9fc9ae3cc35b4009a89ded60
Rudi Grinberg <rudi.grinberg@gmail.com>
2025-05-10 18:59:23 +0100

    refactor(pkg): improve digest errors further (#11777)
    Add location and url or failed digest
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- src/dune_pkg/md5.ml
+++ src/dune_pkg/md5.ml
@@ -19,18 +19,9 @@ let file file =
      the file is not properly closed. [O_SHARE_DELETE] ensures that the main thread can
      delete it even if it is still open. See #8243. *)
   let file = Path.to_string file in
-  let fd =
-    match Unix.openfile file [ Unix.O_RDONLY; O_SHARE_DELETE; O_CLOEXEC ] 0 with
-    | fd -> fd
-    | exception Unix.Unix_error (error, syscall, arg) ->
-      let error = Unix_error.Detailed.create ~syscall ~arg error in
-      User_error.raise
-        [ Pp.textf "Failed to open file for md5 digest: %s" file
-        ; Unix_error.Detailed.pp error
-        ]
-    | exception exn -> reraise exn
-  in
-  Exn.protectx fd ~f:md5_fd ~finally:Unix.close
+  Result.try_with (fun () ->
+    let fd = Unix.openfile file [ Unix.O_RDONLY; O_SHARE_DELETE; O_CLOEXEC ] 0 in
+    Exn.protectx fd ~f:md5_fd ~finally:Unix.close)
 ;;
 
 let string = Stdlib.Digest.string

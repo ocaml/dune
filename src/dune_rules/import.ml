95dc808fe6c746740449fd0118af0e047165aa4f
Rudi Grinberg <me@rgrinberg.com>
2025-05-17 23:04:56 +0100

    refactor: rename Dune_file0 to Dune_file (#11816)
    We don't need the suffix since the two [Dune_file] modules live in
    different libraries
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- src/dune_rules/import.ml
+++ src/dune_rules/import.ml
@@ -6,7 +6,6 @@ module Digest = Dune_digest
 include struct
   open Source
   module Source_dir_status = Source_dir_status
-  module Dune_file0 = Dune_file0
   module Include_stanza = Include_stanza
   module Source_tree = Source_tree
   module Cram_test = Cram_test
--- src/source/source.ml
+++ src/source/source.ml
@@ -1,5 +1,5 @@
 module Source_dir_status = Source_dir_status
-module Dune_file0 = Dune_file0
+module Dune_file = Dune_file
 module Include_stanza = Include_stanza
 module Clflags = Clflags
 module Source_tree = Source_tree

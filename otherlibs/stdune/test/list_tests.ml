edb12010c81393605757aef47ceaf13c03f9d2f0
Stephen Sherratt <stephen@sherra.tt>
2025-05-22 11:59:43 +1000

    Add List.deduplicate
    Signed-off-by: Stephen Sherratt <stephen@sherra.tt>

--- otherlibs/stdune/test/list_tests.ml
+++ otherlibs/stdune/test/list_tests.ml
@@ -18,3 +18,31 @@ let%expect_test _ =
   intersperse [ "foo"; "bar"; "baz" ] ~sep:"sep";
   [%expect {| [ "foo"; "sep"; "bar"; "sep"; "baz" ] |}]
 ;;
+
+let deduplicate t = List.deduplicate t ~equal:String.equal |> list string |> print_dyn
+
+let%expect_test _ =
+  deduplicate [];
+  [%expect {| [] |}]
+;;
+
+let%expect_test _ =
+  deduplicate [ "foo" ];
+  [%expect {| [ "foo" ] |}]
+;;
+
+let%expect_test _ =
+  deduplicate [ "foo"; "foo" ];
+  [%expect {| [ "foo" ] |}]
+;;
+
+let%expect_test _ =
+  deduplicate [ "foo"; "bar"; "foo" ];
+  [%expect {| [ "foo"; "bar" ] |}]
+;;
+
+let%expect_test _ =
+  deduplicate
+    [ "foo"; "bar"; "foo"; "foo"; "foo"; "bar"; "foo"; "baz"; "foo"; "bar"; "qux" ];
+  [%expect {| [ "foo"; "bar"; "baz"; "qux" ] |}]
+;;

66f6c26f98aa337053fb77e4d1f99953e14fbc3f
Ali Caglayan <alizter@gmail.com>
2025-05-27 10:43:01 +0100

    fix: dune test dirtest.t/run.t running cram test incorrectly
    We fix an issue where a user can write
    
    ```
    dune test dirtest.t/run.t
    ```
    
    and the corresponding cram test was being run incorrectly. This was due
    to the buggy parent detection code in runtest.ml. We fix how the parent
    directory of a cram test is actually detected and do some cleanup for
    cram source related code.
    
    The runtest.ml test is updated to better reflect what we expect the
    command to do.
    
    Co-authored-by: Rudi Grinberg <me@rgrinberg.com>
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- src/dune_rules/cram/cram_rules.mli
+++ src/dune_rules/cram/cram_rules.mli
@@ -3,7 +3,11 @@
 open Import
 
 (** The type of errors that can occur when searching for cram tests *)
-type error
+type error = Missing_run_t of Cram_test.t
+
+(** [missing_run_t cram_test] raises an error message explaining that the
+    directory test [cram_test] is missing it's [run.t]. *)
+val missing_run_t : Cram_test.t -> 'a
 
 (** Memoized list of cram tests in a directory. *)
 val cram_tests : Source_tree.Dir.t -> (Cram_test.t, error) result list Memo.t

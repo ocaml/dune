94f80dfe2ba60c0eba8afa3f92e49b282878bede
Marek Kubica <marek@tarides.com>
2025-06-12 16:17:31 +0200

    Split out lock rules

--- src/dune_rules/lock_rules.ml
+++ src/dune_rules/lock_rules.ml
@@ -1,4 +1,5 @@
 open Import
+open Memo.O
 
 module Spec = struct
   type ('path, 'target) t =
@@ -38,4 +39,71 @@ let lock ~target ~lock_dir =
   |> Action.Full.make ~can_go_in_shared_cache:true
   |> Action_builder.With_targets.return
   |> Action_builder.With_targets.add_directories ~directory_targets:[ target ]
+;;
+
+module Gen_rules = Build_config.Gen_rules
+
+let lock_rule ~target lock_dir = lock ~target ~lock_dir |> Memo.return
+
+let rule ?loc { Action_builder.With_targets.build; targets } =
+  (* TODO this ignores the workspace file *)
+  Rule.make ~info:(Rule.Info.of_loc_opt loc) ~targets build |> Rules.Produce.rule
+;;
+
+let setup_lock_rules ctx_name ~lock_dir : Gen_rules.result =
+  let target =
+    let ( / ) = Path.Build.relative in
+    Private_context.t.build_dir
+    / Context_name.to_string ctx_name
+    / ".lock"
+    / lock_dir
+    / "content"
+  in
+  let gen_rules lock_dir =
+    let* lock_rule = lock_rule ~target lock_dir in
+    rule ~loc:Loc.none lock_rule
+  in
+  let rules = Rules.collect_unit (fun () -> gen_rules lock_dir) in
+  let directory_targets = Path.Build.Map.singleton target Loc.none in
+  Gen_rules.make ~directory_targets rules
+;;
+
+let setup_rules ~components ~dir ctx =
+  match components with
+  | [ ".lock" ] ->
+    Gen_rules.make
+      ~build_dir_only_sub_dirs:
+        (Gen_rules.Build_only_sub_dirs.singleton ~dir Subdir_set.all)
+      (Memo.return Rules.empty)
+    |> Memo.return
+  | [ ".lock"; lock_dir ] -> Memo.return @@ setup_lock_rules ctx ~lock_dir
+  | [] ->
+    let sub_dirs = [ ".lock" ] in
+    let build_dir_only_sub_dirs =
+      Gen_rules.Build_only_sub_dirs.singleton ~dir @@ Subdir_set.of_list sub_dirs
+    in
+    Memo.return @@ Gen_rules.make ~build_dir_only_sub_dirs (Memo.return Rules.empty)
+  | _ -> Memo.return @@ Gen_rules.rules_here Gen_rules.Rules.empty
+;;
+
+let setup_tmp_lock_alias =
+  fun ~dir ctx_name ->
+  let alias = Alias.make ~dir Alias0.pkg_lock in
+  let rule =
+    Rules.collect_unit (fun () ->
+      (* careful, need to point to a file that will be created by the rule *)
+      let path =
+        let ( / ) = Path.Build.relative in
+        (* TODO get lock dir name instead of hardcoding `dune.lock` *)
+        Private_context.t.build_dir
+        / Context_name.to_string ctx_name
+        / ".lock"
+        / "dune.lock"
+        / "content"
+      in
+      let deps = Action_builder.path (Path.build path) in
+      Rules.Produce.Alias.add_deps alias deps)
+  in
+  Gen_rules.rules_for ~dir ~allowed_subdirs:Filename.Set.empty rule
+  |> Gen_rules.rules_here
 ;;
--- src/dune_rules/lock_rules.mli
+++ src/dune_rules/lock_rules.mli
@@ -1,3 +1,11 @@
 open Import
 
-val lock : target:Path.Build.t -> lock_dir:string -> Action.Full.t With_targets.t
+(* val lock : target:Path.Build.t -> lock_dir:string -> Action.Full.t With_targets.t *)
+
+val setup_rules
+  :  components:string list
+  -> dir:Path.Build.t (* -> Super_context.t Memo.t *)
+  -> Context_name.t
+  -> Build_config.Gen_rules.t Memo.t
+
+val setup_tmp_lock_alias : dir:Path.Build.t -> Context_name.t -> Build_config.Gen_rules.t

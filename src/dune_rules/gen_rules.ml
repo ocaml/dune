384a296bf3603c38f92a8f7bc9b0c1cf6daf4e27
Marek Kubica <marek@tarides.com>
2025-06-13 15:15:22 +0200

    Attempt to register dependency on project

--- src/dune_rules/gen_rules.ml
+++ src/dune_rules/gen_rules.ml
@@ -662,7 +662,7 @@ let private_context ~dir components _ctx =
     Gen_rules.make ~build_dir_only_sub_dirs (Memo.return Rules.empty)
 ;;
 
-let raise_on_lock_dir_out_of_sync =
+let _raise_on_lock_dir_out_of_sync =
   Per_context.create_by_name ~name:"check-lock-dir" (fun ctx ->
     Memo.lazy_ (fun () ->
       let* lock_dir_available = Lock_dir.lock_dir_active ctx in
@@ -719,10 +719,11 @@ let gen_rules ctx ~dir components =
   then private_context ~dir components ctx
   else if Context_name.equal ctx Fetch_rules.context.name
   then Fetch_rules.gen_rules ~dir ~components
-  else
-    let* () = raise_on_lock_dir_out_of_sync ctx in
+  else (
+    (* TODO enable when autolocking is not enabled *)
+    (* let* () = raise_on_lock_dir_out_of_sync ctx in *)
     let gen_pkg_alias_rule = Pkg_rules.setup_pkg_install_alias ~dir ctx in
     let gen_lock_rule = Lock_rules.setup_tmp_lock_alias ~dir ctx in
     let+ sctx_rules = gen_rules ctx (Super_context.find_exn ctx) ~dir components in
-    Gen_rules.combine (Gen_rules.combine sctx_rules gen_pkg_alias_rule) gen_lock_rule
+    Gen_rules.combine (Gen_rules.combine sctx_rules gen_pkg_alias_rule) gen_lock_rule)
 ;;

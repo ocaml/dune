bff22ad4d6e0c11ac730528706cfd1ef9be9a14a
Rudi Grinberg <me@rgrinberg.com>
2025-06-13 22:51:26 +0100

    refactor: remove some unused imports from dune-rpc
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    <!-- ps-id: bea2074f-9f71-4fc3-9e23-973b0ecc1efb -->

--- otherlibs/dune-rpc/private/import.ml
+++ otherlibs/dune-rpc/private/import.ml
@@ -3,7 +3,6 @@ include struct
   module Sexp = Sexp
   module String = String
   module List = List
-  module Either = Either
   module Int = Int
   module Poly = Poly
   module Code_error = Code_error
@@ -12,8 +11,6 @@ include struct
   module Result = Result
   module Option = Option
   module Table = Table
-  module Set = Set
-  module Io = Io
   module Loc = Loc
   module Fdecl = Fdecl
   module Univ_map = Univ_map

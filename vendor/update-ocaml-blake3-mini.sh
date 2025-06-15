d7fc7a2fcd57497b110cb120199203b497271897
Ali Caglayan <alizter@gmail.com>
2025-06-15 15:30:29 +0200

    modify vendor of blake3 to omit c intrinsics sources
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- vendor/update-ocaml-blake3-mini.sh
+++ vendor/update-ocaml-blake3-mini.sh
@@ -24,6 +24,10 @@ src=$TMP/$pkg
 (
     cp -v $src/src/*.{ml,mli,c} $pkg/
     cp -v $src/vendor/*.{ml,mli,c,h,S,asm} $pkg/
+    rm $pkg/blake3_avx2.c
+    rm $pkg/blake3_avx512.c
+    rm $pkg/blake3_sse2.c
+    rm $pkg/blake3_sse41.c
 ) || true
 
 git checkout $pkg/dune

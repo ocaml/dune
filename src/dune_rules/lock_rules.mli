6bd038d8a6615e961fd427531e943e05ee571ffb
Marek Kubica <marek@tarides.com>
2025-06-17 16:16:56 +0200

    Construct directory from argument passed by rules

--- src/dune_rules/lock_rules.mli
+++ src/dune_rules/lock_rules.mli
@@ -3,7 +3,6 @@ open Import
 val setup_rules
   :  components:string list
   -> dir:Path.Build.t
-  -> Context_name.t
   -> Build_config.Gen_rules.t Memo.t
 
 val setup_tmp_lock_alias : dir:Path.Build.t -> Context_name.t -> Build_config.Gen_rules.t

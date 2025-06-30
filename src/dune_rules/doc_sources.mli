01a4d507a14fe5f63885757b7a454dee2735d03b
Paul-Elliot <peada@free.fr>
2025-06-30 14:01:14 +0200

    Add docs for doc_sources
    Signed-off-by: Paul-Elliot <peada@free.fr>

--- src/dune_rules/doc_sources.mli
+++ src/dune_rules/doc_sources.mli
@@ -1,10 +1,12 @@
 open Import
 
 type mld =
-  { path : Path.Build.t
-  ; in_doc : Path.Local.t
+  { path : Path.Build.t (** The path to the mld/asset file *)
+  ; in_doc : Path.Local.t (** Where in the doc hierarchy should be the mld/asset file *)
   }
 
+(** Builds a map of [mld]s from the [(documentation ...)] stanza, compiling and
+    merging entries from [(mld_files ...)] and [(files ...)] *)
 val build_mlds_map
   :  Dune_file.t
   -> dir:Path.Build.t

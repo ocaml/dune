4785bb2d993a65d11be5ce301af20eefa040cb97
Marek Kubica <marek@tarides.com>
2025-07-04 12:11:20 +0200

    Allow writing lock dir into arbitrary paths

--- test/expect-tests/dune_pkg/dune_pkg_unit_tests.ml
+++ test/expect-tests/dune_pkg/dune_pkg_unit_tests.ml
@@ -67,7 +67,11 @@ end
 let lock_dir_encode_decode_round_trip_test ?commit ~lock_dir_path ~lock_dir () =
   let lock_dir_path = Path.Source.of_string lock_dir_path in
   Lock_dir.Write_disk.(
-    prepare ~portable_lock_dir:false ~lock_dir_path ~files:Package_name.Map.empty lock_dir
+    prepare
+      ~portable_lock_dir:false
+      ~lock_dir_path:(Path.source lock_dir_path)
+      ~files:Package_name.Map.empty
+      lock_dir
     |> commit);
   let lock_dir_round_tripped =
     try Lock_dir.read_disk_exn lock_dir_path with

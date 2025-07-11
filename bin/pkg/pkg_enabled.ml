888c5759c4794999f716d43ecd92b3a6908e12eb
Stephen Sherratt <stephen@sherra.tt>
2025-07-11 12:23:14 +1000

    pkg: command to check if project uses package management (#11997)
    Some users have been using the presence of a lockdir to determine
    whether a project indicates that package management should be used, for
    example when a text editor checks whether ocamllsp should be invoked via
    dune (as a dev tool) or from an opam switch. This is brittle, as dune
    may change the way projects indicate they intend to use package
    management in the future.
    
    This change introduces a new command "dune pkg enabled" which
    communicates via its exit status whether dune detects that a project
    intends to use package management (a status of 0 indicates package
    management is enabled). If/when dune changes how a project should
    indicate that it should use package management, the implementation of
    this command will be changed to match.
    
    Signed-off-by: Stephen Sherratt <stephen@sherra.tt>

---
+++ bin/pkg/pkg_enabled.ml
@@ -0,0 +1,31 @@
+open Import
+
+let term =
+  let+ builder = Common.Builder.term in
+  let common, config = Common.init builder in
+  Scheduler.go_with_rpc_server ~common ~config (fun () ->
+    Memo.run
+    @@
+    let open Memo.O in
+    let+ workspace = Workspace.workspace () in
+    let lock_dir_paths =
+      Pkg_common.Lock_dirs_arg.lock_dirs_of_workspace
+        Pkg_common.Lock_dirs_arg.all
+        workspace
+    in
+    let any_lockdir_exists =
+      List.exists lock_dir_paths ~f:(fun lock_dir_path ->
+        Path.exists (Path.source lock_dir_path))
+    in
+    if any_lockdir_exists then () else exit 1)
+;;
+
+let info =
+  let doc =
+    "Check if the project indicates that dune's package management features should be \
+     enabled. Exits with 0 if package management is enabled and 1 otherwise."
+  in
+  Cmd.info "enabled" ~doc
+;;
+
+let command = Cmd.v info term

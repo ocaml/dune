7031b1ab612b9531d3d75a876200ed4476a10dd3
Rudi Grinberg <me@rgrinberg.com>
2025-07-19 10:52:40 +0100

    refactor: move [Execution_env] to [Stdune]
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    <!-- ps-id: 46332a4a-4049-489c-83ea-35b2a4fdadff -->
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- otherlibs/stdune/src/execution_env.ml
+++ otherlibs/stdune/src/execution_env.ml
@@ -1,12 +1,10 @@
-open Stdune
-
 let inside_emacs = Option.is_some (Env.get Env.initial "INSIDE_EMACS")
 let inside_ci = Option.is_some (Env.get Env.initial "CI")
 
 module Inside_dune = struct
   type t =
     | Yes
-    | In_context of Stdune.Path.Build.t
+    | In_context of Path.Build.t
 
   let var = "INSIDE_DUNE"
 

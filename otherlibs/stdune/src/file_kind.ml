ea255bf36354d2a92b66989f06386dc2eb83528b
Rudi Grinberg <me@rgrinberg.com>
2025-08-04 22:52:52 +0100

    refactor(stdune): hide [File_kind.Option] (#12126)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- otherlibs/stdune/src/file_kind.ml
+++ otherlibs/stdune/src/file_kind.ml
@@ -44,31 +44,4 @@ let equal x y =
   | S_SOCK, S_SOCK -> true
 ;;
 
-module Option = struct
-  [@@@warning "-37"]
-
-  (* The values are constructed on the C-side *)
-  type t =
-    | S_REG
-    | S_DIR
-    | S_CHR
-    | S_BLK
-    | S_LNK
-    | S_FIFO
-    | S_SOCK
-    | UNKNOWN
-
-  let elim ~none ~some t =
-    match t with
-    | S_REG -> some (S_REG : Unix.file_kind)
-    | S_DIR -> some S_DIR
-    | S_CHR -> some S_CHR
-    | S_BLK -> some S_BLK
-    | S_LNK -> some S_LNK
-    | S_FIFO -> some S_FIFO
-    | S_SOCK -> some S_SOCK
-    | UNKNOWN -> none ()
-  ;;
-end
-
 let to_dyn t = Dyn.String (to_string t)
--- otherlibs/stdune/src/file_kind.mli
+++ otherlibs/stdune/src/file_kind.mli
@@ -11,20 +11,3 @@ val to_string : t -> string
 val to_string_hum : t -> string
 val equal : t -> t -> bool
 val to_dyn : t -> Dyn.t
-
-module Option : sig
-  type file_kind := t
-
-  (* The values are constructed on the C-side *)
-  type t =
-    | S_REG
-    | S_DIR
-    | S_CHR
-    | S_BLK
-    | S_LNK
-    | S_FIFO
-    | S_SOCK
-    | UNKNOWN
-
-  val elim : none:(unit -> 'a) -> some:(file_kind -> 'a) -> t -> 'a
-end

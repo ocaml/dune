96bb60fe34e37c6b297d3f3e405bd077d61e5899
Ali Caglayan <alizter@gmail.com>
2025-08-26 20:48:28 +0300

    refactor(cram): lex locations in cram tests
    We add support for lexing locations in cram tests.
    
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- src/dune_rules/cram/cram_lexer.mli
+++ src/dune_rules/cram/cram_lexer.mli
@@ -1,8 +1,10 @@
+open Import
+
 (** .t file parser *)
 
 (** A command or comment. Output blocks are skipped *)
 type 'command block =
-  | Command of 'command
+  | Command of Loc.t * 'command
   | Comment of string list
 
 val block : Lexing.lexbuf -> string list block option

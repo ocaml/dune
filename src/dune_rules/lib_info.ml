ccbb32af7af66023c9d2099e83175c18390c4900
Rudi Grinberg <me@rgrinberg.com>
2025-09-10 10:05:04 +0100

    refactor: remove [Lib_info.is_parameter] (#12298)
    This function is used. We should prefer pattern matching on the library
    kind to using this function anyway.
    
    
    
    <!-- ps-id: d4514b4c-5c76-496b-9785-e354b052e26b -->
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    Co-authored-by: art-w <arthur@tarides.com>

--- src/dune_rules/lib_info.ml
+++ src/dune_rules/lib_info.ml
@@ -368,7 +368,6 @@ let kind t = t.kind
 let default_implementation t = t.default_implementation
 let obj_dir t = t.obj_dir
 let virtual_ t = t.kind = Virtual
-let is_parameter t = t.kind = Parameter
 let implements t = t.implements
 let synopsis t = t.synopsis
 let wrapped t = t.wrapped

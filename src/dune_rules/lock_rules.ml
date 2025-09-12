1a1c74cd2e7560b22dddd842378e85d45f01b85b
Marek Kubica <marek@tarides.com>
2025-09-12 10:54:00 +0200

    Use action builder to compose copy action

--- src/dune_rules/lock_rules.ml
+++ src/dune_rules/lock_rules.ml
@@ -6,81 +6,31 @@ let rule ?loc { Action_builder.With_targets.build; targets } =
   Rule.make ~info:(Rule.Info.of_loc_opt loc) ~targets build |> Rules.Produce.rule
 ;;
 
-module Copy = struct
-  module Spec = struct
-    type ('path, 'target) t =
-      { target : 'target
-      ; lock_dir : Path.Source.t
-      ; files : Path.Source.Set.t
-      }
-
-    let name = "copy-lock-dir"
-    let version = 1
-    let bimap t _ g = { t with target = g t.target }
-    let is_useful_to ~memoize = memoize
-
-    let encode { target; lock_dir; files } _encode_path encode_target : Sexp.t =
-      let contents : Sexp.t list =
-        files
-        |> Path.Source.Set.to_list
-        |> List.sort ~compare:Path.Source.compare
-        |> List.map ~f:(fun p -> Sexp.Atom (Path.Source.to_string p))
-      in
-      List [ encode_target target; Atom (Path.Source.to_string lock_dir); List contents ]
-    ;;
-
-    let action { target; lock_dir; files } ~ectx:_ ~eenv:_ =
-      let open Fiber.O in
-      let+ () = Fiber.return () in
-      Path.mkdir_p (Path.build target);
-      Path.Source.Set.iter files ~f:(fun src ->
-        let suffix =
-          Path.drop_prefix_exn (Path.source src) ~prefix:(Path.source lock_dir)
-        in
-        let dst = Path.Build.append_local target suffix in
-        let parent = Path.Build.parent_exn dst in
-        (* Printf.eprintf "Source to copy %S => %S (parent %S)\n" (Path.Source.to_string src) (Path.Build.to_string dst) (Path.Build.to_string parent); *)
-        Path.mkdir_p (Path.build parent);
-        Io.copy_file ~src:(Path.source src) ~dst:(Path.build dst) ())
-    ;;
-  end
-
-  module A = Action_ext.Make (Spec)
-end
-
-let copy_lock_dir ~target ~lock_dir ~files =
-  Copy.A.action { Copy.Spec.target; lock_dir; files }
-  |> Action.Full.make ~can_go_in_shared_cache:false
-  |> Action_builder.With_targets.return
+let copy_lock_dir ~target ~lock_dir ~deps ~files =
+  let open Action_builder.O in
+  Action_builder.deps deps
+  >>> (Path.Set.to_list_map files ~f:(fun src ->
+         let suffix = Path.drop_prefix_exn src ~prefix:(Path.source lock_dir) in
+         let dst = Path.Build.append_local target suffix in
+         let parent = Path.Build.parent_exn dst in
+         Action.progn [ Action.mkdir parent; Action.copy src dst ])
+       |> Action.concurrent
+       |> Action.Full.make
+       |> Action_builder.return)
+  |> Action_builder.with_no_targets
   |> Action_builder.With_targets.add_directories ~directory_targets:[ target ]
 ;;
 
 let setup_copy_rules ~dir:target ~lock_dir =
-  let+ _deps, file_set = Source_deps.files (Path.source lock_dir) in
+  let+ deps, files = Source_deps.files (Path.source lock_dir) in
   let directory_targets, rules =
-    match Path.Set.is_empty file_set with
+    match Path.Set.is_empty files with
     | true -> Path.Build.Map.empty, Memo.return Rules.empty
     | false ->
       let directory_targets = Path.Build.Map.singleton target Loc.none in
       let rules =
         Rules.collect_unit (fun () ->
-          let files =
-            Path.Set.fold
-              ~init:Path.Source.Set.empty
-              ~f:(fun e acc ->
-                match (e : Path.t) with
-                | In_source_tree p -> Path.Source.Set.add acc p
-                | In_build_dir b ->
-                  Code_error.raise
-                    "Source deps returned build paths"
-                    [ "path", Path.Build.to_dyn b ]
-                | External e ->
-                  Code_error.raise
-                    "Source deps returned external path"
-                    [ "path", Path.External.to_dyn e ])
-              file_set
-          in
-          let copy_rule = copy_lock_dir ~target ~lock_dir ~files in
+          let copy_rule = copy_lock_dir ~target ~lock_dir ~deps ~files in
           rule ~loc:Loc.none copy_rule)
       in
       directory_targets, rules

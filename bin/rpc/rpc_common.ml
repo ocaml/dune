1a0923ef909574c5052931872ffbe29c434b4140
Ambre Austen Suhamy <ambre@tarides.com>
2025-09-12 12:35:23 +0200

    [Nitpicking] Adjust printing to use console, and Pp.paragraph where applicable in Rpc_common (#12391)
    * Adjust printing to use console, and Pp.paragraph where applicable
    * Added test showcasing the output of RPC failures
    ---------
    Signed-off-by: Ambre Austen Suhamy <ambre@tarides.com>

--- bin/rpc/rpc_common.ml
+++ bin/rpc/rpc_common.ml
@@ -5,7 +5,7 @@ module Rpc_error = Dune_rpc.Response.Error
 let active_server () =
   match Dune_rpc_impl.Where.get () with
   | Some p -> Ok p
-  | None -> Error (User_error.make [ Pp.text "RPC server not running." ])
+  | None -> Error (User_error.make [ Pp.paragraph "RPC server not running." ])
 ;;
 
 let active_server_exn () = active_server () |> User_error.ok_exn
@@ -19,8 +19,8 @@ let interpret_kind = function
 
 let raise_rpc_error (e : Rpc_error.t) =
   User_error.raise
-    [ Pp.text "Server returned error: "
-    ; Pp.textf "%s (error kind: %s)" e.message (interpret_kind e.kind)
+    [ Pp.paragraph "Server returned error: "
+    ; Pp.paragraphf "%s (error kind: %s)" e.message (interpret_kind e.kind)
     ]
 ;;
 
@@ -89,29 +89,31 @@ let wrap_build_outcome_exn ~print_on_success f args () =
   let+ response = f args in
   match response with
   | Error (error : Rpc_error.t) ->
-    Printf.eprintf "Error: %s\n%!" (Dyn.to_string (Rpc_error.to_dyn error))
+    Console.print
+      [ Pp.paragraphf "Error: %s" (Dyn.to_string (Rpc_error.to_dyn error))
+        |> Pp.tag User_message.Style.Error
+      ]
   | Ok Dune_rpc.Build_outcome_with_diagnostics.Success ->
     if print_on_success
-    then
-      Console.print_user_message
-        (User_message.make [ Pp.text "Success" |> Pp.tag User_message.Style.Success ])
+    then Console.print [ Pp.text "Success" |> Pp.tag User_message.Style.Success ]
   | Ok (Failure errors) ->
+    let error_msg =
+      match List.length errors with
+      | 0 ->
+        Code_error.raise
+          "Build via RPC failed, but the RPC server did not send an error message."
+          []
+      | 1 -> Pp.paragraph "Build failed with 1 error."
+      | n -> Pp.paragraphf "Build failed with %d errors." n
+    in
     List.iter errors ~f:(fun { Dune_rpc.Compound_user_error.main; _ } ->
       Console.print_user_message main);
-    User_error.raise
-      [ (match List.length errors with
-         | 0 ->
-           Code_error.raise
-             "Build via RPC failed, but the RPC server did not send an error message."
-             []
-         | 1 -> Pp.textf "Build failed with 1 error."
-         | n -> Pp.textf "Build failed with %d errors." n)
-      ]
+    Console.print [ error_msg |> Pp.tag User_message.Style.Error ]
 ;;
 
 let warn_ignore_arguments lock_held_by =
   User_warning.emit
-    [ Pp.textf
+    [ Pp.paragraphf
         "Your build request is being forwarded to a running Dune instance%s. Note that \
          certain command line arguments may be ignored."
         (match lock_held_by with

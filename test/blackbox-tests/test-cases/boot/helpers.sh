31e5dd92d19f45bf5f4d053b7c5ae8f483c617c9
Rudi Grinberg <me@rgrinberg.com>
2025-09-14 15:38:02 +0100

    test(boot): add dummy modules to fixtures (#12447)
    These are present in during dune's bootstrap
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- test/blackbox-tests/test-cases/boot/helpers.sh
+++ test/blackbox-tests/test-cases/boot/helpers.sh
@@ -13,19 +13,23 @@ mkdir -p vendor/re/src
 mkdir -p vendor/spawn/src
 mkdir -p vendor/uutf
 
+touch vendor/re/src/re.ml
+touch vendor/spawn/src/spawn.ml
+touch vendor/uutf/uutf.ml
+
 mkdir bin
 
 # Creates a fake dune executable that depends on [$1] and emits bootstrap info
 # which is then used to bootstrap the fake dune.
 create_dune() {
-  cat > bin/main.ml 
+  cat > bin/main.ml
   cat > bin/dune <<EOF
 (executable
  (name main)
  (libraries $1)
  (bootstrap_info bootstrap-info))
 EOF
-  dune build bin/bootstrap-info 
+  dune build bin/bootstrap-info
   cp _build/default/bin/bootstrap-info boot/libs.ml
   bootstrap.exe || return $?
   _boot/dune.exe

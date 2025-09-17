51c2935a1a71cc5e12d85cb15880932c7fc55ecb
Ambre Austen Suhamy <ambre@tarides.com>
2025-09-17 19:34:00 +0200

    Fix comments
    Signed-off-by: Ambre Austen Suhamy <ambre@tarides.com>

--- bin/runtest_common.mli
+++ bin/runtest_common.mli
@@ -1,6 +1,6 @@
 open! Import
 
-(** [make_request ~dir_or_cram_test_paths] returns a function suitable
+(** [make_request ~dir_or_cram_test_paths ~to_cwd] returns a function suitable
     for passing to [Build_cmd.run_build_system] which runs the tests referred
     to by the elements of [dir_or_cram_test_paths]. *)
 val make_request

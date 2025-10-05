ce1385b390559d549d533cd9544c95c18e3f30dc
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2025-10-04 23:05:02 -0700

    fix(melange + include_qualified): track correct `.cmj` dependencies in emit
    Signed-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>

--- src/dune_rules/dep_rules.mli
+++ src/dune_rules/dep_rules.mli
@@ -27,3 +27,14 @@ val rules
   -> sctx:Super_context.t
   -> dir:Path.Build.t
   -> Dep_graph.Ml_kind.t Memo.t
+
+val deps_of
+  :  obj_dir:Path.Build.t Obj_dir.t
+  -> modules:Modules.With_vlib.t
+  -> sandbox:Sandbox_config.t
+  -> impl:Virtual_rules.t
+  -> dir:Path.Build.t
+  -> sctx:Super_context.t
+  -> Module.t
+  -> ml_kind:Ml_kind.t
+  -> Module.t list Action_builder.t Memo.t

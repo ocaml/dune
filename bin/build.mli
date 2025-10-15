2d11e323f35a2c284786b87868fbc3afb2aba95a
Ambre Austen Suhamy <ambre@tarides.com>
2025-10-15 19:02:02 +0200

    Delete redundant functions build_via_rpc
    Signed-off-by: Ambre Austen Suhamy <ambre@tarides.com>

--- bin/build.mli
+++ bin/build.mli
@@ -1,17 +1,5 @@
 open Import
 
-(** Connect to an RPC server (waiting for the server to start if necessary) and
-    then send a request to the server to build the specified targets. If the
-    build fails then a diagnostic error message is printed. If
-    [print_on_success] is true then this function will also print a message
-    after the build succeeds. *)
-val build_via_rpc_server
-  :  print_on_success:bool
-  -> targets:Dune_lang.Dep_conf.t list
-  -> Common.Builder.t
-  -> Dune_util.Global_lock.Lock_held_by.t
-  -> unit Fiber.t
-
 val run_build_system
   :  common:Common.t
   -> request:(Dune_rules.Main.build_system -> unit Action_builder.t)

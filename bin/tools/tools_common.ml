46d39fa2e164517d312c44f41c5fac042b5da872
Ambre Austen Suhamy <ambre@tarides.com>
2025-10-15 20:09:48 +0200

    Use GADTs to do the same simplification to shutdown as to diagnostics
    Signed-off-by: Ambre Austen Suhamy <ambre@tarides.com>

--- bin/tools/tools_common.ml
+++ bin/tools/tools_common.ml
@@ -38,12 +38,12 @@ let build_dev_tool_via_rpc builder lock_held_by dev_tool =
   let targets = Rpc.Group.Build.prepare_targets [ target ] in
   let open Fiber.O in
   let+ build_outcome =
-    Rpc.Common.fire_request
+    Rpc.Common.fire_message
       ~name:"build"
       ~wait:true
       ~lock_held_by
       builder
-      Dune_rpc_impl.Decl.build
+      (Rpc.Common.Request Dune_rpc_impl.Decl.build)
       targets
   in
   Rpc.Common.wrap_build_outcome_exn ~print_on_success:false build_outcome

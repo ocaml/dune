ec18fd72350f568c0c21b0b522d59c8e4ce045d1
Ambre Austen Suhamy <ambre@tarides.com>
2025-10-15 19:46:27 +0200

    Remove duplicate code in diagnostics
    Signed-off-by: Ambre Austen Suhamy <ambre@tarides.com>

--- bin/diagnostics.ml
+++ bin/diagnostics.ml
@@ -1,30 +1,5 @@
 open Import
 
-let exec () =
-  let open Fiber.O in
-  let where = Rpc.Common.active_server_exn () in
-  let module Client = Dune_rpc_client.Client in
-  let+ errors =
-    let* connect = Client.Connection.connect_exn where in
-    Dune_rpc_impl.Client.client
-      connect
-      (Dune_rpc_private.Initialize.Request.create
-         ~id:(Dune_rpc_private.Id.make (Sexp.Atom "diagnostics_cmd")))
-      ~f:(fun cli ->
-        let* decl =
-          Client.Versioned.prepare_request cli Dune_rpc_private.Public.Request.diagnostics
-        in
-        match decl with
-        | Error e -> raise (Dune_rpc_private.Version_error.E e)
-        | Ok decl -> Client.request cli decl ())
-  in
-  match errors with
-  | Ok errors ->
-    List.iter errors ~f:(fun err ->
-      Console.print_user_message (Dune_rpc.Diagnostic.to_user_message err))
-  | Error e -> Rpc.Common.raise_rpc_error e
-;;
-
 let info =
   let doc = "Fetch and return errors from the current build." in
   Cmd.info "diagnostics" ~doc
@@ -32,7 +7,18 @@ let info =
 
 let term =
   let+ (builder : Common.Builder.t) = Common.Builder.term in
-  Rpc.Common.client_term builder exec
+  Rpc.Common.client_term builder (fun () ->
+    let open Fiber.O in
+    let+ errors =
+      Rpc.Common.fire_request
+        ~name:"diagnostics_cmd"
+        ~wait:false
+        builder
+        Dune_rpc_private.Procedures.Public.diagnostics
+        ()
+    in
+    List.iter errors ~f:(fun err ->
+      Console.print_user_message (Dune_rpc.Diagnostic.to_user_message err)))
 ;;
 
 let command = Cmd.v info term

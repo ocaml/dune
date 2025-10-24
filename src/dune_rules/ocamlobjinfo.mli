ff31b91b121f94479140c6ab28f97599515927e2
Rudi Grinberg <me@rgrinberg.com>
2025-10-24 17:13:57 +0100

    refactor: ocamlobjinfo can parse more than one file
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- src/dune_rules/ocamlobjinfo.mli
+++ src/dune_rules/ocamlobjinfo.mli
@@ -10,8 +10,8 @@ val rules
   :  Ocaml_toolchain.t
   -> dir:Path.Build.t
   -> sandbox:Sandbox_config.t option
-  -> unit:Path.t
-  -> t Action_builder.t
+  -> units:Path.t list
+  -> t list Action_builder.t
 
 (** For testing only *)
-val parse : string -> t
+val parse : string -> t list

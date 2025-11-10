658f0cba081c65084ba7e8cdfb78018f0133ffbd
Rudi Grinberg <me@rgrinberg.com>
2025-11-10 22:34:30 +0000

    Stanza parse remove useless helper (#12708)
    refactor: remove/inline some useless stanza helpers
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- src/dune_rules/stanzas/stanzas.mli
+++ src/dune_rules/stanzas/stanzas.mli
@@ -13,8 +13,3 @@ module Dynamic_include : sig
 end
 
 val stanza_package : Stanza.t -> Package.Id.t option
-
-(** [of_ast project ~dir ast] is the list of [Stanza.t]s derived from decoding
-    the [ast] according to the syntax given by [kind] in the context of the
-    [project] *)
-val of_ast : Dune_project.t -> dir:Path.Source.t -> Dune_lang.Ast.t -> Stanza.t list

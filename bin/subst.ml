3a011cc862710adf04d3ec0155d1b2e77cebea8c
Shon Feder <shon.feder@gmail.com>
2025-11-27 22:17:23 -0500

    Revert "Make promoted files read-only (#12519)"
    This reverts commit bcf324233bf2ff2c74560b2171d41e81fb858df2.
    
    Signed-off-by: Shon Feder <shon.feder@gmail.com>

--- bin/subst.ml
+++ bin/subst.ml
@@ -153,15 +153,7 @@ let subst_file path ~map opam_package_files =
       | None, Some x -> Some x
       | Some x, Some y -> Some (x ^ "\n" ^ y)
     in
-    (match contents with
-     | None -> ()
-     | Some contents ->
-       (try Io.write_file path contents with
-        | Unix.Unix_error (Unix.EACCES, _, _) ->
-          let Unix.{ st_perm; _ } = Path.stat_exn path in
-          Path.chmod path ~mode:(Path.Permissions.add Path.Permissions.write st_perm);
-          Io.write_file path contents;
-          Path.chmod path ~mode:st_perm))
+    Option.iter contents ~f:(Io.write_file path)
 ;;
 
 (* Extending the Dune_project APIs, but adding capability to modify *)

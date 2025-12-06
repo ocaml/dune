fbed933913fa2c14cf43be292df7af7d27b56737
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2025-12-05 22:28:53 -0800

    address code review
    Signed-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>

--- src/dune_rules/rule_mode_expand.ml
+++ src/dune_rules/rule_mode_expand.ml
@@ -1,43 +1,34 @@
 open Import
-include Rule_mode
 
-let expand_path t ~expander ~dir =
+let expand t ~f =
   let open Memo.O in
   match t with
-  | Standard -> Memo.return Rule.Mode.Standard
+  | Rule_mode.Standard -> Memo.return Rule.Mode.Standard
   | Fallback -> Memo.return Rule.Mode.Fallback
+  | Ignore_source_files -> Memo.return Rule.Mode.Ignore_source_files
   | Promote { lifetime; into; only } ->
     let+ into =
-      match into with
-      | None -> Memo.return None
-      | Some { loc; dir = into_dir } ->
-        let+ path, _ =
-          Action_builder.evaluate_and_collect_facts
-            (Expander.expand_path expander into_dir)
-        in
-        let dir = Path.reach ~from:(Path.build dir) path in
-        Some { Rule.Promote.Into.loc; dir }
+      Memo.Option.map into ~f:(fun { loc; dir } ->
+        let+ dir = f dir in
+        { Rule.Promote.Into.loc; dir })
     in
     Rule.Mode.Promote { lifetime; only; into }
-  | Ignore_source_files -> Memo.return Rule.Mode.Ignore_source_files
+;;
+
+let expand_path t ~expander ~dir =
+  expand t ~f:(fun into_dir ->
+    let open Memo.O in
+    let+ path, _ =
+      Action_builder.evaluate_and_collect_facts (Expander.expand_path expander into_dir)
+    in
+    Path.reach ~from:(Path.build dir) path)
 ;;
 
 let expand_str t ~expander =
-  let open Memo.O in
-  match t with
-  | Standard -> Memo.return Rule.Mode.Standard
-  | Fallback -> Memo.return Rule.Mode.Fallback
-  | Promote { lifetime; into; only } ->
-    let+ into =
-      match into with
-      | None -> Memo.return None
-      | Some { loc; dir = into_dir } ->
-        let+ path, _ =
-          Action_builder.evaluate_and_collect_facts
-            (Expander.expand_str expander into_dir)
-        in
-        Some { Rule.Promote.Into.loc; dir = path }
+  expand t ~f:(fun into_dir ->
+    let open Memo.O in
+    let+ path, _ =
+      Action_builder.evaluate_and_collect_facts (Expander.expand_str expander into_dir)
     in
-    Rule.Mode.Promote { lifetime; only; into }
-  | Ignore_source_files -> Memo.return Rule.Mode.Ignore_source_files
+    path)
 ;;

8c6deba80284964dbbcabfd6a9c80035ac0040ce
Rudi Grinberg <me@rgrinberg.com>
2025-12-24 18:58:47 +0000

    refactor: make sure that trace file path appears once
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- bin/trace.ml
+++ bin/trace.ml
@@ -5,15 +5,18 @@ let cat =
   let term =
     let+ () = Term.const () in
     let bytes = Bytes.create (1 lsl 16) in
-    Io.String_path.with_file_in ~binary:true "_build/trace.json" ~f:(fun i ->
-      let rec loop () =
-        match input i bytes 0 (Bytes.length bytes) with
-        | 0 | (exception End_of_file) -> ()
-        | n ->
-          output stdout bytes 0 n;
-          loop ()
-      in
-      loop ())
+    Io.String_path.with_file_in
+      ~binary:true
+      (Path.Local.to_string Common.default_trace_file)
+      ~f:(fun i ->
+        let rec loop () =
+          match input i bytes 0 (Bytes.length bytes) with
+          | 0 | (exception End_of_file) -> ()
+          | n ->
+            output stdout bytes 0 n;
+            loop ()
+        in
+        loop ())
   in
   Cmd.v info term
 ;;

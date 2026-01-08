b2ad1335967fc6f59ab9aac7310d493e92c895a4
Ali Caglayan <alizter@gmail.com>
2026-01-08 16:41:33 +0000

    add overlays for local dune packages
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- flake.nix
+++ flake.nix
@@ -99,11 +99,90 @@
               ocaml-overlays.overlays.default
               (final: prev: {
                 ocamlPackages = prev.ocaml-ng.ocamlPackages_5_4.overrideScope (
-                  oself: osuper: {
-                    dune_3 = osuper.dune_3.overrideAttrs (old: {
-                      src = revdeps-dune;
-                    });
-                  }
+                  oself: osuper:
+                    let
+                      # Helper to build dune subpackages from revdeps-dune source
+                      mkDuneLib =
+                        { pname
+                        , buildInputs ? [ ]
+                        , propagatedBuildInputs ? [ ]
+                        , preBuild ? ""
+                        }:
+                        osuper.buildDunePackage {
+                          inherit pname preBuild;
+                          version = "dev";
+                          src = revdeps-dune;
+                          duneVersion = "3";
+                          dontAddPrefix = true;
+                          inherit buildInputs propagatedBuildInputs;
+                        };
+                    in
+                    {
+                      dune_3 = osuper.dune_3.overrideAttrs (old: {
+                        src = revdeps-dune;
+                      });
+
+                      # Internal dune libraries not packaged in nixpkgs
+                      ordering = mkDuneLib { pname = "ordering"; };
+                      top-closure = mkDuneLib { pname = "top-closure"; };
+                      fs-io = mkDuneLib { pname = "fs-io"; };
+
+                      # Override existing dune subpackages to use revdeps-dune source
+                      dyn = mkDuneLib {
+                        pname = "dyn";
+                        propagatedBuildInputs = with oself; [ pp ordering ];
+                      };
+                      stdune = mkDuneLib {
+                        pname = "stdune";
+                        propagatedBuildInputs = with oself; [
+                          csexp
+                          dyn
+                          ordering
+                          pp
+                          top-closure
+                          fs-io
+                        ];
+                        preBuild = "rm -rf vendor/csexp";
+                      };
+                      dune-private-libs = mkDuneLib {
+                        pname = "dune-private-libs";
+                        propagatedBuildInputs = with oself; [ stdune ];
+                        preBuild = "rm -rf vendor/csexp";
+                      };
+                      dune-site = mkDuneLib {
+                        pname = "dune-site";
+                        propagatedBuildInputs = with oself; [ dune-private-libs ];
+                        preBuild = "rm -rf vendor/csexp";
+                      };
+                      dune-configurator = mkDuneLib {
+                        pname = "dune-configurator";
+                        propagatedBuildInputs = with oself; [ dune-private-libs ];
+                        preBuild = "rm -rf vendor/csexp";
+                      };
+                      dune-build-info = mkDuneLib {
+                        pname = "dune-build-info";
+                        preBuild = "rm -rf vendor/csexp";
+                      };
+                      dune-glob = mkDuneLib {
+                        pname = "dune-glob";
+                        propagatedBuildInputs = with oself; [ stdune ];
+                        preBuild = "rm -rf vendor/csexp";
+                      };
+                      dune-rpc = mkDuneLib {
+                        pname = "dune-rpc";
+                        propagatedBuildInputs = with oself; [ stdune dyn ocamlc-loc ordering pp xdg csexp ];
+                        preBuild = "rm -rf vendor/csexp";
+                      };
+                      xdg = mkDuneLib { pname = "xdg"; };
+                      chrome-trace = mkDuneLib {
+                        pname = "chrome-trace";
+                        propagatedBuildInputs = with oself; [ pp ];
+                      };
+                      ocamlc-loc = mkDuneLib {
+                        pname = "ocamlc-loc";
+                        propagatedBuildInputs = with oself; [ dyn ];
+                      };
+                    }
                 );
               })
             ];

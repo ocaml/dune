ocaml/dune

commit 75b52fa51db99a3e19835ce477de24706ef83cb8

fix: `(select ..)` field + `(include_subdirs ..)` (#13175)
fixes #12450
fixes #4383


- Moves inference of modules generated by `(select ..)` rules to
`Ml_sources`, which takes include_subdirs into account

Signed-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>

test/blackbox-tests/test-cases/select-field/select-validation.t
@@ -28,10 +28,14 @@ Test module path validation for `(select ..)` targets when using
   > EOF
 
   $ dune build
-  Error: foo__Foo-bar/baz corresponds to an invalid module name
-  -> required by _build/default/foo__.ml-gen
-  -> required by alias all
-  -> required by alias default
+  File "dune", lines 5-7, characters 2-99:
+  5 |   (select foo-bar/baz.ml from
+  6 |    (unix -> foo-bar/baz.unix.ml)
+  7 |    (!unix -> foo-bar/baz.nounix.ml))))
+  Error: "foo-bar" is an invalid module name.
+  Module names must be non-empty, start with a letter, and composed only of the
+  following characters: 'A'..'Z', 'a'..'z', '_', ''' or '0'..'9'.
+  Hint: foo_bar would be a correct module name
   [1]
 
   $ mkdir -p foo_bar
@@ -53,16 +57,12 @@ Test module path validation for `(select ..)` targets when using
   > EOF
 
   $ dune build --display=short
-  Error: foo__Foo_bar/baz-qux corresponds to an invalid module name
-  -> required by _build/default/foo__.ml-gen
-  -> required by _build/default/.foo.objs/byte/foo__.cmi
-  -> required by _build/default/.foo.objs/native/foo.cmx
-  -> required by _build/default/foo.a
-  -> required by alias all
-  -> required by alias default
   File "dune", lines 5-7, characters 2-111:
   5 |   (select foo_bar/baz-qux.ml from
   6 |    (unix -> foo_bar/baz-qux.unix.ml)
   7 |    (!unix -> foo_bar/baz-qux.nounix.ml))))
-  Error: No rule found for foo_bar/baz-qux.unix.ml
+  Error: "baz-qux" is an invalid module name.
+  Module names must be non-empty, start with a letter, and composed only of the
+  following characters: 'A'..'Z', 'a'..'z', '_', ''' or '0'..'9'.
+  Hint: baz_qux would be a correct module name
   [1]

src/dune_lang/lib_dep.ml
@@ -5,7 +5,7 @@ module Select = struct
     type t =
       { required : Lib_name.Set.t
       ; forbidden : Lib_name.Set.t
-      ; file : string
+      ; file : Path.Local.t
       }
 
     let decode ~result_fn =
@@ -22,28 +22,33 @@ module Select = struct
                 match String.drop_prefix s ~prefix:"!" with
                 | Some s -> Right (Lib_name.parse_string_exn (loc, s))
                 | None -> Left (Lib_name.parse_string_exn (loc, s)))
-             ~after:(located filename)
+             ~after:(located relative_file)
          in
          match file with
          | None ->
            User_error.raise ~loc [ Pp.textf "(<[!]libraries>... -> <file>) expected" ]
          | Some (loc_file, file) ->
+           let file_str = file in
+           let file = Path.Local.parse_string_exn ~loc:loc_file file in
            let () =
              if dune_version >= (2, 0)
              then (
-               let prefix, suffix =
-                 let name, ext = Filename.split_extension result_fn in
-                 let prefix = name ^ "." in
-                 prefix, ext
+               let result_prefix, result_suffix =
+                 let prefix, ext = Path.Local.split_extension result_fn in
+                 Path.Local.to_string prefix, ext
                in
-               if not (String.is_prefix file ~prefix && String.is_suffix file ~suffix)
-               then
+               match
+                 ( String.is_prefix ~prefix:result_prefix (Path.Local.to_string file)
+                 , String.is_suffix file_str ~suffix:result_suffix )
+               with
+               | true, true -> ()
+               | _result_is_prefix, _result_is_suffix ->
                  User_error.raise
                    ~loc:loc_file
                    [ Pp.textf
-                       "The format for files in this select branch must be %s{name}%s"
-                       prefix
-                       suffix
+                       "The format for files in this select branch must be %s.{name}%s"
+                       result_prefix
+                       result_suffix
                    ])
            in
            let rec loop required forbidden = function
@@ -68,25 +73,27 @@ module Select = struct
       record
         [ "required", Lib_name.Set.to_dyn required
         ; "forbidden", Lib_name.Set.to_dyn forbidden
-        ; "file", string file
+        ; "file", Path.Local.to_dyn file
         ]
     ;;
   end
 
   type t =
-    { result_fn : string
+    { result_fn : Path.Local.t
     ; choices : Choice.t list
     ; loc : Loc.t
     }
 
   let to_dyn { result_fn; choices; loc = _ } =
     let open Dyn in
-    record [ "result_fn", string result_fn; "choices", list Choice.to_dyn choices ]
+    record
+      [ "result_fn", Path.Local.to_dyn result_fn; "choices", list Choice.to_dyn choices ]
   ;;
 
   let decode =
     let open Decoder in
-    let* result_fn = filename in
+    let* result_fn = relative_file in
+    let result_fn = Path.of_string_allow_outside_workspace result_fn |> Path.local_part in
     let+ loc = loc
     and+ () = keyword "from"
     and+ choices = repeat (Choice.decode ~result_fn) in

src/dune_lang/lib_dep.mli
@@ -5,12 +5,12 @@ module Select : sig
     type t =
       { required : Lib_name.Set.t
       ; forbidden : Lib_name.Set.t
-      ; file : string
+      ; file : Path.Local.t
       }
   end
 
   type t =
-    { result_fn : string
+    { result_fn : Path.Local.t
     ; choices : Choice.t list
     ; loc : Loc.t
     }

src/dune_rules/buildable_rules.ml
@@ -25,7 +25,7 @@ let gen_select_rules sctx ~dir compile_info =
   Lib.Compile.resolved_selects compile_info
   |> Resolve.Memo.read_memo
   >>= Memo.parallel_iter ~f:(fun { Lib.Compile.Resolved_select.dst_fn; src_fn; loc } ->
-    let dst = Path.Build.relative dir dst_fn in
+    let dst = Path.Build.append_local dir dst_fn in
     Super_context.add_rule
       sctx
       ~loc
@@ -34,7 +34,7 @@ let gen_select_rules sctx ~dir compile_info =
          ~file_targets:[ dst ]
          (let open Action_builder.O in
           let* src_fn = Resolve.read src_fn in
-          let src = Path.build (Path.Build.relative dir src_fn) in
+          let src = Path.build (Path.Build.append_local dir src_fn) in
           let+ () = Action_builder.path src in
           let context = Super_context.context sctx in
           Action.Full.make (Copy_line_directive.action context ~src ~dst))))

src/dune_rules/dir_contents.ml
@@ -127,15 +127,6 @@ module rec Load : sig
   val get : Super_context.t -> dir:Path.Build.t -> t Memo.t
   val triage : Super_context.t -> dir:Path.Build.t -> triage Memo.t
 end = struct
-  let select_deps_files libraries =
-    (* Manually add files generated by the (select ...)
-       dependencies *)
-    List.filter_map libraries ~f:(fun dep ->
-      match (dep : Lib_dep.t) with
-      | Re_export _ | Direct _ | Instantiate _ -> None
-      | Select s -> Some s.result_fn)
-  ;;
-
   (* As a side-effect, setup user rules and copy_files rules. *)
   let load_text_files sctx st_dir stanzas ~dir ~src_dir =
     let from_source = Source_tree.Dir.filenames st_dir in
@@ -175,16 +166,10 @@ end = struct
           | Library.T { buildable; _ }
           | Executables.T { buildable; _ }
           | Tests.T { exes = { buildable; _ }; _ } ->
-            let select_deps_files = select_deps_files buildable.libraries in
-            let ctypes_files =
-              (* Also manually add files generated by ctypes rules. *)
-              match buildable.ctypes with
-              | None -> []
-              | Some ctypes -> Ctypes_field.generated_ml_and_c_files ctypes
-            in
-            Memo.return (select_deps_files @ ctypes_files)
-          | Melange_stanzas.Emit.T { libraries; _ } ->
-            Memo.return @@ select_deps_files libraries
+            (* Manually add files generated by ctypes rules. *)
+            Option.map buildable.ctypes ~f:Ctypes_field.generated_ml_and_c_files
+            |> Option.value ~default:[]
+            |> Memo.return
           | _ -> Memo.return [])
         >>| List.concat
         >>| Filename.Set.of_list

src/dune_rules/lib.ml
@@ -304,8 +304,8 @@ end
 
 module Resolved_select = struct
   type t =
-    { src_fn : Filename.t Resolve.t
-    ; dst_fn : Filename.t
+    { src_fn : Path.Local.t Resolve.t
+    ; dst_fn : Path.Local.t
     ; loc : Loc.t
     }
 end

src/dune_rules/lib.mli
@@ -93,8 +93,8 @@ module Compile : sig
 
   module Resolved_select : sig
     type t =
-      { src_fn : Filename.t Resolve.t
-      ; dst_fn : Filename.t
+      { src_fn : Path.Local.t Resolve.t
+      ; dst_fn : Path.Local.t
       ; loc : Loc.t
       }
   end

commit cc49e1c2a29be80dc2caf9e8566b575e625f6e69

test(select-field): show false positive "no solution" error (#13271)
this is a regression from #13175

Signed-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>

test/blackbox-tests/test-cases/select-field/select-validation.t
@@ -91,3 +91,29 @@ name
   following characters: 'A'..'Z', 'a'..'z', '_', ''' or '0'..'9'.
   Hint: baz_qux would be a correct module name
   [1]
+
+  $ cat > foo.ml <<EOF
+  > let () = print_endline "hello"
+  > EOF
+
+  $ cat > dune <<EOF
+  > (library
+  >  (name foo)
+  >  (libraries
+  >   (select link-opam-manifest from
+  >    (unix -> link-opam-manifest.pull)
+  >    (!unix -> link-opam-manifest.dummy))))
+  > (rule
+  >  (targets link-opam-manifest.dummy)
+  >  (action (progn (echo "dummy\n") (with-stdout-to link-opam-manifest.dummy (echo "")))))
+  > (rule
+  >  (targets link-opam-manifest.pull)
+  >  (action (progn (echo "pull\n") (with-stdout-to %{targets} (echo "")))))
+  > EOF
+  $ dune build
+  File "dune", lines 4-6, characters 2-110:
+  4 |   (select link-opam-manifest from
+  5 |    (unix -> link-opam-manifest.pull)
+  6 |    (!unix -> link-opam-manifest.dummy))))
+  Error: No solution found for this select form.
+  [1]
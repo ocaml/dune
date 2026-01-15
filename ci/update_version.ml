388e53ce619dedee8a48de76702d1c66501ad32b
Marek Kubica <marek@tarides.com>
2026-01-15 11:58:19 +0100

    Add note about auto-generation
    Signed-off-by: Marek Kubica <marek@tarides.com>

--- ci/update_version.ml
+++ ci/update_version.ml
@@ -4,10 +4,12 @@ open Stdune
 let version = Re.compile (Re.str "%%VERSION%%")
 
 let () =
-  let filename = Sys.argv.(1) |> Path.of_filename_relative_to_initial_cwd in
-  let content = Io.read_file filename in
+  let filename = Sys.argv.(1) in
+  let filepath = Path.of_filename_relative_to_initial_cwd filename in
+  let content = Io.read_file filepath in
   let major, minor = Dune_rpc_private.Version.latest in
   let version_string = sprintf "%d.%d.0" major minor in
   let content = Re.replace_string version ~by:version_string content in
+  Printf.printf "# This file is auto-generated. Edit ci/%s instead\n" filename;
   Printf.printf "%s" content
 ;;

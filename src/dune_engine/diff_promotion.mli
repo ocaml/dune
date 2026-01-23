4e28a288599426e1dcffa9a1e747abe8d2ed3ffd
Rudi Grinberg <me@rgrinberg.com>
2026-01-23 21:43:26 +0000

    refactor: simplify some promotion code (#13443)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- src/dune_engine/diff_promotion.mli
+++ src/dune_engine/diff_promotion.mli
@@ -18,9 +18,6 @@ module File : sig
 
   val to_dyn : t -> Dyn.t
   val in_staging_area : Path.Source.t -> Path.Build.t
-  val compare : t -> t -> ordering
-  val source : t -> Path.Source.t
-  val correction_file : t -> Path.t
 
   (** Register an intermediate file to promote. The build path may point to the
       sandbox and the file will be moved to the staging area. *)

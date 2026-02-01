122f44dde10c0f10de8c60d8938746239d7821af
Rudi Grinberg <me@rgrinberg.com>
2026-02-01 22:20:06 +0000

    refactor: add some dyn conversions to diff actions
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- src/dune_util/action.ml
+++ src/dune_util/action.ml
@@ -3,6 +3,13 @@ module Diff = struct
     type t =
       | Binary
       | Text
+
+    let to_dyn =
+      let open Dyn in
+      function
+      | Binary -> variant "Binary" []
+      | Text -> variant "Text" []
+    ;;
   end
 
   type ('path, 'target) t =
@@ -11,6 +18,16 @@ module Diff = struct
     ; file1 : 'path
     ; file2 : 'target
     }
+
+  let to_dyn path target { optional; mode; file1; file2 } =
+    let open Dyn in
+    record
+      [ "optional", bool optional
+      ; "mode", Mode.to_dyn mode
+      ; "file1", path file1
+      ; "file2", target file2
+      ]
+  ;;
 
   let map t ~path ~target = { t with file1 = path t.file1; file2 = target t.file2 }
 end

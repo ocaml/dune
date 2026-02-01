25c540334d2cb4df6d70883f0fff2fb234475122
Rudi Grinberg <me@rgrinberg.com>
2026-02-01 12:31:11 +0000

    test: move Fpath test where it belongs (#13513)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- otherlibs/stdune/test/fpath_tests.ml
+++ otherlibs/stdune/test/fpath_tests.ml
@@ -1,5 +1,17 @@
 open Stdune
 
+let%expect_test "making a directory for an existing file" =
+  let dir = Temp.create Dir ~prefix:"fpath" ~suffix:"test" in
+  let fn = Path.relative dir "foo" in
+  Io.write_file fn "";
+  (* This does not error, but it will if it ends with a "/" on MacOS *)
+  ignore (Fpath.mkdir (Path.to_string fn));
+  [%expect {| |}];
+  Path.mkdir_p fn;
+  (* This in turn does not error *)
+  [%expect {| |}]
+;;
+
 let%expect_test "is_root" =
   [ ""; "."; "/"; "foo/bar"; "/foo" ]
   |> List.iter ~f:(fun path -> printfn "Fpath.is_root %S = %b" path (Fpath.is_root path));

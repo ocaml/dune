3be196b3fba81b6e211a88c5f7efd0e289b22ad5
Rudi Grinberg <me@rgrinberg.com>
2026-02-05 23:49:11 +0000

    refactor(rpc): move server notifications to separate module (#13552)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- otherlibs/dune-rpc/private/dune_rpc_private.ml
+++ otherlibs/dune-rpc/private/dune_rpc_private.ml
@@ -10,12 +10,7 @@ module Version_error = Versioned.Version_error
 module Decl = Decl
 module Sub = Sub
 module Public = Public
+module Server_notifications = Server_notifications
+module Client = Client
 
 module type Fiber = Fiber_intf.S
-
-module Server_notifications = struct
-  let abort = Procedures.Server_side.abort.decl
-  let log = Procedures.Server_side.log.decl
-end
-
-module Client = Client

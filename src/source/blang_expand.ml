dde2f7689e40c117d65c81d39dd21edbf257557e
Ali Caglayan <alizter@gmail.com>
2026-03-17 12:47:53 +0100

    refactor(blang): move arguments out of recursion in eval
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- src/source/blang_expand.ml
+++ src/source/blang_expand.ml
@@ -1,22 +1,28 @@
 open Import
 open Memo.O
 
-let rec eval (t : Blang.t) ~dir ~f =
-  match t with
-  | Const x -> Memo.return x
-  | Expr sw ->
-    String_expander.Memo.expand sw ~mode:Single ~dir ~f
-    >>| (function
-     | String "true" -> true
-     | String "false" -> false
-     | _ ->
-       let loc = String_with_vars.loc sw in
-       User_error.raise ~loc [ Pp.text "This value must be either true or false" ])
-  | And xs -> Memo.List.map xs ~f:(eval ~f ~dir) >>| List.for_all ~f:Fun.id
-  | Or xs -> Memo.List.map xs ~f:(eval ~f ~dir) >>| List.exists ~f:Fun.id
-  | Not t -> eval t ~f ~dir >>| not
-  | Compare (op, x, y) ->
-    let+ x = String_expander.Memo.expand x ~mode:Many ~dir ~f
-    and+ y = String_expander.Memo.expand y ~mode:Many ~dir ~f in
-    Relop.eval op (Value.L.compare_vals ~dir x y)
+let eval ~dir ~f =
+  let rec eval t =
+    match (t : Blang.t) with
+    | Const x -> Memo.return x
+    | Expr sw ->
+      String_expander.Memo.expand sw ~mode:Single ~dir ~f
+      >>| (function
+       | String "true" -> true
+       | String "false" -> false
+       | _ ->
+         let loc = String_with_vars.loc sw in
+         User_error.raise ~loc [ Pp.text "This value must be either true or false" ])
+    | And xs -> Memo.List.map xs ~f:eval >>| List.for_all ~f:Fun.id
+    | Or xs -> Memo.List.map xs ~f:eval >>| List.exists ~f:Fun.id
+    | Not t -> eval t >>| not
+    | Compare (op, x, y) ->
+      let+ x = String_expander.Memo.expand x ~mode:Many ~dir ~f
+      and+ y = String_expander.Memo.expand y ~mode:Many ~dir ~f in
+      Relop.eval op (Value.L.compare_vals ~dir x y)
+  in
+  fun t -> eval t
+[@@inline]
 ;;
+
+let eval t ~dir ~f = eval ~dir ~f t

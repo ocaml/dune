82f15a28f5ed1062884caa4dd9597e3721626661
Ali Caglayan <alizter@gmail.com>
2026-03-18 00:12:54 +0100

    chore(boot): remove String.starts_with/ends_with compat shims
    String.starts_with and String.ends_with were introduced in OCaml 4.13.
    
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- boot/duneboot.ml
+++ boot/duneboot.ml
@@ -241,23 +241,6 @@ module String = struct
     loop ~acc:[] 0 0 ~last_is_cr:false
   ;;
 
-  [@@@ocaml.warning "-32"]
-
-  let ends_with t ~suffix = Filename.check_suffix t suffix
-
-  let starts_with t ~prefix =
-    let len_s = length t
-    and len_pre = length prefix in
-    let rec aux i =
-      if i = len_pre
-      then true
-      else if unsafe_get t i <> unsafe_get prefix i
-      then false
-      else aux (i + 1)
-    in
-    len_s >= len_pre && aux 0
-  ;;
-
   include String
 end
 

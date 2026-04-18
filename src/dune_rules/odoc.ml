d5803b1c250b0a1e199de3c4fe56f717e651320c
Masayuki Takeda <mtakeda@oeconomia.jp>
2026-04-18 19:23:07 +0800

    fix: check lockdir even if lock_dev_tools is disabled when invoking odoc
    Signed-off-by: Masayuki Takeda <mtakeda@oeconomia.jp>

--- src/dune_rules/odoc.ml
+++ src/dune_rules/odoc.ml
@@ -282,12 +282,18 @@ let odoc_dev_tool_exe_path_building_if_necessary () =
 ;;
 
 let odoc_program sctx dir =
-  let odoc_dev_tool_lock_dir_exists =
-    match Config.get Compile_time.lock_dev_tools with
-    | `Enabled -> true
-    | `Disabled -> false
+  let open Action_builder.O in
+  let* lock_dir_exists =
+    Action_builder.of_memo
+      (match Config.get Compile_time.lock_dev_tools with
+       | `Enabled -> Memo.return true
+       | `Disabled ->
+         (* even if lock_dev_tools is disabled, there might be a lock dir
+            created by `dune tools install odoc` *)
+         let path = Lock_dir.dev_tool_external_lock_dir Odoc in
+         Fs_memo.dir_exists (Path.Outside_build_dir.External path))
   in
-  match odoc_dev_tool_lock_dir_exists with
+  match lock_dir_exists with
   | true -> odoc_dev_tool_exe_path_building_if_necessary ()
   | false ->
     Super_context.resolve_program

4bf3615b373caca2e3f15d70dc4985062a4c646b
Rudi Grinberg <me@rgrinberg.com>
2026-06-06 15:28:22 +0100

    fix(stdune): use intended copyfile benchmark permissions (#14993)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- bench/micro/copyfile.ml
+++ bench/micro/copyfile.ml
@@ -20,9 +20,10 @@ let contents =
 let () =
   let src = Path.relative dir "initial" in
   Io.write_file (Path.relative dir "initial") contents;
-  (* CR-someday rgrinberg: This preserves a likely bug: decimal [444] is
-     [0o674], not the probably intended [0o444]. Fix separately. *)
-  let chmod _ = Permissions.Mode.of_int 444 in
+  let chmod _ =
+    let open Permissions in
+    Mode.create ~user:read ~group:read ~other:read ()
+  in
   for i = 1 to 10_000 do
     let dst = Path.relative dir (sprintf "dst-%d" i) in
     Io.copy_file ~chmod ~src ~dst ()

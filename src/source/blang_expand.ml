e2fff1a38fabad4b054dc96149c9ba225c542af0
Sudha Parimala <sudharg247@gmail.com>
2026-06-23 13:14:20 +0200

    Short-circuit boolean expressions in blang
    Makes the `and` and `or` expressions in blang short-circuit, instead of eagerly
    evaluating it. This fixes the test in `enabled_if/short-circuit.t`. This issue
    was discussed in #9943.
    
    Signed-off-by: Sudha Parimala <sudharg247@gmail.com>

--- src/source/blang_expand.ml
+++ src/source/blang_expand.ml
@@ -12,8 +12,28 @@ let rec eval (t : Blang.t) ~dir ~f =
      | _ ->
        let loc = String_with_vars.loc sw in
        User_error.raise ~loc [ Pp.text "This value must be either true or false" ])
-  | And xs -> Memo.List.map xs ~f:(eval ~f ~dir) >>| List.for_all ~f:Fun.id
-  | Or xs -> Memo.List.map xs ~f:(eval ~f ~dir) >>| List.exists ~f:Fun.id
+  | And xs ->
+      let rec loop = function
+      | [] -> Memo.return true
+      | x :: xs ->
+        let* x = eval ~f ~dir x in
+        match x with
+        | true -> loop xs
+        | false ->
+          (* stop evaluating when a false case is reached *)
+          Memo.return false
+      in loop xs
+  | Or xs ->
+      let rec loop = function
+      | [] -> Memo.return false
+      | x :: xs ->
+        let* x = eval ~f ~dir x in
+        match x with
+        | false -> loop xs
+        | true ->
+          (* stop evaluating when a true case is reached *)
+          Memo.return true
+      in loop xs
   | Not t -> eval t ~f ~dir >>| not
   | Compare (op, x, y) ->
     let+ x = String_expander.Memo.expand x ~mode:Many ~dir ~f

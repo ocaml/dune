e7a919022f5dc93b92997812d6d94ec02ddf2526
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-06-30 00:51:02 -0700

    test(formatting): show regression making env vars missing from fmt actions (#15385)
    Signed-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>

--- test/blackbox-tests/test-cases/formatting/feature.t/fake-tools/refmt.ml
+++ test/blackbox-tests/test-cases/formatting/feature.t/fake-tools/refmt.ml
@@ -1,5 +1,8 @@
 let process args =
   Printf.printf "Sys.argv: %s\n" (String.concat " " (Array.to_list args));
+  (match Sys.getenv_opt "REFMT_PRINT_WIDTH" with
+   | None -> ()
+   | Some value -> Printf.printf "REFMT_PRINT_WIDTH=%s\n" value);
   Printf.printf "refmt output\n"
 
 let () =

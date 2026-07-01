ocaml/dune

commit 517150c6a4b0b2f8f309ae77990c6cb8ad6539dc

fix(melange): preserve original ppx source locations (#14706)
Signed-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>

test/blackbox-tests/test-cases/melange/sandbox-preprocessed-reason-source.t
@@ -62,7 +62,9 @@ diagnostics can print the correct snippet.
 
   $ dune build --root cases --sandbox=symlink @src/all
   Entering directory 'cases'
-  File "src/.melange_src/x.re", line 1, characters 8-12:
+  File "src/x.re", line 1, characters 8-12:
+  1 | let x = "reason"
+              ^^^^
   Error: Unbound type constructor nope
   Leaving directory 'cases'
   [1]

src/dune_rules/pp_spec_rules.ml
@@ -26,6 +26,16 @@ let pp_input_path m ~ml_kind =
   Module.source m ~ml_kind |> Option.value_exn |> pp_input_file ~ml_kind
 ;;
 
+let loc_filename_arg_for_pp ~input_path ~loc_filename =
+  if Path.equal input_path loc_filename
+  then Command.Args.empty
+  else
+    S
+      [ A "-loc-filename"
+      ; A (Path.drop_optional_build_context_maybe_sandboxed loc_filename |> Path.to_string)
+      ]
+;;
+
 let pp_corrected_path m ~ml_kind ~suffix =
   pp_input_path m ~ml_kind
   |> Path.as_in_build_dir_exn
@@ -338,6 +348,11 @@ let pp_one_module
         let* ast = setup_dialect_rules sctx ~sandbox ~dir ~expander m in
         let* () = Memo.when_ lint (fun () -> lint_module ~ast ~source:m) in
         pped_module ast ~f:(fun ml_kind src dst ->
+          let loc_filename_arg =
+            loc_filename_arg_for_pp
+              ~input_path:(Path.build src)
+              ~loc_filename:(pp_input_path m ~ml_kind)
+          in
           Super_context.add_rule
             sctx
             ~loc
@@ -360,6 +375,7 @@ let pp_one_module
                      [ As args
                      ; A "-o"
                      ; Path (Path.build dst)
+                     ; loc_filename_arg
                      ; Command.Ml_kind.ppx_driver_flag ml_kind
                      ; Dep (Path.build src)
                      ; Hidden_deps

test/blackbox-tests/test-cases/melange/sandbox-preprocessed-source.t
@@ -131,7 +131,9 @@ filename even though the PPX reads the copied Melange source.
 
   $ dune build --root cases --sandbox=symlink @logical-ocaml-impl/src/all
   Entering directory 'cases'
-  File "logical-ocaml-impl/src/.melange_src/x.ml", line 1, characters 8-12:
+  File "logical-ocaml-impl/src/x.ml", line 1, characters 8-12:
+  1 | let x = "ocaml"
+              ^^^^
   Error: Unbound type constructor nope
   Leaving directory 'cases'
   [1]
@@ -160,7 +162,9 @@ Melange source filename even though the PPX reads the copied Melange source.
 
   $ dune build --root cases --sandbox=symlink @logical-melange-impl/src/all
   Entering directory 'cases'
-  File "logical-melange-impl/src/.melange_src/x.ml", line 1, characters 8-12:
+  File "logical-melange-impl/src/x.melange.ml", line 1, characters 8-12:
+  1 | let x = "melange"
+              ^^^^
   Error: Unbound type constructor nope
   Leaving directory 'cases'
   [1]
@@ -189,7 +193,9 @@ filename even though the PPX reads the copied Melange source.
 
   $ dune build --root cases --sandbox=symlink @logical-ocaml-intf/src/all
   Entering directory 'cases'
-  File "logical-ocaml-intf/src/.melange_src/x.mli", line 1, characters 8-12:
+  File "logical-ocaml-intf/src/x.mli", line 1, characters 8-12:
+  1 | val x : int
+              ^^^^
   Error: Unbound type constructor nope
   Leaving directory 'cases'
   [1]
@@ -222,7 +228,9 @@ Melange source filename even though the PPX reads the copied Melange source.
 
   $ dune build --root cases --sandbox=symlink @logical-melange-intf/src/all
   Entering directory 'cases'
-  File "logical-melange-intf/src/.melange_src/x.mli", line 1, characters 8-12:
+  File "logical-melange-intf/src/x.melange.mli", line 1, characters 8-12:
+  1 | val x : string
+              ^^^^
   Error: Unbound type constructor nope
   Leaving directory 'cases'
   [1]

commit 7aef8b7d14ccf95b93670fa6a4117fad4d5aa3ee

test(melange): show ppx sandbox regression when preserving original loc (#15393)
this was a regression from https://github.com/ocaml/dune/pull/14706

Signed-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>

test/blackbox-tests/test-cases/melange/sandbox-preprocessed-reason-source.t
@@ -15,10 +15,25 @@ compiler diagnostic should still print the original Reason source line.
   >  (kind ppx_rewriter)
   >  (modules ppx_noop)
   >  (libraries ppxlib))
+  > 
+  > (library
+  >  (name ppx_read_input)
+  >  (kind ppx_rewriter)
+  >  (modules ppx_read_input)
+  >  (libraries ppxlib))
   > EOF
 
   $ cat > cases/ppx/ppx_noop.ml <<'EOF'
   > let () = Ppxlib.Driver.register_transformation "ppx_noop"
+  > EOF
+
+  $ cat > cases/ppx/ppx_read_input.ml <<'EOF'
+  > let impl ast =
+  >   let ic = open_in !Ocaml_common.Location.input_name in
+  >   close_in ic;
+  >   ast
+  > ;;
+  > let () = Ppxlib.Driver.register_transformation "ppx_read_input" ~impl
   > EOF
 
   $ cat > cases/src/dune <<'EOF'
@@ -42,5 +57,30 @@ compiler diagnostic should still print the original Reason source line.
                                               ^^^^^^^^
   Error: This constant has type string but an expression was expected of type
            int
+  Leaving directory 'cases'
+  [1]
+
+PPX rewriters may read the logical input filename passed with [-loc-filename].
+The original Reason source must be available in the PPX sandbox.
+
+  $ mkdir -p cases/read-loc/src
+
+  $ cat > cases/read-loc/src/dune <<'EOF'
+  > (library
+  >  (name read_loc_filename)
+  >  (modes melange)
+  >  (modules x)
+  >  (preprocess
+  >   (pps ppx_read_input)))
+  > EOF
+
+  $ cat > cases/read-loc/src/x.re <<'EOF'
+  > let x = "reason"
+  > EOF
+
+  $ dune build --root cases --sandbox=symlink @read-loc/src/all
+  Entering directory 'cases'
+  File "read-loc/src/x.re", line 1:
+  Error: I/O error: read-loc/src/x.re: No such file or directory
   Leaving directory 'cases'
   [1]
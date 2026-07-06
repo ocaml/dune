580c57c3ec7b33f1e6dd86a777d9a337d443ff70
Rudi Grinberg <me@rgrinberg.com>
2026-07-06 23:20:31 +0100

    refactor(stdune): add Repr.Enum helper
    Add a small helper for representing finite enum-like types with Repr, including
    string conversion. Use it for dune cache storage modes to share the case table
    between the representation and string parsing/printing.
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- src/dune_cache/mode.ml
+++ src/dune_cache/mode.ml
@@ -1,41 +1,27 @@
 open Stdune
 
-type t =
-  | Hardlink
-  | Copy
+module T = struct
+  type t =
+    | Hardlink
+    | Copy
 
-let equal a b =
-  match a, b with
-  | Hardlink, Hardlink | Copy, Copy -> true
-  | _, _ -> false
-;;
+  let all = [ "copy", Copy; "hardlink", Hardlink ]
+  let enum = Repr.Enum.make "cache-mode" all
+  let repr = Repr.Enum.repr enum
+end
 
-let all = [ "hardlink", Hardlink; "copy", Copy ]
+include T
+
+let to_dyn = Repr.to_dyn repr
+let to_string = Repr.Enum.to_string enum
 
 let of_string s =
-  match List.assoc all s with
+  match Repr.Enum.of_string enum s with
   | Some mode -> Result.Ok mode
   | None -> Result.Error (Format.sprintf "invalid cache storage mode: %s" s)
 ;;
 
-let to_string = function
-  | Hardlink -> "hardlink"
-  | Copy -> "copy"
-;;
-
-let repr =
-  Repr.variant
-    "cache-mode"
-    [ Repr.case0 "Copy" ~test:(function
-        | Copy -> true
-        | Hardlink -> false)
-    ; Repr.case0 "Hardlink" ~test:(function
-        | Hardlink -> true
-        | Copy -> false)
-    ]
-;;
-
-let to_dyn = Repr.to_dyn repr
+include Repr.Poly (T)
 
 (* CR-someday amokhov: Check that hard links can actually be created and, if
    not, return [Copy] instead. *)

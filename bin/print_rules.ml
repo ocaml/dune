aace518bd9558d93bf68cc4024942449598efc3b
Nat Karmios <nat@karmios.com>
2026-07-16 17:53:08 +0100

    Make rule locations optional
    Signed-off-by: Nat Karmios <nat@karmios.com>

--- bin/print_rules.ml
+++ bin/print_rules.ml
@@ -230,7 +230,11 @@ let rule_context (rule : Dune_engine.Reflection.Rule.t) =
   | Some (context, _) -> Some (Filename.to_string context)
 ;;
 
-let rule_repr =
+let rule_loc ~with_locs (rule : Dune_engine.Reflection.Rule.t) =
+  if (not with_locs) || Loc.is_none rule.loc then None else Some rule.loc
+;;
+
+let rule_repr ~with_locs =
   Repr.record
     "rule"
     [ Repr.field "deps" deps_repr ~get:(fun rule -> rule.Dune_engine.Reflection.Rule.deps)
@@ -241,11 +245,11 @@ let rule_repr =
         rule.Dune_engine.Reflection.Rule.action)
     ; Repr.field "alias" (Repr.option Alias_name.repr) ~get:(fun rule ->
         rule.Dune_engine.Reflection.Rule.alias)
-    ; Repr.field "loc" Loc.repr ~get:(fun rule -> rule.Dune_engine.Reflection.Rule.loc)
+    ; Repr.field "loc" (Repr.option Loc.repr) ~get:(rule_loc ~with_locs)
     ]
 ;;
 
-let rules_repr = Repr.list rule_repr
+let rules_repr ~with_locs = Repr.list (rule_repr ~with_locs)
 let deps_only_repr = Repr.list deps_repr
 
 let dep_sets_of_rules rules =
@@ -298,10 +302,13 @@ let print_json oc json =
   output_char oc '\n'
 ;;
 
-let print_rules_sexp ppf rules =
+let print_rules_sexp ~with_locs ppf rules =
   Syntax.prepare_formatter ppf;
   Format.pp_open_vbox ppf 0;
-  Format.pp_print_list (fun ppf rule -> print_sexp_repr ppf rule_repr rule) ppf rules;
+  Format.pp_print_list
+    (fun ppf rule -> print_sexp_repr ppf (rule_repr ~with_locs) rule)
+    ppf
+    rules;
   Format.pp_print_flush ppf ()
 ;;
 
@@ -343,6 +350,8 @@ let term =
       value
       & opt (enum Output_format.all) Output_format.Sexp
       & info [ "format" ] ~docv:"FORMAT" ~doc:(Some doc))
+  and+ with_locs =
+    Arg.(value & flag & info [ "with_locs" ] ~doc:(Some "Include locations of rules"))
   (* CR-someday Alizter: document this option *)
   and+ targets = Arg.(value & pos_all dep [] & Arg.info [] ~docv:"TARGET" ~doc:None) in
   let common, config = Common.init builder in
@@ -363,9 +372,10 @@ let term =
       let print oc =
         let ppf = Format.formatter_of_out_channel oc in
         match format, deps_only with
-        | Output_format.Sexp, false -> print_rules_sexp ppf rules
+        | Output_format.Sexp, false -> print_rules_sexp ~with_locs ppf rules
         | Output_format.Sexp, true -> print_rule_deps_only_sexp ppf rules
-        | Json, false -> print_json oc (Json.of_dyn (dyn_of_repr rules_repr rules))
+        | Json, false ->
+          print_json oc (Json.of_dyn (dyn_of_repr (rules_repr ~with_locs) rules))
         | Json, true ->
           print_json
             oc

f48f7f34a24428ab673d302deed50a621e00d6c1
Rudi Grinberg <me@rgrinberg.com>
2026-08-11 23:17:35 +0100

    refactor(describe): share name field rendering (#15782)
    Centralize rendering of private and public library names in
    `Describe_format.name_fields`. Both `dune describe workspace` and `dune
    describe external-lib-deps` now use the same implementation for regular
    and `--split-public-names` output.
    
    This removes duplicate formatting logic without changing command output.
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- bin/describe/describe_external_lib_deps.ml
+++ bin/describe/describe_external_lib_deps.ml
@@ -63,26 +63,7 @@ module Item = struct
     =
     let open Dyn in
     let name_fields =
-      if split_public_names
-      then (
-        let public_names =
-          match public_names with
-          | Some public_names -> public_names
-          | None -> List.map names ~f:(fun _ -> None)
-        in
-        [ "names", (list string) names
-        ; "public_names", (list (option string)) public_names
-        ])
-      else (
-        (* replace each private name by its public name when there is one *)
-        let best_names =
-          match public_names with
-          | None -> names
-          | Some public_names ->
-            List.map2 names public_names ~f:(fun name public_name ->
-              Option.value public_name ~default:name)
-        in
-        [ "names", (list string) best_names ])
+      Describe_format.name_fields ~split_public_names ~names ~public_names
     in
     let record =
       record

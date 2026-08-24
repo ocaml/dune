39374df073072fad4a8afccdddd648690586b0a9
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-08-23 21:44:26 -0700

    test(scheduler): reproduce lost SIGINT under SIGCHLD load
    Signed-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>

---
+++ test/expect-tests/dune_scheduler/signal_watcher_repro.ml
@@ -0,0 +1,87 @@
+open Stdune
+module Event = Dune_scheduler__Event
+module Signal_watcher = Dune_scheduler__Signal_watcher
+module Thread0 = Dune_scheduler__Thread0
+
+let spawn () =
+  Unix.create_process
+    "/bin/sh"
+    [| "/bin/sh"; "-c"; "sleep 0.01" |]
+    Unix.stdin
+    Unix.stdout
+    Unix.stderr
+;;
+
+let rec reap () =
+  match Unix.waitpid [ WNOHANG ] (-1) with
+  | 0, _ -> ()
+  | _, _ -> reap ()
+  | exception Unix.Unix_error (ECHILD, _, _) -> ()
+;;
+
+let rec wait_for_sigint events =
+  match Event.Queue.next events with
+  | Shutdown (Signal Int) -> ()
+  | Shutdown _ | Fiber_fill_ivar _ -> wait_for_sigint events
+  | Job_complete_ready ->
+    reap ();
+    wait_for_sigint events
+;;
+
+let interrupt () =
+  Thread.create
+    (fun () ->
+       Thread.delay 0.002;
+       Unix.kill (Unix.getpid ()) Sys.sigint)
+    ()
+;;
+
+let run () =
+  Thread0.interrupt_signals
+  |> List.map ~f:Signal.to_int
+  |> Unix.sigprocmask SIG_UNBLOCK
+  |> ignore;
+  let events = Event.Queue.create () in
+  let signal_watcher = Signal_watcher.init ~print_ctrl_c_warning:false events in
+  let finished = Atomic.make false in
+  let (_ : Thread.t) =
+    Thread.create
+      (fun () ->
+         Thread.delay 1.;
+         if not (Atomic.get finished)
+         then (
+           prerr_endline "timed out waiting for SIGINT";
+           exit 2))
+      ()
+  in
+  let children = List.init 128 ~f:(fun _ -> spawn ()) in
+  let interrupt = interrupt () in
+  wait_for_sigint events;
+  Thread.join interrupt;
+  List.iter children ~f:(fun pid ->
+    try ignore (Unix.waitpid [] pid : int * Unix.process_status) with
+    | Unix.Unix_error (ECHILD, _, _) -> ());
+  Unix.kill (Unix.getpid ()) (Signal.to_int Thread0.signal_watcher_interrupt);
+  Thread0.join signal_watcher;
+  Atomic.set finished true
+;;
+
+let run_fresh () =
+  let argv = [| Sys.executable_name; "run" |] in
+  let pid =
+    Unix.create_process Sys.executable_name argv Unix.stdin Unix.stdout Unix.stderr
+  in
+  match Unix.waitpid [] pid with
+  | _, WEXITED 0 -> ()
+  | _, WEXITED code -> exit code
+  | _, (WSIGNALED _ | WSTOPPED _) -> exit 2
+;;
+
+let () =
+  if Array.length Sys.argv = 1
+  then
+    for _ = 1 to 20 do
+      run_fresh ()
+    done
+  else run ()
+;;
